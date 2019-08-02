ce40c891b777d6699616f24df0af65ac296d811c
Greg Scullard <greg@scullard.com>
2019-08-02 15:47:07 +0200

    Update postgresInit.sql

--- src/main/resources/postgres/postgresInit.sql
+++ src/main/resources/postgres/postgresInit.sql
@@ -1,23 +1,32 @@
-\set db_name hederamirror
-\set db_user hederamirror
-\set db_password MdM*6-zsdx
+-- Change the values below if you are not installing via Docker
+-- environment variable values come from .env file
+-- name of the database
+\set db_name `echo "$POSTGRES_DB"`
+-- username
+\set db_user `echo "$POSTGRES_USER"`
+-- user password
+\set db_password `echo "$POSTGRES_PASSWORD"`
+-- owner of the database (usually postgres)
+\set db_owner `echo "$POSTGRES_USER"`
 
+-- Change the values below to your preferred API user name and password
 \set api_user api
-\set api_password pdP_mFe6gB
-
-CREATE DATABASE :db_name
-     WITH
-     OWNER = postgres
-     CONNECTION LIMIT = -1;
-
-CREATE USER :db_user WITH
-	LOGIN
-	NOCREATEDB
-	NOCREATEROLE
-	NOINHERIT
-	NOREPLICATION
-	CONNECTION LIMIT -1
-	PASSWORD :'db_password';
+\set api_password mysecretpassword
+-- Uncomment below if you are not installing via Docker
+--
+-- CREATE DATABASE :db_name
+--      WITH
+--      OWNER = :‘db_owner’
+--      CONNECTION LIMIT = -1;
+--
+-- CREATE USER :db_user WITH
+--     LOGIN
+--     NOCREATEDB
+--     NOCREATEROLE
+--     NOINHERIT
+--     NOREPLICATION
+--     CONNECTION LIMIT -1
+--     PASSWORD :‘db_password’;
 
 CREATE USER :api_user WITH
 	LOGIN
