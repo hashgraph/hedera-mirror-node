9fadf72a06f57dfe93049c232d5f59edf4f4ee04
Qian Qiu <qianqiu@swirlds.com>
2019-08-06 17:35:35 -0400

    Add queries 6 and 7,  #16

--- src/main/java/com/hedera/parser/EventStreamAnalytics.java
+++ src/main/java/com/hedera/parser/EventStreamAnalytics.java
@@ -130,23 +130,93 @@ public class EventStreamAnalytics {
 		return count;
 	}
 
+	/**
+	 * get the min, max, median latency for all events that reached consensus in a given time window, per node
+	 *
+	 * @param periodStartInNanos
+	 * @param periodEndInNanos
+	 * @param nodeId
+	 * @return
+	 * @throws SQLException
+	 */
+	public long[] getLatencyStatsForNode(long periodStartInNanos, long periodEndInNanos,
+			long nodeId) throws SQLException {
+		PreparedStatement query = connect.prepareStatement(
+				"SELECT MIN(latency_ns), MAX(latency_ns), percentile_disc(0.5) within GROUP (ORDER BY latency_ns) FROM " +
+						"t_events WHERE consensus_timestamp_ns >= ? AND " +
+						"consensus_timestamp_ns <= ? AND creator_node_id = ?");
+		query.setLong(1, periodStartInNanos);
+		query.setLong(2, periodEndInNanos);
+		query.setLong(3, nodeId);
+		query.execute();
+		ResultSet resultSet = query.getResultSet();
+		long[] latencies = new long[3];
+		if (resultSet.next()) {
+			latencies[0] = resultSet.getLong(1);
+			latencies[1] = resultSet.getLong(2);
+			latencies[2] = resultSet.getLong(3);
+		}
+		resultSet.close();
+		query.close();
+		return latencies;
+	}
+
+	/**
+	 * get the min, max, median latency for all events that reached consensus in a given time window
+	 *
+	 * @param periodStartInNanos
+	 * @param periodEndInNanos
+	 * @return
+	 * @throws SQLException
+	 */
+	public long[] getLatencyStats(long periodStartInNanos, long periodEndInNanos) throws SQLException {
+		PreparedStatement query = connect.prepareStatement(
+				"SELECT MIN(latency_ns), MAX(latency_ns), percentile_disc(0.5) within GROUP (ORDER BY latency_ns) FROM " +
+						"t_events WHERE consensus_timestamp_ns >= ? AND " +
+						"consensus_timestamp_ns <= ?");
+		query.setLong(1, periodStartInNanos);
+		query.setLong(2, periodEndInNanos);
+		query.execute();
+		ResultSet resultSet = query.getResultSet();
+		long[] latencies = new long[3];
+		if (resultSet.next()) {
+			latencies[0] = resultSet.getLong(1);
+			latencies[1] = resultSet.getLong(2);
+			latencies[2] = resultSet.getLong(3);
+		}
+		resultSet.close();
+		query.close();
+		return latencies;
+	}
+
 	public static void main(String[] args) throws SQLException {
 		EventStreamAnalytics analytics = new EventStreamAnalytics();
 		long periodStartInNanos = 1564084641196388000l;
 		long periodEndInNanos = 1564084701826156000l;
-		System.out.println(analytics.getCreatedEventsCount(periodStartInNanos, periodEndInNanos));
+		log.info(MARKER, "Created events count between {} and {}: {}", periodStartInNanos, periodEndInNanos,
+				analytics.getCreatedEventsCount(periodStartInNanos, periodEndInNanos));
 		long nodeId = 0;
-		System.out.println(analytics.getCreatedEventsCountForNode(periodStartInNanos, periodEndInNanos, nodeId));
+		log.info(MARKER, "Created events count of node{} between {} and {}: {}", nodeId, periodStartInNanos,
+				periodEndInNanos, analytics.getCreatedEventsCountForNode(periodStartInNanos, periodEndInNanos, nodeId));
 
 		periodStartInNanos = 1564084641217420000l;
 		periodEndInNanos = 1564084701892894000l;
 		System.out.println(analytics.getConsensusEventsCount(periodStartInNanos, periodEndInNanos));
-		System.out.println(analytics.getConsensusEventsCountForNode(periodStartInNanos, periodEndInNanos, nodeId));
+		log.info(MARKER, "Consensus events count of node{} between {} and {}: {}", nodeId, periodStartInNanos,
+				periodEndInNanos, analytics.getConsensusEventsCountForNode(periodStartInNanos, periodEndInNanos, nodeId));
 
 		periodStartInNanos = 1564084641217420000l;
 		periodEndInNanos = 1564084701916832000l;
 		long[] counts = analytics.getSysAppTxsCountInConsensusEventsForNode(periodStartInNanos, periodEndInNanos,
 				nodeId);
-		System.out.println(counts[0] + " " + counts[1]);
+		log.info(MARKER, "System Txs count: {}, Application Txs count: {}", counts[0], counts[1]);
+
+		long[] latencies = analytics.getLatencyStatsForNode(periodStartInNanos, periodEndInNanos, nodeId);
+		log.info(MARKER, "Node{} From {} to {} Min latency: {}ns, Max latency: {}ns, Median latency: {}ns", nodeId,
+				periodStartInNanos, periodEndInNanos, latencies[0], latencies[1], latencies[2]);
+
+		latencies = analytics.getLatencyStats(periodStartInNanos, periodEndInNanos);
+		log.info(MARKER, "From {} to {} Min latency: {}ns, Max latency: {}ns, Median latency: {}ns", periodStartInNanos,
+				periodEndInNanos, latencies[0], latencies[1], latencies[2]);
 	}
 }
