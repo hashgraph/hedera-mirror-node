420855543daf4c3a3c6a20a79062b8d6a1008a80
Greg Scullard <greg@scullard.com>
2019-08-18 22:19:22 +0200

    Patched f_file_create function

---
+++ src/main/resources/postgres/V1.5__file_create_patch.sql
@@ -0,0 +1,24 @@
+DROP FUNCTION f_file_create(varchar(250));
+
+CREATE FUNCTION f_file_create(_file_name t_record_files.name%TYPE)
+  RETURNS BIGINT AS
+$$
+DECLARE file_id BIGINT;
+BEGIN
+  SELECT id
+  INTO file_id
+  FROM t_record_files
+  WHERE name = _file_name;
+
+  IF NOT FOUND THEN
+    INSERT INTO t_record_files(name, load_start)
+    VALUES (_file_name, extract(epoch from now()))
+    RETURNING id INTO file_id;
+  ELSE
+    file_id = 0;
+  END IF;
+
+  RETURN file_id;
+END;
+$$ LANGUAGE plpgsql
+;
