737fc0f435c9839148f2aedcb375f4e3416da460
Greg Scullard <greg@scullard.com>
2019-08-23 10:32:52 +0200

    Removed extra file name

--- src/main/java/com/hedera/balanceFileLogger/BalanceFileLogger.java
+++ src/main/java/com/hedera/balanceFileLogger/BalanceFileLogger.java
@@ -2,7 +2,6 @@ package com.hedera.balanceFileLogger;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
