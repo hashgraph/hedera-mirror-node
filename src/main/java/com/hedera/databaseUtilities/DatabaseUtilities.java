ffd716646ce022f5c265f35d04c55ecd5f1abe3a
Greg Scullard <greg@scullard.com>
2019-08-26 20:08:59 +0200

    Update DatabaseUtilities.java

--- src/main/java/com/hedera/databaseUtilities/DatabaseUtilities.java
+++ src/main/java/com/hedera/databaseUtilities/DatabaseUtilities.java
@@ -49,15 +49,13 @@ public class DatabaseUtilities {
             try {
                 if (Utility.checkStopFile()) {
                     log.info("Stop file found, stopping.");
-                    throw new RuntimeException("System is stopped");
+                    log.info("Stop file found, stopping.");
+                    System.exit(0);
                 }
 
                 Connection connection = dataSource.getConnection();
                 migrate();
                 return connection;
-            } catch (RuntimeException e) {
-                log.info("Stop file found, stopping.");
-                System.exit(0);
             } catch (Exception e) {
                 log.warn("Unable to connect to database. Will retry in {}ms: {}", dataSource.getConnectionTimeout(), e.getMessage());
             }
