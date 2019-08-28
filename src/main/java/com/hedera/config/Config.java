98819e9de7b9fbe54142ec2ad82511221fc0bebe
Calvin Cheng <calvin@hedera.com>
2019-08-28 15:05:30 +0800

    Config tests

---
+++ src/main/java/com/hedera/config/Config.java
@@ -0,0 +1,64 @@
+package com.hedera.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:config.properties")
+@ConfigurationProperties(prefix = "postgresql")
+public class Config {
+     
+    @Value("${postgresql.url}")
+    private String url;
+
+    @Value("${postgresql.username}")
+    private String username;
+    
+    @Value("${postgresql.password}")
+    private String password;
+ 
+    /**
+     * @return the url
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * @param url the url to set
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
---
+++ src/test/java/com/hedera/config/ConfigTest.java
@@ -0,0 +1,27 @@
+package com.hedera.config;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = { Config.class })
+public class ConfigTest {
+
+  @Autowired
+  private Config config;
+
+  @Test
+  public void shouldHaveDefaultConfig() {
+
+    assertEquals("jdbc:postgresql://localhost:5433/hederamirror", config.getUrl());
+    assertEquals("hederamirror", config.getUsername());
+    assertEquals("mysecretpassword", config.getPassword());
+
+  }
+
+}
