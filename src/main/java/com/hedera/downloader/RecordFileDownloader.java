f3ad50491e4f1e54ebaa116d3aac78f64bd02faa
Steven Sheehy <steven.sheehy@hedera.com>
2019-09-10 13:47:17 -0500

    Fix try/catch
    Signed-off-by: Steven Sheehy <steven.sheehy@hedera.com>

--- src/main/java/com/hedera/downloader/RecordFileDownloader.java
+++ src/main/java/com/hedera/downloader/RecordFileDownloader.java
@@ -119,24 +119,23 @@ public class RecordFileDownloader extends Downloader {
 				break;
 			}
 			boolean valid = false;
+			List<File> sigFiles = sigFilesMap.get(fileName);
 
-			try {
-				List<File> sigFiles = sigFilesMap.get(fileName);
+			// If the number of sigFiles is not greater than 2/3 of number of nodes, we don't need to verify them
+			if (sigFiles == null || !Utility.greaterThanSuperMajorityNum(sigFiles.size(), nodeAccountIds.size())) {
+				log.warn("Signature file count for {} does not exceed 2/3 of nodes", fileName);
+				continue;
+			}
 
-				// If the number of sigFiles is not greater than 2/3 of number of nodes, we don't need to verify them
-				if (sigFiles == null || !Utility.greaterThanSuperMajorityNum(sigFiles.size(), nodeAccountIds.size())) {
-					log.warn("Signature file count for {} does not exceed 2/3 of nodes", fileName);
-					continue;
+			// validSigFiles are signed by node key and contains the same hash which has been agreed by more than 2/3 nodes
+			List<File> validSigFiles = verifier.verifySignatureFiles(sigFiles);
+			for (File validSigFile : validSigFiles) {
+				if (Utility.checkStopFile()) {
+					log.info("Stop file found, stopping");
+					break;
 				}
 
-				// validSigFiles are signed by node key and contains the same hash which has been agreed by more than 2/3 nodes
-				List<File> validSigFiles = verifier.verifySignatureFiles(sigFiles);
-				for (File validSigFile : validSigFiles) {
-					if (Utility.checkStopFile()) {
-						log.info("Stop file found, stopping");
-						break;
-					}
-
+				try {
 					Pair<Boolean, File> rcdFileResult = downloadFile(DownloadType.RCD, validSigFile, tmpDir);
 					File rcdFile = rcdFileResult.getRight();
 					if (rcdFile != null && Utility.hashMatch(validSigFile, rcdFile)) {
@@ -154,11 +153,11 @@ public class RecordFileDownloader extends Downloader {
 							}
 						}
 					} else if (rcdFile != null) {
-						log.warn("Hash of {} doesn't match the hash contained in the signature file. Will try to download a record file with same timestamp from other nodes", rcdFile);
+						log.warn("Hash of {} doesn't match the hash contained in the signature file. Will try to download a record file with same timestamp from other nodes", validSigFile);
 					}
+				} catch (Exception e) {
+					log.error("Unable to verify signature {}", validSigFile, e);
 				}
-			} catch (Exception e) {
-				log.error("Unable to verify signature {}", fileName, e);
 			}
 
 			if (!valid) {
