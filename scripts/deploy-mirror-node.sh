3c12d5242259a95d2e52ccd21a91c13476b5204c
Mike Burrage (Hedera) <50885871+mike-burrage-hedera@users.noreply.github.com>
2019-10-15 12:41:23 -0700

    Fix install/upgrade to use of application.yml for DB config (instead of config.json) (#318)
    * mirror-node.service use application.yml from /usr/etc/mirror-node. Upgrade script to transfer db config params from config.json to application.yml
    
    Signed-off-by: Mike Burrage <mike.burrage@hedera.com>
    
    * Add sudos.
    
    Signed-off-by: Mike Burrage <mike.burrage@hedera.com>
    
    * Expect script to be run as sudo. Fix script bug.
    
    Signed-off-by: Mike Burrage <mike.burrage@hedera.com>
    
    * Fix systemd service file.
    
    Signed-off-by: Mike Burrage <mike.burrage@hedera.com>
    
    * Clean up deploy-mirror-node.sh
    
    Signed-off-by: Mike Burrage <mike.burrage@hedera.com>

--- scripts/deploy-mirror-node.sh
+++ scripts/deploy-mirror-node.sh
@@ -9,44 +9,55 @@ if [ -z "${version}" ]; then
 fi
 
 usrlib=/usr/lib/mirror-node/
+usretc=/usr/etc/mirror-node/
 ts=$(date -u +%s)
 upgrade=0
 
-sudo mkdir -p /usr/etc/mirror-node "${usrlib}" /var/lib/mirror-node
+mkdir -p "${usretc}" "${usrlib}" /var/lib/mirror-node
 
 if [ -f "${usrlib}/mirror-node.jar" ]; then
     upgrade=1
     echo "Upgrading to ${version}"
 
-    echo "Stopping services"
-    sudo touch "${usrlib}/stop"
-    sleep 5
+    # Stop the service
+    echo "Stopping mirror-node service"
+    systemctl stop mirror-node.service || true
 
-    # Optionally stop these since some of these might not exist
-    sudo systemctl stop mirror-balance-downloader.service || true
-    sudo systemctl stop mirror-balance-parser.service || true
-    sudo systemctl stop mirror-record-downloader.service || true
-    sudo systemctl stop mirror-record-parser.service || true
-    sudo systemctl stop mirror-node.service || true
-    sudo rm -f /etc/systemd/systemd/mirror-*.service
+    echo "Backing up binary"
+    mv "${usrlib}/mirror-node.jar" "${usrlib}/mirror-node.jar.${ts}.old"
 
-    echo "Backing up binaries"
-    sudo rm -rf "${usrlib}/lib"* "${usrlib}/logs" "${usrlib}/mirror-node.jar."*
-    sudo mv "${usrlib}/mirror-node.jar" "${usrlib}/mirror-node.jar.${ts}.old"
+    # Handle the upgrade from config.json database params to application.yml database params
+    appyml="${usretc}/application.yml"
+    if [ -f "${usretc}/config.json" ] && [ ! -f  "${appyml}" ]; then
+        echo -e "hedera:\n  mirror:\n    db:" > "${appyml}"
+        cat "${usretc}/config.json" | awk -F: '{gsub(/ |\"/, "", $0); gsub(/,$/, "", $0); print}' | grep -E "^(api|db)" | \
+          sed -e "s/apiUsername:/api-username: /" -e "s/apiPassword:/api-password: /" -e "s/dbName:/name: /" \
+            -e "s/dbUsername:/username: /" -e "s/dbPassword:/password: /" | grep -v dbUrl | \
+            sed -e "s/^/      /" >> "${appyml}"
+        dbUrl=$(grep "dbUrl" "${usretc}/config.json" | sed -e "s#.*//##" -e "s#/.*##")
+        dbhost=$(echo ${dbUrl} | sed -e "s#:.*##")
+        dbport=$(echo ${dbUrl} | grep ':' | sed -e "s#.*:##")
+        if [ -n "${dbhost}" ]; then
+            echo "      host: ${dbhost}" >> "${appyml}"
+        fi
+        if [ -n "${dbport}" ]; then
+            echo "      port: ${dbport}" >> "${appyml}"
+        fi
+        sed -ie '/apiPassword/d;/apiUsername/d;/dbName/d;/dbUsername/d;/dbPassword/d;/dbUrl/d' "${usretc}/config.json"
+    fi
 else
     echo "Fresh install of ${version}"
     echo "Copying config (will need to be edited)"
-    cp -n config/* /usr/etc/mirror-node/
+    cp -n config/* "${usretc}"
 fi
 
-echo "Copying binaries"
-sudo cp mirror-node.jar "${usrlib}"
+echo "Copying new binary"
+cp mirror-node.jar "${usrlib}"
 
-echo "Setting up systemd services"
-sudo cp scripts/*mirror*.service /etc/systemd/system
-sudo systemctl daemon-reload
-sudo systemctl enable mirror-node.service
+echo "Setting up mirror-node systemd service"
+cp scripts/mirror-node.service /etc/systemd/system
+systemctl daemon-reload
+systemctl enable mirror-node.service
 
-echo "Starting services"
-sudo rm -f "${usrlib}/stop"
-sudo systemctl start mirror-node.service
+echo "Starting mirror-node service"
+systemctl start mirror-node.service
