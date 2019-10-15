c8d26b038791044468b04f28f7e6531928534f02
Mike Burrage <mike.burrage@hedera.com>
2019-10-15 14:06:51 -0400

    Expect script to be run as sudo. Fix script bug.
    Signed-off-by: Mike Burrage <mike.burrage@hedera.com>

--- scripts/deploy-mirror-node.sh
+++ scripts/deploy-mirror-node.sh
@@ -13,60 +13,60 @@ usretc=/usr/etc/mirror-node/
 ts=$(date -u +%s)
 upgrade=0
 
-sudo mkdir -p "${usretc}" "${usrlib}" /var/lib/mirror-node
+mkdir -p "${usretc}" "${usrlib}" /var/lib/mirror-node
 
 if [ -f "${usrlib}/mirror-node.jar" ]; then
     upgrade=1
     echo "Upgrading to ${version}"
 
     echo "Stopping services"
-    sudo touch "${usrlib}/stop"
+    touch "${usrlib}/stop"
     sleep 5
 
     # Optionally stop these since some of these might not exist
-    sudo systemctl stop mirror-balance-downloader.service || true
-    sudo systemctl stop mirror-balance-parser.service || true
-    sudo systemctl stop mirror-record-downloader.service || true
-    sudo systemctl stop mirror-record-parser.service || true
-    sudo systemctl stop mirror-node.service || true
-    sudo rm -f /etc/systemd/systemd/mirror-*.service
+    systemctl stop mirror-balance-downloader.service || true
+    systemctl stop mirror-balance-parser.service || true
+    systemctl stop mirror-record-downloader.service || true
+    systemctl stop mirror-record-parser.service || true
+    systemctl stop mirror-node.service || true
+    rm -f /etc/systemd/systemd/mirror-*.service
 
     echo "Backing up binaries"
-    sudo rm -rf "${usrlib}/lib"* "${usrlib}/logs" "${usrlib}/mirror-node.jar."*
-    sudo mv "${usrlib}/mirror-node.jar" "${usrlib}/mirror-node.jar.${ts}.old"
+    rm -rf "${usrlib}/lib"* "${usrlib}/logs" "${usrlib}/mirror-node.jar."*
+    mv "${usrlib}/mirror-node.jar" "${usrlib}/mirror-node.jar.${ts}.old"
 
     # Handle the upgrade from config.json database params to application.yml database params
     appyml="${usretc}/application.yml"
     if [ -f "${usretc}/config.json" ] && [ ! -f  "${appyml}" ]; then
-        sudo echo -e "hedera:\n  mirror:\n    db:" > "${appyml}"
-        sudo cat "${usretc}/config.json" | awk -F: '{gsub(/ |\"/, "", $0); gsub(/,$/, "", $0); print}' | grep -E "^(api|db)" | \
+        echo -e "hedera:\n  mirror:\n    db:" > "${appyml}"
+        cat "${usretc}/config.json" | awk -F: '{gsub(/ |\"/, "", $0); gsub(/,$/, "", $0); print}' | grep -E "^(api|db)" | \
           sed -e "s/apiUsername:/api-username: /" -e "s/apiPassword:/api-password: /" -e "s/dbName:/name: /" \
             -e "s/dbUsername:/username: /" -e "s/dbPassword:/password: /" | grep -v dbUrl | \
-            sudo sed -e "s/^/      /" >> "${appyml}"
-        dbUrl=$(sudo grep "dbUrl" "${usretc}/config.json" | sed -e "s#.*//##" -e "s#/.*##")
+            sed -e "s/^/      /" >> "${appyml}"
+        dbUrl=$(grep "dbUrl" "${usretc}/config.json" | sed -e "s#.*//##" -e "s#/.*##")
         dbhost=$(echo ${dbUrl} | sed -e "s#:.*##")
         dbport=$(echo ${dbUrl} | grep ':' | sed -e "s#.*:##")
         if [ -n "${dbhost}" ]; then
-            sudo echo "      host: ${dbhost}" >> "${appyml}"
+            echo "      host: ${dbhost}" >> "${appyml}"
         fi
         if [ -n "${dbport}" ]; then
-            sudo echo "      port: ${dbport}" >> "${appyml}"
+            echo "      port: ${dbport}" >> "${appyml}"
         fi
-        sudo sed -ie '/apiPassword/d;/apiUsername/d;/dbName/d;/dbUsername/d;/dbPassword/d;/dbUrl/d' "${usretc}/config.json"
+        sed -ie '/apiPassword/d;/apiUsername/d;/dbName/d;/dbUsername/d;/dbPassword/d;/dbUrl/d' "${usretc}/config.json"
     fi
 else
     echo "Fresh install of ${version}"
     echo "Copying config (will need to be edited)"
-    cp -n config/* "#{usretc}"
+    cp -n config/* "${usretc}"
 fi
 
 echo "Copying binaries"
-sudo cp mirror-node.jar "${usrlib}"
+cp mirror-node.jar "${usrlib}"
 
 echo "Setting up systemd services"
-sudo cp scripts/*mirror*.service /etc/systemd/system
-sudo systemctl daemon-reload
-sudo systemctl enable mirror-node.service
+cp scripts/*mirror*.service /etc/systemd/system
+systemctl daemon-reload
+systemctl enable mirror-node.service
 
 echo "Starting services"
-sudo systemctl start mirror-node.service
+systemctl start mirror-node.service
