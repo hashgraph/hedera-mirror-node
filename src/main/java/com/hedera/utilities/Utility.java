6b063481f6ed766084bb4c999ffdcbefa1af8698
Apekshit Sharma <apekshit.sharma@hedera.com>
2019-10-31 13:31:33 -0700

    .
    Signed-off-by: Apekshit Sharma <apekshit.sharma@hedera.com>

--- src/main/java/com/hedera/utilities/Utility.java
+++ src/main/java/com/hedera/utilities/Utility.java
@@ -594,9 +594,6 @@ public class Utility {
      * Convert an Instant to a Long type timestampInNanos
      */
     public static Long convertInstantToNanos(Instant instant) {
-        if (instant == null) {
-            return null;
-        }
         try {
             return Math.addExact(Math.multiplyExact(instant.getEpochSecond(), SCALAR), instant.getNano());
         } catch (ArithmeticException e) {
@@ -614,26 +611,15 @@ public class Utility {
 
 	/**
 	 * Convert Timestamp to a Long type timeStampInNanos
-	 * @param TimeStamp
-	 * @return
 	 */
     public static Long timeStampInNanos(Timestamp timestamp) {
-	  return timestamp.getSeconds() * SCALAR + timestamp.getNanos();
+        try {
+            return Math.addExact(Math.multiplyExact(timestamp.getSeconds(), SCALAR), timestamp.getNanos());
+        } catch (ArithmeticException e) {
+            log.error("Long overflow when converting Timestamp to nanos timestamp : {}", timestamp, e);
+            throw e;
+        }
     }
-
-	/**
-	 * Convert a Long type timestampInNanos to an Instant
-	 * @param bigint
-	 * @return
-	 */
-	public static Instant convertNanosToInstant(Long bigint) {
-		if (bigint == null) {
-			return null;
-		}
-		long seconds = bigint / SCALAR;
-		int nanos = (int) (bigint % SCALAR);
-		return Instant.ofEpochSecond(seconds, nanos);
-	}
 
 	public static boolean hashIsEmpty(String hash) {
 		return StringUtils.isBlank(hash) || hash.equals(EMPTY_HASH);
