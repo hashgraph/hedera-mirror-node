1725051c1412b2a0fd57badc3dc9968198f4dd08
Mike Burrage <mike.burrage@hedera.com>
2019-11-21 20:09:38 -0500

    Setup HCS projects for the GRPC API and mirror node GRPC API protobuf and java wrapper.
    Signed-off-by: Mike Burrage <mike.burrage@hedera.com>

---
+++ hedera-mirror-grpc/scripts/init-pg-users.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+
+    create user mirror_grpc with password 'mirror_grpc_pass';
+
+    grant connect on database "$POSTGRES_DB" to mirror_grpc;
+
+    alter default privileges in schema public
+        grant select on tables to mirror_grpc;
+
+EOSQL
