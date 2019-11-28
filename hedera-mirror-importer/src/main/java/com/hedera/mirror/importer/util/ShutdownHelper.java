5f05f100a398ebcfb90973311dd22726f1f9b3dc
Steven Sheehy <steven.sheehy@hedera.com>
2019-11-27 19:14:54 -0600

    Move Maven configuration to parent and minimize dependencies
    Signed-off-by: Steven Sheehy <steven.sheehy@hedera.com>

--- hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/util/ShutdownHelper.java
+++ hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/util/ShutdownHelper.java
@@ -1,11 +1,11 @@
 package com.hedera.mirror.importer.util;
 
 /*-
- *
- * Hedera Mirror Node
- *
+ * ‌
+ * Hedera Mirror Node (HCS)
+ * ​
  * Copyright (C) 2019 Hedera Hashgraph, LLC
- *
+ * ​
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,7 +17,7 @@ package com.hedera.mirror.importer.util;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ * ‍
  */
 
 import javax.inject.Named;
