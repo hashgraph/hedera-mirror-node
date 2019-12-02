c29358bcf51d54dc308ae827b599f939e9a042a1
Steven Sheehy <steven.sheehy@hedera.com>
2019-12-02 17:11:15 -0600

    Remove non-HCS specific changes
    Signed-off-by: Steven Sheehy <steven.sheehy@hedera.com>

--- hedera-mirror-importer/src/test/java/com/hedera/mirror/importer/downloader/AbstractDownloaderTest.java
+++ hedera-mirror-importer/src/test/java/com/hedera/mirror/importer/downloader/AbstractDownloaderTest.java
@@ -1,11 +1,11 @@
 package com.hedera.mirror.importer.downloader;
 
 /*-
- * ‌
+ *
  * Hedera Mirror Node
- * ​
+ *
  * Copyright (C) 2019 Hedera Hashgraph, LLC
- * ​
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,7 +17,7 @@ package com.hedera.mirror.importer.downloader;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ‍
+ *
  */
 
 import static org.assertj.core.api.Assertions.assertThat;
