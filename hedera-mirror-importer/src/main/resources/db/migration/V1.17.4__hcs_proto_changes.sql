dde8287b010b0fda9b88e113dde07c6a98a8c1c1
Steven Sheehy <steven.sheehy@hedera.com>
2020-01-03 16:36:14 -0600

    Review feedback
    Signed-off-by: Steven Sheehy <steven.sheehy@hedera.com>

--- hedera-mirror-importer/src/main/resources/db/migration/V1.17.4__hcs_proto_changes.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/V1.17.4__hcs_proto_changes.sql
@@ -5,7 +5,7 @@ alter table t_entities drop column exp_time_nanos;
 alter table t_entities add column auto_renew_account_id bigint null;
 alter table t_entities add constraint autorenew_account foreign key (auto_renew_account_id) references t_entities(id);
 
-delete from t_transaction_results where proto_id >= 150;
+delete from t_transaction_results where proto_id >= 150 and proto_id <= 159;
 insert into t_transaction_results (proto_id, result) values (150, 'INVALID_TOPIC_ID');
 insert into t_transaction_results (proto_id, result) values (155, 'INVALID_ADMIN_KEY'); -- 151-154 were deleted in proto
 insert into t_transaction_results (proto_id, result) values (156, 'INVALID_SUBMIT_KEY');
