0586f0bd8b3eea25ba433f1e40c64bace53b7558
Apekshit Sharma <apekshit.sharma@hedera.com>
2020-02-20 11:41:32 -0800

    address review comments
    Signed-off-by: Apekshit Sharma <apekshit.sharma@hedera.com>

--- hedera-mirror-importer/src/main/resources/db/migration/V1.19.0__drop_fks_trigger_unused_indexes.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/V1.19.0__drop_fks_trigger_unused_indexes.sql
@@ -1,18 +1,23 @@
 -- Changes to optimize write performance
--- Drop foreign keys in t_transactions
+-- Drop foreign keys in t_transactions and t_entities
 -- Drop foreign keys referencing t_transactions(consensus_timestamp) in t_cryptotransferlists, t_file_data,
 --   t_livehashes and t_contract_result.
--- Drop trigger in topic_message
+-- Drop trigger in topic_message and associated function
 -- Drop unused indexes: idx_t_transactions_rec_file, idx__t_cryptotransferlist_amount
 
 alter table t_transactions
-    drop constraint fk_cud_entity_id;
+    drop constraint if exists fk_cud_entity_id;
 alter table t_transactions
-    drop constraint fk_node_account_id;
+    drop constraint if exists fk_node_account_id;
 alter table t_transactions
-    drop constraint fk_payer_account_id;
+    drop constraint if exists fk_payer_account_id;
 alter table t_transactions
-    drop constraint fk_rec_file_id;
+    drop constraint if exists fk_rec_file_id;
+
+alter table t_entities
+    drop constraint if exists autorenew_account;
+alter table t_entities
+    drop constraint if exists fk_ent_type_id;
 
 alter table t_cryptotransferlists
     drop constraint if exists fk__t_transactions;
@@ -23,7 +28,8 @@ alter table t_livehashes
 alter table t_contract_result
     drop constraint if exists fk__t_transactions;
 
-drop trigger topic_message_trigger on topic_message;
+drop trigger if exists topic_message_trigger on topic_message;
+drop function if exists topic_message_notifier();
 
-drop index idx_t_transactions_rec_file;
-drop index idx__t_cryptotransferlist_amount;
+drop index if exists idx_t_transactions_rec_file;
+drop index if exists idx__t_cryptotransferlist_amount;
