72a8652dfe2604095d6c0fa833025cca31b1f56c
Apekshit Sharma <apekshit.sharma@hedera.com>
2020-03-13 12:26:19 -0700

    add the migration file
    Signed-off-by: Apekshit Sharma <apekshit.sharma@hedera.com>

---
+++ hedera-mirror-importer/src/main/resources/db/migration/V1.20__drop_file_id.sql
@@ -0,0 +1,13 @@
+-- Drop id in t_record_files
+
+ALTER TABLE IF EXISTS t_record_files
+    DROP COLUMN IF EXISTS id;
+ALTER TABLE IF EXISTS t_transactions
+    DROP COLUMN IF EXISTS fk_rec_file_id;
+
+DROP FUNCTION IF EXISTS f_file_create(_file_name t_record_files.name%TYPE);
+DROP FUNCTION IF EXISTS f_file_complete (
+    _file_id t_record_files.id%TYPE
+    ,_file_hash t_record_files.file_hash%TYPE
+    ,_prev_hash t_record_files.prev_hash%TYPE
+);
