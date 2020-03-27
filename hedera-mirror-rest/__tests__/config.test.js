3316a21faecf91971ebad1a3c129399d8be146eb
Steven Sheehy <17552371+steven-sheehy@users.noreply.github.com>
2020-03-27 10:47:12 -0500

    Fix REST tests in CircleCI (#630)
    Signed-off-by: Steven Sheehy <steven.sheehy@hedera.com>

--- hedera-mirror-rest/__tests__/config.test.js
+++ hedera-mirror-rest/__tests__/config.test.js
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -139,6 +139,12 @@ describe('Custom CONFIG_NAME:', () => {
 });
 
 function cleanup() {
-  fs.unlink(path.join('.', 'application.yml'), e => {});
-  fs.unlink(path.join('.', 'application.yaml'), e => {});
+  unlink(path.join('.', 'application.yml'));
+  unlink(path.join('.', 'application.yaml'));
+}
+
+function unlink(file) {
+  if (fs.existsSync(file)) {
+    fs.unlinkSync(file);
+  }
 }
