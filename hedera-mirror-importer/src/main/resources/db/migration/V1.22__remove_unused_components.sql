358b5bca84b14905c0b927214589cb003e6449ce
Apekshit Sharma <apekshit.sharma@hedera.com>
2020-04-08 14:19:10 -0700

    remove codes from t_application_status
    Signed-off-by: Apekshit Sharma <apekshit.sharma@hedera.com>

--- hedera-mirror-importer/src/main/resources/db/migration/V1.22__remove_unused_components.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/V1.22__remove_unused_components.sql
@@ -2,6 +2,11 @@
 drop table if exists t_events;
 drop sequence if exists s_events_id_seq;
 
+DELETE FROM t_application_status WHERE status_code = 'LAST_VALID_DOWNLOADED_EVENT_FILE';
+DELETE FROM t_application_status WHERE status_code = 'LAST_VALID_DOWNLOADED_EVENT_FILE_HASH';
+DELETE FROM t_application_status WHERE status_code = 'EVENT_HASH_MISMATCH_BYPASS_UNTIL_AFTER';
+DELETE FROM t_application_status WHERE status_code = 'LAST_PROCESSED_EVENT_HASH';
+
 -- Remove unused components
 drop table if exists entity_types;
 drop view if exists v_entities;
