98e2df15da3a96c02962fd18ad86fac82bb40281
Nana-EC <56320167+Nana-EC@users.noreply.github.com>
2020-04-29 11:51:40 -0500

    Make default version configurable and update documentation
    Signed-off-by: Nana-EC <56320167+Nana-EC@users.noreply.github.com>

--- hedera-mirror-importer/src/main/resources/db/migration/V1.23.1__add_topicmessage_runninghashversion.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/V1.23.1__add_topicmessage_runninghashversion.sql
@@ -1,4 +1,4 @@
 -- add the column conditionally
 alter table topic_message add column if not exists running_hash_version smallint null;
 
-UPDATE topic_message SET running_hash_version = CASE WHEN consensus_timestamp < ${r5-rc4-deployDate} THEN 1 ELSE 2 END WHERE running_hash_version IS NULL;
+UPDATE topic_message SET running_hash_version = CASE WHEN consensus_timestamp < ${r5-rc4-deployDate} THEN 1 ELSE ${runningHashDefaultVersion} END WHERE running_hash_version IS NULL;
