c0eb3641d8c5db7ae732fbabf35785d509401409
Nana-EC <56320167+Nana-EC@users.noreply.github.com>
2020-05-01 14:12:38 -0500

    Added jest test to capture missing bundledDependencies values
    Signed-off-by: Nana-EC <56320167+Nana-EC@users.noreply.github.com>

---
+++ hedera-mirror-rest/__tests__/package.test.js
@@ -0,0 +1,38 @@
+/*-
+ * ‌
+ * Hedera Mirror Node
+ * ​
+ * Copyright (C) 2019 - 2020 Hedera Hashgraph, LLC
+ * ​
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ‍
+ */
+
+const fs = require('fs');
+const path = require('path');
+
+describe('npm dependency check for packaging', () => {
+  test('bundledDependencies should match dependencies keys', () => {
+    const basePath = path.resolve(process.cwd(), 'package.json');
+    const packageJSON = JSON.parse(fs.readFileSync(basePath, 'utf8'));
+
+    expect(packageJSON).not.toBeNull();
+    expect(packageJSON.dependencies).not.toBeNull();
+    expect(packageJSON.bundledDependencies).not.toBeNull();
+
+    const dependencyKeys = Object.keys(packageJSON.dependencies).sort();
+    const dependencyBundleKeys = packageJSON.bundledDependencies.sort();
+
+    expect(dependencyBundleKeys).toStrictEqual(dependencyKeys);
+  });
+});
