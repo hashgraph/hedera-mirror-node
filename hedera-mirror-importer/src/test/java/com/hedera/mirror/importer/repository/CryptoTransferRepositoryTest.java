317e5e924a9625849038fbfac5cf27de7f07ca9a
Apekshit Sharma <apekshit.sharma@hedera.com>
2020-06-22 12:31:02 -0700

    add crypto repo test
    Signed-off-by: Apekshit Sharma <apekshit.sharma@hedera.com>

---
+++ hedera-mirror-importer/src/test/java/com/hedera/mirror/importer/repository/CryptoTransferRepositoryTest.java
@@ -0,0 +1,53 @@
+package com.hedera.mirror.importer.repository;
+
+/*-
+ * ‌
+ * Hedera Mirror Node
+ * ​
+ * Copyright (C) 2019 - 2020 Hedera Hashgraph, LLC
+ * ​
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ‍
+ */
+
+import static com.hedera.mirror.importer.domain.EntityTypeEnum.ACCOUNT;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import com.hedera.mirror.importer.domain.CryptoTransfer;
+import com.hedera.mirror.importer.domain.EntityId;
+import com.hedera.mirror.importer.domain.Transaction;
+
+public class CryptoTransferRepositoryTest extends AbstractRepositoryTest {
+    @Test
+    void findByConsensusTimestampAndEntityNum() {
+        Transaction transaction = insertTransaction("CRYPTOTRANSFER");
+
+        long consensusNs = transaction.getConsensusNs();
+        EntityId entity = EntityId.of(0L, 1L, 2L, ACCOUNT);
+        long amount = 40L;
+        CryptoTransfer cryptoTransfer = new CryptoTransfer();
+        cryptoTransfer.setConsensusTimestamp(consensusNs);
+        cryptoTransfer.setEntityId(entity);
+        cryptoTransfer.setAmount(amount);
+
+        cryptoTransfer = cryptoTransferRepository.save(cryptoTransfer);
+
+        assertThat(
+                cryptoTransferRepository.findByConsensusTimestampAndEntityIdAndAmount(consensusNs, entity, amount).get())
+                .isNotNull()
+                .isEqualTo(cryptoTransfer);
+    }
+}
