986f0e08a59a27037a704fd78b7078294f0e1bc8
Xin Li <xin.li@swirlds.com>
2020-07-15 12:01:28 -0500

    resolve merge conflict
    Signed-off-by: Xin Li <xin.li@swirlds.com>

--- hedera-mirror-importer/src/test/java/com/hedera/mirror/importer/downloader/AbstractDownloaderTest.java
+++ hedera-mirror-importer/src/test/java/com/hedera/mirror/importer/downloader/AbstractDownloaderTest.java
@@ -53,7 +53,6 @@ import com.hedera.mirror.importer.MirrorProperties;
 import com.hedera.mirror.importer.addressbook.NetworkAddressBook;
 import com.hedera.mirror.importer.config.MetricsExecutionInterceptor;
 import com.hedera.mirror.importer.config.MirrorImporterConfiguration;
-import com.hedera.mirror.importer.domain.HederaNetwork;
 import com.hedera.mirror.importer.repository.ApplicationStatusRepository;
 import com.hedera.mirror.importer.util.Utility;
 
@@ -110,7 +109,7 @@ public abstract class AbstractDownloaderTest {
 
         initProperties();
         s3AsyncClient = new MirrorImporterConfiguration(
-                null, commonDownloaderProperties, new MetricsExecutionInterceptor(meterRegistry))
+                mirrorProperties, commonDownloaderProperties, new MetricsExecutionInterceptor(meterRegistry))
                 .s3CloudStorageClient();
         networkAddressBook = new NetworkAddressBook(mirrorProperties);
         downloader = getDownloader();
@@ -133,10 +132,9 @@ public abstract class AbstractDownloaderTest {
     private void initProperties() {
         mirrorProperties = new MirrorProperties();
         mirrorProperties.setDataPath(dataPath);
-        mirrorProperties.setNetwork(HederaNetwork.TESTNET);
+        mirrorProperties.setNetwork(MirrorProperties.HederaNetwork.TESTNET);
 
-        commonDownloaderProperties = new CommonDownloaderProperties();
-        commonDownloaderProperties.setBucketName("test");
+        commonDownloaderProperties = new CommonDownloaderProperties(mirrorProperties);
         commonDownloaderProperties.setEndpointOverride("http://localhost:" + S3_MOCK_PORT);
         commonDownloaderProperties.setAccessKey("x"); // https://github.com/findify/s3mock/issues/147
         commonDownloaderProperties.setSecretKey("x");
