449ce2b3c64bea13ceb9daf45fa18d3f8a4bd7fb
Xin Li <xin.li@swirlds.com>
2020-08-19 09:16:22 -0500

    use non-default topic number in test case
    Signed-off-by: Xin Li <xin.li@swirlds.com>

--- hedera-mirror-grpc/src/test/java/com/hedera/mirror/grpc/listener/NotifyingTopicListenerTest.java
+++ hedera-mirror-grpc/src/test/java/com/hedera/mirror/grpc/listener/NotifyingTopicListenerTest.java
@@ -74,7 +74,7 @@ public class NotifyingTopicListenerTest extends AbstractSharedTopicListenerTest 
                 "\"running_hash\":\"BAUG\"," +
                 "\"running_hash_version\":2," +
                 "\"sequence_number\":1," +
-                "\"topic_num\":0," +
+                "\"topic_num\":1001," +
                 "\"valid_start_timestamp\":1594401416000000000" +
                 "}";
 
@@ -87,11 +87,12 @@ public class NotifyingTopicListenerTest extends AbstractSharedTopicListenerTest 
                 .runningHash(new byte[] {4, 5, 6})
                 .runningHashVersion(2)
                 .sequenceNumber(1L)
-                .topicNum(0)
+                .topicNum(1001)
                 .validStartTimestamp(Instant.ofEpochSecond(1594401416)).build();
 
         TopicMessageFilter filter = TopicMessageFilter.builder()
                 .startTime(Instant.EPOCH)
+                .topicNum(1001)
                 .build();
 
         topicListener.listen(filter)
