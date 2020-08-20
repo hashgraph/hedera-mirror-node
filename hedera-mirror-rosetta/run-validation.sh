c77eddd350d2678cd306a94e8b2952828c85f375
failfmi <oscurocalma@gmail.com>
2020-08-20 16:17:12 +0300

    Feature(hedera-mirror-rosetta): Rosetta validation config for testnet (#89)
    * test: add rosetta validation config for testnet
    
    * test(hedera-mirror-rosetta): reduce tx concurrency to 8

--- hedera-mirror-rosetta/run-validation.sh
+++ hedera-mirror-rosetta/run-validation.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
-
 echo Getting Rosetta CLI...
-
 # Temporarily using Git Clone instead of Go Get. New version with support of start & end indexes is not released yet
 git clone https://github.com/coinbase/rosetta-cli.git
 cd ./rosetta-cli || exit 1
-
-echo Running Rosetta Data API Validation \#1
-if ! go run main.go check:data --configuration-file=./../validation/validate-from-genesis.json; then
-    echo Failed to Pass API Validation \#1
-    exit 1
-fi
-echo Running Rosetta Data API Validation \#2
-if ! go run main.go check:data --configuration-file=./../validation/validate-from-block-10.json; then
-    echo Failed to Pass API Validation \#2
-    exit 1
-fi
-
+network="$1"
+function run_from_genesis() {
+    echo Running Rosetta Data API Validation \#1
+    if ! go run main.go check:data --configuration-file=./../validation/$network/validate-from-genesis.json; then
+        echo Failed to Pass API Validation \#1
+        exit 1
+    fi
+}
+function run_demo() {
+    echo Running DEMO Validation
+    run_from_genesis
+    echo Running Rosetta Data API Validation \#2
+    if ! go run main.go check:data --configuration-file=./../validation/$network/validate-from-block-10.json; then
+        echo Failed to Pass API Validation \#2
+        exit 1
+    fi
+}
+function run_testnet() {
+    echo Running TESTNET Validation
+    run_from_genesis
+}
+case $network in
+    "testnet")
+        run_testnet
+    ;;
+    *)
+        network="demo"
+        run_demo
+    ;;
+esac
 echo Rosetta Validation Passed Successfully!
