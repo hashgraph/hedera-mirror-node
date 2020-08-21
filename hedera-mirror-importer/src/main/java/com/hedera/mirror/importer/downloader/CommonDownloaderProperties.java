248f09e9f4c3288b1a29481dfe2639adc86175b4
Ian Jungmann <ian.jungmann@hedera.com>
2020-08-21 16:30:00 -0500

    Changes for PR comments (documentation and properties rework)
    Signed-off-by: Ian Jungmann <ian.jungmann@hedera.com>

--- hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/downloader/CommonDownloaderProperties.java
+++ hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/downloader/CommonDownloaderProperties.java
@@ -27,6 +27,7 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
@@ -61,11 +62,7 @@ public class CommonDownloaderProperties {
 
     private String gcpProjectId;
 
-    private String roleArn;
-
-    private String externalId;
-
-    private String roleSessionName = "hedera-mirror-node";
+    private final S3 s3;
 
     @Getter
     @RequiredArgsConstructor
@@ -74,5 +71,17 @@ public class CommonDownloaderProperties {
         GCP("https://storage.googleapis.com");
 
         private final String endpoint;
+    }
+
+    @Data
+    @Validated
+    public static class S3 {
+
+        private String externalId;
+
+        private String roleArn;
+
+        private String roleSessionName = "hedera-mirror-node";
+
     }
 }
