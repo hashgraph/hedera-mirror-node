b5e871f4b5e9355aef156527cba4bb5a6bfffa65
Ian Jungmann <ian.jungmann@hedera.com>
2020-08-21 19:55:48 -0500

    Adding copyright and fixing class name typo
    Signed-off-by: Ian Jungmann <ian.jungmann@hedera.com>

--- hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/config/CredentialsProviderConfiguration.java
+++ hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/config/CredentialsProviderConfiguration.java
@@ -1,5 +1,25 @@
 package com.hedera.mirror.importer.config;
 
+/*-
+ * ‌
+ * Hedera Mirror Node
+ * ​
+ * Copyright (C) 2019 - 2020 Hedera Hashgraph, LLC
+ * ​
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ‍
+ */
+
 import com.hedera.mirror.importer.config.condition.AwsAssumeRoleCondition;
 import com.hedera.mirror.importer.config.condition.StaticCredentialsCondition;
 import com.hedera.mirror.importer.downloader.CommonDownloaderProperties;
@@ -26,7 +46,7 @@ import software.amazon.awssdk.services.sts.model.AssumeRoleRequest;
 @EnableAsync
 @Log4j2
 @RequiredArgsConstructor
-public class CredentiialProviderConfiguration {
+public class CredentialsProviderConfiguration {
 
     private final CommonDownloaderProperties downloaderProperties;
 
