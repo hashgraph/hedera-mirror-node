2fe0a2708170b50032b479ee52d67bdd7b3d9c7a
Nana-EC <56320167+Nana-EC@users.noreply.github.com>
2020-08-24 13:37:01 -0500

    State proof js demo (#969)
    * Initial scaffolding for StateProof CLI
    
    - Adds a node based CLI for state proof under the /state-proof-demo folder. 
    - Adds a startUp.js file that displays a welcome screen and retrieves user input of the desired transaction Id to validate
    - Adds a stateProofHandler.js file that orchestrates the validation logic and performs the parsing of files
    - Adds a recordFile.js responsible for parsing record file buffer content. From this it calculates the file hash and a map of transactionIds present
    - Adds an addresBook.js responsible for parsing file buffer content. It pull out the nodes and public key info
    - Adds a signatureFile.js  responsible for parsing file buffer content. It pulls out the signature and hash details from the files.
    - Adds a transactionsValidator.js that performs the validation logic for public keys and signatures, consensus of hashes and matching of hashes.
    - Adds support for sample API response to test.
    - Adds supports for multiple environments including local
    - Adds simple jest unit tests
    
    Signed-off-by: Nana-EC <56320167+Nana-EC@users.noreply.github.com>

---
+++ hedera-mirror-rest/state-proof-demo/addressBook.js
@@ -0,0 +1,62 @@
+/*-
+ * ‌
+ * Hedera Mirror Node
+ * ​
+ * Copyright (C) 2019 - 2020 Hedera Hashgraph, LLC
+ * ​
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ‍
+ */
+
+'uses strict';
+
+// addressBook object. Parse string to object, provide methods to pull info
+
+// external libraries
+const _ = require('lodash');
+const {NodeAddressBook} = require('@hashgraph/sdk/lib/generated/BasicTypes_pb');
+
+class AddressBook {
+  /**
+   * Parses address book file storing map of nodeid -> rsa public key
+   */
+  constructor(addressBook) {
+    this.parseAddressBookBuffer(addressBook);
+    this.setNodeIdPublicKeyPairs();
+  }
+
+  parseAddressBookBuffer(addressBookBuffer) {
+    const addressBookObject = NodeAddressBook.deserializeBinary(addressBookBuffer);
+    console.log(`${addressBookObject.getNodeaddressList().length} node(s) found in address book`);
+    this.nodeList = addressBookObject.getNodeaddressList();
+  }
+
+  setNodeIdPublicKeyPairs() {
+    this.nodeIdPublicKeyPairs = {};
+    _.forEach(this.nodeList, (nodeAddress) => {
+      let node;
+      // For some address books node id does nt contain node id. In those cases retrieve id from memo field
+      if (_.isUndefined(nodeAddress.getNodeid()) || nodeAddress.getNodeid().indexOf('.') < 1) {
+        node = Buffer.from(nodeAddress.getMemo()).toString('utf-8');
+      } else {
+        node = nodeAddress.getNodeid();
+      }
+
+      this.nodeIdPublicKeyPairs[node] = {publicKey: nodeAddress.getRsaPubkey()};
+    });
+  }
+}
+
+module.exports = {
+  AddressBook,
+};
---
+++ hedera-mirror-rest/state-proof-demo/recordFile.js
@@ -0,0 +1,161 @@
+/*-
+ * ‌
+ * Hedera Mirror Node
+ * ​
+ * Copyright (C) 2019 - 2020 Hedera Hashgraph, LLC
+ * ​
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ‍
+ */
+
+'uses strict';
+
+// external libraries
+const _ = require('lodash');
+const crypto = require('crypto');
+const {TransactionRecord} = require('@hashgraph/sdk/lib/generated/TransactionRecord_pb');
+
+class RecordFile {
+  /**
+   * Parses rcd file storing hash and transactionId map for future verification
+   */
+  constructor(recordFileBuffer) {
+    this.parseRecordFileBuffer(recordFileBuffer);
+  }
+
+  parseRecordFileBuffer(recordFileBuffer) {
+    const recordFileHash = crypto.createHash('sha384');
+    let recordFileContentsHash = recordFileHash;
+
+    // read record file format version
+    const recordFormatVersion = this.readIntFromBufferAndUpdateHash(recordFileBuffer, 0, recordFileHash);
+    if (recordFormatVersion >= 2) {
+      recordFileContentsHash = crypto.createHash('sha384');
+    }
+
+    // version
+    this.readIntFromBufferAndUpdateHash(recordFileBuffer, 4, recordFileHash);
+
+    const fileHashSize = 48; // number of bytes
+    let index = 8;
+
+    this.transactionIdMap = {};
+    while (index < recordFileBuffer.length) {
+      const typeDelimiter = recordFileBuffer[index++];
+
+      switch (typeDelimiter) {
+        case 1:
+          // RECORD_TYPE_PREV_HASH
+          recordFileHash.update(Buffer.from([typeDelimiter]));
+          this.prevHash = this.readBytesFromBufferAndUpdateHash(
+            recordFileBuffer,
+            index,
+            index + fileHashSize,
+            recordFileHash
+          );
+          index += fileHashSize;
+          break;
+        case 2:
+          // RECORD_TYPE_RECORD
+          recordFileContentsHash.update(Buffer.from([typeDelimiter]));
+
+          // transaction raw bytes
+          const transactionRawBytesLength = this.readIntFromBufferAndUpdateHash(
+            recordFileBuffer,
+            index,
+            recordFileContentsHash
+          );
+          index += 4;
+
+          this.readBytesFromBufferAndUpdateHash(
+            recordFileBuffer,
+            index,
+            index + transactionRawBytesLength,
+            recordFileContentsHash
+          );
+          index += transactionRawBytesLength;
+
+          // record raw bytes
+          const recordRawBytesLength = this.readIntFromBufferAndUpdateHash(
+            recordFileBuffer,
+            index,
+            recordFileContentsHash
+          );
+          index += 4;
+
+          // recordRawBuffer
+          const transactionRecordRawBuffer = this.readBytesFromBufferAndUpdateHash(
+            recordFileBuffer,
+            index,
+            index + recordRawBytesLength,
+            recordFileContentsHash
+          );
+          index += recordRawBytesLength;
+
+          this.mapSuccessfulTransactions(transactionRecordRawBuffer);
+
+          break;
+        default:
+          throw new Error(`Unexpected type delimiter '${typeDelimiter}' in record file at index '${index - 1}'`);
+      }
+    }
+
+    if (recordFormatVersion === 2) {
+      recordFileHash.update(recordFileContentsHash.digest());
+    }
+
+    // set recordFile hash
+    this.hash = recordFileHash.digest('hex');
+  }
+
+  containsTransaction(transactionId) {
+    return !_.isUndefined(this.transactionIdMap[transactionId]);
+  }
+
+  readIntFromBufferAndUpdateHash(buffer, start, cryptoHash) {
+    const buf = buffer.subarray(start, start + 4);
+    cryptoHash.update(buf);
+    return buf.readInt32BE(0);
+  }
+
+  readByteFromBufferAndUpdateHash(buffer, index, cryptoHash) {
+    const buf = buffer[index];
+    cryptoHash.update(Buffer.from([buf]));
+    return buf;
+  }
+
+  readBytesFromBufferAndUpdateHash(buffer, start, end, cryptoHash) {
+    const buf = buffer.subarray(start, end);
+    cryptoHash.update(buf);
+    return buf;
+  }
+
+  mapSuccessfulTransactions(transactionRecordRawBuffer) {
+    const deserializedTransactionRecord = TransactionRecord.deserializeBinary(transactionRecordRawBuffer);
+    const transactionReceipt = deserializedTransactionRecord.getReceipt();
+    const status = transactionReceipt.getStatus();
+
+    // check if status was SUCCESS, if so add to map
+    if (status === 22) {
+      const transactionIdFromBody = deserializedTransactionRecord.getTransactionid();
+      const accountId = transactionIdFromBody.getAccountid();
+      const timestamp = transactionIdFromBody.getTransactionvalidstart();
+      const parsedTransactionIdString = `${accountId.getShardnum()}_${accountId.getRealmnum()}_${accountId.getAccountnum()}_${timestamp.getSeconds()}_${timestamp.getNanos()}`;
+      this.transactionIdMap[parsedTransactionIdString] = transactionIdFromBody;
+    }
+  }
+}
+
+module.exports = {
+  RecordFile,
+};
