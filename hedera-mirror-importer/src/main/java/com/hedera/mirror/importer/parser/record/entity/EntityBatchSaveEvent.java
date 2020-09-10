bde1f0e321cdc6975cd39bae55b924c0f60437d9
Steven Sheehy <steven.sheehy@hedera.com>
2020-09-10 16:29:22 -0500

    Clear topic notifications on error
    Signed-off-by: Steven Sheehy <steven.sheehy@hedera.com>

--- hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/parser/record/entity/EntityBatchSaveEvent.java
+++ hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/parser/record/entity/EntityBatchSaveEvent.java
@@ -22,7 +22,7 @@ package com.hedera.mirror.importer.parser.record.entity;
 
 import org.springframework.context.ApplicationEvent;
 
-public class EntityBatchEvent extends ApplicationEvent {
+public class EntityBatchSaveEvent extends ApplicationEvent {
     private static final long serialVersionUID = -5121039174183266247L;
 
     /**
@@ -31,7 +31,7 @@ public class EntityBatchEvent extends ApplicationEvent {
      * @param source the object on which the event initially occurred or with which the event is associated (never
      *               {@code null})
      */
-    public EntityBatchEvent(Object source) {
+    public EntityBatchSaveEvent(Object source) {
         super(source);
     }
 }
