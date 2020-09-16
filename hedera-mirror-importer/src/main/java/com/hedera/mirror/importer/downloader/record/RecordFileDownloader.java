0a06a359fce5a4e11473cd00fab46bd463ab4748
Steven Sheehy <17552371+steven-sheehy@users.noreply.github.com>
2020-09-16 13:39:52 -0500

    Fix polling cloud storage more often than the configured poll period (#1054)
    - Fix polling cloud storage more often than the configured poll period
    - Fix memory leak when in catch up mode by not queuing polls in the background
    
    Signed-off-by: Steven Sheehy <steven.sheehy@hedera.com>

--- hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/downloader/record/RecordFileDownloader.java
+++ hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/downloader/record/RecordFileDownloader.java
@@ -46,19 +46,23 @@ public class RecordFileDownloader extends Downloader {
     public RecordFileDownloader(
             S3AsyncClient s3Client, ApplicationStatusRepository applicationStatusRepository,
             AddressBookService addressBookService, RecordDownloaderProperties downloaderProperties,
-            TransactionTemplate transactionTemplate, MeterRegistry meterRegistry, RecordFileRepository recordFileRepository) {
-        super(s3Client, applicationStatusRepository, addressBookService, downloaderProperties, transactionTemplate, meterRegistry);
+            TransactionTemplate transactionTemplate, MeterRegistry meterRegistry,
+            RecordFileRepository recordFileRepository) {
+        super(s3Client, applicationStatusRepository, addressBookService, downloaderProperties, transactionTemplate,
+                meterRegistry);
         this.recordFileRepository = recordFileRepository;
     }
 
+    @Override
     @Leader
-    @Scheduled(fixedRateString = "${hedera.mirror.importer.downloader.record.frequency:500}")
+    @Scheduled(fixedDelayString = "${hedera.mirror.importer.downloader.record.frequency:500}")
     public void download() {
         downloadNextBatch();
     }
 
     /**
      * Reads the record file.
+     *
      * @param file data file object
      * @return StreamFile object
      */
@@ -69,6 +73,6 @@ public class RecordFileDownloader extends Downloader {
 
     @Override
     protected void saveStreamFileRecord(StreamFile streamFile) {
-        recordFileRepository.save((RecordFile)streamFile);
+        recordFileRepository.save((RecordFile) streamFile);
     }
 }
