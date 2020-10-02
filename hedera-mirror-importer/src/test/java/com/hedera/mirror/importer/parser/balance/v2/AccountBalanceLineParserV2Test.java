2327b82f8cb742caa04117ad6942f175c9f87e96
Ian Jungmann <ian.jungmann@hedera.com>
2020-10-02 16:54:40 -0500

    Change parser v2 token balances from base64 proto to list of values
    Signed-off-by: Ian Jungmann <ian.jungmann@hedera.com>

--- hedera-mirror-importer/src/test/java/com/hedera/mirror/importer/parser/balance/v2/AccountBalanceLineParserV2Test.java
+++ hedera-mirror-importer/src/test/java/com/hedera/mirror/importer/parser/balance/v2/AccountBalanceLineParserV2Test.java
@@ -3,12 +3,11 @@ package com.hedera.mirror.importer.parser.balance.v2;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.hederahashgraph.api.proto.java.TokenBalances;
-import com.hederahashgraph.api.proto.java.TokenID;
+import com.google.common.base.Splitter;
 import java.io.IOException;
-import java.util.Collections;
 import java.util.List;
-import org.apache.commons.codec.binary.Base64;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -28,7 +27,7 @@ class AccountBalanceLineParserV2Test {
     @ParameterizedTest(name = "from \"{0}\"")
     @CsvSource(value = {
             "'0,0,123,700,CggKAxjsBxCEBwoHCgMY7QcQGQoKCgMY7gcQwKilBAoICgMY8gcQhAcKBwoDGPMHEBkKCgoDGPQHEMCopQQ';false;" +
-                    "0;123;700;CggKAxjsBxCEBwoHCgMY7QcQGQoKCgMY7gcQwKilBAoICgMY8gcQhAcKBwoDGPMHEBkKCgoDGPQHEMCopQQ",
+                    "0;123;700;1004=900,1005=25,1006=9000000,1010=900,1011=25,1012=9000000",
             "'0,0,123,700,';false;" +
                     "0;123;700;;",
             "'0,0,123,700,CggKAxjsBxCEBwoHCgMY7QcQGQoKCgMY7gcQwKilBAoICgMY8gcQhAcKBwoDGPMHEBkKCgo';true;;;;",
@@ -55,15 +54,9 @@ class AccountBalanceLineParserV2Test {
             ";true;;;;"
     }, delimiter = ';')
     void parse(String line, boolean expectThrow, Long expectedRealm, Long expectedAccount, Long expectedBalance,
-               String expectedTokenBalanceListString) throws IOException {
+               String tokenBalances) throws IOException {
         AccountBalanceLineParserV2 parser = new AccountBalanceLineParserV2();
         if (!expectThrow) {
-            List<com.hederahashgraph.api.proto.java.TokenBalance> expectedTokenBalanceList = StringUtils
-                    .isNotBlank(expectedTokenBalanceListString) ?
-                    TokenBalances.parseFrom(Base64.decodeBase64(expectedTokenBalanceListString.getBytes()))
-                            .getTokenBalancesList() :
-                    Collections.emptyList();
-
             AccountBalance accountBalance = parser.parse(line, timestamp, systemShardNum);
             var id = accountBalance.getId();
 
@@ -73,23 +66,29 @@ class AccountBalanceLineParserV2Test {
             assertThat(id.getConsensusTimestamp()).isEqualTo(timestamp);
 
             List<TokenBalance> actualTokenBalanceList = accountBalance.getTokenBalances();
-            assertThat(actualTokenBalanceList.size()).isEqualTo(expectedTokenBalanceList.size());
-            for (int i = 0; i < expectedTokenBalanceList.size(); i++) {
-                TokenBalance actualTokenBalance = actualTokenBalanceList.get(i);
-                com.hederahashgraph.api.proto.java.TokenBalance expectedTokenBalance =
-                        expectedTokenBalanceList.get(i);
-                TokenBalance.Id actualId = actualTokenBalance.getId();
-                TokenID expectedId = expectedTokenBalance.getTokenId();
+            if (StringUtils.isNotBlank(tokenBalances)) {
+                Map<Long, Long> expectedTokenBalances = Splitter.on(',').withKeyValueSeparator('=')
+                        .split(tokenBalances).entrySet().stream().collect(Collectors
+                                .toMap(entry -> Long.parseLong(entry.getKey()), entry -> Long
+                                        .parseLong(entry.getValue())));
+                assertThat(actualTokenBalanceList.size()).isEqualTo(expectedTokenBalances.size());
+                for (int i = 0; i < actualTokenBalanceList.size(); i++) {
+                    TokenBalance actualTokenBalance = actualTokenBalanceList.get(i);
+                    TokenBalance.Id actualId = actualTokenBalance.getId();
 
-                assertThat(actualTokenBalance.getBalance()).isEqualTo(expectedTokenBalance.getBalance());
-                assertThat(actualId.getConsensusTimestamp()).isEqualTo(timestamp);
-                assertThat(actualId.getAccountId().getRealmNum()).isEqualTo(expectedRealm);
-                assertThat(actualId.getAccountId().getEntityNum()).isEqualTo(expectedAccount);
+                    assertThat(expectedTokenBalances.containsKey(actualId.getTokenId().getEntityNum()));
+                    assertThat(actualTokenBalance.getBalance())
+                            .isEqualTo(expectedTokenBalances.get(actualId.getTokenId().getEntityNum()));
+                    assertThat(actualId.getConsensusTimestamp()).isEqualTo(timestamp);
+                    assertThat(actualId.getAccountId().getRealmNum()).isEqualTo(expectedRealm);
+                    assertThat(actualId.getAccountId().getEntityNum()).isEqualTo(expectedAccount);
 
-                assertThat(actualId.getTokenId().getShardNum()).isEqualTo(expectedId.getShardNum());
-                assertThat(actualId.getTokenId().getRealmNum()).isEqualTo(expectedId.getRealmNum());
-                assertThat(actualId.getTokenId().getEntityNum()).isEqualTo(expectedId.getTokenNum());
-                assertThat(actualId.getTokenId().getType()).isEqualTo(EntityTypeEnum.TOKEN.getId());
+                    assertThat(actualId.getTokenId().getShardNum()).isEqualTo(systemShardNum);
+                    assertThat(actualId.getTokenId().getRealmNum()).isEqualTo(expectedRealm);
+                    assertThat(actualId.getTokenId().getType()).isEqualTo(EntityTypeEnum.TOKEN.getId());
+                }
+            } else {
+                assertThat(actualTokenBalanceList.size()).isEqualTo(0);
             }
         } else {
             assertThrows(InvalidDatasetException.class, () -> {
