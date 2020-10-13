361877957292bf41774dd95f05bd9cf375a1ec77
failfmi <oscurocalma@gmail.com>
2020-10-13 14:07:14 +0300

    fix(hedera-mirror-rosetta) - (all-in-one dockerfile): remove yq, disable redis (#145)
    Signed-off-by: failfmi <oscurocalma@gmail.com>

--- hedera-mirror-rosetta/build/run_supervisord.sh
+++ hedera-mirror-rosetta/build/run_supervisord.sh
@@ -5,34 +5,17 @@ function run_offline_mode() {
 }
 
 function run_online_mode() {
-  if [[ -n "$HEDERA_MIRROR_IMPORTER_DOWNLOADER_ACCESS_KEY" ]]; then
-    yq write -i application.yml hedera.mirror.importer.downloader.accessKey $HEDERA_MIRROR_IMPORTER_DOWNLOADER_ACCESS_KEY
-  fi
-
-  if [[ -n "$HEDERA_MIRROR_IMPORTER_DOWNLOADER_BUCKET_NAME" ]]; then
-    yq write -i application.yml hedera.mirror.importer.downloader.bucketName $HEDERA_MIRROR_IMPORTER_DOWNLOADER_BUCKET_NAME
-  fi
-
-  if [[ -n "$HEDERA_MIRROR_IMPORTER_DOWNLOADER_CLOUD_PROVIDER" ]]; then
-    yq write -i application.yml hedera.mirror.importer.downloader.cloudProvider $HEDERA_MIRROR_IMPORTER_DOWNLOADER_CLOUD_PROVIDER
-  fi
-
-  if [[ -n "$HEDERA_MIRROR_IMPORTER_DOWNLOADER_GCP_PROJECT_ID" ]]; then
-    yq write -i application.yml hedera.mirror.importer.downloader.gcpProjectId $HEDERA_MIRROR_IMPORTER_DOWNLOADER_GCP_PROJECT_ID
-  fi
-
-  if [[ -n "$HEDERA_MIRROR_IMPORTER_DOWNLOADER_SECRET_KEY" ]]; then
-    yq write -i application.yml hedera.mirror.importer.downloader.secretKey $HEDERA_MIRROR_IMPORTER_DOWNLOADER_SECRET_KEY
-  fi
-
   supervisord
 }
 
 function main() {
   if [[ -n "$NETWORK" ]]; then
-    yq write -i application.yml hedera.mirror.importer.network $NETWORK
+    export HEDERA_MIRROR_IMPORTER_NETWORK=$NETWORK
     export HEDERA_MIRROR_ROSETTA_NETWORK=$NETWORK
   fi
+
+  # Disable Redis because gRPC process is not run within this execution
+  export HEDERA_MIRROR_IMPORTER_PARSER_RECORD_ENTITY_REDIS_ENABLED=false
 
   case $MODE in
     "offline")
