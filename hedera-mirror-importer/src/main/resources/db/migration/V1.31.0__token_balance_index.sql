7f94f9f3ab17d6a7a34749a91b479041de809678
Xin Li <xin.li@swirlds.com>
2020-10-14 13:24:06 -0500

    remove db migration which creates index on token_balance
    Signed-off-by: Xin Li <xin.li@swirlds.com>

--- hedera-mirror-importer/src/main/resources/db/migration/V1.31.0__token_balance_index.sql
+++
@@ -1,3 +0,0 @@
--- Add index for query by consensus_timestamp followed by token_id
-create index if not exists token_balance__timestamp_token_account
-    on token_balance (consensus_timestamp desc, token_id desc, account_id desc);
