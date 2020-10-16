66ea0f03e951444bf0e023fa0ed01cc493864f02
Nana-EC <56320167+Nana-EC@users.noreply.github.com>
2020-10-16 10:36:49 -0500

    Updated Acceptance test to support base HTS scenarios (#1131)
    * Updated Acceptance test to support base HTS scenarios
    
    - Add hts.feature file to describe basic HTS tests
    - Add setup.feature file to support occasional setup operations such as funding accounts and establishing 2 well funded and associated token accounts
    - Update sdk version with new its alpha support in v1
    - Update client files to better share transaction execution logic
    - Add logic to easily calculate transaction costs based on tests run
    - Add logic to hit REST API endpoints for a transaction 
    
    Signed-off-by: Nana-EC <56320167+Nana-EC@users.noreply.github.com>

---
+++ hedera-mirror-test/src/test/java/com/hedera/mirror/test/e2e/acceptance/client/AbstractNetworkClient.java
@@ -0,0 +1,95 @@
+package com.hedera.mirror.test.e2e.acceptance.client;
+
+/*-
+ * ‌
+ * Hedera Mirror Node
+ * ​
+ * Copyright (C) 2019 - 2020 Hedera Hashgraph, LLC
+ * ​
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ‍
+ */
+
+import java.util.List;
+import lombok.Data;
+import lombok.extern.log4j.Log4j2;
+
+import com.hedera.hashgraph.sdk.Client;
+import com.hedera.hashgraph.sdk.HederaStatusException;
+import com.hedera.hashgraph.sdk.Transaction;
+import com.hedera.hashgraph.sdk.TransactionBuilder;
+import com.hedera.hashgraph.sdk.TransactionId;
+import com.hedera.hashgraph.sdk.TransactionList;
+import com.hedera.hashgraph.sdk.TransactionReceipt;
+import com.hedera.hashgraph.sdk.account.AccountBalanceQuery;
+import com.hedera.hashgraph.sdk.crypto.PrivateKey;
+import com.hedera.mirror.test.e2e.acceptance.props.NetworkTransactionResponse;
+
+@Log4j2
+@Data
+public abstract class AbstractNetworkClient {
+    protected final SDKClient sdkClient;
+    protected final Client client;
+
+    public AbstractNetworkClient(SDKClient sdkClient) {
+        this.sdkClient = sdkClient;
+        client = sdkClient.getClient();
+    }
+
+    public TransactionId executeTransaction(TransactionBuilder transactionBuilder, PrivateKey key) throws HederaStatusException {
+
+        Transaction builtTransaction = (Transaction) transactionBuilder.build(client);
+
+        if (key != null) {
+            builtTransaction = builtTransaction.sign(key);
+        }
+
+        TransactionId transactionId = builtTransaction.execute(client);
+        log.debug("Executed transaction {}.", transactionId);
+
+        return transactionId;
+    }
+
+    public NetworkTransactionResponse executeTransactionAndRetrieveReceipt(TransactionBuilder transactionBuilder,
+                                                                           PrivateKey key) throws HederaStatusException {
+        long startBalance = getBalance();
+        TransactionId transactionId = executeTransaction(transactionBuilder, key);
+        TransactionReceipt transactionReceipt = transactionId.getReceipt(client);
+        log.trace("Executed transaction {} cost {} tℏ", transactionId, startBalance - getBalance());
+        return new NetworkTransactionResponse(transactionId, transactionReceipt);
+    }
+
+    public List<TransactionId> executeTransactionList(TransactionBuilder transactionBuilder, PrivateKey key) throws HederaStatusException {
+
+        TransactionList transactionList = (TransactionList) transactionBuilder.build(client);
+
+        if (key != null) {
+            transactionList = transactionList.sign(key);
+        }
+
+        List<TransactionId> transactionIdList = transactionList.executeAll(client);
+        if (transactionIdList.size() == 1) {
+            log.debug("Executed transaction {}.", transactionIdList.get(0));
+        } else {
+            log.debug("Executed {} transactions.", transactionIdList.size());
+        }
+
+        return transactionIdList;
+    }
+
+    public long getBalance() throws HederaStatusException {
+        return new AccountBalanceQuery()
+                .setAccountId(sdkClient.getOperatorId())
+                .execute(client).asTinybar();
+    }
+}
---
+++ hedera-mirror-test/src/test/resources/features/hts/hts.feature
@@ -0,0 +1,42 @@
+@TokenBase @FullSuite
+Feature: HTS Base Coverage Feature
+
+#    @Acceptance
+    Scenario Outline: Validate Base Token Flow - Create, Associate, Transfer
+        Given I successfully create a new token
+        When I associate a new account with token
+        Then I transfer <amount> tokens to recipient
+        Then the mirror node REST API should return status <httpStatusCode>
+        Examples:
+            | amount | httpStatusCode |
+            | 2350   | 200            |
+
+#    @Sanity @Acceptance
+    Scenario Outline: Validate Freeze and KYC Flow - Create, Unfreeze, GrankKyc, Transfer
+        Given I successfully create a new token with freeze status <initialFreezeStatus> and kyc status <initialKycStatus>
+        When I associate a new account with token
+        And I set new account freeze status to <newFreezeStatus>
+        And I set new account kyc status to <newKycStatus>
+        Then I transfer <amount> tokens to recipient
+        Then the mirror node REST API should return status <httpStatusCode>
+        Examples:
+            | initialFreezeStatus | initialKycStatus | newFreezeStatus | newKycStatus | amount | httpStatusCode |
+            | 1                   | 2                | 2               | 1            | 2350   | 200            |
+
+#    @Negative @Acceptance
+    Scenario Outline: Validate Negative Association
+        Given I successfully create a new token
+        Then the network should observe an error associating a token <errorCode>
+        Examples:
+            | errorCode                             |
+            | "TOKEN_ALREADY_ASSOCIATED_TO_ACCOUNT" |
+
+#    @Negative @Acceptance
+    Scenario Outline: Validate token creation with bad symbols
+        Given I provide a token symbol <tokenId>
+        Then the network should observe an error creating a token <errorCode>
+        Examples:
+            | tokenId     | errorCode              |
+            | ""          | "MISSING_TOKEN_SYMBOL" |
+            | "q1MG"      | "INVALID_TOKEN_SYMBOL" |
+            | "hashGRAPH" | "INVALID_TOKEN_SYMBOL" |
