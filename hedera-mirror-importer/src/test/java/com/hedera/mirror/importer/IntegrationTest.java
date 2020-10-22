95123d0fe6b83498b398c0337045fbadc8048b9b
Xin Li <xin.li@swirlds.com>
2020-10-22 12:17:10 -0500

    revert changes to IntegrationTest since now cleanup.sql locks t_application_status table to serializes writes to it
    Signed-off-by: Xin Li <xin.li@swirlds.com>

--- hedera-mirror-importer/src/test/java/com/hedera/mirror/importer/IntegrationTest.java
+++ hedera-mirror-importer/src/test/java/com/hedera/mirror/importer/IntegrationTest.java
@@ -28,6 +28,7 @@ import org.springframework.test.context.jdbc.Sql;
         mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
 // Same database is used for all tests, so clean it up before each test.
 @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:db/scripts/cleanup.sql")
+@Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:db/scripts/cleanup.sql")
 @SpringBootTest
 public class IntegrationTest {
 }
