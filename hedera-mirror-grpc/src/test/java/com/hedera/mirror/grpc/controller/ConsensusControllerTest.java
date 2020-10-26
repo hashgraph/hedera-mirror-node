5bc4e580762d249c08abb550f546c90cfb927664
Xin Li <xin.li@swirlds.com>
2020-10-25 19:11:55 -0500

    enable failing grpc test
    Signed-off-by: Xin Li <xin.li@swirlds.com>

--- hedera-mirror-grpc/src/test/java/com/hedera/mirror/grpc/controller/ConsensusControllerTest.java
+++ hedera-mirror-grpc/src/test/java/com/hedera/mirror/grpc/controller/ConsensusControllerTest.java
@@ -33,7 +33,6 @@ import lombok.extern.log4j.Log4j2;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -218,7 +217,6 @@ public class ConsensusControllerTest extends GrpcIntegrationTest {
                 .verify(Duration.ofMillis(1000));
     }
 
-    @Disabled("This test fails in GitHub Action 'Deploy Development'")
     @Test
     void fragmentedMessagesGroupAcrossHistoricAndIncoming() {
         Instant now = Instant.now();
