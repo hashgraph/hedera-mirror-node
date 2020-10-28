f041a2c60f35dc16e6ddfb415693bbcb31a8f9ca
Nana-EC <56320167+Nana-EC@users.noreply.github.com>
2020-10-28 13:29:08 -0500

    Add support for case insensitive query params to REST API (#1168)
    * Add support for case insensitive query params to REST API
    
    - Add a middleware function using Proxy object to support lookup of values regardless of case
    - Convert filter query name to lower case on creation
    - Update a few test specs with camel case variations of query keys
    - Set filter keys to lowercase for easy comparison
    
    Signed-off-by: Nana-EC <56320167+Nana-EC@users.noreply.github.com>

---
+++ hedera-mirror-rest/middleware/requestHandler.js
@@ -0,0 +1,65 @@
+/*-
+ * ‌
+ * Hedera Mirror Node
+ * ​
+ * Copyright (C) 2019 - 2020 Hedera Hashgraph, LLC
+ * ​
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ‍
+ */
+
+'use strict';
+
+const qs = require('qs');
+
+const requestLogger = function (req, res, next) {
+  logger.debug(`Client: [ ${req.ip} ] URL: ${req.originalUrl}`);
+  return next();
+};
+
+/**
+ * Manage request query params to support case insensitive keys
+ * Express default query parer uses qs, other option is querystring, both are case sensitive
+ * Parse using default qs logic and use to populate a new map in which all keys are lowercased
+ * @param queryString
+ * @returns Query string map object
+ */
+const requestQueryParser = (queryString) => {
+  // parse first to benefit from qs query handling
+  const parsedQueryString = qs.parse(queryString);
+
+  const caseInsensitiveQueryString = {};
+  for (const key of Object.keys(parsedQueryString)) {
+    const lowerKey = key.toLowerCase();
+    let currentValue = caseInsensitiveQueryString[lowerKey];
+    if (currentValue) {
+      // handle repeated values. Add to array if applicable or convert to array
+      if (!Array.isArray(currentValue)) {
+        // create a new array of current value
+        currentValue = [currentValue];
+      }
+
+      // assign value as concat of current value array and new value which may also be an array
+      caseInsensitiveQueryString[lowerKey] = currentValue.concat(parsedQueryString[key]);
+    } else {
+      caseInsensitiveQueryString[lowerKey] = parsedQueryString[key];
+    }
+  }
+
+  return caseInsensitiveQueryString;
+};
+
+module.exports = {
+  requestLogger,
+  requestQueryParser,
+};
