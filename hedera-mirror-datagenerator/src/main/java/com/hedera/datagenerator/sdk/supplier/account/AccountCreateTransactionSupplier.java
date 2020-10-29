ae67c9d7ded037da06b1ef7e73275028c66140be
Ian Jungmann <ian.jungmann@hedera.com>
2020-10-29 12:41:31 -0500

    Remove key logging when creating account
    Signed-off-by: Ian Jungmann <ian.jungmann@hedera.com>

--- hedera-mirror-datagenerator/src/main/java/com/hedera/datagenerator/sdk/supplier/account/AccountCreateTransactionSupplier.java
+++ hedera-mirror-datagenerator/src/main/java/com/hedera/datagenerator/sdk/supplier/account/AccountCreateTransactionSupplier.java
@@ -9,9 +9,9 @@ package com.hedera.datagenerator.sdk.supplier.account;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -54,9 +54,6 @@ public class AccountCreateTransactionSupplier implements TransactionSupplier<Acc
     private Ed25519PublicKey generateKeys() {
         Ed25519PrivateKey privateKey = Ed25519PrivateKey.generate();
         Ed25519PublicKey publicKey = privateKey.publicKey;
-
-        log.debug("Private key = {}", privateKey);
-        log.debug("Public key = {}", publicKey);
         return publicKey;
     }
 }
