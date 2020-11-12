a5abd8653c362426b5e7cbb504437b38e3618ff2
Xin Li <xin.li@swirlds.com>
2020-11-12 09:22:43 -0600

    minor, all lower case
    Signed-off-by: Xin Li <xin.li@swirlds.com>

--- hedera-mirror-importer/src/main/resources/db/migration/V1.31.0__token_symbol_size_100.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/V1.31.0__token_symbol_size_100.sql
@@ -1,2 +1,2 @@
 -- HAPI changes the max symbol size to 100 characters, apply the same to db table
-alter table if exists token alter column symbol TYPE character varying(100);
+alter table if exists token alter column symbol type character varying(100);
