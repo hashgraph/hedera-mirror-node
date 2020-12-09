cf21c2b2a190fbc9f10c1d2a55c644f26b3a8089
Nana-EC <56320167+Nana-EC@users.noreply.github.com>
2020-12-09 15:04:28 -0600

    Address feedback on timesacle db init migrations regarding domains, placeholders and distributed hyper tables
    Signed-off-by: Nana-EC <56320167+Nana-EC@users.noreply.github.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v2/V2.0.1__hyper_tables.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/v2/V2.0.1__hyper_tables.sql
@@ -4,53 +4,52 @@
 -- add TIMESTAMPTZ data type column to tables where no monotonically increasing id exists
 -------------------
 
--- \set default_chunk_time_interval 604800000000000;
--- \set default_chunk_id_interval 10000;
+-- Extend the database with TimescaleDB - assumes super user permissions on database
+--create extension if not exists timescaledb cascade;
 
--- Extend the database with TimescaleDB
-create extension if not exists timescaledb cascade;
-
--- account_balance
-select create_hypertable('account_balance', 'consensus_timestamp', chunk_time_interval => 604800000000000,
-                         if_not_exists => true);
+-- account_balance, distribute across account_id
+select create_distributed_hypertable('account_balance', 'consensus_timestamp', chunk_time_interval => ${default_chunk_time_interval},
+                         if_not_exists => true, partitioning_column => 'account_id');
 
 -- account_balance_file
 select create_hypertable('account_balance_file', 'consensus_timestamp',
-                         chunk_time_interval => 604800000000000, if_not_exists => true);
+                         chunk_time_interval => ${default_chunk_time_interval}, if_not_exists => true);
 
 -- account_balance_sets
 select create_hypertable('account_balance_sets', 'consensus_timestamp',
-                         chunk_time_interval => 604800000000000, if_not_exists => true);
+                         chunk_time_interval => ${default_chunk_time_interval}, if_not_exists => true);
 
--- address_book skipped because foreign keys are supported with hyper tables, also we don't expect high traffic inserts
+-- address_book skipped because foreign keys are not supported with hyper tables, also we don't expect high traffic inserts
 
 -- contract_result
-select create_hypertable('contract_result', 'consensus_timestamp', chunk_time_interval => 604800000000000,
+select create_hypertable('contract_result', 'consensus_timestamp', chunk_time_interval => ${default_chunk_time_interval},
                          if_not_exists => true);
 
--- crypto_transfer
-select create_hypertable('crypto_transfer', 'consensus_timestamp', chunk_time_interval => 604800000000000,
-                         if_not_exists => true);
+-- crypto_transfer, distribute across entity_id
+select create_distributed_hypertable('crypto_transfer', 'consensus_timestamp', chunk_time_interval => ${default_chunk_time_interval},
+                         if_not_exists => true, partitioning_column => 'entity_id');
 
--- file_data
-select create_hypertable('file_data', 'consensus_timestamp', chunk_time_interval => 604800000000000,
-                         if_not_exists => true);
+-- file_data, distribute across entity_id
+select create_distributed_hypertable('file_data', 'consensus_timestamp', chunk_time_interval => ${default_chunk_time_interval},
+                         if_not_exists => true, partitioning_column => 'entity_id');
 
 -- live_hash
-select create_hypertable('live_hash', 'consensus_timestamp', chunk_time_interval => 604800000000000,
+select create_hypertable('live_hash', 'consensus_timestamp', chunk_time_interval => ${default_chunk_time_interval},
                          if_not_exists => true);
 
 -- non_fee_transfer
-select create_hypertable('non_fee_transfer', 'consensus_timestamp', chunk_time_interval => 604800000000000,
+select create_hypertable('non_fee_transfer', 'consensus_timestamp', chunk_time_interval => ${default_chunk_time_interval},
                          if_not_exists => true);
 
--- record_file
-select create_hypertable('record_file', 'id', chunk_time_interval => 1000, if_not_exists => true);
+-- record_file, distribute across node_account_id
+select create_distributed_hypertable('record_file', 'consensus_start', chunk_time_interval => ${default_chunk_id_interval},
+    if_not_exists => true, partitioning_column => 'node_account_id');
 
 -- t_application_status
 
--- t_entities
-select create_hypertable('t_entities', 'id', chunk_time_interval => 10000, if_not_exists => true);
+-- t_entities, distribute across entity_type
+select create_distributed_hypertable('t_entities', 'id', chunk_time_interval => ${default_chunk_id_interval},
+    if_not_exists => true, partitioning_column => 'entity_type');
 
 -- t_entity_types hyper table skipped as table rarely gets updated
 
@@ -58,25 +57,26 @@ select create_hypertable('t_entities', 'id', chunk_time_interval => 10000, if_no
 
 -- t_transaction_types hyper table skipped as table rarely gets updated
 
--- token
-select create_hypertable('token', 'token_id', chunk_time_interval => 10000, if_not_exists => true);
+-- token, distribute across treasury_account_id
+select create_distributed_hypertable('token', 'created_timestamp', chunk_time_interval => ${default_chunk_time_interval},
+    if_not_exists => true, partitioning_column => 'treasury_account_id');
 
--- token_account
-select create_hypertable('token_account', 'id', chunk_time_interval => 10000,
-                         if_not_exists => true);
+-- token_account, distribute across kyc_status
+select create_distributed_hypertable('token_account', 'created_timestamp', chunk_time_interval => ${default_chunk_time_interval},
+                         if_not_exists => true, partitioning_column => 'kyc_status');
 
--- token_balance
-select create_hypertable('token_balance', 'consensus_timestamp', chunk_time_interval => 604800000000000,
-                         if_not_exists => true);
+-- token_balance, distribute across account_id
+select create_distributed_hypertable('token_balance', 'consensus_timestamp', chunk_time_interval => ${default_chunk_time_interval},
+                         if_not_exists => true, partitioning_column => 'account_id');
 
--- token_transfer
-select create_hypertable('token_transfer', 'consensus_timestamp', chunk_time_interval => 604800000000000,
-                         if_not_exists => true);
+-- token_transfer, distribute across account_id
+select create_distributed_hypertable('token_transfer', 'consensus_timestamp', chunk_time_interval => ${default_chunk_time_interval},
+                         if_not_exists => true, partitioning_column => 'account_id');
 
--- topic_message
-select create_hypertable('topic_message', 'consensus_timestamp', chunk_time_interval => 604800000000000,
-                         if_not_exists => true);
+-- topic_message, distribute across topic_num
+select create_distributed_hypertable('topic_message', 'consensus_timestamp', chunk_time_interval => ${default_chunk_time_interval},
+                         if_not_exists => true, partitioning_column => 'topic_num');
 
--- transaction
-select create_hypertable('transaction', 'consensus_ns', chunk_time_interval => 604800000000000,
-                         if_not_exists => true);
+-- transaction, distribute across type
+select create_distributed_hypertable('transaction', 'consensus_ns', chunk_time_interval => ${default_chunk_time_interval},
+                         if_not_exists => true, partitioning_column => 'type');
