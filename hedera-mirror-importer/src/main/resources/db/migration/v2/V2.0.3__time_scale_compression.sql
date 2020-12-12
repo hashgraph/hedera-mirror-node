869d165da5916720174908625036b387c38ae4b9
Nana-EC <56320167+Nana-EC@users.noreply.github.com>
2020-12-11 20:28:15 -0600

    Addressed feedback on compression and missing hyper tables
    Signed-off-by: Nana-EC <56320167+Nana-EC@users.noreply.github.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v2/V2.0.3__time_scale_compression.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/v2/V2.0.3__time_scale_compression.sql
@@ -15,6 +15,7 @@ $$;
 select set_integer_now_func('account_balance', 'unix_now');
 select set_integer_now_func('account_balance_file', 'unix_now');
 select set_integer_now_func('account_balance_sets', 'unix_now');
+select set_integer_now_func('address_book_entry', 'unix_now');
 select set_integer_now_func('contract_result', 'unix_now');
 select set_integer_now_func('crypto_transfer', 'unix_now');
 select set_integer_now_func('file_data', 'unix_now');
@@ -30,37 +31,39 @@ select set_integer_now_func('transaction', 'unix_now');
 
 -- turn compression on
 alter table account_balance
-    set (timescaledb.compress, timescaledb.compress_orderby = 'consensus_timestamp desc', timescaledb.compress_segmentby = 'account_id');
+    set (timescaledb.compress, timescaledb.compress_segmentby = 'account_id');
 
 alter table account_balance_file
-    set (timescaledb.compress, timescaledb.compress_orderby = 'consensus_timestamp desc', timescaledb.compress_segmentby = 'node_account_id');
+    set (timescaledb.compress, timescaledb.compress_segmentby = 'node_account_id');
 
 alter table account_balance_sets
-    set (timescaledb.compress, timescaledb.compress_orderby = 'consensus_timestamp desc');
+    set (timescaledb.compress);
+
+alter table address_book_entry
+    set (timescaledb.compress, timescaledb.compress_segmentby = 'consensus_timestamp,memo');
 
 -- address_book skipped skipped since not a hyper table
 
--- address_book_entry skipped skipped since not a hyper table
-
 alter table contract_result
-    set (timescaledb.compress, timescaledb.compress_orderby = 'consensus_timestamp desc');
+    set (timescaledb.compress);
 
 alter table crypto_transfer
-    set (timescaledb.compress, timescaledb.compress_orderby = 'consensus_timestamp desc', timescaledb.compress_segmentby = 'entity_id');
+    set (timescaledb.compress, timescaledb.compress_segmentby = 'entity_id');
 
 alter table file_data
-    set (timescaledb.compress, timescaledb.compress_orderby = 'consensus_timestamp desc', timescaledb.compress_segmentby = 'entity_id');
+    set (timescaledb.compress, timescaledb.compress_segmentby = 'entity_id');
 
 -- live_hash skipped
+
 alter table non_fee_transfer
-    set (timescaledb.compress, timescaledb.compress_orderby = 'consensus_timestamp desc');
+    set (timescaledb.compress, timescaledb.compress_segmentby = 'entity_id');
 
 alter table record_file
-    set (timescaledb.compress, timescaledb.compress_orderby = 'consensus_start desc', timescaledb.compress_segmentby = 'node_account_id');
+    set (timescaledb.compress, timescaledb.compress_segmentby = 'node_account_id');
 
 -- t_application_status skipped
 alter table t_entities
-    set (timescaledb.compress, timescaledb.compress_orderby = 'id desc', timescaledb.compress_segmentby = 'deleted');
+    set (timescaledb.compress, timescaledb.compress_segmentby = 'fk_entity_type_id');
 
 -- t_entity_types skipped as not a hyper table
 
@@ -69,28 +72,29 @@ alter table t_entities
 -- t_transaction_types skipped as not a hyper table
 
 alter table token
-    set (timescaledb.compress, timescaledb.compress_orderby = 'created_timestamp desc', timescaledb.compress_segmentby = 'freeze_default');
+    set (timescaledb.compress, timescaledb.compress_segmentby = 'token_id');
 
 alter table token_account
-    set (timescaledb.compress, timescaledb.compress_orderby = 'created_timestamp desc', timescaledb.compress_segmentby = 'account_id');
+    set (timescaledb.compress, timescaledb.compress_segmentby = 'account_id');
 
 alter table token_balance
-    set (timescaledb.compress, timescaledb.compress_orderby = 'consensus_timestamp desc', timescaledb.compress_segmentby = 'account_id,token_id');
+    set (timescaledb.compress, timescaledb.compress_segmentby = 'account_id, token_id');
 
 alter table token_transfer
-    set (timescaledb.compress, timescaledb.compress_orderby = 'consensus_timestamp desc', timescaledb.compress_segmentby = 'account_id');
+    set (timescaledb.compress, timescaledb.compress_segmentby = 'account_id');
 
 alter table topic_message
-    set (timescaledb.compress, timescaledb.compress_orderby = 'consensus_timestamp desc', timescaledb.compress_segmentby = 'topic_num');
+    set (timescaledb.compress, timescaledb.compress_segmentby = 'realm_num, topic_num');
 
 alter table transaction
-    set (timescaledb.compress, timescaledb.compress_orderby = 'consensus_ns desc', timescaledb.compress_segmentby = 'type');
+    set (timescaledb.compress, timescaledb.compress_segmentby = 'payer_account_id, type');
 
 
 -- add compression policy
 select add_compression_policy('account_balance', bigint '${compressionAge}');
 select add_compression_policy('account_balance_file', bigint '${compressionAge}');
 select add_compression_policy('account_balance_sets', bigint '${compressionAge}');
+select add_compression_policy('address_book_entry', bigint '${compressionAge}');
 select add_compression_policy('contract_result', bigint '${compressionAge}');
 select add_compression_policy('crypto_transfer', bigint '${compressionAge}');
 select add_compression_policy('file_data', bigint '${compressionAge}');
