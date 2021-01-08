0e8aa40b97740df723a5d5a3faa4b5bfaca3eb64
Steven Sheehy <17552371+steven-sheehy@users.noreply.github.com>
2021-01-08 09:45:06 -0600

    Add a monitor warmup period property (#1418)
    - Add a warmup period property to monitor with a default of 30s
    - Moved "Should be changed from default" warnings from each property to a global warning at the top since my formatter kept breaking it
    
    Signed-off-by: Steven Sheehy <steven.sheehy@hedera.com>

--- hedera-mirror-monitor/src/main/java/com/hedera/mirror/monitor/generator/ConfigurableTransactionGenerator.java
+++ hedera-mirror-monitor/src/main/java/com/hedera/mirror/monitor/generator/ConfigurableTransactionGenerator.java
@@ -57,7 +57,7 @@ public class ConfigurableTransactionGenerator implements TransactionGenerator {
         this.expressionConverter = expressionConverter;
         this.properties = properties;
         this.transactionSupplier = Suppliers.memoize(this::convert);
-        this.rateLimiter = RateLimiter.create(properties.getTps());
+        this.rateLimiter = RateLimiter.create(properties.getTps(), properties.getWarmupPeriod());
         remaining = new AtomicLong(properties.getLimit());
         stopTime = System.nanoTime() + properties.getDuration().toNanos();
         builder = PublishRequest.builder()
