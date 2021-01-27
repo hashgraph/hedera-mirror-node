b8588f50f7e6d2b1d9bce246a7971c48088f5f50
Steven Sheehy <steven.sheehy@hedera.com>
2021-01-27 17:21:49 -0600

    Fix whitespace in license header
    Signed-off-by: Steven Sheehy <steven.sheehy@hedera.com>

--- hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/domain/TokenBalance.java
+++ hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/domain/TokenBalance.java
@@ -3,9 +3,9 @@ package com.hedera.mirror.importer.domain;
 /*-
  * ‌
  * Hedera Mirror Node
- *
+ * ​
  * Copyright (C) 2019 - 2021 Hedera Hashgraph, LLC
- *
+ * ​
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
