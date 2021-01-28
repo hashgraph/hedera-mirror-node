092de6e220347a6fcd795a3f3ee05c589fd25963
Xin Li <xin.li@hedera.com>
2021-01-27 19:16:04 -0600

    increase prefetch size to improve the consuming speed so as to reduce the chance of failure in slow environment
    Signed-off-by: Xin Li <xin.li@hedera.com>

--- hedera-mirror-grpc/src/test/java/com/hedera/mirror/grpc/listener/AbstractSharedTopicListenerTest.java
+++ hedera-mirror-grpc/src/test/java/com/hedera/mirror/grpc/listener/AbstractSharedTopicListenerTest.java
@@ -9,9 +9,9 @@ package com.hedera.mirror.grpc.listener;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -41,7 +41,7 @@ public abstract class AbstractSharedTopicListenerTest extends AbstractTopicListe
     @DisplayName("slow subscriber receives overflow exception and normal subscriber is not affected")
     void slowSubscriberOverflowException() {
         int maxBufferSize = 16;
-        int prefetch = 1;
+        int prefetch = 4;
 
         // step verifier requests 2 messages on subscription, and there are downstream buffers after the backpressure
         // buffer, to ensure overflow, set the number of topic messages to send as follows
