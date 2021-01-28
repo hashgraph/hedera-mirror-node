f848b397b4fe2f56197b16ec09573364acdd578e
Ian Jungmann <ian.jungmann@hedera.com>
2021-01-28 12:54:45 -0600

    Fix issue with incomplete stubbing on Redis Slow Publish Test
    Signed-off-by: Ian Jungmann <ian.jungmann@hedera.com>

--- hedera-mirror-importer/src/test/java/com/hedera/mirror/importer/parser/record/entity/redis/RedisEntityListenerTest.java
+++ hedera-mirror-importer/src/test/java/com/hedera/mirror/importer/parser/record/entity/redis/RedisEntityListenerTest.java
@@ -9,9 +9,9 @@ package com.hedera.mirror.importer.parser.record.entity.redis;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,7 +24,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.doCallRealMethod;
 
 import java.time.Duration;
 import java.util.ArrayList;
@@ -153,7 +152,7 @@ class RedisEntityListenerTest extends BatchEntityListenerTest {
                 .verify(Duration.ofMillis(1000));
 
         // reset the stub
-        doCallRealMethod().when(redisOperations).convertAndSend(anyString(), any(Object.class));
+        Mockito.reset(redisOperations);
     }
 
     @TestConfiguration
