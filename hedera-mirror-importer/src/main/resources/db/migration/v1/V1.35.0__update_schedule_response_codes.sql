7899c40beffefe677c2ed36dfe0459195b842e5d
Ian Jungmann <ian.jungmann@hedera.com>
2021-02-09 12:36:23 -0600

    Update response codes
    Signed-off-by: Ian Jungmann <ian.jungmann@hedera.com>

---
+++ hedera-mirror-importer/src/main/resources/db/migration/v1/V1.35.0__update_schedule_response_codes.sql
@@ -0,0 +1,17 @@
+-------------------
+-- Support new response codes
+-------------------
+
+-- Update existing transaction result types since SCHEDULE_WAS_DELETED was deleted
+update t_transaction_results set result = 'INVALID_SCHEDULE_PAYER_ID' WHERE proto_id = 203;
+update t_transaction_results set result = 'INVALID_SCHEDULE_ACCOUNT_ID' WHERE proto_id = 204;
+
+-- Add new schedule transaction result types
+UPDATE t_transaction_results set result = 'NO_NEW_VALID_SIGNATURES' WHERE proto_id = 205;
+
+insert into t_transaction_results (proto_id, result)
+values (206, 'UNRESOLVABLE_REQUIRED_SIGNERS'),
+       (207, 'UNPARSEABLE_SCHEDULED_TRANSACTION'),
+       (208, 'UNSCHEDULABLE_TRANSACTION'),
+       (209, 'SOME_SIGNATURES_WERE_INVALID'),
+       (210, 'TRANSACTION_ID_FIELD_NOT_ALLOWED');
