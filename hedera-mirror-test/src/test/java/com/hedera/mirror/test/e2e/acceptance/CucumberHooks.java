5e0b06cc2eefedc0c7961496c013af38f2900a5a
Nana-EC <56320167+Nana-EC@users.noreply.github.com>
2021-03-03 12:21:16 -0600

    used a collection autowire
    Signed-off-by: Nana-EC <56320167+Nana-EC@users.noreply.github.com>

--- hedera-mirror-test/src/test/java/com/hedera/mirror/test/e2e/acceptance/CucumberHooks.java
+++ hedera-mirror-test/src/test/java/com/hedera/mirror/test/e2e/acceptance/CucumberHooks.java
@@ -1,34 +1,25 @@
 package com.hedera.mirror.test.e2e.acceptance;
 
 import io.cucumber.plugin.ConcurrentEventListener;
-import io.cucumber.plugin.event.EventHandler;
 import io.cucumber.plugin.event.EventPublisher;
 import io.cucumber.plugin.event.TestRunFinished;
+import java.util.Collection;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.hedera.mirror.test.e2e.acceptance.client.MirrorNodeClient;
-import com.hedera.mirror.test.e2e.acceptance.client.TokenClient;
-import com.hedera.mirror.test.e2e.acceptance.client.TopicClient;
+import com.hedera.mirror.test.e2e.acceptance.client.AbstractNetworkClient;
 
 @Log4j2
 public class CucumberHooks implements ConcurrentEventListener {
     @Autowired
-    private MirrorNodeClient mirrorClient;
-    @Autowired
-    private TokenClient tokenClient;
-    @Autowired
-    private TopicClient topicClient;
+    private Collection<AbstractNetworkClient> clients;
 
-    private final EventHandler<TestRunFinished> afterAll = event -> {
-        // close clients
-        mirrorClient.close();
-        tokenClient.close();
-        topicClient.close();
-    };
+    private void afterAll(TestRunFinished event) {
+        clients.forEach(AbstractNetworkClient::close);
+    }
 
     @Override
     public void setEventPublisher(EventPublisher eventPublisher) {
-        eventPublisher.registerHandlerFor(TestRunFinished.class, afterAll);
+        eventPublisher.registerHandlerFor(TestRunFinished.class, this::afterAll);
     }
 }
