0a79e9920702af128470a923e657169c28d07859
Nana-EC <56320167+Nana-EC@users.noreply.github.com>
2021-03-03 14:19:03 -0600

    Used constructor injection over autowire
    Signed-off-by: Nana-EC <56320167+Nana-EC@users.noreply.github.com>

--- hedera-mirror-test/src/test/java/com/hedera/mirror/test/e2e/acceptance/CucumberHooks.java
+++ hedera-mirror-test/src/test/java/com/hedera/mirror/test/e2e/acceptance/CucumberHooks.java
@@ -4,22 +4,26 @@ import io.cucumber.plugin.ConcurrentEventListener;
 import io.cucumber.plugin.event.EventPublisher;
 import io.cucumber.plugin.event.TestRunFinished;
 import java.util.Collection;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import com.hedera.mirror.test.e2e.acceptance.client.AbstractNetworkClient;
 
 @Log4j2
+@RequiredArgsConstructor
 public class CucumberHooks implements ConcurrentEventListener {
-    @Autowired
-    private Collection<AbstractNetworkClient> clients;
+    private final Collection<AbstractNetworkClient> clients;
 
-    private void afterAll(TestRunFinished event) {
-        clients.forEach(AbstractNetworkClient::close);
+    public CucumberHooks() {
+        clients = null;
     }
 
     @Override
     public void setEventPublisher(EventPublisher eventPublisher) {
         eventPublisher.registerHandlerFor(TestRunFinished.class, this::afterAll);
+    }
+
+    private void afterAll(TestRunFinished event) {
+        clients.forEach(AbstractNetworkClient::close);
     }
 }
