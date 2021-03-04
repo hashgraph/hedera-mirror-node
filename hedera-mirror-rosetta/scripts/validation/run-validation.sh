8a97dbb75a74650eef71499f66a81297db1cd215
Steven Sheehy <steven.sheehy@hedera.com>
2021-03-03 21:26:20 -0600

    Fix CI
    Signed-off-by: Steven Sheehy <steven.sheehy@hedera.com>

--- hedera-mirror-rosetta/scripts/validation/run-validation.sh
+++ hedera-mirror-rosetta/scripts/validation/run-validation.sh
@@ -3,41 +3,25 @@ set -euo pipefail
 
 parent_path="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"
 cd "${parent_path}"
-network="${1}"
+network="${1:-demo}"
 
-if (./rosetta-cli | grep -q 'CLI for the Rosetta API'); then
+if [[ "${network}" != "demo" && "${network}" != "testnet" ]]; then
+    echo "Unsupported network ${network}"
+    exit 1
+fi
+
+if (./rosetta-cli 2>&1 | grep -q 'CLI for the Rosetta API'); then
     echo "Rosetta CLI already installed. Skipping installation"
 else
     echo "Installing Rosetta CLI"
     curl -sSfL https://raw.githubusercontent.com/coinbase/rosetta-cli/master/scripts/install.sh | sh -s -- -b .
 fi
 
-function run_from_genesis() {
-    echo "Running Rosetta Data API Validation #1"
-    if (! ./rosetta-cli check:data --configuration-file="./${network}/validate-from-genesis.json"); then
-        echo "Failed to Pass API Validation #1"
-        exit 1
-    fi
-}
+echo "Running Rosetta Data API Validation"
 
-function run_demo() {
-    echo "Running DEMO Validation"
-    run_from_genesis
-}
-
-function run_testnet() {
-    echo "Running TESTNET Validation"
-    run_from_genesis
-}
-
-case $network in
-    "testnet")
-        run_testnet
-    ;;
-    *)
-        network="demo"
-        run_demo
-    ;;
-esac
+if (! ./rosetta-cli check:data --configuration-file="./${network}/validate-from-genesis.json"); then
+    echo "Failed to Pass API Validation"
+    exit 1
+fi
 
 echo "Rosetta Validation Passed Successfully!"
