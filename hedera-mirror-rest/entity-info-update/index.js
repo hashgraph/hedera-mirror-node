500707cface34f49edd4869ce62bae49aab92360
Nana-EC <56320167+Nana-EC@users.noreply.github.com>
2021-03-17 13:48:56 -0500

    Add support for key encoding
    Signed-off-by: Nana-EC <56320167+Nana-EC@users.noreply.github.com>

--- hedera-mirror-rest/entity-info-update/index.js
+++ hedera-mirror-rest/entity-info-update/index.js
@@ -24,29 +24,37 @@
 // external libraries
 const log4js = require('log4js');
 
-const logger = log4js
-  .configure({
-    appenders: {
-      console: {
-        layout: {
-          pattern: '%d{yyyy-MM-ddThh:mm:ss.SSSO} %p %m',
-          type: 'pattern',
+const getConfiguredLogger = (loglevel) => {
+  return log4js
+    .configure({
+      appenders: {
+        console: {
+          layout: {
+            pattern: '%d{yyyy-MM-ddThh:mm:ss.SSSO} %p %m',
+            type: 'pattern',
+          },
+          type: 'stdout',
         },
-        type: 'stdout',
       },
-    },
-    categories: {
-      default: {
-        appenders: ['console'],
-        level: 'debug',
+      categories: {
+        default: {
+          appenders: ['console'],
+          level: loglevel,
+        },
       },
-    },
-  })
-  .getLogger();
+    })
+    .getLogger();
+};
+
+// configure prior to local loads such as config to ensure we at least get debug logging initially
+let logger = getConfiguredLogger('debug');
 
 // local
+const config = require('./config');
 const entityUpdateHandler = require('./entityUpdateHandler');
 const utils = require('./utils');
+
+logger = getConfiguredLogger(config.log.level);
 
 // get entity objects from CSV
 const entitiesToValidate = utils.readEntityCSVFileSync();
@@ -55,46 +63,14 @@ const getUpdateList = async (entities) => {
   return entityUpdateHandler.getUpdateList(entities);
 };
 
-// console.log(`*** entitiesToUpdate[0]: ${JSON.stringify(entitiesToUpdate[0])}`);
 getUpdateList(entitiesToValidate).then(async (entitiesToUpdate) => {
-  await entityUpdateHandler.updateStaleDBEntities(entitiesToUpdate);
+  if (config.dryRun === false) {
+    logger.info(`Updating stale db entries with updated information ...`);
+    await entityUpdateHandler.updateStaleDBEntities(entitiesToUpdate);
+  } else {
+    logger.info(
+      `Db update of entities will be skipped as 'hedera.mirror.entityUpdate.dryRun' is set to ${config.dryRun}`
+    );
+  }
   logger.info(`End of entity-info-update`);
 });
-
-// const accountInfo = networkEntityService.getAccountInfo(entitiesToUpdate[0].entity).then((accountResponse) => {
-//   // console.log(`*** Index Retrieved account: ${JSON.stringify(accountResponse)}`);
-//
-//   const accountInfoCurrent = networkEntityService.getAccountInfo(accountResponse.accountId, entitiesToUpdate[0].keyDatabase).then((accountInfoCurrentResponse) => {
-//     // console.log(`*** accountInfoCurrent: ${JSON.stringify(accountInfoCurrentResponse)}`);
-//     return accountInfoCurrentResponse;
-//   }).then((networkresp) => {
-//     const entityFromDBb = dbEntityService.getEntity(entitiesToUpdate[0].entity).then((dbResponse) => {
-//       // console.log(`*** Index Retrieved entity from db: ${JSON.stringify(dbResponse)}`);
-//       console.log(`*** CSV entity key: ${entitiesToUpdate[0].keyDatabase}`);
-//       console.log(`*** Network entity key: ${networkresp.key._keys.toString()}`);
-//       // console.log(`*** Network entity Publickey: ${PublicKey.fromString(networkresp.key._keys).toString()}`);
-//       console.log(`*** DB entity key: ${dbResponse.key.toString('hex')}`);
-//       return dbResponse;
-//     });
-//   });
-// });
-//
-// logger.info(`*** End of entity-info-update`);
-
-// utils.readEntityCSVFile().then((entitiesToUpdate) => {
-//   const accountInfo = entityService.getAccountInfo(entitiesToUpdate[0].entity).then((accountResponse) => {
-//     console.log(`Index Retrieved account`);
-//   });
-//
-//   console.log(`*** End of entity-info-update`);
-// });
-
-// determine input - single account (x.y.z), range (x1.y1.z1-x2.y2.z2), CSV (file-path)
-
-// if CSV read in file and get list of entityIds
-
-// for each entity do a select from DB and a get AccountInfo from network.
-
-// compare items. Where it differs create a new list to be inserted. Not cases where you merge results vs just update db
-
-// Have dedicated logic for pulling from db where possible, comparing objects, inserting into db
