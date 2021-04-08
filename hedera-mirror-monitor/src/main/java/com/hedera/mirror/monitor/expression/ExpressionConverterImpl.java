308b34b8f7d09f208c93acfcb444a23847787d93
Xin Li <xin.li@hedera.com>
2021-04-08 13:10:06 -0500

    oops
    Signed-off-by: Xin Li <xin.li@hedera.com>

--- hedera-mirror-monitor/src/main/java/com/hedera/mirror/monitor/expression/ExpressionConverterImpl.java
+++ hedera-mirror-monitor/src/main/java/com/hedera/mirror/monitor/expression/ExpressionConverterImpl.java
@@ -20,6 +20,7 @@ package com.hedera.mirror.monitor.expression;
  * ‍
  */
 
+import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Map;
@@ -102,7 +103,7 @@ public class ExpressionConverterImpl implements ExpressionConverter {
                     .logResponse(true)
                     .receipt(true)
                     .scenarioName(expression.toString())
-//                    .timeout(Duration.ofSeconds(10L))
+                    .timeout(Duration.ofSeconds(10L))
                     .timestamp(Instant.now())
                     .transaction(transactionSupplier.get())
                     .type(type.getTransactionType())
