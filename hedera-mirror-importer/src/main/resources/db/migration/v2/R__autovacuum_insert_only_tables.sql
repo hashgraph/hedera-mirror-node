5dce57e5043b01ca791fc70364c522faf69f6545
Xin Li <xin.li@hedera.com>
2021-04-16 10:44:23 -0500

    rename placeholders and sort properly
    Signed-off-by: Xin Li <xin.li@hedera.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v2/R__autovacuum_insert_only_tables.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/v2/R__autovacuum_insert_only_tables.sql
@@ -4,18 +4,18 @@
 
 alter table if exists crypto_transfer set (
     autovacuum_vacuum_insert_scale_factor = 0,
-    autovacuum_vacuum_insert_threshold = ${cryptoTransferAutovacuumInsertThreshold},
+    autovacuum_vacuum_insert_threshold = ${autovacuumVacuumInsertThresholdCryptoTransfer},
     log_autovacuum_min_duration = 0
     );
 
 alter table if exists token_transfer set (
     autovacuum_vacuum_insert_scale_factor = 0,
-    autovacuum_vacuum_insert_threshold = ${tokenTransferAutovacuumInsertThreshold},
+    autovacuum_vacuum_insert_threshold = ${autovacuumVacuumInsertThresholdTokenTransfer},
     log_autovacuum_min_duration = 0
     );
 
 alter table if exists transaction set (
     autovacuum_vacuum_insert_scale_factor = 0,
-    autovacuum_vacuum_insert_threshold = ${transactionAutovacuumInsertThreshold},
+    autovacuum_vacuum_insert_threshold = ${autovacuumVacuumInsertThresholdTransaction},
     log_autovacuum_min_duration = 0
     );
