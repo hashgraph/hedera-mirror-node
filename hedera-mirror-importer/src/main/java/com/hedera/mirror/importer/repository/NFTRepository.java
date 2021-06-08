ab8272cb58d5e072f9fa6753ea5ea7b33ec87959
Ian Jungmann <ian.jungmann@hedera.com>
2021-06-07 19:31:42 -0500

    Repositories + tests
    Signed-off-by: Ian Jungmann <ian.jungmann@hedera.com>

---
+++ hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/repository/NFTRepository.java
@@ -0,0 +1,32 @@
+package com.hedera.mirror.importer.repository;
+
+/*-
+ * ‌
+ * Hedera Mirror Node
+ * ​
+ * Copyright (C) 2019 - 2020 Hedera Hashgraph, LLC
+ * ​
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ‍
+ */
+
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+
+import com.hedera.mirror.importer.domain.EntityId;
+import com.hedera.mirror.importer.domain.NFT;
+
+public interface NFTRepository extends CrudRepository<NFT, Long> {
+
+    Optional<NFT> findByTokenIdAndSerialNumber(EntityId tokenId, long serialNumber);
+}
