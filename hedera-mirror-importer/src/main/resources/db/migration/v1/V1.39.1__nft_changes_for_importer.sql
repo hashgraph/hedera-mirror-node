54a67ccaf1849e723938d5fda48bdbf87ad2b82b
Ian Jungmann <ian.jungmann@hedera.com>
2021-06-15 15:08:28 -0500

    Fix migration v1 and lower case
    Signed-off-by: Ian Jungmann <ian.jungmann@hedera.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v1/V1.39.1__nft_changes_for_importer.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/v1/V1.39.1__nft_changes_for_importer.sql
@@ -4,15 +4,16 @@
 
 -- Change nft primary key
 alter table if exists nft
-    drop constraint nft_pkey;
-alter table if exists nft
-    drop constraint nft__token_id_serial_num;
+    drop constraint if exists nft_pkey;
+
+drop index if exists nft__token_id_serial_num;
+
 alter table nft
     add primary key (token_id, serial_number);
 
 -- Remove not null constraints on nft_tranfer account ids
-ALTER TABLE nft_transfer ALTER COLUMN receiver_account_id DROP NOT NULL;
-ALTER TABLE nft_transfer ALTER COLUMN sender_account_id DROP NOT NULL;
+alter table nft_transfer alter column receiver_account_id drop not null;
+alter table nft_transfer alter column sender_account_id drop not null;
 
 
 -- Insert new response codes
