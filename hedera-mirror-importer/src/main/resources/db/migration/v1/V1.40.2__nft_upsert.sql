ffabf81665793a4aca2ca9dbd8430b2ed992ae66
Nana-EC <nana.essilfie-conduah@hedera.com>
2021-07-06 10:58:19 -0500

    comment update on migration
    Signed-off-by: Nana-EC <nana.essilfie-conduah@hedera.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v1/V1.40.2__nft_upsert.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/v1/V1.40.2__nft_upsert.sql
@@ -2,7 +2,7 @@
 -- Update NFT tables with upsert support
 -------------------
 
--- Remove not null constraints on nft created timestamp to allow for partial data inserts
+-- Remove not null constraints on nft created timestamp to allow for partial data selection during updates
 alter table nft
     alter column deleted drop not null,
     alter column metadata drop not null;
