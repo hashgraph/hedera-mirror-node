e0160d28371e82dfc9ffb5527baa619b29f89ae8
Nana-EC <nana.essilfie-conduah@hedera.com>
2021-08-27 19:48:52 -0500

    revert code smell
    Signed-off-by: Nana-EC <nana.essilfie-conduah@hedera.com>

--- hedera-mirror-importer/src/test/java/com/hedera/mirror/importer/parser/record/RecordFileParserIntegrationTest.java
+++ hedera-mirror-importer/src/test/java/com/hedera/mirror/importer/parser/record/RecordFileParserIntegrationTest.java
@@ -117,9 +117,7 @@ class RecordFileParserIntegrationTest extends IntegrationTest {
         verifyFinalDatabaseState(recordFileDescriptor1);
 
         // when
-        Assertions.assertThrows(
-                ParserException.class,
-                () -> recordFileParser.parse(recordFileDescriptor2.getRecordFile()));
+        Assertions.assertThrows(ParserException.class, () -> recordFileParser.parse(recordFileDescriptor2.getRecordFile()));
 
         // then
         verifyFinalDatabaseState(recordFileDescriptor1);
