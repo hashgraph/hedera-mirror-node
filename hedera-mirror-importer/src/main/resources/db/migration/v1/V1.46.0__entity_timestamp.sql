d0252841c8bd29f1a2f44d15b8542fac46aa5a78
Xin Li <xin.li@hedera.com>
2021-10-08 17:26:41 -0500

    add integration tests
    Signed-off-by: Xin Li <xin.li@hedera.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v1/V1.46.0__entity_timestamp.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/v1/V1.46.0__entity_timestamp.sql
@@ -7,11 +7,11 @@ with entity_timestamp as (
     from transaction
     where result = 22
       and entity_id is not null
-      and type in (8, 9, 11, 12, 15, 17, 18, 19, 22, 24, 25, 26, 29, 35, 36, 42)
+      and type in (8, 9, 11, 12, 15, 17, 18, 19, 22, 24, 25, 26, 29, 35, 36, 42, 44)
     group by entity_id
 )
 update entity
-set created_timestamp = entity_timestamp.created_timestamp,
+set created_timestamp = coalesce(entity.created_timestamp, entity_timestamp.created_timestamp),
     modified_timestamp = entity_timestamp.modified_timestamp
 from entity_timestamp
 where entity.id = entity_timestamp.entity_id;
