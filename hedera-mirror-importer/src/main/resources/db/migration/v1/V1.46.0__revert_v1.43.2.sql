338c398e58ae80581d571c6c6b5903956adcf88e
Xin Li <xin.li@hedera.com>
2021-10-11 13:14:02 -0500

    fix access issue when db owner and importer user are different
    Signed-off-by: Xin Li <xin.li@hedera.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v1/V1.46.0__revert_v1.43.2.sql
+++
@@ -1,6 +0,0 @@
--------------------
--- Revert V1.43.2 which limits changes to token_account table
--------------------
-
-drop function if exists add_missing_token_account_association() cascade;
-drop table if exists last_transaction;
