c5ef628d648d8234ffa1a714686946059156c94c
Xin Li <xin.li@hedera.com>
2021-11-01 18:22:17 -0500

    add all common columns in entity and contract
    Signed-off-by: Xin Li <xin.li@hedera.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v1/V1.46.12__account_contract_view_and_index.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/v1/V1.46.12__account_contract_view_and_index.sql
@@ -1,29 +1,41 @@
 -- create a view for accounts and contracts
 create or replace view account_contract as
   select
-    id,
-    expiration_timestamp,
     auto_renew_period,
-    key,
+    created_timestamp,
     deleted,
-    type,
-    public_key,
+    expiration_timestamp,
+    id,
+    key,
     max_automatic_token_associations,
     memo,
+    num,
+    public_key,
+    proxy_account_id,
+    realm,
+    shard,
+    timestamp_range,
+    type,
     receiver_sig_required
   from entity
   where type = 1
   union all
   select
-    id,
-    expiration_timestamp,
     auto_renew_period,
-    key,
+    created_timestamp,
     deleted,
-    type,
-    public_key,
+    expiration_timestamp,
+    id,
+    key,
     null as max_automatic_token_associations,
     memo,
+    num,
+    public_key,
+    proxy_account_id,
+    realm,
+    shard,
+    timestamp_range,
+    type,
     null as receiver_sig_required
   from contract;
 
