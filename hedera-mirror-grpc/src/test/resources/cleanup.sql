371b8f998619ceaf5bdc36685adff71dc6f25639
Ian Jungmann <ian.jungmann@hedera.com>
2021-11-10 14:18:25 -0600

    Fix cleanup script
    Signed-off-by: Ian Jungmann <ian.jungmann@hedera.com>

--- hedera-mirror-grpc/src/test/resources/cleanup.sql
+++ hedera-mirror-grpc/src/test/resources/cleanup.sql
@@ -3,11 +3,11 @@ do
 declare
     t record;
 begin
-    for t in select table_name
-             from information_schema.tables
-             where table_schema = ''public'' and table_name !~ ''.*(flyway|transaction_type|transaction_result|citus_).*'' and table_type <> ''VIEW''
-        loop
-            execute format(''truncate %s restart identity cascade'', t.table_name);
-        end loop;
+for t in select table_name
+from information_schema.tables
+where table_schema = ''public'' and table_name !~ ''.*(flyway|transaction_type|transaction_result|citus_|_\d+).*'' and table_type <> ''VIEW''
+    loop
+    execute format(''truncate %s restart identity cascade'', t.table_name);
+    end loop;
 end;
 ';
