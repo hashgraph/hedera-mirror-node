9d315eee72cf4aad8772b4b76292ceb408e759d6
Ian Jungmann <ian.jungmann@hedera.com>
2021-11-10 15:03:55 -0600

    Tabs
    Signed-off-by: Ian Jungmann <ian.jungmann@hedera.com>

--- hedera-mirror-importer/src/main/resources/db/scripts/cleanup.sql
+++ hedera-mirror-importer/src/main/resources/db/scripts/cleanup.sql
@@ -1,13 +1,13 @@
 do
 '
-declare
-    t record;
-begin
-    for t in select table_name
-             from information_schema.tables
-             where table_schema = ''public'' and table_name !~ ''.*(flyway|transaction_type|transaction_result|citus_|_\d+).*'' and table_type <> ''VIEW''
-        loop
-            execute format(''truncate %s restart identity cascade'', t.table_name);
-        end loop;
-end;
+    declare
+        t record;
+    begin
+        for t in select table_name
+                 from information_schema.tables
+                 where table_schema = ''public'' and table_name !~ ''.*(flyway|transaction_type|transaction_result|citus_|_\d+).*'' and table_type <> ''VIEW''
+            loop
+                execute format(''truncate %s restart identity cascade'', t.table_name);
+            end loop;
+    end;
 ';
