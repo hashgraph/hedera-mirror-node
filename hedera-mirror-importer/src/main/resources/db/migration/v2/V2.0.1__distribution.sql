3294ed2ec36000c4ae3aad970a8c7a11d175c994
Ian Jungmann <ian.jungmann@hedera.com>
2021-11-12 12:53:16 -0600

    Move everything under entity
    Signed-off-by: Ian Jungmann <ian.jungmann@hedera.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v2/V2.0.1__distribution.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/v2/V2.0.1__distribution.sql
@@ -4,43 +4,45 @@
 -------------------
 
 
--- Entity tables
-SELECT create_distributed_table('entity', 'id');
-SELECT create_distributed_table('contract', 'id');
-SELECT create_distributed_table('schedule', 'schedule_id', colocate_with => 'entity');
-SELECT create_distributed_table('token', 'token_id', colocate_with => 'entity');
+-- Entity tables distributed on id
+select create_distributed_table('entity', 'id');
 
+select create_distributed_table('contract', 'id');
 
-SELECT create_distributed_table('entity_history', 'id', colocate_with => 'entity');
-SELECT create_distributed_table('contract_history', 'id', colocate_with => 'contract');
+-- Entity sub-tables distibuted on entity_ids and colocated with corresponding entity
 
--- Entity sub-tables
+select create_distributed_table('account_balance', 'account_id', colocate_with => 'entity');
 
+select create_distributed_table('account_balance_file', 'node_account_id', colocate_with => 'entity');
 
-SELECT create_distributed_table('account_balance', 'account_id', colocate_with => 'entity');
+select create_distributed_table('contract_history', 'id', colocate_with => 'contract');
 
-SELECT create_distributed_table('account_balance_file', 'node_account_id', colocate_with => 'entity');
+select create_distributed_table('contract_log', 'payer_account_id', colocate_with => 'entity');
 
-SELECT create_distributed_table('contract_log', 'payer_account_id', colocate_with => 'entity');
+select create_distributed_table('contract_result', 'payer_account_id', colocate_with => 'entity');
 
-SELECT create_distributed_table('contract_result', 'payer_account_id', colocate_with => 'entity');
+select create_distributed_table('custom_fee', 'token_id', colocate_with => 'entity');
 
-SELECT create_distributed_table('custom_fee', 'token_id', colocate_with => 'token');
+select create_distributed_table('entity_history', 'id', colocate_with => 'entity');
 
-SELECT create_distributed_table('event_file', 'node_account_id', colocate_with => 'entity');
+select create_distributed_table('event_file', 'node_account_id', colocate_with => 'entity');
 
-SELECT create_distributed_table('file_data', 'entity_id', colocate_with => 'contract');
+select create_distributed_table('file_data', 'entity_id', colocate_with => 'entity');
 
-SELECT create_distributed_table('nft', 'token_id', colocate_with => 'token');
+select create_distributed_table('nft', 'token_id', colocate_with => 'entity');
 
-SELECT create_distributed_table('record_file', 'node_account_id', colocate_with => 'entity');
+select create_distributed_table('record_file', 'node_account_id', colocate_with => 'entity');
 
-SELECT create_distributed_table('token_account', 'token_id', colocate_with => 'token');
+select create_distributed_table('schedule', 'schedule_id', colocate_with => 'entity');
 
-SELECT create_distributed_table('token_balance', 'account_id', colocate_with => 'entity');
+select create_distributed_table('token', 'token_id', colocate_with => 'entity');
 
-SELECT create_distributed_table('topic_message', 'topic_id', colocate_with => 'entity');
+select create_distributed_table('token_account', 'token_id', colocate_with => 'entity');
 
-SELECT create_distributed_table('transaction', 'payer_account_id', colocate_with => 'entity');
+select create_distributed_table('token_balance', 'account_id', colocate_with => 'entity');
 
-SELECT create_distributed_table('transaction_signature', 'entity_id', colocate_with => 'transaction');
+select create_distributed_table('topic_message', 'topic_id', colocate_with => 'entity');
+
+select create_distributed_table('transaction', 'payer_account_id', colocate_with => 'entity');
+
+select create_distributed_table('transaction_signature', 'entity_id', colocate_with => 'entity');
