369d482a94badbca82825b9bf9669599cbd926fc
Ian Jungmann <ian.jungmann@hedera.com>
2021-11-12 10:04:45 -0600

    First attempt at making distributions
    Signed-off-by: Ian Jungmann <ian.jungmann@hedera.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v2/V2.0.1__distribution.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/v2/V2.0.1__distribution.sql
@@ -1,5 +1,46 @@
 -------------------
 -- Create distributed tables for tables that can be distributed around an entity.
 -- Colocate where possible with matching entity
--- Create reference tables for tables used only to describe with rare insert/updates
 -------------------
+
+
+-- Entity tables
+SELECT create_distributed_table('entity', 'id');
+SELECT create_distributed_table('contract', 'id');
+SELECT create_distributed_table('schedule', 'schedule_id', colocate_with => 'entity');
+SELECT create_distributed_table('token', 'token_id', colocate_with => 'entity');
+
+
+SELECT create_distributed_table('entity_history', 'id', colocate_with => 'entity');
+SELECT create_distributed_table('contract_history', 'id', colocate_with => 'contract');
+
+-- Entity sub-tables
+
+
+SELECT create_distributed_table('account_balance', 'account_id', colocate_with => 'entity');
+
+SELECT create_distributed_table('account_balance_file', 'node_account_id', colocate_with => 'entity');
+
+SELECT create_distributed_table('contract_log', 'payer_account_id', colocate_with => 'entity');
+
+SELECT create_distributed_table('contract_result', 'payer_account_id', colocate_with => 'entity');
+
+SELECT create_distributed_table('custom_fee', 'token_id', colocate_with => 'token');
+
+SELECT create_distributed_table('event_file', 'node_account_id', colocate_with => 'entity');
+
+SELECT create_distributed_table('file_data', 'entity_id', colocate_with => 'contract');
+
+SELECT create_distributed_table('nft', 'token_id', colocate_with => 'token');
+
+SELECT create_distributed_table('record_file', 'node_account_id', colocate_with => 'entity');
+
+SELECT create_distributed_table('token_account', 'token_id', colocate_with => 'token');
+
+SELECT create_distributed_table('token_balance', 'account_id', colocate_with => 'entity');
+
+SELECT create_distributed_table('topic_message', 'topic_id', colocate_with => 'entity');
+
+SELECT create_distributed_table('transaction', 'payer_account_id', colocate_with => 'entity');
+
+SELECT create_distributed_table('transaction_signature', 'entity_id', colocate_with => 'transaction');
