126449c9abb2e4ecdcd0bd740bda07f7cc08f699
Ian Jungmann <ian.jungmann@hedera.com>
2021-11-15 16:16:57 -0600

    Contract results and logs now with entity
    Signed-off-by: Ian Jungmann <ian.jungmann@hedera.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v2/V2.0.1__distribution.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/v2/V2.0.1__distribution.sql
@@ -18,9 +18,9 @@ select create_reference_table('account_balance_file');
 
 select create_distributed_table('contract_history', 'id', colocate_with => 'contract');
 
-select create_distributed_table('contract_log', 'payer_account_id', colocate_with => 'contract');
+select create_distributed_table('contract_log', 'payer_account_id', colocate_with => 'entity');
 
-select create_distributed_table('contract_result', 'payer_account_id', colocate_with => 'contract');
+select create_distributed_table('contract_result', 'payer_account_id', colocate_with => 'entity');
 
 select create_distributed_table('custom_fee', 'token_id', colocate_with => 'entity');
 
