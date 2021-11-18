f9fd4949867f3387ba2e715c6538444b8bd7633c
Ian Jungmann <ian.jungmann@hedera.com>
2021-11-18 13:33:22 -0600

    Put types back
    Signed-off-by: Ian Jungmann <ian.jungmann@hedera.com>

--- hedera-mirror-importer/src/main/resources/db/scripts/cleanup.sql
+++ hedera-mirror-importer/src/main/resources/db/scripts/cleanup.sql
@@ -5,7 +5,7 @@ do
     begin
         for t in select table_name
                  from information_schema.tables
-                 where table_schema = ''public'' and table_name !~ ''.*(flyway|citus_|_\d+).*'' and table_type <> ''VIEW''
+                 where table_schema = ''public'' and table_name !~ ''.*(flyway|transaction_type|citus_|_\d+).*'' and table_type <> ''VIEW''
             loop
                 execute format(''delete from %s'', t.table_name);
             end loop;
