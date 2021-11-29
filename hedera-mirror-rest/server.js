84fb49244abcbc61b8d88927d881ee64e3cfdd40
Ian Jungmann <ian.jungmann@hedera.com>
2021-11-29 17:47:41 -0600

    OpenApi test validation (#2894)
    * Add OpenApi validation to tests
    * Correct OpenApi based on test validation
    * Correct tests
    * Have false balance flag in AccountInfo return null balance instead of undefined
    
    Signed-off-by: Ian Jungmann <ian.jungmann@hedera.com>

--- hedera-mirror-rest/server.js
+++ hedera-mirror-rest/server.js
@@ -46,7 +46,7 @@ const transactions = require('./transactions');
 const {getPoolClass, isTestEnv, loadPgRange} = require('./utils');
 const {handleError} = require('./middleware/httpErrorHandler');
 const {metricsHandler, recordIpAndEndpoint} = require('./middleware/metricsHandler');
-const {serveSwaggerDocs} = require('./middleware/openapiHandler');
+const {serveSwaggerDocs, openApiValidator} = require('./middleware/openapiHandler');
 const {responseHandler} = require('./middleware/responseHandler');
 const {requestLogger, requestQueryParser} = require('./middleware/requestHandler');
 const fs = require('fs');
@@ -118,6 +118,9 @@ app.set('port', port);
 app.set('query parser', requestQueryParser);
 
 serveSwaggerDocs(app);
+if (isTestEnv()) {
+  openApiValidator(app);
+}
 
 // middleware functions, Prior to v0.5 define after sets
 app.use(
