b9751cbd9306b6b2ad61e3046fe3954b336803e8
Ian Jungmann <ian.jungmann@hedera.com>
2021-11-30 17:08:59 -0600

    Starting stuff
    Signed-off-by: Ian Jungmann <ian.jungmann@hedera.com>

---
+++ hedera-mirror-rest/service/baseService.js
@@ -0,0 +1,46 @@
+/*-
+ * ‌
+ * Hedera Mirror Node
+ * ​
+ * Copyright (C) 2019 - 2021 Hedera Hashgraph, LLC
+ * ​
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ‍
+ */
+
+'use strict';
+
+const _ = require('lodash');
+
+/**
+ * Base service class that other services should inherit from for their retrieval business logic
+ */
+class BaseService {
+  getOrderByQuery(column, order) {
+    return `order by ${column} ${order}`;
+  }
+
+  getLimitQuery(limitParamCount) {
+    return `limit $${limitParamCount}`;
+  }
+
+  async getRows(query, params, functionName = '') {
+    logger.trace(`${functionName} query: ${query}, params: ${params}`);
+    return pool.queryQuietly(query, params).then((result) => {
+      logger.trace(`${functionName} ${result.rows.length} entries`);
+      return result.rows;
+    });
+  }
+}
+
+module.exports = BaseService;
---
+++ hedera-mirror-rest/service/contractService.js
@@ -0,0 +1,97 @@
+/*-
+ * ‌
+ * Hedera Mirror Node
+ * ​
+ * Copyright (C) 2019 - 2021 Hedera Hashgraph, LLC
+ * ​
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ‍
+ */
+
+'use strict';
+
+const _ = require('lodash');
+
+const BaseService = require('./baseService');
+const {ContractResult, ContractLog} = require('../model');
+const {
+  response: {
+    limit: {default: defaultLimit},
+  },
+} = require('../config');
+const {orderFilterValues} = require('../constants');
+
+/**
+ * Contract retrieval business logic
+ */
+class ContractService extends BaseService {
+  constructor() {
+    super();
+  }
+
+  static contractResultsByIdQuery = `select *
+    from ${ContractResult.tableName} ${ContractResult.tableAlias}`;
+
+  static contractLogsByIdQuery = `select *
+    from ${ContractLog.tableName} ${ContractLog.tableAlias}`;
+
+  async getContractResultsByIdAndFilters(
+    whereConditions = [],
+    whereParams = [],
+    order = orderFilterValues.DESC,
+    limit = defaultLimit
+  ) {
+    const [query, params] = this.getContractResultsByIdAndFiltersQuery(whereConditions, whereParams, order, limit);
+    const rows = await super.getRows(query, params, 'getContractResultsByIdAndFilters');
+    return _.isEmpty(rows) ? null : rows.map((cr) => new ContractResult(cr));
+  }
+
+  async getContractLogsByIdAndFilters(
+    whereConditions = [],
+    whereParams = [],
+    order = orderFilterValues.DESC,
+    limit = defaultLimit
+  ) {
+    const [query, params] = this.getContractResultsByIdAndFiltersQuery(whereConditions, whereParams, order, limit);
+    const rows = await super.getRows(query, params, 'getContractResultsByIdAndFilters');
+    return _.isEmpty(rows) ? null : rows.map((cr) => new ContractResult(cr));
+  }
+
+  getContractResultsByIdAndFiltersQuery(whereConditions, whereParams, order, limit) {
+    const params = whereParams;
+    const query = [
+      ContractService.contractResultsByIdQuery,
+      whereConditions.length > 0 ? `where ${whereConditions.join(' and ')}` : '',
+      super.getOrderByQuery(ContractResult.getFullName(ContractResult.CONSENSUS_TIMESTAMP), order),
+      super.getLimitQuery(whereParams.length + 1),
+    ].join('\n');
+    params.push(limit);
+
+    return [query, params];
+  }
+
+  getContractLogsByIdAndFiltersQuery(whereConditions, whereParams, order, limit) {
+    const params = whereParams;
+    const query = [
+      ContractService.contractLogsByIdQuery,
+      whereConditions.length > 0 ? `where ${whereConditions.join(' and ')}` : '',
+      super.getOrderByQuery(ContractResult.getFullName(ContractResult.CONSENSUS_TIMESTAMP), order),
+      super.getLimitQuery(whereParams.length + 1),
+    ].join('\n');
+    params.push(limit);
+
+    return [query, params];
+  }
+}
+
+module.exports = new ContractService();
