99d7ad1d8d3e758bd5dab961a8b7f6b28a5d0915
Ian Jungmann <ian.jungmann@hedera.com>
2021-12-06 12:19:09 -0600

    Remove backfill
    Signed-off-by: Ian Jungmann <ian.jungmann@hedera.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v1/V1.50.3__contract_logs_parent_id.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/v1/V1.50.3__contract_logs_parent_id.sql
@@ -2,14 +2,6 @@
 alter table contract_log
     add column parent_contract_id bigint;
 
--- Backfill parent_contract_id based on index
-update contract_log child
-set parent_contract_id = parent.contract_id
-from contract_log parent
-where parent.consensus_timestamp = child.consensus_timestamp
-  and parent.index = 0
-  and child.index <> 0;
-
 -- Create index on contract_id and timestamp for contract logs REST API
 create index if not exists contract_log__contract_id_timestamp
     on contract_log (contract_id, consensus_timestamp desc, index);
