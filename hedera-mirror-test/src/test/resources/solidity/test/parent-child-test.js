e7a64505832e8dad6ef221ac3830682e2470cb8a
Nana-EC <nana.essilfie-conduah@hedera.com>
2021-12-13 20:15:37 -0600

    Simplify parent child contract
    Signed-off-by: Nana-EC <nana.essilfie-conduah@hedera.com>

--- hedera-mirror-test/src/test/resources/solidity/test/parent-child-test.js
+++ hedera-mirror-test/src/test/resources/solidity/test/parent-child-test.js
@@ -2,7 +2,8 @@ const chai = require("chai");
 chai.use(require('chai-as-promised'))
 
 const expect = chai.expect;
-const { ethers } = require("hardhat");
+const { ethers, waffle } = require("hardhat");
+const provider = waffle.provider;
 
 describe("ParentChild", function () {
   it("Should contain zero balances on deployment", async function () {
@@ -11,8 +12,7 @@ describe("ParentChild", function () {
     await parent.deployed();
 
     // verify passed in defaults
-    expect(await parent.getBalance()).to.equal(0);
-    expect(await parent.getChildBalance(0)).to.equal(0);    
+    expect(await provider.getBalance(parent.address)).to.equal(0);
   });
 
   it("Should contain non zero parent balance on funded deployment", async function () {
@@ -21,24 +21,7 @@ describe("ParentChild", function () {
     await parent.deployed();
 
     // verify passed in defaults
-    expect(await parent.getBalance()).to.equal(100000000000000000n);
-    expect(await parent.getChildBalance(0)).to.equal(0);    
-  });
-
-  it("Should reflect increase balance on parent balance post donate", async function () {
-    const Parent = await ethers.getContractFactory("contracts/Parent.sol:Parent");
-    const parent = await Parent.deploy({ value: ethers.utils.parseEther("0.2") });
-    await parent.deployed();
-
-    const [account1] = await ethers.getSigners();
-
-    const submitDonateTx = await parent.connect(account1).donate({ value: ethers.utils.parseEther("0.3") });
-
-    // wait until the transaction is mined
-    await submitDonateTx.wait();
-
-    // verify updated emergent defaults
-    expect(await parent.getBalance()).to.equal(500000000000000000n);
+    expect(await provider.getBalance(parent.address)).to.equal(100000000000000000n);
   });
 
   it("Should contain zero child balance post createChild", async function () {
@@ -51,14 +34,15 @@ describe("ParentChild", function () {
     const submitCreateChildTx = await parent.connect(account1).createChild(0);
 
     // wait until the transaction is mined
-    await submitCreateChildTx.wait();
+    var contractFunctionResult = await submitCreateChildTx.wait();
 
     // verify updated emergent defaults
-    expect(await parent.getChildBalance(1)).to.equal(0);
+    expect(await provider.getBalance(contractFunctionResult.to)).to.equal(400000000000000000n); // parent from transaction
   });
 
   it("Should contain non zero child balance post createChild with transfer amount", async function () {
     const Parent = await ethers.getContractFactory("contracts/Parent.sol:Parent");
+    const Child = await ethers.getContractFactory("contracts/Parent.sol:Child");
     const parent = await Parent.deploy({ value: ethers.utils.parseEther("0.4") });
     await parent.deployed();
 
@@ -67,76 +51,21 @@ describe("ParentChild", function () {
     const submitCreateChildTx = await parent.connect(account1).createChild(5);
 
     // wait until the transaction is mined
-    await submitCreateChildTx.wait();
+    var contractFunctionResult = await submitCreateChildTx.wait();
 
     // verify updated emergent defaults
-    expect(await parent.getChildBalance(1)).to.equal(5);
-  });
+    expect(await provider.getBalance(contractFunctionResult.to)).to.equal(399999999999999995n); // parent from transaction
+    const event0 = contractFunctionResult.events[0];
+    expect(event0.args[0]).to.equal('Created second child contract');
+    expect(event0.event).to.equal('ParentActivityLog');
+    expect(event0.logIndex).to.equal(0);
 
-  it("Should contain non zero child balances post transfer", async function () {
-    const Parent = await ethers.getContractFactory("contracts/Parent.sol:Parent");
-    const parent = await Parent.deploy({ value: ethers.utils.parseEther("0.6") });
-    await parent.deployed();
-
-    const [account1] = await ethers.getSigners();
-
-    const submitCreateChildTx = await parent.connect(account1).createChild(0);
-    await submitCreateChildTx.wait();
-
-    const transferToFirstChildTx = await parent.connect(account1).transferToChild(1, 0);
-    await transferToFirstChildTx.wait();
-
-    const transferToSecondChildTx = await parent.connect(account1).transferToChild(2, 1);
-    await transferToSecondChildTx.wait();
-
-    // verify
-    expect(await parent.getBalance()).to.equal(599999999999999997n);
-
-    // contract to contract call
-    expect(await parent.getChildBalance(0)).to.equal(1);
-    expect(await parent.getChildBalance(1)).to.equal(2);
-
-    // child contract calls
-    var firstAddress = await parent.getChildAddress(0);
-    var secondAddress = await parent.getChildAddress(1);
-    const Child = await ethers.getContractFactory("contracts/Parent.sol:Child");
-    const firstChild = Child.attach(firstAddress);
-    const secondChild = Child.attach(secondAddress);
+    const event1 = contractFunctionResult.events[1];
+    expect(event1.logIndex).to.equal(1);
+    expect(event1.data).to.equal('0x0000000000000000000000000000000000000000000000000000000000000005');
     
-    expect(await firstChild.getAmount()).to.equal(1);
-    expect(await secondChild.getAmount()).to.equal(2);
-  });
-
-  it("Should update child balance upon spending", async function () {
-    const Parent = await ethers.getContractFactory("contracts/Parent.sol:Parent");
-    const parent = await Parent.deploy({ value: ethers.utils.parseEther("0.6") });
-    await parent.deployed();
-
-    const [account1] = await ethers.getSigners();
-
-    const submitCreateChildTx = await parent.connect(account1).createChild(0);
-    await submitCreateChildTx.wait();
-
-    const transferToFirstChildTx = await parent.connect(account1).transferToChild(ethers.utils.parseEther("0.2"), 0);
-    await transferToFirstChildTx.wait();
-
-    const transferToSecondChildTx = await parent.connect(account1).transferToChild(ethers.utils.parseEther("0.3"), 1);
-    await transferToSecondChildTx.wait();
-
-    var firstAddress = await parent.getChildAddress(0);
-    var secondAddress = await parent.getChildAddress(1);
-    const Child = await ethers.getContractFactory("contracts/Parent.sol:Child");
-    const firstChild = Child.attach(firstAddress);
-    const secondChild = Child.attach(secondAddress);
-
-    const firstChildSpendTx = await firstChild.spend(ethers.utils.parseEther("0.1"));
-    await firstChildSpendTx.wait();
-
-    const secondChildSpendTx = await secondChild.spend(ethers.utils.parseEther("0.1"));
-    await secondChildSpendTx.wait();
-
-    // verify updated emergent defaults
-    expect(await parent.getChildBalance(0)).to.equal(ethers.utils.parseEther("0.1"));
-    expect(await parent.getChildBalance(1)).to.equal(ethers.utils.parseEther("0.2"));
+    var childAddress = event1.address;
+    expect(ethers.utils.isAddress(childAddress)).to.equal(true);
+    expect(await provider.getBalance(childAddress)).to.equal(5); // child from transaction
   });
 });
