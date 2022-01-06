fc46f5aede5a0791ee53259300bcacd3329bcc86
Ian Jungmann <ian.jungmann@hedera.com>
2022-01-06 12:53:25 -0600

    Put not null back in v1
    Signed-off-by: Ian Jungmann <ian.jungmann@hedera.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v1/v1.52.0__topic_message_add_payer_account_id.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/v1/v1.52.0__topic_message_add_payer_account_id.sql
@@ -5,3 +5,6 @@ set payer_account_id = t.payer_account_id
 from transaction t
 where tm.consensus_timestamp = t.consensus_timestamp
 and tm.payer_account_id is null;
+
+alter table topic_message
+alter column payer_account_id set not null;
