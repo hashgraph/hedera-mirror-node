15e802590e57339d76b3ecaa635eecabf183ed3c
Xin Li <59580070+xin-hedera@users.noreply.github.com>
2022-01-10 09:40:38 -0600

    Support account alias in rest API (#3109)
    - Add RFC4648 no padding base32 codec
    - Add account alias parser
    - Add accountAlias path param to /v1/api/accounts/:accountAliasOrAccountId
    - Add alias to account object in json response
    - Add new response code UNEXPECTED_TOKEN_DECIMALS
    
    Signed-off-by: Xin Li <xin.li@hedera.com>

---
+++ hedera-mirror-rest/__tests__/base32.test.js
@@ -0,0 +1,65 @@
+/*-
+ * ‌
+ * Hedera Mirror Node
+ * ​
+ * Copyright (C) 2019 - 2021 Hedera Hashgraph, LLC
+ * ​
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ‍
+ */
+
+'use strict';
+
+const base32 = require('../base32');
+
+describe('decode', () => {
+  describe('valid', () => {
+    test('32W35LY', () => {
+      expect(base32.decode('32W353Y')).toEqual(Uint8Array.from(Buffer.from('deadbeef', 'hex')));
+    });
+    test('32W35366', () => {
+      expect(base32.decode('32W35366')).toEqual(Uint8Array.from(Buffer.from('deadbeefde', 'hex')));
+    });
+    test('null', () => {
+      expect(base32.decode(null)).toBeNull();
+    });
+  });
+
+  describe('invalid', () => {
+    const invalidBase32Strs = [
+      // A base32 group without padding can have 2, 4, 5, 7 or 8 characters from its alphabet
+      'A',
+      'AAA',
+      'AAAAAA',
+      // non-base32 characters, note due to the loose option, 0, 1, and 8 will be auto corrected to O, L, and B
+      '9',
+    ];
+    invalidBase32Strs.forEach((invalidBase32Str) => {
+      test(`${invalidBase32Str}`, () => {
+        expect(() => base32.decode(invalidBase32Str)).toThrowErrorMatchingSnapshot();
+      });
+    });
+  });
+});
+
+describe('encode', () => {
+  test('0xdeadbeef', () => {
+    expect(base32.encode(Buffer.from('deadbeef', 'hex'))).toBe('32W353Y');
+  });
+  test('0xdeadbeefde', () => {
+    expect(base32.encode(Buffer.from('deadbeefde', 'hex'))).toBe('32W35366');
+  });
+  test('null', () => {
+    expect(base32.encode(null)).toBeNull();
+  });
+});
