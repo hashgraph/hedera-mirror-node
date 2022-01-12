f115089778f7b88cd762de278effad7a51ed8506
Ian Jungmann <ian.jungmann@hedera.com>
2022-01-12 15:00:39 -0600

    Remove not null constraint
    Signed-off-by: Ian Jungmann <ian.jungmann@hedera.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v1/V1.53.0__topic_message_add_payer_account_id_and_initial_transaction_id.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/v1/V1.53.0__topic_message_add_payer_account_id_and_initial_transaction_id.sql
@@ -1,6 +1,6 @@
 -------------------
--- Set topic_message payer_account_id where null, set payer_account_id to not null, and add a new column for
--- initial_transaction_id to hold the TransactionID protobuf bytes
+-- Set topic_message payer_account_id where null and add a new column for initial_transaction_id to
+-- hold the TransactionID protobuf bytes
 -------------------
 
 -- Set topic_message.payer_account_id for non-chunked messages (chunked messages already handled by importer)
@@ -9,9 +9,6 @@ set payer_account_id = t.payer_account_id
 from transaction t
 where tm.consensus_timestamp = t.consensus_timestamp
 and tm.payer_account_id is null;
-
-alter table topic_message
-alter column payer_account_id set not null;
 
 -- Add a column for the protobuf initial transaction id
 alter table topic_message
