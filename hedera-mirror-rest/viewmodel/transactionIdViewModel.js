f9519fc9e2821b7239ed9e94144e73a7e099e406
Nana-EC <nana.essilfie-conduah@hedera.com>
2022-01-26 19:28:30 -0600

    Commit missing transactionIdViewModel.js
    Signed-off-by: Nana-EC <nana.essilfie-conduah@hedera.com>

---
+++ hedera-mirror-rest/viewmodel/transactionIdViewModel.js
@@ -0,0 +1,57 @@
+/*-
+ * ‌
+ * Hedera Mirror Node
+ * ​
+ * Copyright (C) 2019 - 2022 Hedera Hashgraph, LLC
+ * ​
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ‍
+ */
+
+'use strict';
+
+const _ = require('lodash');
+
+const EntityId = require('../entityId');
+const utils = require('../utils');
+
+/**
+ * TransactionId view model
+ */
+class TransactionIdViewModel {
+  /**
+   * Constructs transactionId view model from proto transaction id or object of applicable keys
+   *
+   * @param {Object|TransactionID} transactionId
+   */
+  constructor(transactionId) {
+    if (_.isNil(transactionId.accountID)) {
+      // handle db format
+      this.account_id = EntityId.parse(transactionId.payerAccountId).toString();
+      this.nonce = _.isNil(transactionId.nonce) ? null : Number(transactionId.nonce);
+      this.scheduled = _.isNil(transactionId.scheduled) ? null : transactionId.scheduled;
+      this.transaction_valid_start = utils.nsToSecNs(transactionId.validStartTimestamp);
+    } else {
+      // handle proto format
+      const {accountID, transactionValidStart, nonce, scheduled} = transactionId;
+      this.account_id = EntityId.of(accountID.shardNum, accountID.realmNum, accountID.accountNum).toString();
+      this.nonce = _.isNil(nonce) ? null : Number(nonce);
+      this.scheduled = scheduled;
+      this.transaction_valid_start = `${transactionValidStart.seconds}.${transactionValidStart.nanos
+        .toString()
+        .padStart(9, '0')}`;
+    }
+  }
+}
+
+module.exports = TransactionIdViewModel;
