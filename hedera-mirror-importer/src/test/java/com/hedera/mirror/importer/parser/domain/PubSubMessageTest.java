931247785b419cd2ec680d3c41d0a6c69209281c
Nana-EC <nana.essilfie-conduah@hedera.com>
2022-01-28 16:14:39 -0600

    Add adjustments to PubSubMessageTest
    Signed-off-by: Nana-EC <nana.essilfie-conduah@hedera.com>

--- hedera-mirror-importer/src/test/java/com/hedera/mirror/importer/parser/domain/PubSubMessageTest.java
+++ hedera-mirror-importer/src/test/java/com/hedera/mirror/importer/parser/domain/PubSubMessageTest.java
@@ -182,7 +182,10 @@ class PubSubMessageTest {
                 "  \"tokenTransferLists\":[]," +
                 "  \"assessedCustomFees\":[]," +
                 "  \"automaticTokenAssociations\":[]," +
-                "  \"alias\":\"\"" +
+                "  \"alias\":\"\"," +
+                "  \"cryptoAdjustments\":[]," +
+                "  \"nftAdjustments\":[]," +
+                "  \"tokenAdjustments\":[]" +
                 "}";
     }
 
