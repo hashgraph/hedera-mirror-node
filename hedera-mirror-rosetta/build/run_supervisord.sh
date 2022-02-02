8d08759c3c207a8548f60cfe1ef309d95ccbfe39
Steven Sheehy <steven.sheehy@hedera.com>
2022-02-02 00:06:58 -0600

    Add auto restore functionality to Rosetta unified image
    Signed-off-by: Steven Sheehy <steven.sheehy@hedera.com>

--- hedera-mirror-rosetta/build/run_supervisord.sh
+++ hedera-mirror-rosetta/build/run_supervisord.sh
@@ -1,20 +1,67 @@
 #!/bin/bash
+set -eo pipefail
 
 function run_offline_mode() {
-  supervisord --configuration supervisord-offline.conf
+  echo "Running in offline mode"
+  supervisord --configuration /app/supervisord-offline.conf
 }
 
 function run_online_mode() {
-  supervisord
+  echo "Running in online mode"
+  supervisord --configuration /app/supervisord.conf
+}
+
+function cleanup() {
+  /etc/init.d/postgresql stop || true
+  cp /app/postgresql.conf "${PGCONF}/conf.d"
+  rm -rf "${TMPDIR}"
+  cd /app
+}
+
+function restore() {
+  if [[ -z "${RESTORE}" ]]; then
+    echo "Skipping database restore"
+    return
+  fi
+
+  DATA_DIR="data_dump"
+  TMPDIR=$(mktemp -d)
+  export PGPASSWORD="${HEDERA_MIRROR_IMPORTER_DB_OWNERPASSWORD:-mirror_node_pass}"
+
+  cp /app/postgresql-restore.conf "${PGCONF}/conf.d/postgresql.conf"
+  /etc/init.d/postgresql start
+
+  if (psql -h localhost -d mirror_node -U mirror_node -c 'select count(*) from flyway_schema_history'); then
+    echo "Skipping restore since database already contains data"
+    cleanup
+    return
+  fi
+
+  echo "Downloading and restoring from database backup: ${RESTORE}"
+  cd "${TMPDIR}"
+  curl --fail -L --retry 3 "${RESTORE}" -o db.tar
+  tar -xvf db.tar
+
+  if [[ ! -d "${DATA_DIR}" ]]; then
+    echo "Database dump does not contain the required '${DATA_DIR}' directory"
+    cleanup
+    exit 1
+  fi
+
+  pg_restore -h localhost -U mirror_node --exit-on-error --format=directory --no-owner --no-acl -j 6 -d mirror_node "${DATA_DIR}"
+
+  cleanup
 }
 
 function main() {
-  if [[ -n "$NETWORK" ]]; then
-    export HEDERA_MIRROR_IMPORTER_NETWORK=$NETWORK
-    export HEDERA_MIRROR_ROSETTA_NETWORK=$NETWORK
+  if [[ -n "${NETWORK}" ]]; then
+    export HEDERA_MIRROR_IMPORTER_NETWORK="${NETWORK}"
+    export HEDERA_MIRROR_ROSETTA_NETWORK="${NETWORK}"
   fi
 
-  case $MODE in
+  restore
+
+  case "${MODE}" in
     "offline")
       run_offline_mode
     ;;
