9d280472f801380fe0dab000b1f8f65d26235d37
Steven Sheehy <steven.sheehy@hedera.com>
2022-02-17 12:58:04 -0600

    Fix PostgreSQL & Redis metrics
    Signed-off-by: Steven Sheehy <steven.sheehy@hedera.com>

--- hedera-mirror-grpc/scripts/db/init.sh
+++ hedera-mirror-grpc/scripts/db/init.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "mirror_node" --dbname "mirror_node" <<-EOSQL
 
     create user mirror_grpc with password 'mirror_grpc_pass';
-
-    grant connect on database "$POSTGRES_DB" to mirror_grpc;
-
+    grant connect on database mirror_node to mirror_grpc;
     alter default privileges in schema public grant select on tables to mirror_grpc;
-
     grant select on all tables in schema public to mirror_grpc;
 
 EOSQL
--- hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/config/MetricsConfiguration.java
+++ hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/config/MetricsConfiguration.java
@@ -26,21 +26,24 @@ import io.lettuce.core.metrics.MicrometerCommandLatencyRecorder;
 import io.lettuce.core.metrics.MicrometerOptions;
 import io.lettuce.core.resource.ClientResources;
 import io.lettuce.core.resource.DefaultClientResources;
+import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.BaseUnits;
 import io.micrometer.core.instrument.binder.MeterBinder;
-import io.micrometer.core.instrument.binder.db.DatabaseTableMetrics;
 import io.micrometer.core.instrument.binder.db.PostgreSQLDatabaseMetrics;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.util.Collection;
 import java.util.LinkedHashSet;
+import java.util.function.ToDoubleFunction;
 import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.jdbc.core.JdbcOperations;
 
 import com.hedera.mirror.importer.db.DBProperties;
 
@@ -50,11 +53,12 @@ import com.hedera.mirror.importer.db.DBProperties;
 class MetricsConfiguration {
 
     private final DataSource dataSource;
-    private final DataSourceProperties dataSourceProperties;
+    private final DBProperties dbProperties;
+    private final MeterRegistry meterRegistry;
 
     // Override default ClientResources to disable histogram metrics
     @Bean(destroyMethod = "shutdown")
-    ClientResources clientResources(MeterRegistry meterRegistry) {
+    ClientResources clientResources() {
         MicrometerOptions options = MicrometerOptions.builder().build();
         var commandLatencyRecorder = new MicrometerCommandLatencyRecorder(meterRegistry, options);
         return DefaultClientResources.builder().commandLatencyRecorder(commandLatencyRecorder).build();
@@ -72,22 +76,28 @@ class MetricsConfiguration {
 
     @Bean
     MeterBinder postgreSQLDatabaseMetrics() {
-        return new PostgreSQLDatabaseMetrics(dataSource, dataSourceProperties.getName());
+        return new PostgreSQLDatabaseMetrics(dataSource, dbProperties.getName());
     }
 
     @Bean
     @ConditionalOnProperty(prefix = "management.metrics.table", name = "enabled", havingValue = "true",
             matchIfMissing = true)
-    MeterBinder tableMetrics(DBProperties dbProperties) {
-        // select count(*) is very slow on large tables, so we use the stats table to provide an estimate
-        final String query = "select n_live_tup from pg_stat_all_tables where schemaname = '%s' and relname = '%s'";
-        String schema = dbProperties.getSchema();
-        String name = dataSourceProperties.getName();
+    MeterBinder tableMetrics(@Lazy JdbcOperations jdbcOperations) {
+        return registry -> getTablesNames().stream().forEach(t -> registerTableMetric(jdbcOperations, t));
+    }
 
-        return registry -> getTablesNames().stream()
-                .map(table -> new DatabaseTableMetrics(dataSource, String.format(query, schema, table),
-                        name, table, null))
-                .forEach(mb -> mb.bindTo(registry));
+    // select count(*) is very slow on large tables, so we use the stats table to provide an estimate
+    private void registerTableMetric(JdbcOperations jdbcOperations, String tableName) {
+        final String query = "select n_live_tup from pg_stat_all_tables where schemaname = ? and relname = ?";
+        ToDoubleFunction<DataSource> totalRows = ds -> jdbcOperations.queryForObject(query, Long.class,
+                dbProperties.getSchema(), tableName);
+
+        Gauge.builder("db.table.size", dataSource, totalRows)
+                .tag("db", dbProperties.getName())
+                .tag("table", tableName)
+                .description("Number of rows in a database table")
+                .baseUnit(BaseUnits.ROWS)
+                .register(meterRegistry);
     }
 
     private Collection<String> getTablesNames() {
