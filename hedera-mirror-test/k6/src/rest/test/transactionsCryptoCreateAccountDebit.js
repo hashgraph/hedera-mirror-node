81afb8a52358b1606ac0628c608fb8a87f846dfa
Cory Huebner <chuebner86@gmail.com>
2022-03-28 08:20:50 -0500

    add the new transaction tests to the main rest test suite
    Signed-off-by: Cory Huebner <chuebner86@gmail.com>

--- hedera-mirror-test/k6/src/rest/test/transactionsCryptoCreateAccountDebit.js
+++ hedera-mirror-test/k6/src/rest/test/transactionsCryptoCreateAccountDebit.js
@@ -22,8 +22,8 @@
  * This is a very particular test case related to https://github.com/hashgraph/hedera-mirror-node/issues/2385.
  * While testing performance issues, it was found that calls using both the transaction type (e.g. CRYPTOCREATEACCOUNT)
  * and balance modification type (e.g. debit) query string parameters, performance was especially slow. API calls would timeout after 20 seconds.
- * This test uses hard-coded transaction type and balance modification type because slow performance seems to be
- * associated with fewer records of a given transaction type/balance modification type combination.
+ * This test uses hard-coded transaction type and balance modification type values because slow performance seems to be
+ * associated with a less frequently used transaction type.
  * An attempt to make this test more generic seems to have low-value while also making variable names confusing.
  */
 
