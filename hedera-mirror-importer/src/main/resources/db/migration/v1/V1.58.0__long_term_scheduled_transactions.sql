bb90e83061c4498760b790dd6b997b2b2fd2db5f
Xin Li <xin.li@hedera.com>
2022-04-19 17:48:04 -0500

    bool -> boolean
    Signed-off-by: Xin Li <xin.li@hedera.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v1/V1.58.0__long_term_scheduled_transactions.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/v1/V1.58.0__long_term_scheduled_transactions.sql
@@ -1,3 +1,3 @@
 alter table if exists schedule
   add column if not exists expiration_time bigint null,
-  add column if not exists wait_for_expiry bool not null default false;
+  add column if not exists wait_for_expiry boolean not null default false;
