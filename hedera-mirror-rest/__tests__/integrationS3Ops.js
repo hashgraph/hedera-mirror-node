a15ff264d8d4a50f4629a262c7ecce6b60e5d4aa
Edwin Greene <edwin.greene@hedera.com>
2022-04-27 10:27:13 -0400

    Update localstack version to latest
    Signed-off-by: Edwin Greene <edwin.greene@hedera.com>

--- hedera-mirror-rest/__tests__/integrationS3Ops.js
+++ hedera-mirror-rest/__tests__/integrationS3Ops.js
@@ -25,7 +25,7 @@ const {GenericContainer} = require('testcontainers');
 const {isDockerInstalled} = require('./integrationUtils');
 
 const localstackImageName = 'localstack/localstack';
-const localstackImageTag = '0.14.2';
+const localstackImageTag = 'latest';
 const defaultS3Port = 4566;
 
 class S3Ops {
