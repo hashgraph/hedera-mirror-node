0fbb1821efab3ba3772069764e395a4e36d4a788
Nana-EC <nana.essilfie-conduah@hedera.com>
2022-04-27 13:58:51 -0500

    Added file reader test coverage
    Signed-off-by: Nana-EC <nana.essilfie-conduah@hedera.com>

--- hedera-mirror-importer/src/test/java/com/hedera/mirror/importer/reader/record/RecordFileReaderTest.java
+++ hedera-mirror-importer/src/test/java/com/hedera/mirror/importer/reader/record/RecordFileReaderTest.java
@@ -9,9 +9,9 @@ package com.hedera.mirror.importer.reader.record;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -38,11 +38,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.core.io.ClassPathResource;
 
-import com.hedera.mirror.importer.TestRecordFiles;
 import com.hedera.mirror.common.domain.transaction.RecordFile;
+import com.hedera.mirror.common.domain.transaction.RecordItem;
+import com.hedera.mirror.importer.TestRecordFiles;
 import com.hedera.mirror.importer.domain.StreamFileData;
 import com.hedera.mirror.importer.exception.InvalidStreamFileException;
-import com.hedera.mirror.common.domain.transaction.RecordItem;
 
 @ExtendWith(MockitoExtension.class)
 abstract class RecordFileReaderTest {
@@ -84,6 +84,14 @@ abstract class RecordFileReaderTest {
                             .block();
                     assertThat(timestamps).first().isEqualTo(recordFile.getConsensusStart());
                     assertThat(timestamps).last().isEqualTo(recordFile.getConsensusEnd());
+                    assertThat(timestamps).doesNotHaveDuplicates().isSorted();
+
+                    List<Integer> transactionBlockIndexs = actual.getItems()
+                            .map(RecordItem::getTransactionBlockIndex)
+                            .collectList()
+                            .block();
+                    assertThat(transactionBlockIndexs).first().isEqualTo(0);
+                    assertThat(transactionBlockIndexs).last().isEqualTo(recordFile.getCount().intValue() - 1);
                     assertThat(timestamps).doesNotHaveDuplicates().isSorted();
                 });
     }
