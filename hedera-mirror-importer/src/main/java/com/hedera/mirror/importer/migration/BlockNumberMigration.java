ebc8629309bf6a8ca3bb5b7c567309e66aabf1fb
Matheus DallRosa <matheus.dallrosa@swirldslabs.com>
2022-05-02 17:25:09 -0300

    update consensus end and index for testnet
    Signed-off-by: Matheus DallRosa <matheus.dallrosa@swirldslabs.com>

--- hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/migration/BlockNumberMigration.java
+++ hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/migration/BlockNumberMigration.java
@@ -50,7 +50,7 @@ public class BlockNumberMigration extends MirrorBaseJavaMigration {
 
     private static final Map<HederaNetwork, Pair<Long, Long>> CONSENSUS_END_BLOCK_NUMBER_PER_NET =
             Map.of(
-                    TESTNET, Pair.of(1651500123344619566L, 20484961L),
+                    TESTNET, Pair.of(1651523056529307198L, 20494237L),
                     MAINNET, Pair.of( 1651522971766268772L, 31850269L));
 
     private final RecordFileRepository recordFileRepository;
