d5d2a0920e873498003eb042f975beb770f9cb7c
matheus-dallrosa <83226580+matheus-dallrosa@users.noreply.github.com>
2022-05-06 12:09:52 -0300

    03591 add new fields to schedules (#3733)
    Adds wait_for_expiry and expiration_time to /api/v1/schedules and /api/v1/schedules/{id}
    
    Signed-off-by: Matheus DallRosa <matheus.dallrosa@swirldslabs.com>

--- hedera-mirror-rest/__tests__/integrationDomainOps.js
+++ hedera-mirror-rest/__tests__/integrationDomainOps.js
@@ -949,6 +949,7 @@ const addSchedule = async (schedule) => {
     creator_account_id: '0.0.1024',
     payer_account_id: '0.0.1024',
     transaction_body: Buffer.from([1, 1, 2, 2, 3, 3]),
+    wait_for_expiry: false,
     ...schedule,
   };
 
@@ -958,8 +959,10 @@ const addSchedule = async (schedule) => {
                            executed_timestamp,
                            payer_account_id,
                            schedule_id,
-                           transaction_body)
-     VALUES ($1, $2, $3, $4, $5, $6)`,
+                           transaction_body,
+                           expiration_time,
+                           wait_for_expiry)
+     VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
     [
       schedule.consensus_timestamp,
       EntityId.parse(schedule.creator_account_id).getEncodedId().toString(),
@@ -967,6 +970,8 @@ const addSchedule = async (schedule) => {
       EntityId.parse(schedule.payer_account_id).getEncodedId().toString(),
       EntityId.parse(schedule.schedule_id).getEncodedId().toString(),
       schedule.transaction_body,
+      schedule.expiration_time,
+      schedule.wait_for_expiry,
     ]
   );
 };
