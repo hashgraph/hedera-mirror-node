f3f1a1c4d36dbf6466f04527d35e973bbaa3d469
ar-conmit <47981732+ar-conmit@users.noreply.github.com>
2022-05-31 23:11:29 +0300

    Add contract logs pagination (#3853)
    Add pagination to contract logs contracts/{id}/results/logs & contracts/results/logs
    
    Signed-off-by: Anton Rusev <anton.rusev.code@gmail.com>

--- hedera-mirror-rest/controllers/baseController.js
+++ hedera-mirror-rest/controllers/baseController.js
@@ -97,6 +97,100 @@ class BaseController {
       throw new InvalidArgumentError(`Multiple range params not allowed for ${filter.key}`);
     }
   };
+
+  /**
+   * Gets filters for the lower part of the multi-union query.
+   *
+   * @param {Bound} primaryBound
+   * @param {Bound} secondaryBound
+   * @return {{key: string, operator: string, value: *}[]}
+   */
+  getLowerFilters(primaryBound, secondaryBound) {
+    let filters = [];
+    if (!secondaryBound.hasBound()) {
+      // no secondary bound filters or no secondary filters at all, everything goes into the lower part and there
+      // shouldn't be inner or upper part.
+      filters = [primaryBound.equal, primaryBound.lower, primaryBound.upper, secondaryBound.equal];
+    } else if (primaryBound.hasLower() && secondaryBound.hasLower()) {
+      // both have lower. If primary has lower and secondary doesn't have lower, the lower bound of primary
+      // will go into the inner part.
+      filters = [{...primaryBound.lower, operator: utils.opsMap.eq}, secondaryBound.lower];
+    }
+    return filters.filter((f) => !_.isNil(f));
+  }
+
+  /**
+   * Gets filters for the inner part of the multi-union query
+   *
+   * @param {Bound} primaryBound
+   * @param {Bound} secondaryBound
+   * @return {{key: string, operator: string, value: *}[]}
+   */
+  getInnerFilters(primaryBound, secondaryBound) {
+    if (!primaryBound.hasBound() || !secondaryBound.hasBound()) {
+      return [];
+    }
+
+    return [
+      // if secondary has lower bound, the primary filter should be > ?
+      {filter: primaryBound.lower, newOperator: secondaryBound.hasLower() ? utils.opsMap.gt : null},
+      // if secondary has upper bound, the primary filter should be < ?
+      {filter: primaryBound.upper, newOperator: secondaryBound.hasUpper() ? utils.opsMap.lt : null},
+    ]
+      .filter((f) => !_.isNil(f.filter))
+      .map((f) => ({...f.filter, operator: f.newOperator || f.filter.operator}));
+  }
+
+  /**
+   * Gets filters for the upper part of the multi-union query
+   *
+   * @param {Bound} primaryBound
+   * @param {Bound} secondaryBound
+   * @return {{key: string, operator: string, value: *}[]}
+   */
+  getUpperFilters(primaryBound, secondaryBound) {
+    if (!primaryBound.hasUpper() || !secondaryBound.hasUpper()) {
+      return [];
+    }
+    // the upper part should always have primary filter = ?
+    return [{...primaryBound.upper, operator: utils.opsMap.eq}, secondaryBound.upper];
+  }
+
+  /**
+   * Gets the pagination link based on 2 main query params
+   *
+   * @param {Request} req
+   * @param {*[]} rows
+   * @param {{string:Bound}} bounds
+   * @param {{primary:string,secondary:string}} boundKeys
+   * @param {number} limit
+   * @param {string} order
+   * @return {string|null}
+   */
+  getPaginationLink(req, rows, bounds, boundKeys, limit, order) {
+    const primaryBound = bounds[boundKeys.primary];
+    const secondaryBound = bounds[boundKeys.secondary];
+
+    if (rows.length < limit || (primaryBound.hasEqual() && secondaryBound.hasEqual())) {
+      // fetched all matching rows or the query is for a specific combination
+      return null;
+    }
+
+    const lastRow = _.last(rows);
+    const lastValues = {};
+    if (primaryBound.hasBound() || primaryBound.isEmpty()) {
+      // the primary param has bound or no primary param at all
+      // primary param should be exclusive when the secondary operator is eq
+      lastValues[boundKeys.primary] = {value: lastRow[boundKeys.primary], inclusive: !secondaryBound.hasEqual()};
+    }
+
+    if (secondaryBound.hasBound() || secondaryBound.isEmpty()) {
+      // the secondary param has bound or no secondary param at all
+      lastValues[boundKeys.secondary] = {value: lastRow[boundKeys.secondary]};
+    }
+
+    return utils.getPaginationLink(req, false, lastValues, order);
+  }
 }
 
 module.exports = BaseController;
