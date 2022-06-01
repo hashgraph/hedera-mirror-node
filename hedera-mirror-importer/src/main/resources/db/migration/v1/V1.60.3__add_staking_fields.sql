21397f6713d0ff54fbfb9303a218a023dc36b96c
Matheus DallRosa <matheus.dallrosa@swirldslabs.com>
2022-06-01 16:24:18 -0300

    improve migration script
    Signed-off-by: Matheus DallRosa <matheus.dallrosa@swirldslabs.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v1/V1.60.3__add_staking_fields.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/v1/V1.60.3__add_staking_fields.sql
@@ -1,19 +1,23 @@
-alter table contract add column decline_reward boolean not null default false;
-alter table contract add column staked_account_id bigint null;
-alter table contract add column staked_node_id bigint null;
-alter table contract add column stake_period_start bigint null;
+alter table if exists contract
+    add column if not exists decline_reward     boolean not null default false,
+    add column if not exists staked_account_id  bigint  null,
+    add column if not exists staked_node_id     bigint  null,
+    add column if not exists stake_period_start bigint  null;
 
-alter table contract_history add column decline_reward boolean not null default false;
-alter table contract_history add column staked_account_id bigint null;
-alter table contract_history add column staked_node_id bigint null;
-alter table contract_history add column stake_period_start bigint null;
+alter table if exists contract_history
+    add column if not exists decline_reward     boolean not null default false,
+    add column if not exists staked_account_id  bigint  null,
+    add column if not exists staked_node_id     bigint  null,
+    add column if not exists stake_period_start bigint  null;
 
-alter table entity add column decline_reward boolean not null default false;
-alter table entity add column staked_account_id bigint null;
-alter table entity add column staked_node_id bigint null;
-alter table entity add column stake_period_start bigint null;
+alter table if exists entity
+    add column if not exists decline_reward     boolean not null default false,
+    add column if not exists staked_account_id  bigint  null,
+    add column if not exists staked_node_id     bigint  null,
+    add column if not exists stake_period_start bigint  null;
 
-alter table entity_history add column decline_reward boolean not null default false;
-alter table entity_history add column staked_account_id bigint null;
-alter table entity_history add column staked_node_id bigint null;
-alter table entity_history add column stake_period_start bigint null;
+alter table if exists entity_history
+    add column if not exists decline_reward     boolean not null default false,
+    add column if not exists staked_account_id  bigint  null,
+    add column if not exists staked_node_id     bigint  null,
+    add column if not exists stake_period_start bigint  null;
