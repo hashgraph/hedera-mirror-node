37ccaacbcd5dcbf13ce9a9934abd07d2b75113e3
Ivan Kavaldzhiev <ivankavaldzhiev@Ivans-MacBook-Pro-4.local>
2022-06-29 13:27:52 +0300

    fix: constructor definition
    Signed-off-by: Ivan Kavaldzhiev <ivankavaldzhiev@Ivans-MacBook-Pro-4.local>

--- hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/evm/properties/SimulatedBlockMetaSource.java
+++ hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/evm/properties/SimulatedBlockMetaSource.java
@@ -2,6 +2,7 @@ package com.hedera.mirror.web3.evm.properties;
 
 import java.time.Instant;
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.units.bigints.UInt256;
@@ -12,12 +13,13 @@ import org.hyperledger.besu.evm.frame.BlockValues;
  * Mirror-node adapted {@link BlockValues}
  */
 @Value
+@RequiredArgsConstructor
 public class SimulatedBlockMetaSource implements BlockValues {
-
-    Instant consTimestamp;
+    
     Bytes difficultyBytes = UInt256.ZERO;
     Optional<Wei> baseFee = Optional.of(Wei.ZERO);
-    long gasLimit;
-    long number;
+    final long gasLimit;
+    final long number;
+    final Instant consTimestamp;
     long timestamp = consTimestamp.getEpochSecond();
 }
