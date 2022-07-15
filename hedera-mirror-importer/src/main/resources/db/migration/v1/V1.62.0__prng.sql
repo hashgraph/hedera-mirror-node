2a1aad836ad2b66f2c11c20bb81e1a1a8b6f35d3
Edwin Greene <edwin.greene@hedera.com>
2022-07-15 13:32:37 -0400

    Remove editing of prng.sql
    Signed-off-by: Edwin Greene <edwin.greene@hedera.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v1/V1.62.0__prng.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/v1/V1.62.0__prng.sql
@@ -2,7 +2,7 @@ create table if not exists prng
 (
     consensus_timestamp bigint  not null,
     range               integer not null,
-    prng_bytes          bytea   null,
-    prng_number         integer null,
+    prng_bytes  bytea   null,
+    prng_number integer null,
     primary key (consensus_timestamp)
 )
