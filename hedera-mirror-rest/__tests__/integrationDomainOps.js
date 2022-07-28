8bb4aa30e057850774e16a7ef1d5a84decbfa738
Xin Li <xin.li@swirldslabs.com>
2022-07-28 10:07:06 -0500

    add doc, fix minor code smell
    Signed-off-by: Xin Li <xin.li@swirldslabs.com>

--- hedera-mirror-rest/__tests__/integrationDomainOps.js
+++ hedera-mirror-rest/__tests__/integrationDomainOps.js
@@ -433,7 +433,7 @@ const addEntity = async (defaults, entity) => {
 
 const addEthereumTransaction = async (ethereumTransaction) => {
   // any attribute starting with '_' is not a db column
-  ethereumTransaction = _.omitBy(ethereumTransaction, (v, k) => k.startsWith('_'));
+  ethereumTransaction = _.omitBy(ethereumTransaction, (_v, k) => k.startsWith('_'));
   const localDefaults = {
     access_list: null,
     call_data_id: null,
