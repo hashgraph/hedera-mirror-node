5a3a42cc000468b161428dad188e842c21486f0f
ivanparnarev <ivan.parnarev@limechain.tech>
2022-08-04 16:41:15 +0300

    rebase from 'eth-call-poc'

--- hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/repository/ContractRepository.java
+++ hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/repository/ContractRepository.java
@@ -31,7 +31,6 @@ public interface ContractRepository extends CrudRepository<Contract, Long> {
     @Query(value = "select num from contract order by id desc limit 1", nativeQuery=true)
     Optional<Long> findLatestNum();
 
-
     @Query(value = "select encode(runtime_bytecode, 'hex') from contract where id = ?1", nativeQuery=true)
     Optional<String> getRuntimeBytecodeOfTestContract(Long num);
 }
