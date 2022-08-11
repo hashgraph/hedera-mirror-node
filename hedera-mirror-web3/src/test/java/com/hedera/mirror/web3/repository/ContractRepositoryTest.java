b5cd948135dc0a607acb19d4d8ad2b2a19ff24df
IvanKavaldzhiev <ivankavaldzhiev@gmail.com>
2022-08-11 09:50:12 +0300

    Add tests for eth_gasEstimate for transferring hbars in smart contract (#4263)
    * test: fix test organisation and add tests for eth_gasEstimate transfering hbars in smart contract
    
    Signed-off-by: Ivan <ivankavaldzhiev@gmail.com>
    
    * refactor: extract common fields in a utility class, resolve PR comments
    
    Signed-off-by: Ivan <ivankavaldzhiev@gmail.com>
    
    Signed-off-by: Ivan <ivankavaldzhiev@gmail.com>

--- hedera-mirror-web3/src/test/java/com/hedera/mirror/web3/repository/ContractRepositoryTest.java
+++ hedera-mirror-web3/src/test/java/com/hedera/mirror/web3/repository/ContractRepositoryTest.java
@@ -1,19 +1,17 @@
 package com.hedera.mirror.web3.repository;
 
-import com.hedera.mirror.web3.Web3IntegrationTest;
-
-import org.junit.Test;
-
 import javax.annotation.Resource;
 
-public class ContractRepositoryTest extends Web3IntegrationTest {
+import com.hedera.mirror.web3.ApiContractIntegrationTest;
+
+public class ContractRepositoryTest extends ApiContractIntegrationTest {
 
     private long timestamp = 0;
 
     @Resource
     private ContractRepository contractRepository;
 
-    @Test
+//    @Test
     void getContract() {
 
     }
