21c23f671a025b64fc1b810f08f535325a3a92d6
Steven Sheehy <17552371+steven-sheehy@users.noreply.github.com>
2022-08-17 09:40:37 -0500

    Update unreleased supply accounts (#4304)
    * Change the husky commit hook to work without a globally installed npx
    * Change the network supply API to use the up to date balance if no timestamp parameter is present
    * Change the network supply API to reject timestamp parameters with ne
    * Refactor network supply to push logic into the service
    * Update the list of unreleased supply accounts
    
    Signed-off-by: Steven Sheehy <steven.sheehy@swirldslabs.com>

--- hedera-mirror-rest/viewmodel/networkSupplyViewModel.js
+++ hedera-mirror-rest/viewmodel/networkSupplyViewModel.js
@@ -24,20 +24,21 @@ import {nsToSecNs} from '../utils';
  * Network supply view model
  */
 class NetworkSupplyViewModel {
+  static totalSupply = 5000000000000000000n;
+
   /**
    * Constructs network supply view model
    *
    * @param {Object} networkSupply
-   * @param {BigInt} totalSupply
    */
-  constructor(networkSupply, totalSupply) {
+  constructor(networkSupply) {
     const unreleasedSupply = BigInt(networkSupply.unreleased_supply);
-    const releasedSupply = totalSupply - unreleasedSupply;
+    const releasedSupply = NetworkSupplyViewModel.totalSupply - unreleasedSupply;
 
     // Convert numbers to string since Express doesn't support BigInt
     this.released_supply = `${releasedSupply}`;
     this.timestamp = nsToSecNs(networkSupply.consensus_timestamp);
-    this.total_supply = `${totalSupply}`;
+    this.total_supply = `${NetworkSupplyViewModel.totalSupply}`;
   }
 }
 
