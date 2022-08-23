914ab55b7ec57449a0c3543afa97eb22c1c21b58
ivanparnarev <105442787+ivanparnarev@users.noreply.github.com>
2022-08-23 12:05:34 +0300

    Impl: Validate that eth_call works for reading contract storage (#4288)
    * feat: eth_call - add test for reading contract's storage
    
    Signed-off-by: ivanparnarev <ivan.parnarev@limechain.tech>
    
    * fix: update PostgreSQL query to get correct storage
    
    Signed-off-by: ivanparnarev <ivan.parnarev@limechain.tech>
    
    * nit: remove unused import
    
    Signed-off-by: ivanparnarev <ivan.parnarev@limechain.tech>
    
    * fix: fix error that breaks the build
    
    Signed-off-by: ivanparnarev <ivan.parnarev@limechain.tech>
    
    * fix: fix the test that simulates reading from contract storage
    
    Signed-off-by: ivanparnarev <ivan.parnarev@limechain.tech>
    
    * fix: create query that gets storage value by id and key
    
    Signed-off-by: ivanparnarev <ivan.parnarev@limechain.tech>
    
    * fix: escape unnecessary casting
    
    Signed-off-by: ivanparnarev <ivan.parnarev@limechain.tech>
    
    Signed-off-by: ivanparnarev <ivan.parnarev@limechain.tech>

---
+++ hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/repository/ContractStateChangeRepository.java
@@ -0,0 +1,18 @@
+package com.hedera.mirror.web3.repository;
+
+import com.hedera.mirror.common.domain.contract.Contract;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import java.util.Optional;
+
+public interface ContractStateChangeRepository extends CrudRepository<Contract, Long> {
+    @Query(value = """
+                SELECT encode(value_read, 'hex')
+                FROM contract_state_change
+                INNER JOIN entity
+                ON contract_state_change.contract_id = entity.id
+                WHERE entity.evm_address = ?1
+                AND slot = ?2""", nativeQuery = true)
+    Optional<String> findStorageValue(byte[] id, byte[] key);
+}
