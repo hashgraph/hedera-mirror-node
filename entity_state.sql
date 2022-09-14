4b0d81e47f3b56d1fb8f727e6ee5aefdee6bef58
Xin Li <xin.li@swirldslabs.com>
2022-09-14 12:25:54 -0500

    v1, 600s, testnet
    Signed-off-by: Xin Li <xin.li@swirldslabs.com>

--- entity_state.sql
+++ entity_state.sql
@@ -16,29 +16,22 @@ union all
     stake_period_start
   from entity_history
   where deleted is not true and type in ('ACCOUNT', 'CONTRACT') and timestamp_range @> 1663113600714587002
-), balance_timestamp as (
-  select consensus_timestamp from account_balance_file where consensus_timestamp < 1663113600714587002 order by consensus_timestamp desc limit 1
 ), balance_snapshot as (
   select account_id, balance
   from account_balance
-  join entity_state on account_id = id
-  join balance_timestamp on balance_timestamp.consensus_timestamp = account_balance.consensus_timestamp
-), balance_change as (
-  select entity_id, sum(amount) as amount
+  where consensus_timestamp = (select consensus_timestamp from account_balance_file where consensus_timestamp < 1663113600714587002 order by consensus_timestamp desc limit 1)
+), crypto_transfer_range as (
+  select entity_id, amount
   from crypto_transfer
-  join balance_timestamp on crypto_transfer.consensus_timestamp > balance_timestamp.consensus_timestamp
-  join entity_state on entity_id = id
-  where crypto_transfer.consensus_timestamp < 1663113600714587002
-  group by entity_id
+  where crypto_transfer.consensus_timestamp < 1663113600714587002 and
+    consensus_timestamp > (select consensus_timestamp from account_balance_file where consensus_timestamp < 1663113600714587002 order by consensus_timestamp desc limit 1)
 )
 select
-  coalesce(balance, 0) + coalesce(amount, 0)
+  coalesce((select balance from balance_snapshot where account_id = id), 0) +
+    coalesce((select sum(amount) from crypto_transfer_range where entity_id = id), 0) as balance,
   decline_reward,
   id,
   coalesce(staked_account_id, 0)   as staked_account_id,
   coalesce(staked_node_id, -1)     as staked_node_id,
   coalesce(stake_period_start, -1) as stake_period_start
-from entity_state
-left join balance_snapshot on account_id = id
-left join balance_change on entity_id = id
-order by id limit 1000;
+from entity_state;
