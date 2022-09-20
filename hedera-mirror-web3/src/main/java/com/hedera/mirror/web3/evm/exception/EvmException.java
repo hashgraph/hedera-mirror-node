7cc41e4e2f00c5e3c1f3c08046aeee3e241f6b87
Yanko Nikolov <nikolovqnko@gmail.com>
2022-09-20 15:18:24 +0300

    fix: sonar issue
    Signed-off-by: Yanko Nikolov <nikolovqnko@gmail.com>

--- hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/evm/exception/EvmException.java
+++ hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/evm/exception/EvmException.java
@@ -22,15 +22,15 @@ package com.hedera.mirror.web3.evm.exception;
 
 public abstract class EvmException extends RuntimeException {
 
-    public EvmException(String message) {
+    protected EvmException(String message) {
         super(message);
     }
 
-    public EvmException(Throwable throwable) {
+    protected EvmException(Throwable throwable) {
         super(throwable);
     }
 
-    public EvmException(String message, Throwable throwable) {
+    protected EvmException(String message, Throwable throwable) {
         super(message, throwable);
     }
 }
