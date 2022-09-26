b55493c1b5fabc7251a361054a63ee58e36b44f1
mgoelswirlds <106084778+mgoelswirlds@users.noreply.github.com>
2022-09-26 16:46:08 -0400

    Update hedera-mirror-importer/src/main/resources/db/migration/v1/V1.66.1__custom_fee_blanket_exemption_enhancement.sql
    Co-authored-by: Xin Li <59580070+xin-hedera@users.noreply.github.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v1/V1.66.1__custom_fee_blanket_exemption_enhancement.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/v1/V1.66.1__custom_fee_blanket_exemption_enhancement.sql
@@ -5,5 +5,3 @@
 alter table if exists custom_fee
     add column all_collectors_are_exempt boolean not null default false;
 
--- set net_of_transfers of historical fractional fees to false
-update custom_fee set all_collectors_are_exempt = false;
