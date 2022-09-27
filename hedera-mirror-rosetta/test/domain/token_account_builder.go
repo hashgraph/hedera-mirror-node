44741fa8f57cddc8f7c4009dacfebbc3123dc254
Xin Li <xin.li@swirldslabs.com>
2022-09-27 17:47:21 -0500

    rosetta changes
    Signed-off-by: Xin Li <xin.li@swirldslabs.com>

--- hedera-mirror-rosetta/test/domain/token_account_builder.go
+++ hedera-mirror-rosetta/test/domain/token_account_builder.go
@@ -23,37 +23,68 @@ package domain
 import (
 	"github.com/hashgraph/hedera-mirror-node/hedera-mirror-rosetta/app/interfaces"
 	"github.com/hashgraph/hedera-mirror-node/hedera-mirror-rosetta/app/persistence/domain"
+	"github.com/jackc/pgtype"
+	"gorm.io/gorm/clause"
 )
 
 type TokenAccountBuilder struct {
 	dbClient     interfaces.DbClient
+	historical   bool
 	tokenAccount domain.TokenAccount
 }
 
 func (b *TokenAccountBuilder) CreatedTimestamp(createdTimestamp int64) *TokenAccountBuilder {
 	b.tokenAccount.CreatedTimestamp = createdTimestamp
-	b.tokenAccount.ModifiedTimestamp = createdTimestamp
+	b.tokenAccount.TimestampRange = getTimestampRangeWithLower(createdTimestamp)
 	return b
 }
 
 func (b *TokenAccountBuilder) Associated(associated bool, modifiedTimestamp int64) *TokenAccountBuilder {
 	b.tokenAccount.Associated = associated
-	b.tokenAccount.ModifiedTimestamp = modifiedTimestamp
+	b.tokenAccount.TimestampRange = getTimestampRangeWithLower(modifiedTimestamp)
+	return b
+}
+
+func (b *TokenAccountBuilder) Historical(historical bool) *TokenAccountBuilder {
+	b.historical = historical
+	return b
+}
+
+func (b *TokenAccountBuilder) TimestampRange(lowerInclusive, upperExclusive int64) *TokenAccountBuilder {
+	b.tokenAccount.TimestampRange = pgtype.Int8range{
+		Lower:     pgtype.Int8{Int: lowerInclusive, Status: pgtype.Present},
+		Upper:     pgtype.Int8{Int: upperExclusive, Status: pgtype.Present},
+		LowerType: pgtype.Inclusive,
+		UpperType: pgtype.Exclusive,
+		Status:    pgtype.Present,
+	}
 	return b
 }
 
 func (b *TokenAccountBuilder) Persist() domain.TokenAccount {
-	b.dbClient.GetDb().Create(&b.tokenAccount)
+	tableName := b.tokenAccount.TableName()
+	if b.historical {
+		tableName = b.tokenAccount.HistoryTableName()
+	}
+	tx := b.dbClient.GetDb().Table(tableName)
+	if !b.historical {
+		// only entity table has unique id column
+		tx = tx.Clauses(clause.OnConflict{
+			Columns:   []clause.Column{{Name: "account_id"}, {Name: "token_id"}},
+			DoUpdates: clause.AssignmentColumns([]string{"associated", "timestamp_range"}),
+		})
+	}
+	tx.Create(&b.tokenAccount)
 	return b.tokenAccount
 }
 
 func NewTokenAccountBuilder(dbClient interfaces.DbClient, accountId, tokenId, timestamp int64) *TokenAccountBuilder {
 	tokenAccount := domain.TokenAccount{
-		AccountId:         domain.MustDecodeEntityId(accountId),
-		Associated:        true,
-		CreatedTimestamp:  timestamp,
-		ModifiedTimestamp: timestamp,
-		TokenId:           domain.MustDecodeEntityId(tokenId),
+		AccountId:        domain.MustDecodeEntityId(accountId),
+		Associated:       true,
+		CreatedTimestamp: timestamp,
+		TimestampRange:   getTimestampRangeWithLower(timestamp),
+		TokenId:          domain.MustDecodeEntityId(tokenId),
 	}
 	return &TokenAccountBuilder{dbClient: dbClient, tokenAccount: tokenAccount}
 }
