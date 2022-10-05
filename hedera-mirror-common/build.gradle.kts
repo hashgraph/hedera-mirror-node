3050d19c664467d72480944fa34a71f313b13f1d
Steven Sheehy <steven.sheehy@swirldslabs.com>
2022-10-05 17:52:57 -0500

    Initial gradle support
    Signed-off-by: Steven Sheehy <steven.sheehy@swirldslabs.com>

---
+++ hedera-mirror-common/build.gradle.kts
@@ -0,0 +1,43 @@
+/*-
+ * ‌
+ * Hedera Mirror Node
+ * ​
+ * Copyright (C) 2019 - 2022 Hedera Hashgraph, LLC
+ * ​
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ‍
+ */
+
+description = "Hedera Mirror Node Common"
+
+plugins {
+    id("java-conventions")
+}
+
+dependencies {
+    val testClasses by configurations.creating
+    api("com.fasterxml.jackson.core:jackson-databind")
+    api("com.google.guava:guava")
+    api("com.google.protobuf:protobuf-java")
+    api("com.hedera.hashgraph:hedera-protobuf-java-api") { isTransitive = false }
+    api("com.vladmihalcea:hibernate-types-55")
+    api("commons-codec:commons-codec")
+    api("io.projectreactor:reactor-core")
+    api("org.apache.commons:commons-lang3")
+    api("org.apache.tuweni:tuweni-bytes")
+    api("org.hibernate:hibernate-jpamodelgen")
+    api("org.springframework.boot:spring-boot-starter-data-jpa")
+    api("org.springframework.boot:spring-boot-starter-log4j2")
+    testImplementation("org.hyperledger.besu:evm")
+    testClasses(sourceSets["test"].output)
+}
--- hedera-mirror-rest/build.gradle.kts
+++ hedera-mirror-rest/build.gradle.kts
@@ -18,18 +18,9 @@
  * ‍
  */
 
-import {jest} from '@jest/globals';
-import matchers from 'jest-extended';
-import log4js from 'log4js';
+description = "Hedera Mirror Node REST API"
 
-global.logger = log4js.getLogger();
-
-expect.extend(matchers); // add matchers from jest-extended
-jest.setTimeout(4000);
-
-// set test configuration file path
-process.env.CONFIG_PATH = '__tests__';
-
-beforeEach(() => {
-  logger.info(expect.getState().currentTestName);
-});
+plugins {
+    id("docker-conventions")
+    id("javascript-conventions")
+}
