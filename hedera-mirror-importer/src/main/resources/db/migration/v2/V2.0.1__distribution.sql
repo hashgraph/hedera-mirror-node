c0ce8c95eab916e6b46b95447c92ff6ea93cb1d3
Xin Li <xin.li@swirldslabs.com>
2022-10-05 14:21:21 -0500

    update v2 schema
    Signed-off-by: Xin Li <xin.li@swirldslabs.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v2/V2.0.1__distribution.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/v2/V2.0.1__distribution.sql
@@ -6,12 +6,18 @@
 
 
 -- Tables distributed on id
-select create_distributed_table('contract', 'id');
+select create_distributed_table('account_balance_file', 'node_id');
 
 select create_distributed_table('entity', 'id');
 
+select create_distributed_table('event_file', 'node_id');
+
+select create_distributed_table('record_file', 'node_id');
+
 -- Tables that will be colocated with tables above
 select create_distributed_table('account_balance', 'account_id', colocate_with => 'entity');
+
+select create_distributed_table('contract', 'id', colocate_with => 'entity');
 
 select create_distributed_table('contract_action', 'caller', colocate_with => 'entity');
 
@@ -31,8 +37,6 @@ select create_distributed_table('custom_fee', 'token_id', colocate_with => 'enti
 
 select create_distributed_table('entity_history', 'id', colocate_with => 'entity');
 
-select create_distributed_table('event_file', 'node_account_id', colocate_with => 'entity');
-
 select create_distributed_table('file_data', 'entity_id', colocate_with => 'entity');
 
 select create_distributed_table('nft', 'token_id', colocate_with => 'entity');
@@ -41,15 +45,13 @@ select create_distributed_table('nft_allowance', 'owner', colocate_with => 'enti
 
 select create_distributed_table('nft_allowance_history', 'owner', colocate_with => 'nft_allowance');
 
-select create_distributed_table('record_file', 'node_account_id', colocate_with => 'entity');
-
 select create_distributed_table('schedule', 'schedule_id', colocate_with => 'entity');
 
 select create_distributed_table('token', 'token_id', colocate_with => 'entity');
 
-select create_distributed_table('token_account', 'token_id', colocate_with => 'entity');
+select create_distributed_table('token_account', 'account_id', colocate_with => 'entity');
 
-select create_distributed_table('token_account_history', 'token_id', colocate_with => 'token_account');
+select create_distributed_table('token_account_history', 'account_id', colocate_with => 'token_account');
 
 select create_distributed_table('token_allowance', 'owner', colocate_with => 'entity');
 
@@ -64,4 +66,3 @@ select create_distributed_table('transaction', 'payer_account_id', colocate_with
 select create_distributed_table('transaction_signature', 'entity_id', colocate_with => 'entity');
 
 -- Reference tables
-select create_reference_table('account_balance_file');
