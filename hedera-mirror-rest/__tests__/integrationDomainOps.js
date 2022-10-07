d8b8e591c78ee7126ddcab019d8c8c1f184f001c
mgoelswirlds <mugdha.goel@swirldslabs.com>
2022-10-07 16:47:56 -0400

    Fixing breaking tests to remove node_account_id and add node_id

--- hedera-mirror-rest/__tests__/integrationDomainOps.js
+++ hedera-mirror-rest/__tests__/integrationDomainOps.js
@@ -629,7 +629,7 @@ const addAccount = async (account) => {
 const addAccountBalanceFile = async (accountBalanceFile) => {
   accountBalanceFile = {
     consensus_timestamp: 0,
-    node_account_id: 0,
+    node_id: 0,
     file_hash: 'dee34bdd8bbe32fdb53ce7e3cf764a0495fa5e93b15ca567208cfb384231301bedf821de07b0d8dc3fb55c5b3c90ac61',
     load_end: 1629298236,
     load_start: 1629298233,
@@ -639,7 +639,7 @@ const addAccountBalanceFile = async (accountBalanceFile) => {
 
   await pool.query(
     `insert into account_balance_file (consensus_timestamp,count,load_start,load_end,
-                                  file_hash,name,node_account_id,bytes,time_offset)
+                                  file_hash,name,node_id,bytes,time_offset)
     values ($1, $2, $3, $4, $5, $6, $7, $8, $9);`,
     [
       accountBalanceFile.consensus_timestamp,
@@ -648,7 +648,7 @@ const addAccountBalanceFile = async (accountBalanceFile) => {
       accountBalanceFile.load_end,
       accountBalanceFile.file_hash,
       accountBalanceFile.name,
-      accountBalanceFile.node_account_id,
+      accountBalanceFile.node_id,
       accountBalanceFile.bytes,
       accountBalanceFile.time_offset,
     ]
