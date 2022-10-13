1b4c0a10c4334327fe2fd79ae2c0618ef1cb3bd4
Marc Kriguer <marc.kriguer@swirldslabs.com>
2022-10-12 17:08:00 -0700

    Add a try with resources (to properly close Client).
    Signed-off-by: Marc Kriguer <marc.kriguer@swirldslabs.com>

--- hedera-mirror-test/src/test/java/com/hedera/mirror/test/e2e/acceptance/client/StartupProbe.java
+++ hedera-mirror-test/src/test/java/com/hedera/mirror/test/e2e/acceptance/client/StartupProbe.java
@@ -77,79 +77,80 @@ public class StartupProbe {
         }
         // any of these can throw IllegalArgumentException; if it does, retries wouldn't help, so exit early (with that
         // exception) if it happens.
-        Client client = Client.forName(acceptanceTestProperties.getNetwork().toString().toLowerCase());
         AccountId operator = AccountId.fromString(acceptanceTestProperties.getOperatorId());
         PrivateKey privateKey = PrivateKey.fromString(acceptanceTestProperties.getOperatorKey());
-        client.setOperator(operator, privateKey);  // this account pays for (and signs) all transactions run here.
+        try (Client client = Client.forName(acceptanceTestProperties.getNetwork().toString().toLowerCase())) {
+            client.setOperator(operator, privateKey);  // this account pays for (and signs) all transactions run here.
 
-        Stopwatch stopwatch = Stopwatch.createStarted();
-        // step 1: Create a new topic for the subsequent HCS submit message action.
-        RetryTemplate retryTemplate = RetryTemplate.builder()
-                .infiniteRetry()
-                .notRetryOn(TimeoutException.class)
-                .fixedBackoff(1000L)
-                .build();
+            Stopwatch stopwatch = Stopwatch.createStarted();
+            // step 1: Create a new topic for the subsequent HCS submit message action.
+            RetryTemplate retryTemplate = RetryTemplate.builder()
+                    .infiniteRetry()
+                    .notRetryOn(TimeoutException.class)
+                    .fixedBackoff(1000L)
+                    .build();
         
-        TransactionResponse response = retryTemplate.execute(x -> new TopicCreateTransaction()
-            .setGrpcDeadline(Duration.ofSeconds(10L))
-            .setMaxAttempts(Integer.MAX_VALUE)
-            .execute(client, startupTimeout.minus(stopwatch.elapsed())));
-        TransactionId transactionId = response.transactionId;
+            TransactionResponse response = retryTemplate.execute(x -> new TopicCreateTransaction()
+                .setGrpcDeadline(Duration.ofSeconds(10L))
+                .setMaxAttempts(Integer.MAX_VALUE)
+                .execute(client, startupTimeout.minus(stopwatch.elapsed())));
+            TransactionId transactionId = response.transactionId;
 
-        // step 2: Query for the receipt of that HCS submit transaction (until successful or time runs out)
-        TransactionReceipt transactionReceipt = response.getReceipt(client,
-                startupTimeout.minus(stopwatch.elapsed()));
+            // step 2: Query for the receipt of that HCS submit transaction (until successful or time runs out)
+            TransactionReceipt transactionReceipt = response.getReceipt(client,
+                    startupTimeout.minus(stopwatch.elapsed()));
 
-        // step 3: Query the mirror node for the transaction by ID (until successful or time runs out)
-        // Instead of using MirrorNodeClient.getTransactions(restTransactionId), we directly call webClient.
-        // retry infinitely or until timeout (or success), not a maximum of properties.getMaxAttempts() times.
-        var properties = acceptanceTestProperties.getRestPollingProperties();
-        RetryBackoffSpec retrySpec = Retry.backoff(Integer.MAX_VALUE, properties.getMinBackoff())
-                .maxBackoff(properties.getMaxBackoff())
-                .filter(this::shouldRetryRestCall);
+            // step 3: Query the mirror node for the transaction by ID (until successful or time runs out)
+            // Instead of using MirrorNodeClient.getTransactions(restTransactionId), we directly call webClient.
+            // retry infinitely or until timeout (or success), not a maximum of properties.getMaxAttempts() times.
+            var properties = acceptanceTestProperties.getRestPollingProperties();
+            RetryBackoffSpec retrySpec = Retry.backoff(Integer.MAX_VALUE, properties.getMinBackoff())
+                    .maxBackoff(properties.getMaxBackoff())
+                    .filter(this::shouldRetryRestCall);
 
-        String restTransactionId = transactionId.accountId.toString() + "-" + transactionId.validStart.getEpochSecond()
-                + "-" + transactionId.validStart.getNano();
+            String restTransactionId = transactionId.accountId.toString() + "-"
+                    + transactionId.validStart.getEpochSecond() + "-" + transactionId.validStart.getNano();
 
-        webClient.get()
-                .uri("/transactions/", restTransactionId)
-                .accept(MediaType.APPLICATION_JSON)
-                .retrieve()
-                .bodyToMono(String.class)
-                .retryWhen(retrySpec)
-                .doOnError(x -> log.error("Endpoint failed, returning: {}", x.getMessage()))
-                .timeout(startupTimeout.minus(stopwatch.elapsed()))
-                .block();
+            webClient.get()
+                    .uri("/transactions/", restTransactionId)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .retryWhen(retrySpec)
+                    .doOnError(x -> log.error("Endpoint failed, returning: {}", x.getMessage()))
+                    .timeout(startupTimeout.minus(stopwatch.elapsed()))
+                    .block();
 
-        // step 4: Query the mirror node gRPC API for a message on the topic created in step 2, until successful or
-        //         time runs out
-        TopicId topicId = transactionReceipt.topicId;
-        AtomicBoolean messageReceived = new AtomicBoolean();
+            // step 4: Query the mirror node gRPC API for a message on the topic created in step 2, until successful or
+            //         time runs out
+            TopicId topicId = transactionReceipt.topicId;
+            AtomicBoolean messageReceived = new AtomicBoolean();
 
-        retryTemplate.execute(x -> new TopicMessageQuery()
-                .setTopicId(topicId)
-                .subscribe(client, resp -> {
-                    messageReceived.set(true);
-                }));
-
-        retryTemplate.execute(x -> {
-            int counter = 0;
-            while (!(messageReceived.get()) && startupTimeout.minus(stopwatch.elapsed()).toMillis() > 0) {
-                new TopicMessageSubmitTransaction()
+            retryTemplate.execute(x -> new TopicMessageQuery()
                     .setTopicId(topicId)
-                    .setMessage("Hello, HCS! " + ++counter)
-                    .execute(client, startupTimeout.minus(stopwatch.elapsed()))
-                    .getReceipt(client);
-                Uninterruptibles.sleepUninterruptibly(1, TimeUnit.SECONDS);
-            }
-            return null;
-        });
+                    .subscribe(client, resp -> {
+                        messageReceived.set(true);
+                    }));
 
-        if (messageReceived.get()) {
-            log.info("Startup probe successful.");
-            validated = true;
-        } else {
-            throw new TimeoutException("Timer expired while trying to receive topic messages.");
+            retryTemplate.execute(x -> {
+                int counter = 0;
+                while (!(messageReceived.get()) && startupTimeout.minus(stopwatch.elapsed()).toMillis() > 0) {
+                    new TopicMessageSubmitTransaction()
+                            .setTopicId(topicId)
+                            .setMessage("Hello, HCS! " + ++counter)
+                            .execute(client, startupTimeout.minus(stopwatch.elapsed()))
+                            .getReceipt(client);
+                    Uninterruptibles.sleepUninterruptibly(1, TimeUnit.SECONDS);
+                }
+                return null;
+            });
+
+            if (messageReceived.get()) {
+                log.info("Startup probe successful.");
+                validated = true;
+            } else {
+                throw new TimeoutException("Timer expired while trying to receive topic messages.");
+            }
         }
     }
 
