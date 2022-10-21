0cab3cb83fd11731db920ac07147e219f0041bcd
Xin Li <xin.li@swirldslabs.com>
2022-10-21 16:24:27 -0500

    force using swirlds-common:0.31.0
    Signed-off-by: Xin Li <xin.li@swirldslabs.com>

--- hedera-mirror-web3/build.gradle.kts
+++ hedera-mirror-web3/build.gradle.kts
@@ -27,7 +27,10 @@ plugins {
 dependencies {
     implementation(platform("org.springframework.cloud:spring-cloud-dependencies"))
     implementation(project(":common"))
-    implementation("com.hedera.evm:hedera-evm-api")
+    implementation("com.hedera.evm:hedera-evm-api") {
+        exclude("com.swirlds", "swirlds-common")
+    }
+    implementation("com.swirlds:swirlds-common:0.31.0")
     implementation("io.github.mweirauch:micrometer-jvm-extras")
     implementation("io.micrometer:micrometer-registry-prometheus")
     implementation(group = "io.netty", name = "netty-resolver-dns-native-macos", classifier = "osx-aarch_64")
