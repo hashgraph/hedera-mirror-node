6e7578bea18cc68b8afc34eb182bef34890a266e
ivanparnarev <ivan.parnarev@limechain.tech>
2022-10-23 22:45:40 +0300

    feat: fee schedule initial implementation
    Signed-off-by: ivanparnarev <ivan.parnarev@limechain.tech>

--- hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/controller/PricesAndFeesImpl.java
+++ hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/controller/PricesAndFeesImpl.java
@@ -5,17 +5,31 @@ import com.google.protobuf.InvalidProtocolBufferException;
 import com.hedera.mirror.common.domain.entity.EntityId;
 import com.hedera.mirror.common.domain.entity.EntityType;
 import com.hedera.mirror.web3.evm.fees.PricesAndFeesProvider;
+import com.hedera.mirror.web3.evm.fees.RequiredPriceTypes;
 import com.hedera.mirror.web3.repository.PricesAndFeesRepository;
 
+import com.hederahashgraph.api.proto.java.CurrentAndNextFeeSchedule;
 import com.hederahashgraph.api.proto.java.ExchangeRate;
 import com.hederahashgraph.api.proto.java.ExchangeRateSet;
+import com.hederahashgraph.api.proto.java.FeeComponents;
 import com.hederahashgraph.api.proto.java.FeeData;
+import com.hederahashgraph.api.proto.java.FeeSchedule;
 import com.hederahashgraph.api.proto.java.HederaFunctionality;
+import com.hederahashgraph.api.proto.java.SubType;
 import com.hederahashgraph.api.proto.java.Timestamp;
+import com.hederahashgraph.api.proto.java.TimestampSeconds;
+import com.hederahashgraph.api.proto.java.TransactionFeeSchedule;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import java.time.Instant;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+import static com.hederahashgraph.api.proto.java.SubType.DEFAULT;
 
 @Component
 @RequiredArgsConstructor
@@ -25,10 +39,27 @@ public class PricesAndFeesImpl implements PricesAndFeesProvider {
     private static final EntityId EXCHANGE_RATE_ENTITY_ID = new EntityId(0L, 0L, 112L, EntityType.FILE);
     private ExchangeRateSet grpcRates = null;
     private final PricesAndFeesRepository pricesAndFeesRepository;
+    private Timestamp currFunctionUsagePricesExpiry;
+    private Timestamp nextFunctionUsagePricesExpiry;
+    private EnumMap<HederaFunctionality, Map<SubType, FeeData>> currFunctionUsagePrices;
+    private EnumMap<HederaFunctionality, Map<SubType, FeeData>> nextFunctionUsagePrices;
+    private static final long DEFAULT_FEE = 100_000L;
+    private static final FeeComponents DEFAULT_PROVIDER_RESOURCE_PRICES = FeeComponents.newBuilder()
+            .setMin(DEFAULT_FEE)
+            .setMax(DEFAULT_FEE)
+            .setConstant(0).setBpt(0).setVpt(0).setRbh(0).setSbh(0).setGas(0).setTv(0).setBpr(0).setSbpr(0)
+            .build();
+    public static final Map<SubType, FeeData> DEFAULT_RESOURCE_PRICES = Map.of(DEFAULT, FeeData.newBuilder()
+            .setNetworkdata(DEFAULT_PROVIDER_RESOURCE_PRICES)
+            .setNodedata(DEFAULT_PROVIDER_RESOURCE_PRICES)
+            .setServicedata(DEFAULT_PROVIDER_RESOURCE_PRICES)
+            .build());
+    private static final EntityId FEE_SCHEDULE_ENTITY_ID = new EntityId(0L, 0L, 111L, EntityType.FILE);
 
     @Override
     public FeeData defaultPricesGiven(HederaFunctionality function, Timestamp at) {
-        return null;
+        loadPriceSchedules(at.getSeconds());
+        return pricesGiven(function, at).get(DEFAULT);
     }
 
     @Override
@@ -39,6 +70,13 @@ public class PricesAndFeesImpl implements PricesAndFeesProvider {
     @Override
     public long estimatedGasPriceInTinybars(HederaFunctionality function, Timestamp at) {
         return 0;
+    }
+
+    private ExchangeRate rateAt(final long now) {
+        loadRates(now);
+        final var currentRate = grpcRates.getCurrentRate();
+        final var currentExpiry = currentRate.getExpirationTime().getSeconds();
+        return (now < currentExpiry) ? currentRate : grpcRates.getNextRate();
     }
 
     public void loadRates(final long now) {
@@ -53,10 +91,105 @@ public class PricesAndFeesImpl implements PricesAndFeesProvider {
         }
     }
 
-    private ExchangeRate rateAt(final long now) {
-        loadRates(now);
-        final var currentRate = grpcRates.getCurrentRate();
-        final var currentExpiry = currentRate.getExpirationTime().getSeconds();
-        return (now < currentExpiry) ? currentRate : grpcRates.getNextRate();
+    private void loadPriceSchedules(final long now) {
+        byte[] feeScheduleFile = new byte[0];
+        if (now > 0) {
+            feeScheduleFile =
+                    pricesAndFeesRepository.getFeeSchedule(now);
+        }
+
+        try {
+            final var schedules = CurrentAndNextFeeSchedule.parseFrom(feeScheduleFile);
+            setFeeSchedules(schedules);
+        } catch (InvalidProtocolBufferException e) {
+            log.warn("Corrupt fee schedules file at {}, may require remediation!", FEE_SCHEDULE_ENTITY_ID.toString(),
+                    e);
+            throw new IllegalStateException(
+                    String.format("Fee schedule %s is corrupt!", FEE_SCHEDULE_ENTITY_ID));
+        }
+    }
+
+    public void setFeeSchedules(final CurrentAndNextFeeSchedule feeSchedules) {
+
+        currFunctionUsagePrices = functionUsagePricesFrom(feeSchedules.getCurrentFeeSchedule());
+        currFunctionUsagePricesExpiry = asTimestamp(feeSchedules.getCurrentFeeSchedule().getExpiryTime());
+
+        nextFunctionUsagePrices = functionUsagePricesFrom(feeSchedules.getNextFeeSchedule());
+        nextFunctionUsagePricesExpiry = asTimestamp(feeSchedules.getNextFeeSchedule().getExpiryTime());
+    }
+
+    public Map<SubType, FeeData> pricesGiven(HederaFunctionality function, Timestamp at) {
+        try {
+            Map<HederaFunctionality, Map<SubType, FeeData>> functionUsagePrices = applicableUsagePrices(at);
+            Map<SubType, FeeData> usagePrices = functionUsagePrices.get(function);
+            Objects.requireNonNull(usagePrices);
+            return usagePrices;
+        } catch (Exception e) {
+            log.debug(
+                    "Default usage price will be used, no specific usage prices available for function {} @ {}!",
+                    function, Instant.ofEpochSecond(at.getSeconds(), at.getNanos()));
+        }
+        return DEFAULT_RESOURCE_PRICES;
+    }
+
+    private Map<HederaFunctionality, Map<SubType, FeeData>> applicableUsagePrices(final Timestamp at) {
+        if (onlyNextScheduleApplies(at)) {
+            return nextFunctionUsagePrices;
+        } else {
+            return currFunctionUsagePrices;
+        }
+    }
+
+    EnumMap<HederaFunctionality, Map<SubType, FeeData>> functionUsagePricesFrom(final FeeSchedule feeSchedule) {
+        final EnumMap<HederaFunctionality, Map<SubType, FeeData>> allPrices = new EnumMap<>(HederaFunctionality.class);
+        for (var pricingData : feeSchedule.getTransactionFeeScheduleList()) {
+            final var function = pricingData.getHederaFunctionality();
+            Map<SubType, FeeData> pricesMap = allPrices.get(function);
+            if (pricesMap == null) {
+                pricesMap = new EnumMap<>(SubType.class);
+            }
+            final Set<SubType> requiredTypes = RequiredPriceTypes.requiredTypesFor(function);
+            ensurePricesMapHasRequiredTypes(pricingData, pricesMap, requiredTypes);
+            allPrices.put(pricingData.getHederaFunctionality(), pricesMap);
+        }
+        return allPrices;
+    }
+
+    private Timestamp asTimestamp(final TimestampSeconds ts) {
+        return Timestamp.newBuilder().setSeconds(ts.getSeconds()).build();
+    }
+
+    void ensurePricesMapHasRequiredTypes(
+            final TransactionFeeSchedule tfs,
+            final Map<SubType, FeeData> pricesMap,
+            final Set<SubType> requiredTypes
+    ) {
+        /* The deprecated prices are the final fallback; if even they are not set, the function will be free */
+        final var oldDefaultPrices = tfs.getFeeData();
+        FeeData newDefaultPrices = null;
+        for (var typedPrices : tfs.getFeesList()) {
+            final var type = typedPrices.getSubType();
+            if (requiredTypes.contains(type)) {
+                pricesMap.put(type, typedPrices);
+            }
+            if (type == DEFAULT) {
+                newDefaultPrices = typedPrices;
+            }
+        }
+        for (var type : requiredTypes) {
+            if (!pricesMap.containsKey(type)) {
+                if (newDefaultPrices != null) {
+                    pricesMap.put(type, newDefaultPrices.toBuilder().setSubType(type).build());
+                } else {
+                    pricesMap.put(type, oldDefaultPrices.toBuilder().setSubType(type).build());
+                }
+            }
+        }
+    }
+
+    private boolean onlyNextScheduleApplies(final Timestamp at) {
+        return at.getSeconds() >= currFunctionUsagePricesExpiry.getSeconds() &&
+                at.getSeconds() < nextFunctionUsagePricesExpiry.getSeconds();
     }
 }
+
--- hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/repository/PricesAndFeesRepository.java
+++ hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/repository/PricesAndFeesRepository.java
@@ -43,4 +43,22 @@ public interface PricesAndFeesRepository extends CrudRepository<Entity, Long> {
                       and ?1 >= l.consensus_timestamp
                     group by file_data.entity_id""", nativeQuery = true)
     byte[] getExchangeRate(final long now);
+
+    @Query(value = """
+                with latest_create as (
+                      select max(file_data.consensus_timestamp) as consensus_timestamp
+                      from file_data
+                      where file_data.entity_id = 111 and file_data.transaction_type in (17, 19)
+                      group by file_data.entity_id
+                      order by consensus_timestamp desc
+                    )
+                    select
+                    string_agg(file_data.file_data, '' order by file_data.consensus_timestamp) as file_data
+                    from file_data
+                    join latest_create l on file_data.consensus_timestamp >= l.consensus_timestamp
+                    where file_data.entity_id = 111 and file_data.transaction_type in (16, 17, 19)
+                      and ?1 >= l.consensus_timestamp
+                    group by file_data.entity_id""", nativeQuery = true)
+    byte[] getFeeSchedule(final long now);
+
 }
