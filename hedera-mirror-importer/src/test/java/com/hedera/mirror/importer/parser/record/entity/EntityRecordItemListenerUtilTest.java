8ec208e1a3c101073a6f48bf635e4e26be52bf8f
Steven Sheehy <17552371+steven-sheehy@users.noreply.github.com>
2022-11-10 01:13:41 -0600

    Fix invalid transaction type (0.67) (#4845)
    Signed-off-by: Steven Sheehy <steven.sheehy@swirldslabs.com>
    Signed-off-by: Xin Li <xin.li@swirldslabs.com>
    Co-authored-by: Xin Li <xin.li@swirldslabs.com>

--- hedera-mirror-importer/src/test/java/com/hedera/mirror/importer/parser/record/entity/EntityRecordItemListenerUtilTest.java
+++ hedera-mirror-importer/src/test/java/com/hedera/mirror/importer/parser/record/entity/EntityRecordItemListenerUtilTest.java
@@ -23,11 +23,17 @@ package com.hedera.mirror.importer.parser.record.entity;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.google.protobuf.ByteString;
+import com.hederahashgraph.api.proto.java.Transaction;
+import com.hederahashgraph.api.proto.java.TransactionBody;
+import com.hederahashgraph.api.proto.java.TransactionRecord;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.util.Version;
 
 import com.hedera.mirror.common.domain.transaction.Prng;
+import com.hedera.mirror.common.domain.transaction.RecordItem;
 import com.hedera.mirror.importer.repository.PrngRepository;
 
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -74,6 +80,39 @@ class EntityRecordItemListenerUtilTest extends AbstractEntityRecordItemListenerT
                         .returns(0, Prng::getRange)
                         .returns(pseudorandomBytes, Prng::getPrngBytes)
                         .returns(null, Prng::getPrngNumber)
+        );
+    }
+
+    /**
+     * This test writes a TransactionBody that contains an invalid transaction body without unknown fields or a valid
+     * transaction body and verifies it is still inserted into the database.
+     * <p>
+     * See https://github.com/hashgraph/hedera-mirror-node/issues/4843
+     */
+    @Test
+    void invalidPrngTransaction() {
+        byte[] invalidBytes = new byte[] {10, 23, 10, 21, 10, 11, 8, -23, -105, -78, -101, 6, 16, -115, -95, -56, 47,
+                18, 4, 24, -108, -74, 85, 32, 1};
+        var transaction = Transaction.newBuilder()
+                .setSignedTransactionBytes(ByteString.copyFrom(invalidBytes))
+                .build();
+
+        var recordItem = RecordItem.builder()
+                .hapiVersion(new Version(0, 30, 0))
+                .record(TransactionRecord.newBuilder().build())
+                .transaction(transaction)
+                .build();
+
+        parseRecordItemAndCommit(recordItem);
+
+        assertAll(
+                () -> assertEquals(0, entityRepository.count()),
+                () -> assertEquals(0, prngRepository.count()),
+                () -> assertThat(transactionRepository.findAll())
+                        .hasSize(1).
+                        first()
+                        .extracting(com.hedera.mirror.common.domain.transaction.Transaction::getType)
+                        .isEqualTo(TransactionBody.DataCase.DATA_NOT_SET.getNumber())
         );
     }
 }
