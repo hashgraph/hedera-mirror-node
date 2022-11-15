c667198323c7853ac0729f12a7783f0346e12421
Xin Li <59580070+xin-hedera@users.noreply.github.com>
2022-11-15 10:50:26 -0600

    Fix incorrect staking info and pending reward  (#4838)
    - Fix incorrect stake period start after reward payout is triggered
    - Use account 800's end_stake_period to check if pending reward calculation is up-to-date
    - Recalculate mainnet pending reward in a db migration
    - Clear account's staking setting which started to stake to a node before services 0.27.0 went alive
    
    Signed-off-by: Xin Li <xin.li@swirldslabs.com>

--- hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/repository/EntityStakeRepository.java
+++ hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/repository/EntityStakeRepository.java
@@ -33,10 +33,12 @@ public interface EntityStakeRepository extends CrudRepository<EntityStake, Long>
             with last_epoch_day as (
               select coalesce((select epoch_day from node_stake order by consensus_timestamp desc limit 1), -1) as epoch_day
             ), entity_stake_info as (
-              select coalesce((select end_stake_period from entity_stake limit 1), -1) as end_stake_period
+              select coalesce((select end_stake_period from entity_stake where id = 800), -1) as end_stake_period
+            ), staking_reward_account as (
+              select (select id from entity where id = 800) as account_id
             )
-            select epoch_day = -1 or epoch_day = end_stake_period
-            from last_epoch_day, entity_stake_info
+            select account_id is null or epoch_day = -1 or epoch_day = end_stake_period
+            from last_epoch_day, entity_stake_info, staking_reward_account
             """, nativeQuery = true)
     boolean updated();
 
@@ -50,23 +52,40 @@ public interface EntityStakeRepository extends CrudRepository<EntityStake, Long>
      * decline_reward_start is true (decline reward for the ending staking period), OR it didn't stake to a node for the
      * ending staking period, the new pending reward is 0
      * <p>
-     * 2.IF there is no node stake info for the node the entity staked to, the pending reward keeps the same
+     * 2. IF there is no node stake info for the node the entity staked to, the pending reward keeps the same
      * <p>
-     * 3. IF the current stake_period_start >= the last epochDay from node stake update (either its staking metadata
-     * or balance changed in the ending staking period), calculate the reward it has earned in the ending staking period
-     * as its pending reward
+     * 3. IF the current stake_period_start > last epochDay - 1, no reward's should be earned, set pending reward to 0
      * <p>
-     * 4. Otherwise, there's no staking metadata or balance change for the entity since the start of the ending staking
+     * 4. IF the current stake_period_start equals to the last epochDay (either its staking metadata or balance changed
+     * in the previous staking period), calculate the reward it has earned in the ending staking period as its pending
+     * reward
+     * <p>
+     * 5. Otherwise, there's no staking metadata or balance change for the entity since the start of the ending staking
      * period, add the reward earned in the ending period to the current as the new pending reward
      *
      * @return Number of entity state inserted and updated
      */
     @Modifying
     @Query(value = """
-            with ending_period_node_stake as (
-              select node_id, epoch_day, reward_rate
+            with ending_period as (
+              select epoch_day, consensus_timestamp
               from node_stake
               where consensus_timestamp = (select max(consensus_timestamp) from node_stake)
+              limit 1
+            ), ending_period_stake_state as (
+              select
+                decline_reward_start,
+                id as entity_id,
+                pending_reward,
+                staked_node_id_start,
+                stake_total_start,
+                reward_rate
+              from entity_stake es
+              left join (
+                select node_id, reward_rate
+                from node_stake ns, ending_period
+                where ns.consensus_timestamp = ending_period.consensus_timestamp
+              ) node_stake on es.staked_node_id_start = node_id
             ), proxy_staking as (
               select staked_account_id, sum(balance) as staked_to_me
               from entity_state_start
@@ -75,16 +94,17 @@ public interface EntityStakeRepository extends CrudRepository<EntityStake, Long>
             ), updated as (
               select
                 ess.decline_reward as decline_reward_start,
-                (select epoch_day from ending_period_node_stake limit 1) as end_stake_period,
+                epoch_day as end_stake_period,
                 ess.id,
                 (case
-                   when coalesce(es.decline_reward_start, true) is true
-                        or coalesce(es.staked_node_id_start, -1) = -1
+                   when coalesce(decline_reward_start, true) is true
+                        or coalesce(staked_node_id_start, -1) = -1
                         then 0
-                   when node_id is null then es.pending_reward
-                   when ess.stake_period_start >= epoch_day
-                        then reward_rate * (es.stake_total_start / 100000000)
-                   else es.pending_reward + reward_rate * (es.stake_total_start / 100000000)
+                   when reward_rate is null then pending_reward
+                   when ess.stake_period_start > epoch_day - 1 then 0
+                   when ess.stake_period_start = epoch_day - 1
+                        then reward_rate * (stake_total_start / 100000000)
+                   else pending_reward + reward_rate * (stake_total_start / 100000000)
                   end) as pending_reward,
                 ess.staked_node_id as staked_node_id_start,
                 coalesce(ps.staked_to_me, 0) as staked_to_me,
@@ -92,9 +112,9 @@ public interface EntityStakeRepository extends CrudRepository<EntityStake, Long>
                       else ess.balance + coalesce(ps.staked_to_me, 0)
                   end) as stake_total_start
               from entity_state_start ess
-              left join entity_stake es on es.id = ess.id
-              left join ending_period_node_stake on node_id = es.staked_node_id_start
-              left join proxy_staking ps on ps.staked_account_id = ess.id
+                left join ending_period_stake_state on entity_id = ess.id
+                left join proxy_staking ps on ps.staked_account_id = ess.id,
+                ending_period
             )
             insert into entity_stake
             table updated
