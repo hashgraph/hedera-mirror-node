f095c0cd27e9a4c79ffc47c99c456e2287530459
Mustafa Uzun <mustafa.uzun@limechain.tech>
2022-11-16 17:50:53 +0200

    test: add integration test EntityRepository
    Signed-off-by: Mustafa Uzun <mustafa.uzun@limechain.tech>

---
+++ hedera-mirror-web3/src/test/java/com/hedera/mirror/web3/repository/EntityRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.hedera.mirror.web3.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.hedera.mirror.common.domain.entity.Entity;
+import com.hedera.mirror.web3.Web3IntegrationTest;
+
+import javax.annotation.Resource;
+import org.junit.jupiter.api.Test;
+
+public class EntityRepositoryTest extends Web3IntegrationTest {
+
+    private Long entityId = 78L;
+
+    private static final byte[] KEY = new byte[33];
+
+    @Resource
+    private EntityRepository entityRepository;
+
+    @Test
+    void findByIdAndDeletedFalseSuccessfulCall() {
+        Entity entity = entity();
+        entityRepository.save(entity);
+        final var result = entityRepository.findByIdAndDeletedFalse(entityId);
+        assertThat(result).get().isEqualTo(entity);
+
+    }
+
+    @Test
+    void findByAliasAndDeletedFalseSuccessfulCall() {
+        Entity entity = entity();
+        entityRepository.save(entity);
+        final var result = entityRepository.findByAliasAndDeletedFalse(KEY);
+        assertThat(result).get().isEqualTo(entity);
+    }
+
+    private Entity entity() {
+        Entity entity = new Entity();
+        entity.setId(++entityId);
+        entity.setDeleted(false);
+        entity.setAlias(KEY);
+        return entity;
+    }
+}
