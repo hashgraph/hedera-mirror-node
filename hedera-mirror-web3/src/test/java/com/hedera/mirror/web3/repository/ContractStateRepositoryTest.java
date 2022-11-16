80e0c713fdd95d41f101b5a575373e4a96fe7063
Mustafa Uzun <mustafa.uzun@limechain.tech>
2022-11-16 16:19:13 +0200

    test: add integration test ContractStateRepository
    Signed-off-by: Mustafa Uzun <mustafa.uzun@limechain.tech>

---
+++ hedera-mirror-web3/src/test/java/com/hedera/mirror/web3/repository/ContractStateRepositoryTest.java
@@ -0,0 +1,53 @@
+package com.hedera.mirror.web3.repository;
+
+/*-
+ * ‌
+ * Hedera Mirror Node
+ * ​
+ * Copyright (C) 2019 - 2022 Hedera Hashgraph, LLC
+ * ​
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ‍
+ */
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.hedera.mirror.common.domain.contract.ContractState;
+import com.hedera.mirror.web3.Web3IntegrationTest;
+
+import javax.annotation.Resource;
+import org.junit.jupiter.api.Test;
+
+public class ContractStateRepositoryTest extends Web3IntegrationTest {
+
+    private Long contractId = 78L;
+    private static final byte[] KEY_VALUE = new byte[32];
+
+    @Resource
+    private ContractStateRepository contractStateRepository;
+
+    @Test
+    void findStorageSuccessfulCall() {
+        contractStateRepository.save(contractState());
+        final var result = contractStateRepository.findStorage(contractId, KEY_VALUE);
+        assertThat(result).get().isEqualTo(KEY_VALUE);
+    }
+
+    private ContractState contractState() {
+        ContractState contractState = new ContractState();
+        contractState.setContractId(++contractId);
+        contractState.setSlot(KEY_VALUE);
+        contractState.setValue(KEY_VALUE);
+        return contractState;
+    }
+}
