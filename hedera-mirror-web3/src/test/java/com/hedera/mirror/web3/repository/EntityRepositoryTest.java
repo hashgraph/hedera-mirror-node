7646f54584413748033aa1505fa3348e0d02b08f
Mustafa Uzun <mustafa.uzun@limechain.tech>
2022-11-16 19:03:10 +0200

    test: add more field to mock
    Signed-off-by: Mustafa Uzun <mustafa.uzun@limechain.tech>

--- hedera-mirror-web3/src/test/java/com/hedera/mirror/web3/repository/EntityRepositoryTest.java
+++ hedera-mirror-web3/src/test/java/com/hedera/mirror/web3/repository/EntityRepositoryTest.java
@@ -1,14 +1,21 @@
 package com.hedera.mirror.web3.repository;
 
+import static com.hedera.mirror.common.domain.entity.EntityType.ACCOUNT;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.google.common.collect.Range;
+
 import com.hedera.mirror.common.domain.entity.Entity;
+import com.hedera.mirror.common.util.DomainUtils;
 import com.hedera.mirror.web3.Web3IntegrationTest;
 
+import java.time.Instant;
 import javax.annotation.Resource;
 import org.junit.jupiter.api.Test;
 
 class EntityRepositoryTest extends Web3IntegrationTest {
+
+    private final Instant now = Instant.now();
 
     private Long entityId = 78L;
 
@@ -41,6 +48,10 @@ class EntityRepositoryTest extends Web3IntegrationTest {
         entity.setMemo("entity");
         entity.setNum(entityId);
         entity.setDeclineReward(false);
+        entity.setRealm(0L);
+        entity.setShard(0L);
+        entity.setTimestampRange(Range.atLeast(DomainUtils.convertToNanosMax(now.getEpochSecond(), now.getNano()) + entityId));
+        entity.setType(ACCOUNT);
         return entity;
     }
 }
