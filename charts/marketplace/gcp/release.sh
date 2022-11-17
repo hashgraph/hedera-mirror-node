7e352742a57ff0405cbc1231bb4f57c93abd3891
mgoelswirlds <mugdha.goel@swirldslabs.com>
2022-11-17 12:41:26 -0500

    Adding amd64 platform flag.
    Signed-off-by: mgoelswirlds <mugdha.goel@swirldslabs.com>

--- charts/marketplace/gcp/release.sh
+++ charts/marketplace/gcp/release.sh
@@ -34,7 +34,7 @@ function retag() {
         target="${registry}"
     fi
 
-    docker pull "${source}"
+    docker pull "${source}" --platform linux/amd64
     docker tag "${source}" "${target}:${target_tag}"
     docker push "${target}:${target_tag}"
 
@@ -49,7 +49,7 @@ function retag() {
 sed -i .bak "s/version: .*/version: ${target_tag}/" values.yaml
 
 # Build Marketplace deployer image
-docker build -f ./Dockerfile -t "${registry}/deployer:${target_tag}" --build-arg TAG="${target_tag}" ../..
+docker build -f ./Dockerfile -t "${registry}/deployer:${target_tag}" --platform linux/amd64 --build-arg TAG="${target_tag}" ../..
 docker push "${registry}/deployer:${target_tag}"
 
 # Retag other images
