fcd83c67980a0130ae5c02b62f206bce0162c01f
Yanko Nikolov <38240027+nikolovyanko@users.noreply.github.com>
2022-11-18 17:41:02 +0200

    Add EntityAccess implementation (#4632)
    Add an implementation for EntityAccess interface defined currently here in miror-node project til it gets exported to the evm-module library.
    
    Signed-off-by: Yanko Nikolov <nikolovqnko@gmail.com>
    Signed-off-by: Mustafa Uzun <mustafa.uzun@limechain.tech>
    Co-authored-by: Mustafa Uzun <mustafa.uzun@limechain.tech>

--- hedera-mirror-web3/src/test/java/com/hedera/mirror/web3/Web3IntegrationTest.java
+++ hedera-mirror-web3/src/test/java/com/hedera/mirror/web3/Web3IntegrationTest.java
@@ -9,9 +9,9 @@ package com.hedera.mirror.web3;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,19 +20,27 @@ package com.hedera.mirror.web3;
  * ‍
  */
 
+import com.hedera.mirror.common.domain.DomainBuilder;
+import com.hedera.mirror.web3.config.IntegrationTestConfiguration;
+import javax.annotation.Resource;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.jdbc.Sql;
 
 @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:cleanup.sql")
 @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:cleanup.sql")
 @SpringBootTest
+@Import(IntegrationTestConfiguration.class)
 public abstract class Web3IntegrationTest {
 
     protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Resource
+    protected DomainBuilder domainBuilder;
 
     @BeforeEach
     void logTest(TestInfo testInfo) {
