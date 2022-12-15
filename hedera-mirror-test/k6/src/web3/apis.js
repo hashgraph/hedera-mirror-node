b26f3c2a997e40bdd94ef87dd9bd41eadacaf1c7
Xin Li <59580070+xin-hedera@users.noreply.github.com>
2022-12-15 16:40:28 -0600

    Write k6 test report.md to custom directory (#5053)
    Signed-off-by: Xin Li <xin.li@swirldslabs.com>

--- hedera-mirror-test/k6/src/web3/apis.js
+++ hedera-mirror-test/k6/src/web3/apis.js
@@ -21,13 +21,13 @@
 import exec from 'k6/execution';
 import {textSummary} from 'https://jslib.k6.io/k6-summary/0.0.2/index.js';
 
-import {markdownReport} from '../lib/common.js';
+import {getTestReportFilename, markdownReport} from '../lib/common.js';
 import {funcs, options, scenarioDurationGauge, scenarios} from './test/index.js';
 
 function handleSummary(data) {
   return {
     stdout: textSummary(data, {indent: ' ', enableColors: true}),
-    'report.md': markdownReport(data, false, scenarios),
+    [getTestReportFilename()]: markdownReport(data, false, scenarios),
   };
 }
 
