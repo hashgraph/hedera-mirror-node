6676d2e97a3f7490fc7f07b307d6fd1c318d6698
mgoelswirlds <106084778+mgoelswirlds@users.noreply.github.com>
2022-12-20 11:17:41 -0500

    Changing the type for expiry_timestamp back to a number  (#5083) (#5090)
    Description:
    
    Fixing accidental changes from the PR #4914
    Fixing a bug that the actual type in the response mismatches what's defined in our our openapi spec prior to 0.70.0.
    
    This PR:
    *Changes the type expiry_timestamp back to a number
    *Fixes the openapi spec to change the return type to a number for expiry_timestamp.
    
    Related issue(s):
    Fixes #5080
    
    Signed-off-by: mgoelswirlds <mugdha.goel@swirldslabs.com>

--- hedera-mirror-rest/accounts.js
+++ hedera-mirror-rest/accounts.js
@@ -60,11 +60,11 @@ const processRow = (row) => {
     deleted: row.deleted,
     ethereum_nonce: row.ethereum_nonce,
     evm_address: evmAddress,
-    expiry_timestamp: utils.calculateExpiryTimestamp(
+    expiry_timestamp: utils.nsToSecNs(utils.calculateExpiryTimestamp(
       row.auto_renew_period,
       row.created_timestamp,
       row.expiration_timestamp
-    ),
+    )),
     key: utils.encodeKey(row.key),
     max_automatic_token_associations: row.max_automatic_token_associations,
     memo: row.memo,
