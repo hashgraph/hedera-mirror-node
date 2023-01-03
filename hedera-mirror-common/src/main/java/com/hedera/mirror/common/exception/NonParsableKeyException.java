5f039cfcbba9d8e11b2c1a0d1fbaac2d1bd4dc3a
Jesse Nelson <jesse@swirldslabs.com>
2023-01-02 21:04:32 -0600

    added missing class
    Signed-off-by: Jesse Nelson <jesse@swirldslabs.com>

---
+++ hedera-mirror-common/src/main/java/com/hedera/mirror/common/exception/NonParsableKeyException.java
@@ -0,0 +1,8 @@
+package com.hedera.mirror.common.exception;
+
+public class NonParsableKeyException extends MirrorNodeException {
+
+    public NonParsableKeyException(Throwable throwable) {
+        super(throwable);
+    }
+}
