71b70823259536367b5fd92c0262861314776b34
Steven Sheehy <17552371+steven-sheehy@users.noreply.github.com>
2023-01-27 15:30:40 -0600

    Update copyright year in license to 2023 (#5284)
    Signed-off-by: Steven Sheehy <steven.sheehy@swirldslabs.com>
    Signed-off-by: Marc Kriguer <marc.kriguer@swirldslabs.com>

--- hedera-mirror-importer/src/test/java/com/hedera/mirror/importer/parser/record/entity/performance/EntityRecordItemListenerPerformanceCryptoTest.java
+++ hedera-mirror-importer/src/test/java/com/hedera/mirror/importer/parser/record/entity/performance/EntityRecordItemListenerPerformanceCryptoTest.java
@@ -4,7 +4,7 @@ package com.hedera.mirror.importer.parser.record.entity.performance;
  * ‌
  * Hedera Mirror Node
  * ​
- * Copyright (C) 2019 - 2022 Hedera Hashgraph, LLC
+ * Copyright (C) 2019 - 2023 Hedera Hashgraph, LLC
  * ​
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
