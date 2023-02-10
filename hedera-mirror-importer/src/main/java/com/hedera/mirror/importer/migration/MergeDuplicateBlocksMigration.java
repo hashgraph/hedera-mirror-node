2123b9b8a6f2a63876c57312bade339b7e066873
Steven Sheehy <17552371+steven-sheehy@users.noreply.github.com>
2023-02-09 18:14:25 -0600

    Workaround block number issue by merging blocks (0.73) (#5373)
    Workaround duplicate block issue by merging blocks (#5371)
    
    Signed-off-by: Steven Sheehy <steven.sheehy@swirldslabs.com>

---
+++ hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/migration/MergeDuplicateBlocksMigration.java
@@ -0,0 +1,90 @@
+package com.hedera.mirror.importer.migration;
+
+/*-
+ * ‌
+ * Hedera Mirror Node
+ * ​
+ * Copyright (C) 2019 - 2023 Hedera Hashgraph, LLC
+ * ​
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ‍
+ */
+
+import java.io.IOException;
+import java.util.Map;
+import javax.inject.Named;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+
+import com.hedera.mirror.importer.MirrorProperties;
+
+@Named
+class MergeDuplicateBlocksMigration extends RepeatableMigration {
+
+    private static final String SQL = """
+            with block1 as (
+              delete from record_file
+              where consensus_end = 1675962000231859003 and index = 44029066
+              returning *
+            ),
+            merged_block as (
+              update record_file block2 set
+              consensus_start = block1.consensus_start,
+              count = block1.count + block2.count,
+              gas_used = block1.gas_used + block2.gas_used,
+              load_start = block1.load_start,
+              name = block1.name,
+              prev_hash = block1.prev_hash,
+              sidecar_count = block1.sidecar_count + block2.sidecar_count,
+              size = block1.size + block2.size
+              from block1
+              where block2.consensus_end = 1675962001984524003 and block1.index = block2.index
+              returning block2.*
+            ),
+            transaction_index as (
+              select row_number() over (order by t.consensus_timestamp asc) - 1 as new_index, t.consensus_timestamp
+              from transaction t, merged_block b
+              where t.consensus_timestamp >= b.consensus_start and t.consensus_timestamp <= b.consensus_end
+              order by consensus_timestamp asc
+            )
+            update transaction t
+            set index = ti.new_index
+            from transaction_index ti
+            where t.consensus_timestamp = ti.consensus_timestamp;
+            """;
+
+    private final NamedParameterJdbcOperations jdbcOperations;
+    private final MirrorProperties mirrorProperties;
+
+    @Lazy
+    protected MergeDuplicateBlocksMigration(NamedParameterJdbcOperations jdbcOperations,
+                                            MirrorProperties mirrorProperties) {
+        super(mirrorProperties.getMigration());
+        this.jdbcOperations = jdbcOperations;
+        this.mirrorProperties = mirrorProperties;
+    }
+
+    @Override
+    protected void doMigrate() throws IOException {
+        if (mirrorProperties.getNetwork() != MirrorProperties.HederaNetwork.MAINNET) {
+            return;
+        }
+
+        jdbcOperations.update(SQL, Map.of());
+    }
+
+    @Override
+    public String getDescription() {
+        return "Fix duplicate block number issue on mainnet by merging them";
+    }
+}
---
+++ hedera-mirror-importer/src/test/java/com/hedera/mirror/importer/migration/MergeDuplicateBlocksMigrationTest.java
@@ -0,0 +1,134 @@
+package com.hedera.mirror.importer.migration;
+
+/*-
+ * ‌
+ * Hedera Mirror Node
+ * ​
+ * Copyright (C) 2019 - 2023 Hedera Hashgraph, LLC
+ * ​
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ‍
+ */
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+
+import com.hedera.mirror.common.domain.transaction.RecordFile;
+import com.hedera.mirror.common.domain.transaction.Transaction;
+import com.hedera.mirror.importer.IntegrationTest;
+import com.hedera.mirror.importer.MirrorProperties;
+import com.hedera.mirror.importer.repository.RecordFileRepository;
+import com.hedera.mirror.importer.repository.TransactionRepository;
+
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+class MergeDuplicateBlocksMigrationTest extends IntegrationTest {
+
+    private static final long NUMBER = 44029066L;
+    private static final long TIMESTAMP1 = 1675962000231859003L;
+    private static final long TIMESTAMP2 = 1675962001984524003L;
+
+    private final MergeDuplicateBlocksMigration migration;
+    private final MirrorProperties mirrorProperties;
+    private final RecordFileRepository recordFileRepository;
+    private final TransactionRepository transactionRepository;
+
+    private RecordFile block1;
+    private RecordFile block2;
+
+    @BeforeEach
+    void setupData() {
+        block1 = block(TIMESTAMP1);
+        block2 = block(TIMESTAMP2);
+
+        transaction(TIMESTAMP1 - 1, 0);
+        transaction(TIMESTAMP1, 1);
+        transaction(TIMESTAMP2 - 1, 0);
+        transaction(TIMESTAMP2, 1);
+    }
+
+    @Test
+    void notMainnet() throws Exception {
+        // Given
+        mirrorProperties.setNetwork(MirrorProperties.HederaNetwork.TESTNET);
+
+        // When
+        migration.doMigrate();
+
+        // Then
+        assertThat(recordFileRepository.findById(block1.getConsensusEnd())).get().isEqualTo(block1);
+        assertThat(recordFileRepository.findById(block2.getConsensusEnd())).get().isEqualTo(block2);
+    }
+
+    @Test
+    void mainnet() throws Exception {
+        // Given
+        mirrorProperties.setNetwork(MirrorProperties.HederaNetwork.MAINNET);
+
+        // When
+        migration.doMigrate();
+
+        // Then
+        assertThat(recordFileRepository.existsById(block1.getConsensusEnd())).isFalse();
+        assertThat(recordFileRepository.findById(block2.getConsensusEnd()))
+                .get()
+                .returns(block2.getBytes(), RecordFile::getBytes)
+                .returns(block2.getConsensusEnd(), RecordFile::getConsensusEnd)
+                .returns(block1.getConsensusStart(), RecordFile::getConsensusStart)
+                .returns(block1.getCount() + block2.getCount(), RecordFile::getCount)
+                .returns(block2.getDigestAlgorithm(), RecordFile::getDigestAlgorithm)
+                .returns(block2.getFileHash(), RecordFile::getFileHash)
+                .returns(block1.getGasUsed() + block2.getGasUsed(), RecordFile::getGasUsed)
+                .returns(block2.getHapiVersionMajor(), RecordFile::getHapiVersionMajor)
+                .returns(block2.getHapiVersionMinor(), RecordFile::getHapiVersionMinor)
+                .returns(block2.getHapiVersionPatch(), RecordFile::getHapiVersionPatch)
+                .returns(block2.getHash(), RecordFile::getHash)
+                .returns(NUMBER, RecordFile::getIndex)
+                .returns(block2.getLoadEnd(), RecordFile::getLoadEnd)
+                .returns(block1.getLoadStart(), RecordFile::getLoadStart)
+                .returns(block2.getLogsBloom(), RecordFile::getLogsBloom)
+                .returns(block1.getName(), RecordFile::getName)
+                .returns(block1.getPreviousHash(), RecordFile::getPreviousHash)
+                .returns(block1.getSidecarCount() + block2.getSidecarCount(), RecordFile::getSidecarCount)
+                .returns(block1.getSize() + block2.getSize(), RecordFile::getSize)
+                .returns(block2.getVersion(), RecordFile::getVersion);
+
+        assertThat(transactionRepository.findAll(Sort.by("consensusTimestamp").ascending()))
+                .hasSize(4)
+                .extracting(Transaction::getIndex)
+                .containsExactly(0, 1, 2, 3);
+    }
+
+    @Test
+    void idempotence() throws Exception {
+        mainnet();
+        mainnet();
+    }
+
+    private RecordFile block(long timestamp) {
+        return domainBuilder.recordFile()
+                .customize(r -> r.consensusEnd(timestamp).consensusStart(timestamp - 1).index(NUMBER)
+                        .hapiVersionMinor((int) domainBuilder.id()))
+                .persist();
+    }
+
+    private Transaction transaction(long timestamp, int index) {
+        return domainBuilder.transaction()
+                .customize(t -> t.consensusTimestamp(timestamp).index(index))
+                .persist();
+    }
+}
