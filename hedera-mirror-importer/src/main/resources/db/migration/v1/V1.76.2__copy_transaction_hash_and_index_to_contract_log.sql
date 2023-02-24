5cc1d95d2a315e0056df7394bf3c875747baebc0
Marc Kriguer <marc.kriguer@swirldslabs.com>
2023-02-24 15:59:31 -0800

    Combine V1.76.3 and V1.76.2 migrations into a single file.
    Signed-off-by: Marc Kriguer <marc.kriguer@swirldslabs.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v1/V1.76.2__copy_transaction_hash_and_index_to_contract_log.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/v1/V1.76.2__copy_transaction_hash_and_index_to_contract_log.sql
@@ -8,3 +8,9 @@ set transaction_hash   = cr.transaction_hash,
     transaction_index  = cr.transaction_index
 from contract_result cr
 where cl.consensus_timestamp = cr.consensus_timestamp;
+
+-- contract_result's contract_id column should no longer be nullable
+update contract_result set contract_id = 0 where contract_id is null;
+
+alter table contract_result
+    alter column contract_id set not null;
--- hedera-mirror-importer/src/main/resources/db/migration/v1/V1.76.3__contract_result_contract_id_not_null.sql
+++
@@ -1,5 +0,0 @@
---- contract_result's contract_id should no longer be nullable
-update contract_result set contract_id = 0 where contract_id is null;
-
-alter table contract_result
-    alter column contract_id set not null;
