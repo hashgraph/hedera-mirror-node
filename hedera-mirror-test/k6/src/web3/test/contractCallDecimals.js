70ba63ca02879755667b625bee272afceef4a596
Edwin Greene <edwin.greene@swirldslabs.com>
2023-03-06 12:46:35 -0500

    Fix tests
    Signed-off-by: Edwin Greene <edwin.greene@swirldslabs.com>

--- hedera-mirror-test/k6/src/web3/test/contractCallDecimals.js
+++ hedera-mirror-test/k6/src/web3/test/contractCallDecimals.js
@@ -1,11 +1,6 @@
-package com.hedera.mirror.importer.domain;
-
-/*-
- * ‌
- * Hedera Mirror Node
- * ​
- * Copyright (C) 2019 - 2023 Hedera Hashgraph, LLC
- * ​
+/*
+ * Copyright (C) 2019-2023 Hedera Hashgraph, LLC
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,10 +12,19 @@ package com.hedera.mirror.importer.domain;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ‍
  */
 
-public enum AliasNotFoundAction {
-    ERROR,
-    FALLTHROUGH,
-}
+import {ContractCallTestScenarioBuilder} from './common.js';
+
+const contract = __ENV.ERC_CONTRACT_ADDRESS;
+const selector = '0xd449a832';
+const token = __ENV.TOKEN_ADDRESS;
+
+const {options, run} = new ContractCallTestScenarioBuilder()
+  .name('contractCallDecimals') // use unique scenario name among all tests
+  .selector(selector)
+  .args([token])
+  .to(contract)
+  .build();
+
+export {options, run};
