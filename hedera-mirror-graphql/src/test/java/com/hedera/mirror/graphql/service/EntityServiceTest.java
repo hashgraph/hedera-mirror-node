1e847f02e29b0be632a13df6b2aa4e603907cb6e
Syed Mehdi <117643207+syed-bcw@users.noreply.github.com>
2023-03-17 00:27:14 +0500

    Add search for account by alias or EVM address to GraphQL API (#5553)
    * Added support for account(id: {alias}) and account(id: {evmAddress}) lookup. Executes the same entity lookup as /api/v1/accounts/{idOrAliasOrEvmAddress} does.
    * Added unit/integration tests for the above
    * Added a query to postman
    
    Signed-off-by: Syed Mehdi <syed@bcw.group>
    Signed-off-by: Marc Kriguer <marc.kriguer@swirldslabs.com>

--- hedera-mirror-graphql/src/test/java/com/hedera/mirror/graphql/service/EntityServiceTest.java
+++ hedera-mirror-graphql/src/test/java/com/hedera/mirror/graphql/service/EntityServiceTest.java
@@ -23,7 +23,13 @@ package com.hedera.mirror.graphql.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
+import java.nio.ByteBuffer;
 import java.util.Optional;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Base32;
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -38,6 +44,8 @@ import com.hedera.mirror.graphql.repository.EntityRepository;
 class EntityServiceTest {
 
     private final DomainBuilder domainBuilder = new DomainBuilder();
+    private final Base32 base32 = new Base32();
+    private final int EVM_ADDRESS_BYTE_LENGTH = 20;
 
     @Mock
     private EntityRepository entityRepository;
@@ -52,6 +60,28 @@ class EntityServiceTest {
     }
 
     @Test
+    void getByAliasAndTypeMissing() {
+        var entity = domainBuilder.entity().get();
+        assertThat(entityService.getByAliasAndType(base32.encodeAsString(entity.getAlias()), entity.getType())).isEmpty();
+    }
+
+    @Test
+    void getByEvmAddressAndTypeMissing() {
+        var entity = domainBuilder.entity().get();
+        assertThat(entityService.getByEvmAddressAndType(Hex.encodeHexString(entity.getEvmAddress()), entity.getType()))
+                .isEmpty();
+    }
+
+    @Test
+    void getByIdAsEvmAddressAndTypeMissing() {
+        var entity = domainBuilder.entity().get();
+        ByteBuffer evmBuffer = ByteBuffer.allocate(EVM_ADDRESS_BYTE_LENGTH);
+        evmBuffer.putLong(EVM_ADDRESS_BYTE_LENGTH - Long.BYTES, entity.getId());
+        assertThat(entityService.getByEvmAddressAndType(Hex.encodeHexString(evmBuffer), entity.getType()))
+                .isEmpty();
+    }
+
+    @Test
     void getByIdAndTypeMismatch() {
         var entity = domainBuilder.entity().get();
         when(entityRepository.findById(entity.getId())).thenReturn(Optional.of(entity));
@@ -59,9 +89,79 @@ class EntityServiceTest {
     }
 
     @Test
+    void getByAliasAndTypeMismatch() {
+        var entity = domainBuilder.entity().get();
+        when(entityRepository.findByAlias(entity.getAlias())).thenReturn(Optional.of(entity));
+        assertThat(entityService.getByAliasAndType(base32.encodeAsString(entity.getAlias()), EntityType.CONTRACT)).isEmpty();
+    }
+
+    @Test
+    void getByEvmAddressAndTypeMismatch() {
+        var entity = domainBuilder.entity().get();
+        when(entityRepository.findByEvmAddress(entity.getEvmAddress())).thenReturn(Optional.of(entity));
+        assertThat(entityService.getByEvmAddressAndType(Hex.encodeHexString(entity.getEvmAddress()),
+                EntityType.CONTRACT)).isEmpty();
+    }
+
+    @Test
+    void getByIdAsEvmAddressAndTypeMismatch() {
+
+        var entity = domainBuilder.entity().get();
+        ByteBuffer evmBuffer = ByteBuffer.allocate(EVM_ADDRESS_BYTE_LENGTH);
+        evmBuffer.putLong(EVM_ADDRESS_BYTE_LENGTH - Long.BYTES, entity.getId());
+        when(entityRepository.findById(entity.getId())).thenReturn(Optional.of(entity));
+        assertThat(entityService.getByEvmAddressAndType(Hex.encodeHexString(evmBuffer),
+                EntityType.CONTRACT)).isEmpty();
+    }
+
+    @Test
     void getByIdAndTypeFound() {
         var entity = domainBuilder.entity().get();
         when(entityRepository.findById(entity.getId())).thenReturn(Optional.of(entity));
         assertThat(entityService.getByIdAndType(entity.toEntityId(), entity.getType())).get().isEqualTo(entity);
+    }
+
+    @Test
+    void getByAliasAndTypeFound() {
+        var entity = domainBuilder.entity().get();
+        when(entityRepository.findByAlias(entity.getAlias())).thenReturn(Optional.of(entity));
+        assertThat(entityService.getByAliasAndType(base32.encodeAsString(entity.getAlias()), entity.getType())).get()
+                .isEqualTo(entity);
+    }
+
+    @Test
+    void getByEvmAddressAndTypeFound() {
+        var entity = domainBuilder.entity().get();
+        when(entityRepository.findByEvmAddress(entity.getEvmAddress())).thenReturn(Optional.of(entity));
+        assertThat(entityService.getByEvmAddressAndType(Hex.encodeHexString(entity.getEvmAddress()),
+                entity.getType())).get()
+                .isEqualTo(entity);
+    }
+
+    @Test
+    void getByEvmAddressLookAlikeEntityAndTypeFound() {
+        var entity = domainBuilder.entity().get();
+        int integerStringLength = Integer.BYTES * 2;
+        String padZero = StringUtils.repeat('0', integerStringLength);
+        String evmAddress = padZero + Hex.encodeHexString(entity.getEvmAddress()).substring(integerStringLength);
+        try {
+            when(entityRepository.findByEvmAddress(Hex.decodeHex(evmAddress))).thenReturn(Optional.of(entity));
+        } catch (DecoderException e) {
+            throw new RuntimeException(e);
+        }
+        assertThat(entityService.getByEvmAddressAndType(evmAddress,
+                entity.getType())).get()
+                .isEqualTo(entity);
+    }
+
+    @Test
+    void getByIdAsEvmAddressAndTypeFound() {
+        var entity = domainBuilder.entity().get();
+        ByteBuffer evmBuffer = ByteBuffer.allocate(EVM_ADDRESS_BYTE_LENGTH);
+        evmBuffer.putLong(EVM_ADDRESS_BYTE_LENGTH - Long.BYTES, entity.getId());
+        when(entityRepository.findById(entity.getId())).thenReturn(Optional.of(entity));
+        assertThat(entityService.getByEvmAddressAndType(Hex.encodeHexString(evmBuffer),
+                entity.getType())).get()
+                .isEqualTo(entity);
     }
 }
