5eb2129dfc8aaf9eb5b5107cb1c63cdc11b3e3ee
Mustafa Uzun <mustafa.uzun@limechain.tech>
2023-03-17 13:50:24 +0200

    test: add unit tests
    Signed-off-by: Mustafa Uzun <mustafa.uzun@limechain.tech>

--- hedera-mirror-web3/src/test/java/com/hedera/mirror/web3/evm/account/MirrorEvmContractAliasesTest.java
+++ hedera-mirror-web3/src/test/java/com/hedera/mirror/web3/evm/account/MirrorEvmContractAliasesTest.java
@@ -24,6 +24,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
+import com.hedera.mirror.common.domain.entity.EntityId;
 import com.hedera.mirror.common.domain.entity.EntityType;
 import com.hedera.mirror.web3.exception.InvalidParametersException;
 
@@ -44,8 +45,11 @@ class MirrorEvmContractAliasesTest {
     private static final String HEX = "0x00000000000000000000000000000000000004e4";
     private static final Address ADDRESS = Address.fromHexString(HEX);
     private static final String HEX2 = "0x00000000000000000000000000000000000004e5";
+    private static final String HEX3 = "0x0000000000000000000000000000000000000003";
     private static final Address ADDRESS2 = Address.fromHexString(HEX2);
+    private static final Address ADDRESS3 = Address.fromHexString(HEX3);
     private static final String INVALID_HEX_ADDRESS = "0x000000000000000000000000004e5";
+    private static final EntityId entityId = new EntityId(0L, 0L, 3L, EntityType.TOKEN);
     private static final Address INVALID_ADDRESS = Address.fromHexString(INVALID_HEX_ADDRESS);
 
     @Mock
@@ -67,6 +71,24 @@ class MirrorEvmContractAliasesTest {
         when(entity.getEvmAddress()).thenReturn(ADDRESS2.toArray());
         final var result = mirrorEvmContractAliases.resolveForEvm(ADDRESS);
         assertThat(result).isEqualTo(ADDRESS2);
+    }
+
+    @Test
+    void resolveForEvmTokenSuccess() {
+        when(mirrorEntityAccess.findEntity(ADDRESS)).thenReturn(Optional.of(entity));
+        when(entity.getType()).thenReturn(EntityType.TOKEN);
+        when(entity.toEntityId()).thenReturn(entityId);
+        final var result = mirrorEvmContractAliases.resolveForEvm(ADDRESS);
+        assertThat(result).isEqualTo(ADDRESS3);
+    }
+
+    @Test
+    void resolveForEvmFailDifferentType() {
+        when(mirrorEntityAccess.findEntity(ADDRESS)).thenReturn(Optional.of(entity));
+        when(entity.getType()).thenReturn(EntityType.TOPIC);
+        assertThatThrownBy(() -> mirrorEvmContractAliases.resolveForEvm(ADDRESS))
+                .isInstanceOf(InvalidParametersException.class)
+                .hasMessage("No such contract or token");
     }
 
     @Test
