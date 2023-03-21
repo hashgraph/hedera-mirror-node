f308ae4b3e4d5b5a369df3ac71f3dc44e10607c6
Steven Sheehy <17552371+steven-sheehy@users.noreply.github.com>
2023-03-21 11:27:00 -0500

    Bump Spring Boot from 2.7.6 to 2.7.9 (#5631)
    * Bump Spring Boot from 2.7.6 to 2.7.9
    * Bump various container images
    * Fix all code smells reported on SonarCloud
    
    Signed-off-by: Steven Sheehy <steven.sheehy@swirldslabs.com>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
     implementation("org.openapitools:openapi-generator-gradle-plugin:6.4.0")
     implementation("org.owasp:dependency-check-gradle:8.1.2")
     implementation("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:4.0.0.2929")
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.7.6")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.7.9")
 }
 
 val gitHook = tasks.register<Exec>("gitHook") {
