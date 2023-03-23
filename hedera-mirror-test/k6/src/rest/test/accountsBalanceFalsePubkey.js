ac5ab94376f3ca9c2e5a88b7ba140491759e188d
Xin Li <xin@swirldslabs.com>
2023-03-23 10:24:51 -0500

    a lot of changes
    Signed-off-by: Xin Li <xin@swirldslabs.com>

--- hedera-mirror-test/k6/src/rest/test/accountsBalanceFalsePubkey.js
+++ hedera-mirror-test/k6/src/rest/test/accountsBalanceFalsePubkey.js
@@ -18,25 +18,22 @@
  * ‍
  */
 
-import http from "k6/http";
+import http from 'k6/http';
 
-import {TestScenarioBuilder} from '../../lib/common.js';
-import {accountListName, urlPrefix} from '../../lib/constants.js';
-import {isValidListResponse} from "./common.js";
-import {setupTestParameters} from "./bootstrapEnvParameters.js";
+import {isValidListResponse, RestTestScenarioBuilder} from '../libex/common.js';
+import {accountListName} from '../libex/constants.js';
 
 const urlTag = '/accounts?balance=false&account.publickey={publicKey}';
 
-const {options, run} = new TestScenarioBuilder()
+const {options, run, setup} = new RestTestScenarioBuilder()
   .name('accountsBalanceFalsePubkey') // use unique scenario name among all tests
   .tags({url: urlTag})
   .request((testParameters) => {
-    const url = `${testParameters['BASE_URL']}${urlPrefix}/accounts?balance=false&account.publickey=${testParameters['DEFAULT_PUBLIC_KEY']}`;
+    const url = `${testParameters['BASE_URL_PREFIX']}/accounts?balance=false&account.publickey=${testParameters['DEFAULT_PUBLIC_KEY']}`;
     return http.get(url);
   })
+  .requiredParameters('DEFAULT_PUBLIC_KEY')
   .check('Accounts balance false with publickey OK', (r) => isValidListResponse(r, accountListName))
   .build();
 
-export {options, run};
-
-export const setup = setupTestParameters;
+export {options, run, setup};
