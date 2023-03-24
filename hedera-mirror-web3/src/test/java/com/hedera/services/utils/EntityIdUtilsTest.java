9d8d684193c43693635176d6622ba59c19ef8b49
Ivan <ivanivanov.ii726@gmail.com>
2023-03-24 08:46:25 +0200

    sonar: remove conflicting annotations

--- hedera-mirror-web3/src/test/java/com/hedera/services/utils/EntityIdUtilsTest.java
+++ hedera-mirror-web3/src/test/java/com/hedera/services/utils/EntityIdUtilsTest.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.hedera.services.utils;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,23 +22,10 @@ import com.hederahashgraph.api.proto.java.ContractID;
 import com.hederahashgraph.api.proto.java.TokenID;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class EntityIdUtilsTest {
-
-
-    @ParameterizedTest
-    @CsvSource({
-        "0,Cannot parse '0' due to only 0 dots",
-        "0.a.0,Argument 'literal=0.a.0' is not an account",
-        "...,Argument 'literal=...' is not an account",
-        "1.2.3.4,Argument 'literal=1.2.3.4' is not an account",
-        "1.2.three,Argument 'literal=1.2.three' is not an account",
-        "1.2.333333333333333333333,Cannot parse '1.2.333333333333333333333' due to overflow"
-    })
 
     @Test
     void asSolidityAddressBytesWorksProperly() {
