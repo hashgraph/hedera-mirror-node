aa0bcc0a72a73cfcdf5d43cc0508305ae5afd26e
Ivan Ivanov <ivanivanov.ii726@gmail.com>
2023-03-28 10:31:17 +0300

    refactor: add licenses
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>

--- hedera-mirror-web3/src/test/java/com/hedera/services/store/contracts/precompile/FungibleTokenTransferTest.java
+++ hedera-mirror-web3/src/test/java/com/hedera/services/store/contracts/precompile/FungibleTokenTransferTest.java
@@ -1,19 +1,33 @@
+/*
+ * Copyright (C) 2022-2023 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hedera.services.store.contracts.precompile;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.hederahashgraph.api.proto.java.AccountID;
 import com.hederahashgraph.api.proto.java.TokenID;
-import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 
 class FungibleTokenTransferTest {
 
-     static final long secondAmount = 200;
-     static final AccountID a = asAccount("0.0.2");
-     static final AccountID b = asAccount("0.0.3");
-     static final TokenID fungible = asToken("0.0.555");
+    static final long secondAmount = 200;
+    static final AccountID a = asAccount("0.0.2");
+    static final AccountID b = asAccount("0.0.3");
+    static final TokenID fungible = asToken("0.0.555");
     static final TokenID nonFungible = asToken("0.0.666");
 
     @Test
@@ -39,7 +53,6 @@ class FungibleTokenTransferTest {
                 .setTokenNum(nativeParts[2])
                 .build();
     }
-
 
     //copied from IdUtils
     static long[] asDotDelimitedLongArray(String s) {
