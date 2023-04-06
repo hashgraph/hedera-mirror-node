1242e47e54bab112531116a3613f1528d5c7e5dd
Mustafa Uzun <mustafa.uzun@limechain.tech>
2023-04-06 15:07:51 +0300

    test: add tests
    Signed-off-by: Mustafa Uzun <mustafa.uzun@limechain.tech>

--- hedera-mirror-web3/src/test/java/com/hedera/mirror/web3/repository/CustomFeeRepositoryTest.java
+++ hedera-mirror-web3/src/test/java/com/hedera/mirror/web3/repository/CustomFeeRepositoryTest.java
@@ -22,6 +22,7 @@ package com.hedera.mirror.web3.repository;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,12 +35,38 @@ import com.hedera.mirror.web3.Web3IntegrationTest;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 class CustomFeeRepositoryTest extends Web3IntegrationTest {
     private final CustomFeeRepository customFeeRepository;
+    private final CustomFeeRepositoryImpl customFeeRepositoryImpl;
+
+    @Test
+    void findByTokenIdRepositoryImpl() {
+        final var customFee = persistDifferentFeesWithSameId();
+        final var tokenId = customFee.get(0).getId().getTokenId().getId();
+
+        assertThat(customFeeRepositoryImpl.findByTokenId(tokenId).get(0).getAmount()).isEqualTo(customFee.get(0).getAmount());
+        assertThat(customFeeRepositoryImpl.findByTokenId(tokenId).get(1).getAmountDenominator()).isEqualTo(customFee.get(1).getAmountDenominator());
+    }
 
     @Test
     void findByTokenId() {
         final var customFee = persistMultipleFees();
         final var tokenId = customFee.getId() != null ? customFee.getId().getTokenId().getId() : 0L;
         assertThat(customFeeRepository.findByTokenId(tokenId).get(0)).isEqualTo(customFee);
+    }
+
+    private List<CustomFee> persistDifferentFeesWithSameId() {
+        var rightEntityId = EntityId.of(0, 0, 12, EntityType.TOKEN);
+        var timestamp = System.currentTimeMillis();
+
+        var fee1 = domainBuilder.customFee().customize(fee -> fee
+                        .id(new CustomFee.Id(timestamp, rightEntityId))
+                        .amount((long) 1).amountDenominator(100L))
+                .persist();
+        var fee2 = domainBuilder.customFee().customize(fee -> fee
+                        .id(new CustomFee.Id(timestamp, rightEntityId))
+                        .amount((long) 2).amountDenominator(200L))
+                .persist();
+
+        return List.of(fee1, fee2);
     }
 
     private CustomFee persistMultipleFees() {
