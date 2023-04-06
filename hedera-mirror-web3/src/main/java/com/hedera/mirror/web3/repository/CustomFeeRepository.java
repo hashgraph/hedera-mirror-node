8a0cf97e0277c22c60b5456f55cd46ef74aa3e4c
Mustafa Uzun <mustafa.uzun@limechain.tech>
2023-04-06 14:21:21 +0300

    refactor: PR comments
    Signed-off-by: Mustafa Uzun <mustafa.uzun@limechain.tech>

--- hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/repository/CustomFeeRepository.java
+++ hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/repository/CustomFeeRepository.java
@@ -30,7 +30,7 @@ import org.springframework.data.repository.CrudRepository;
 import com.hedera.mirror.common.domain.transaction.CustomFee;
 import com.hedera.mirror.common.domain.transaction.CustomFee.Id;
 
-public interface CustomFeeRepository extends CrudRepository<CustomFee, Id> {
+public interface CustomFeeRepository extends CrudRepository<CustomFee, Id>, CustomFeeRepositoryExtra {
 
     @Query(value = "select * from custom_fee where token_id = ?1 and created_timestamp = (select created_timestamp from custom_fee where token_id = ?1 order by created_timestamp desc limit 1)",
             nativeQuery = true)
