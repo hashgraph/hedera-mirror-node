764e79a3a478d4559ad13624d7ef36c76a3928fa
David Bakin <117694041+david-bakin-sl@users.noreply.github.com>
2023-04-11 18:18:58 -0700

    `DatabaseBackedStateFrame` with accessor interface `GroundTruthAccessor` for stacked state frames
    Signed-off-by: David Bakin <117694041+david-bakin-sl@users.noreply.github.com>

---
+++ hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/evm/store/DatabaseBackedStateFrame.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2023 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hedera.mirror.web3.evm.store;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import lombok.NonNull;
+
+/** A CachingStateFrame that answers reads by getting entities from some other source - a database! - and
+ * disallows all local updates/deletes. */
+@SuppressWarnings(
+        "java:S1192") // "define a constant instead of duplicating this literal" - worse readability if applied to small
+// literals
+public class DatabaseBackedStateFrame<K> extends CachingStateFrame<K> {
+
+    @NonNull
+    final Map<Class<?>, GroundTruthAccessor<K, ?>> databaseAccessors;
+
+    public DatabaseBackedStateFrame(
+            @NonNull final List<GroundTruthAccessor<K, ?>> accessors, @NonNull final Class<?>[] valueClasses) {
+        super(
+                Optional.empty(),
+                valueClasses); // superclass of this frame will create/hold useless UpdatableReferenceCaches
+
+        databaseAccessors = accessors.stream().collect(Collectors.toMap(GroundTruthAccessor::getVClass, a -> a));
+    }
+
+    @Override
+    @NonNull
+    public Optional<Object> getValue(
+            @NonNull final Class<?> klass, @NonNull final UpdatableReferenceCache<K> cache, @NonNull final K key) {
+        return databaseAccessors.get(klass).get(key).flatMap(o -> Optional.of(klass.cast(o)));
+    }
+
+    @Override
+    public void setValue(
+            @NonNull final Class<?> klass,
+            @NonNull final UpdatableReferenceCache<K> cache,
+            @NonNull final K key,
+            @NonNull final Object value) {
+        throw new UnsupportedOperationException("Cannot add/update a value in a database-backed StateFrame");
+    }
+
+    @Override
+    public void deleteValue(
+            @NonNull final Class<?> klass, @NonNull final UpdatableReferenceCache<K> cache, @NonNull final K key) {
+        throw new UnsupportedOperationException("Cannot delete a value in a database-backed StateFrame");
+    }
+
+    @Override
+    public void updatesFromDownstream(@NonNull final CachingStateFrame<K> childFrame) {
+        throw new UnsupportedOperationException("Cannot commit to a database-backed StateFrame (oddly enough)");
+    }
+
+    @Override
+    public void commit() {
+        throw new UnsupportedOperationException("Cannot commit to a database-backed StateFrame (oddly enough)");
+    }
+}
---
+++ hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/evm/store/GroundTruthAccessor.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2023 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hedera.mirror.web3.evm.store;
+
+import java.util.Optional;
+import lombok.NonNull;
+
+/** Interface for a database accessor to get some domain type V with primary key K from the database */
+public interface GroundTruthAccessor<K, V> {
+    @NonNull
+    Class<K> getKClass();
+
+    @NonNull
+    Class<V> getVClass();
+
+    // Given address return an account record from the DB
+    @NonNull
+    Optional<V> get(@NonNull final K key);
+}
