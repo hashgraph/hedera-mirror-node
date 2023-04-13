727e73bfe9fdbdd2b1faa4752071afe9d4779670
Mustafa Uzun <mustafa.uzun@limechain.tech>
2023-04-13 12:06:16 +0300

    feat: add database accessors
    Signed-off-by: Mustafa Uzun <mustafa.uzun@limechain.tech>

---
+++ hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/evm/store/TokenDatabaseAccessor.java
@@ -0,0 +1,39 @@
+package com.hedera.mirror.web3.evm.store;
+
+/*
+ * Copyright (C) 2023 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.hedera.mirror.common.domain.token.Token;
+import com.hedera.mirror.common.domain.token.TokenId;
+
+import com.hedera.mirror.web3.repository.TokenRepository;
+
+import java.util.Optional;
+import javax.inject.Named;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Named
+@RequiredArgsConstructor
+public class TokenDatabaseAccessor extends DatabaseAccessor<TokenId, Token>{
+
+    private final TokenRepository tokenRepository;
+
+    @Override
+    public @NonNull Optional<Token> get(@NonNull TokenId key) {
+        return tokenRepository.findById(key);
+    }
+}
