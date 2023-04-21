e54054e186c70811e5fc73739be7c3e8b1186bb3
David Bakin <117694041+david-bakin-sl@users.noreply.github.com>
2023-04-21 09:20:04 -0700

    CR comments addressed
    Signed-off-by: David Bakin <117694041+david-bakin-sl@users.noreply.github.com>

--- hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/evm/exception/EvmException.java
+++ hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/evm/exception/EvmException.java
@@ -1,11 +1,6 @@
-package com.hedera.mirror.web3.evm.exception;
-
-/*-
- * ‌
- * Hedera Mirror Node
- * ​
- * Copyright (C) 2019 - 2023 Hedera Hashgraph, LLC
- * ​
+/*
+ * Copyright (C) 2023 Hedera Hashgraph, LLC
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,12 +12,12 @@ package com.hedera.mirror.web3.evm.exception;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ‍
  */
 
-import java.io.Serial;
+package com.hedera.mirror.web3.evm.exception;
 
 import com.hedera.mirror.common.exception.MirrorNodeException;
+import java.io.Serial;
 
 @SuppressWarnings("java:S110")
 public abstract class EvmException extends MirrorNodeException {
@@ -32,5 +27,9 @@ public abstract class EvmException extends MirrorNodeException {
 
     protected EvmException(String message) {
         super(message);
+    }
+
+    protected EvmException(String message, Throwable throwable) {
+        super(message, throwable);
     }
 }
