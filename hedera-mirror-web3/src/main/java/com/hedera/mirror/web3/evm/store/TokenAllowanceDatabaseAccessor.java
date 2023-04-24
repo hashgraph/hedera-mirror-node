cef1c05f6416663e6b87096c00d996a0b1a3b94c
Mustafa Uzun <mustafa.uzun@limechain.tech>
2023-04-24 08:56:48 +0300

    feat: add more database accessors
    Signed-off-by: Mustafa Uzun <mustafa.uzun@limechain.tech>

---
+++ hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/evm/store/TokenAllowanceDatabaseAccessor.java
@@ -0,0 +1,43 @@
+package com.hedera.mirror.web3.evm.store;
+
+/*-
+ * ‌
+ * Hedera Mirror Node
+ * ​
+ * Copyright (C) 2019 - 2023 Hedera Hashgraph, LLC
+ * ​
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ‍
+ */
+
+import com.hedera.mirror.common.domain.entity.TokenAllowance;
+import com.hedera.mirror.common.domain.entity.AbstractTokenAllowance.Id;
+
+import com.hedera.mirror.web3.repository.TokenAllowanceRepository;
+
+import java.util.Optional;
+import javax.inject.Named;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Named
+@RequiredArgsConstructor
+public class TokenAllowanceDatabaseAccessor extends DatabaseAccessor<Id, TokenAllowance> {
+
+    private final TokenAllowanceRepository tokenAllowanceRepository;
+
+    @Override
+    public @NonNull Optional<TokenAllowance> get(@NonNull Id key) {
+        return tokenAllowanceRepository.findById(key);
+    }
+}
