b423b23e85e1556ce77f1783178c47ebb8c0876c
Mustafa Uzun <mustafa.uzun@limechain.tech>
2023-04-28 01:27:04 +0300

    HIP-584: DatabaseAccessor implementations for StackedStateFrame (#5910)
    DatabaseAccessor implementations for StackedStateFrame. This is where you go to the database for requested entities
    
    Signed-off-by: Mustafa Uzun <mustafa.uzun@limechain.tech>

---
+++ hedera-mirror-web3/src/test/java/com/hedera/mirror/web3/evm/store/accessor/NftAllowanceDatabaseAccessorTest.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2023 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hedera.mirror.web3.evm.store.accessor;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import com.hedera.mirror.web3.Web3IntegrationTest;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+class NftAllowanceDatabaseAccessorTest extends Web3IntegrationTest {
+
+    private final NftAllowanceDatabaseAccessor nftAllowanceDatabaseAccessor;
+
+    @Test
+    void testGet() {
+        final var allowance = domainBuilder.nftAllowance().persist();
+
+        assertThat(nftAllowanceDatabaseAccessor.get(allowance.getId())).contains(allowance);
+    }
+}
