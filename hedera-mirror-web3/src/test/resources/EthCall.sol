848c2e962095bb6c106138cefe3dc6c585110495
IvanKavaldzhiev <ivankavaldzhiev@gmail.com>
2023-04-28 17:01:33 +0300

    nit: increase code coverage
    Signed-off-by: IvanKavaldzhiev <ivankavaldzhiev@gmail.com>

--- hedera-mirror-web3/src/test/resources/EthCall.sol
+++ hedera-mirror-web3/src/test/resources/EthCall.sol
@@ -2,13 +2,14 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "./HederaTokenService.sol";
 
-contract EthCall {
+contract EthCall is HederaTokenService {
 
     string constant storageData  = "test";
     string emptyStorageData = "";
 
-    // Pure function without arguments that multiplies two numbers (e.g. return 2*2)
+    // Public pure function without arguments that multiplies two numbers (e.g. return 2*2)
     function multiplySimpleNumbers() public pure returns (uint) {
         return 2 * 2;
     }
@@ -24,24 +25,29 @@ contract EthCall {
         return emptyStorageData;
     }
 
-    // External function that retrieves the hbar balance of a given account
+    // External view function that retrieves the hbar balance of a given account
     function getAccountBalance(address _owner) external view returns (uint) {
         return _owner.balance;
     }
 
-    // External function that returns a storage field as a function result
+    // External view function that returns a storage field as a function result
     function returnStorageData(string memory s) external view returns (string memory) {
         return storageData;
     }
 
-    // External function that has an argument for a token address and using open zeppelin IERC20 interface as a wrapper, returns the token’s name
+    // External view function that has an argument for a token address and using open zeppelin IERC20 interface as a wrapper, returns the token’s name
     function getTokenName(address _tokenAddress) external view returns (string memory) {
         return IERC20Metadata(_tokenAddress).name();
     }
 
-    // External function that has an argument for a token address and using open zeppelin IERC20 interface as a wrapper, returns the token’s symbol
+    // External view function that has an argument for a token address and using open zeppelin IERC20 interface as a wrapper, returns the token’s symbol
     function getTokenSymbol(address _tokenAddress) external view returns (string memory) {
         return IERC20Metadata(_tokenAddress).symbol();
+    }
+
+    // External function that freezes a given token for the message sender
+    function freezeToken(address _tokenAddress) external {
+        HederaTokenService.freezeToken(_tokenAddress, msg.sender);
     }
 
     function testRevert() external pure {
