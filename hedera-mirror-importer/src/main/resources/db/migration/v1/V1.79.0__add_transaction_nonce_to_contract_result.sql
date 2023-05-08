06c941f844e1353d925a2b8e06570b2567e7d3bb
Marc Kriguer <marc.kriguer@swirldslabs.com>
2023-05-08 16:45:07 -0700

    Address all feedback from this morning.
    Signed-off-by: Marc Kriguer <marc.kriguer@swirldslabs.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v1/V1.79.0__add_transaction_nonce_to_contract_result.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/v1/V1.79.0__add_transaction_nonce_to_contract_result.sql
@@ -1,6 +1,6 @@
 -- copy nonce from transaction to contract_result
 alter table if exists contract_result
-    add column if not exists transaction_nonce integer not null;
+    add column if not exists transaction_nonce integer default 0 not null;
 
 update contract_result cr
 set transaction_nonce = t.nonce
