c6afc10aba62d0645812a54a52e67eeb9ca95007
Mustafa Uzun <mustafa.uzun@limechain.tech>
2023-05-09 12:40:37 +0300

    feat: add more log info
    Signed-off-by: Mustafa Uzun <mustafa.uzun@limechain.tech>

--- hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/evm/contracts/execution/traceability/MirrorOperationTracer.java
+++ hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/evm/contracts/execution/traceability/MirrorOperationTracer.java
@@ -1,15 +1,33 @@
+/*
+ * Copyright (C) 2023 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.hedera.mirror.web3.evm.contracts.execution.traceability;
+
+import static com.hedera.mirror.web3.evm.utils.EvmTokenUtils.entityIdFromEvmAddress;
 
 import com.hedera.mirror.web3.evm.properties.TracingProperties;
 import com.hedera.node.app.service.evm.contracts.execution.traceability.HederaEvmOperationTracer;
+import java.nio.charset.StandardCharsets;
+import javax.inject.Named;
 import lombok.CustomLog;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.tuweni.bytes.Bytes;
 import org.hyperledger.besu.evm.frame.MessageFrame;
 import org.hyperledger.besu.evm.operation.Operation;
-
-import javax.inject.Named;
-
-import static com.hedera.mirror.web3.evm.utils.EvmTokenUtils.entityIdFromEvmAddress;
 
 @CustomLog
 @Named
@@ -20,23 +38,37 @@ public class MirrorOperationTracer implements HederaEvmOperationTracer {
 
     @Override
     public void tracePostExecution(final MessageFrame currentFrame, final Operation.OperationResult operationResult) {
-        if(tracingProperties.isEnabled()) {
-            if(!tracingProperties.getStatus().isEmpty() && !tracingProperties.getStatus().contains(currentFrame.getState().name())) {
+        if (tracingProperties.isEnabled()) {
+            if (!tracingProperties.getStatus().isEmpty()
+                    && !tracingProperties
+                            .getStatus()
+                            .contains(currentFrame.getState().name())) {
                 return;
             }
 
             final var recipientAddress = currentFrame.getRecipientAddress();
             final var recipientNum = entityIdFromEvmAddress(recipientAddress);
 
-            if(!tracingProperties.getContract().isEmpty() && !tracingProperties.getContract().contains(recipientNum)) {
+            if (!tracingProperties.getContract().isEmpty()
+                    && !tracingProperties.getContract().contains(recipientNum)) {
                 return;
             }
 
-            //TODO FINISH FULL LOG
-            final var inputData = new String(currentFrame.getInputData() != null ? currentFrame.getInputData().toArray() : new byte[0]);
-            log.debug("MessageFrame {} has input data {}, ...", currentFrame.toString(), inputData);
-
+            final var inputData = new String(
+                    currentFrame.getInputData() != null
+                            ? currentFrame.getInputData().toArray()
+                            : new byte[0]);
+            log.debug(
+                    "MessageFrame {} has input data {}, call depth {}, remaining gas {}, sender address {}, recipient address {}, contract address {} and revert reason {}",
+                    currentFrame.toString(),
+                    inputData,
+                    currentFrame.getMessageStackDepth(),
+                    currentFrame.getRemainingGas(),
+                    currentFrame.getSenderAddress(),
+                    currentFrame.getRecipientAddress(),
+                    currentFrame.getContractAddress(),
+                    StringUtils.toEncodedString(
+                            currentFrame.getRevertReason().orElse(Bytes.EMPTY).toArray(), StandardCharsets.UTF_16));
         }
     }
-
 }
