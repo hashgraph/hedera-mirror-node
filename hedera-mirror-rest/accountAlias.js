540857dc290867f76947cbbc517e00ca3e2f68d3
Edwin Greene <edwin@swirldslabs.com>
2023-05-12 18:05:21 -0400

    Add support for EVM address to balances endpoint (#5979)
    * Add evm address and alias support to balances endpoint
    
    Signed-off-by: Edwin Greene <edwin@swirldslabs.com>

--- hedera-mirror-rest/accountAlias.js
+++ hedera-mirror-rest/accountAlias.js
@@ -20,6 +20,7 @@ import {InvalidArgumentError} from './errors';
 // limit the alias to the base32 alphabet excluding padding, other checks will be done in base32.decode. We need
 // the check here because base32.decode allows lower case letters, padding, and auto corrects some typos.
 const accountAliasRegex = /^(\d{1,5}\.){0,2}[A-Z2-7]+$/;
+const noShardRealmAccountAliasRegex = /^[A-Z2-7]+$/;
 
 class AccountAlias {
   /**
@@ -50,10 +51,12 @@ class AccountAlias {
   /**
    * Checks if the accountAlias string is valid
    * @param {string} accountAlias
+   * @param {boolean} noShardRealm If shard realm is allowed as a part of the alias.
    * @return {boolean}
    */
-  static isValid(accountAlias) {
-    return typeof accountAlias === 'string' && accountAliasRegex.test(accountAlias);
+  static isValid(accountAlias, noShardRealm = false) {
+    const regex = noShardRealm ? noShardRealmAccountAliasRegex : accountAliasRegex;
+    return typeof accountAlias === 'string' && regex.test(accountAlias);
   }
 
   /**
