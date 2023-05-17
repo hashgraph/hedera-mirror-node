17f0624d6cc46c9e10170974e45ddf7bce1fd53e
Stoyan Stoyanov <stoyan.stoyanov@limechain.tech>
2023-05-17 16:41:35 +0300

    refactor: clean up CustomFeeDatabaseAccessor
    Signed-off-by: Stoyan Stoyanov <stoyan.stoyanov@limechain.tech>

--- hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/evm/store/accessor/CustomFeeDatabaseAccessor.java
+++ hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/evm/store/accessor/CustomFeeDatabaseAccessor.java
@@ -25,13 +25,12 @@ import com.hedera.node.app.service.evm.store.contracts.precompile.codec.CustomFe
 import com.hedera.node.app.service.evm.store.contracts.precompile.codec.FixedFee;
 import com.hedera.node.app.service.evm.store.contracts.precompile.codec.FractionalFee;
 import com.hedera.node.app.service.evm.store.contracts.precompile.codec.RoyaltyFee;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import javax.inject.Named;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.util.CollectionUtils;
 
 @Named
 @RequiredArgsConstructor
@@ -42,56 +41,51 @@ public class CustomFeeDatabaseAccessor extends DatabaseAccessor<Long, List<Custo
 
     @Override
     public @NonNull Optional<List<CustomFee>> get(@NonNull Long tokenId) {
-        final List<CustomFee> customFees = new ArrayList<>();
-        final var customFeesCollection = customFeeRepository.findByTokenId(tokenId);
+        final var customFeesList = customFeeRepository.findByTokenId(tokenId).stream()
+                .filter(customFee -> customFee.getCollectorAccountId() != null)
+                .map(this::mapCustomFee)
+                .collect(Collectors.toList());
 
-        if (CollectionUtils.isEmpty(customFeesCollection)) {
-            return Optional.of(customFees);
+        return Optional.of(customFeesList);
+    }
+
+    private CustomFee mapCustomFee(com.hedera.mirror.common.domain.transaction.CustomFee customFee) {
+        final var collector = entityDatabaseAccessor.evmAddressFromId(customFee.getCollectorAccountId());
+        final long amount = requireNonNullElse(customFee.getAmount(), 0L);
+        final var denominatingTokenId = customFee.getDenominatingTokenId();
+        final var denominatingTokenAddress =
+                denominatingTokenId == null ? EMPTY_EVM_ADDRESS : toAddress(denominatingTokenId);
+        final long amountDenominator = requireNonNullElse(customFee.getAmountDenominator(), 0L);
+        final var maximumAmount = requireNonNullElse(customFee.getMaximumAmount(), 0L);
+        final var minimumAmount = customFee.getMinimumAmount();
+        final var netOfTransfers = requireNonNullElse(customFee.getNetOfTransfers(), false);
+        final long royaltyDenominator = requireNonNullElse(customFee.getRoyaltyDenominator(), 0L);
+        final long royaltyNumerator = requireNonNullElse(customFee.getRoyaltyNumerator(), 0L);
+
+        CustomFee customFeeConstructed = new CustomFee();
+
+        if (royaltyNumerator > 0 && royaltyDenominator > 0) {
+            final var royaltyFee = new RoyaltyFee(
+                    royaltyNumerator,
+                    royaltyDenominator,
+                    amount,
+                    denominatingTokenAddress,
+                    denominatingTokenId == null,
+                    collector);
+            customFeeConstructed.setRoyaltyFee(royaltyFee);
+
+        } else if (amountDenominator > 0) {
+            final var fractionFee = new FractionalFee(
+                    amount, amountDenominator, minimumAmount, maximumAmount, netOfTransfers, collector);
+            customFeeConstructed.setFractionalFee(fractionFee);
+
+        } else {
+            final var fixedFee =
+                    new FixedFee(amount, denominatingTokenAddress, denominatingTokenId == null, false, collector);
+
+            customFeeConstructed.setFixedFee(fixedFee);
         }
 
-        for (final var customFee : customFeesCollection) {
-            final var collectorId = customFee.getCollectorAccountId();
-            if (collectorId == null) {
-                continue;
-            }
-
-            final var collector = entityDatabaseAccessor.evmAddressFromId(collectorId);
-            final long amount = requireNonNullElse(customFee.getAmount(), 0L);
-            final var denominatingTokenId = customFee.getDenominatingTokenId();
-            final var denominatingTokenAddress =
-                    denominatingTokenId == null ? EMPTY_EVM_ADDRESS : toAddress(denominatingTokenId);
-            final long amountDenominator = requireNonNullElse(customFee.getAmountDenominator(), 0L);
-            final var maximumAmount = requireNonNullElse(customFee.getMaximumAmount(), 0L);
-            final var minimumAmount = customFee.getMinimumAmount();
-            final var netOfTransfers = requireNonNullElse(customFee.getNetOfTransfers(), false);
-            final long royaltyDenominator = requireNonNullElse(customFee.getRoyaltyDenominator(), 0L);
-            final long royaltyNumerator = requireNonNullElse(customFee.getRoyaltyNumerator(), 0L);
-
-            CustomFee customFeeConstructed = new CustomFee();
-
-            if (royaltyNumerator > 0 && royaltyDenominator > 0) {
-                final var royaltyFee = new RoyaltyFee(
-                        royaltyNumerator,
-                        royaltyDenominator,
-                        amount,
-                        denominatingTokenAddress,
-                        denominatingTokenId == null,
-                        collector);
-                customFeeConstructed.setRoyaltyFee(royaltyFee);
-
-            } else if (amountDenominator > 0) {
-                final var fractionFee = new FractionalFee(
-                        amount, amountDenominator, minimumAmount, maximumAmount, netOfTransfers, collector);
-                customFeeConstructed.setFractionalFee(fractionFee);
-
-            } else {
-                final var fixedFee =
-                        new FixedFee(amount, denominatingTokenAddress, denominatingTokenId == null, false, collector);
-
-                customFeeConstructed.setFixedFee(fixedFee);
-            }
-            customFees.add(customFeeConstructed);
-        }
-        return Optional.of(customFees);
+        return customFeeConstructed;
     }
 }
