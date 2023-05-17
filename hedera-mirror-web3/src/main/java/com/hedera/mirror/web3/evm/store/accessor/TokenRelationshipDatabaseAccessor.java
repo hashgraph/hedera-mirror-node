e06057b95760260bd0e4bb2c8136e149c5629299
Stoyan Stoyanov <stoyan.stoyanov@limechain.tech>
2023-05-17 10:33:14 +0300

    feat: add token relationship accessor
    Signed-off-by: Stoyan Stoyanov <stoyan.stoyanov@limechain.tech>

---
+++ hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/evm/store/accessor/TokenRelationshipDatabaseAccessor.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2023 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hedera.mirror.web3.evm.store.accessor;
+
+import com.hedera.mirror.common.domain.entity.EntityIdEndec;
+import com.hedera.mirror.common.domain.token.AbstractTokenAccount;
+import com.hedera.mirror.common.domain.token.TokenAccount;
+import com.hedera.mirror.common.domain.token.TokenFreezeStatusEnum;
+import com.hedera.mirror.common.domain.token.TokenKycStatusEnum;
+import com.hedera.mirror.web3.evm.store.accessor.key.TokenRelationshipKey;
+import com.hedera.mirror.web3.repository.TokenAccountRepository;
+import com.hedera.services.store.models.Account;
+import com.hedera.services.store.models.Token;
+import com.hedera.services.store.models.TokenRelationship;
+import java.util.Optional;
+import javax.inject.Named;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.hyperledger.besu.datatypes.Address;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Named
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class TokenRelationshipDatabaseAccessor extends DatabaseAccessor<TokenRelationshipKey, TokenRelationship> {
+    private final TokenDatabaseAccessor tokenDatabaseAccessor;
+    private final AccountDatabaseAccessor accountDatabaseAccessor;
+
+    private final TokenAccountRepository tokenAccountRepository;
+
+    @Override
+    public @NonNull Optional<TokenRelationship> get(@NonNull TokenRelationshipKey key) {
+        return findAccount(key.accountAddress())
+                .flatMap(account -> findToken(key.tokenAddress()).flatMap(token -> findTokenAccount(token, account)
+                        .map(tokenAccount -> new TokenRelationship(
+                                token,
+                                account,
+                                tokenAccount.getBalance(),
+                                TokenFreezeStatusEnum.FROZEN.equals(tokenAccount.getFreezeStatus()),
+                                TokenKycStatusEnum.GRANTED.equals(tokenAccount.getKycStatus()),
+                                false,
+                                false,
+                                Boolean.TRUE.equals(tokenAccount.getAutomaticAssociation()),
+                                0))));
+    }
+
+    private Optional<Account> findAccount(Address address) {
+        return accountDatabaseAccessor.get(address);
+    }
+
+    private Optional<Token> findToken(Address address) {
+        return tokenDatabaseAccessor.get(address);
+    }
+
+    private Optional<TokenAccount> findTokenAccount(Token token, Account account) {
+        AbstractTokenAccount.Id id = new AbstractTokenAccount.Id();
+        id.setTokenId(EntityIdEndec.encode(
+                token.getId().shard(), token.getId().realm(), token.getId().num()));
+        id.setAccountId(EntityIdEndec.encode(
+                account.getId().shard(),
+                account.getId().realm(),
+                account.getId().num()));
+        return tokenAccountRepository.findById(id);
+    }
+}
