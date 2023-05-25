537e2303fb1a74532de09020535e08c6100851ad
Ivan Ivanov <56980328+0xivanov@users.noreply.github.com>
2023-05-25 23:10:44 +0300

    HIP-584: Integrate StackedStateFrames into Updater components (#6066)
    We need to integrate the special state for speculative writes into the mirror-node Updater components, so that we keep track of the changes from previous transactions. As an example this might be invoking contractCreate multiple times (several nested contract creates). In addition we need to make possible the Updaters to commit the accumulated changes from a single frame into the state.
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    Signed-off-by: IvanKavaldzhiev <ivankavaldzhiev@gmail.com>

--- hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/evm/contracts/execution/EvmOperationConstructionUtil.java
+++ hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/evm/contracts/execution/EvmOperationConstructionUtil.java
@@ -24,7 +24,6 @@ import com.hedera.mirror.web3.evm.store.StackedStateFrames;
 import com.hedera.mirror.web3.evm.properties.MirrorNodeEvmProperties;
 import com.hedera.mirror.web3.evm.store.contract.precompile.MirrorHTSPrecompiledContract;
 import com.hedera.mirror.web3.evm.store.contract.precompile.PrecompileMapper;
-import com.hedera.node.app.service.evm.contracts.execution.HederaEvmMessageCallProcessor;
 import com.hedera.node.app.service.evm.contracts.operations.CreateOperationExternalizer;
 import com.hedera.node.app.service.evm.contracts.operations.HederaBalanceOperation;
 import com.hedera.node.app.service.evm.contracts.operations.HederaDelegateCallOperation;
