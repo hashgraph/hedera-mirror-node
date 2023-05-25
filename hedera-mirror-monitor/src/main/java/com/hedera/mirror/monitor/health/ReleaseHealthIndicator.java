0d01fd300c4949fcd89b0fc346e1eab03dc7fbe1
Steven Sheehy <steven.sheehy@swirldslabs.com>
2023-05-25 10:32:19 -0500

    Fix acceptance tests not working on throttled networks
    Signed-off-by: Steven Sheehy <steven.sheehy@swirldslabs.com>

--- hedera-mirror-monitor/src/main/java/com/hedera/mirror/monitor/health/ReleaseHealthIndicator.java
+++ hedera-mirror-monitor/src/main/java/com/hedera/mirror/monitor/health/ReleaseHealthIndicator.java
@@ -92,7 +92,8 @@ public class ReleaseHealthIndicator implements ReactiveHealthIndicator {
                 .filter(condition -> StringUtils.equals(condition.get("type"), "Ready"))
                 .findFirst()
                 .map(this::mapStatus)
-                .orElse(DOWN);
+                .orElse(DOWN)
+                .doOnNext(health -> log.info("Release status: {}", health));
     }
 
     private Mono<Health> mapStatus(Map<String, String> condition) {
