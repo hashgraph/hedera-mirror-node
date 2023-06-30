b507e6c8258dfd96bf640c29ae2acf4f63df6fb0
Ivan Ivanov <ivanivanov.ii726@gmail.com>
2023-06-30 09:43:02 +0300

    test: add tests for Store
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>

--- hedera-mirror-web3/src/test/java/com/hedera/mirror/web3/evm/store/StoreImplTest.java
+++ hedera-mirror-web3/src/test/java/com/hedera/mirror/web3/evm/store/StoreImplTest.java
@@ -16,8 +16,10 @@
 
 package com.hedera.mirror.web3.evm.store;
 
+import static com.hedera.services.utils.EntityIdUtils.accountIdFromEvmAddress;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -44,8 +46,12 @@ import com.hedera.mirror.web3.repository.TokenAccountRepository;
 import com.hedera.mirror.web3.repository.TokenAllowanceRepository;
 import com.hedera.mirror.web3.repository.TokenRepository;
 import com.hedera.mirror.web3.repository.projections.TokenAccountAssociationsCount;
+import com.hedera.services.state.submerkle.RichInstant;
+import com.hedera.services.store.models.Account;
 import com.hedera.services.store.models.Id;
 import com.hedera.services.store.models.NftId;
+import com.hedera.services.store.models.TokenRelationship;
+import com.hedera.services.store.models.UniqueToken;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -87,6 +93,8 @@ class StoreImplTest {
             });
     private static final Address TOKEN_ADDRESS = Address.ALTBN128_ADD;
     private static final Address ACCOUNT_ADDRESS = Address.BLS12_MAP_FP2_TO_G2;
+    private static final Id TOKEN_ID = Id.fromGrpcAccount(accountIdFromEvmAddress(TOKEN_ADDRESS));
+    private static final Id ACCOUNT_ID = Id.fromGrpcAccount(accountIdFromEvmAddress(ACCOUNT_ADDRESS));
 
     @Mock
     private EntityDatabaseAccessor entityDatabaseAccessor;
@@ -172,6 +180,14 @@ class StoreImplTest {
     }
 
     @Test
+    void updateAccount() {
+        final var account = new Account(1L, ACCOUNT_ID, 1L);
+        subject.wrap();
+        subject.updateAccount(account);
+        assertEquals(account, subject.getAccount(ACCOUNT_ADDRESS, OnMissing.DONT_THROW));
+    }
+
+    @Test
     void getTokenWithoutThrow() {
         when(entityDatabaseAccessor.get(TOKEN_ADDRESS)).thenReturn(Optional.of(tokenModel));
         when(tokenModel.getId()).thenReturn(6L);
@@ -185,6 +201,14 @@ class StoreImplTest {
     void getTokenThrowIfMissing() {
         assertThatThrownBy(() -> subject.getToken(TOKEN_ADDRESS, OnMissing.THROW))
                 .isInstanceOf(InvalidTransactionException.class);
+    }
+
+    @Test
+    void updateToken() {
+        final var token = new com.hedera.services.store.models.Token(TOKEN_ID);
+        subject.wrap();
+        subject.updateToken(token);
+        assertEquals(token, subject.getToken(TOKEN_ADDRESS, OnMissing.DONT_THROW));
     }
 
     @Test
@@ -212,6 +236,18 @@ class StoreImplTest {
     }
 
     @Test
+    void updateTokenRelationship() {
+        final var tokenRel = new TokenRelationship(
+                new com.hedera.services.store.models.Token(TOKEN_ID), new Account(0L, ACCOUNT_ID, 0L));
+        subject.wrap();
+        subject.updateTokenRelationship(tokenRel);
+        assertEquals(
+                tokenRel,
+                subject.getTokenRelationship(
+                        new TokenRelationshipKey(TOKEN_ADDRESS, ACCOUNT_ADDRESS), OnMissing.DONT_THROW));
+    }
+
+    @Test
     void getUniqueTokenWithoutThrow() {
         final var nftId = new NftId(0, 0, 6, 1);
         when(nftRepository.findActiveById(6, 1)).thenReturn(Optional.of(nft));
@@ -227,5 +263,20 @@ class StoreImplTest {
         final var nftId = new NftId(0, 0, 6, 1);
         assertThatThrownBy(() -> subject.getUniqueToken(nftId, OnMissing.THROW))
                 .isInstanceOf(InvalidTransactionException.class);
+    }
+
+    @Test
+    void updateUniqueToken() {
+        final var nftId = new NftId(0, 0, 0, 1);
+        final var nft = new UniqueToken(
+                Id.DEFAULT,
+                1,
+                RichInstant.MISSING_INSTANT,
+                Id.fromGrpcAccount(accountIdFromEvmAddress(ACCOUNT_ADDRESS)),
+                null,
+                null);
+        subject.wrap();
+        subject.updateUniqueToken(nft);
+        assertEquals(nft, subject.getUniqueToken(nftId, OnMissing.DONT_THROW));
     }
 }
