dcfdec88b0e733d6453d19237a1e315c2c350d9b
Jeff Schmidt <jeffrey.schmidt@swirldslabs.com>
2023-07-14 12:14:15 -0600

    Update mergeTokenAllowance() to reflect new login mergeCryptoAllowance().
    Signed-off-by: Jeff Schmidt <jeffrey.schmidt@swirldslabs.com>

--- hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/parser/record/entity/sql/SqlEntityListener.java
+++ hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/parser/record/entity/sql/SqlEntityListener.java
@@ -563,11 +563,6 @@ public class SqlEntityListener implements EntityListener, RecordStreamFileListen
         // Current is an allowance grant so will override all mutable fields
         if (current.isHistory()) {
             previous.setTimestampUpper(current.getTimestampLower());
-
-            if (current.getCreatedTimestamp() == null) {
-                current.setCreatedTimestamp(previous.getCreatedTimestamp());
-            }
-
             return current;
         }
 
@@ -903,17 +898,14 @@ public class SqlEntityListener implements EntityListener, RecordStreamFileListen
     }
 
     private TokenAllowance mergeTokenAllowance(TokenAllowance previous, TokenAllowance current) {
-        /*
-         * Current allowance without a timestamp indicates it was generated due to an approved token transfer.
-         * Adjust the amount of the previous allowance by the amount of the current allowance, which is a
-         * negative value representing the debit of the transfer which now also debits the allowance amount.
-         */
-        if (current.getTimestampRange() == null) {
-            previous.setAmount(previous.getAmount() + current.getAmount());
-        } else if (previous.getTimestampRange() != null) {
+        // Current is an allowance grant so will override all mutable fields
+        if (current.isHistory()) {
             previous.setTimestampUpper(current.getTimestampLower());
             return current;
         }
+
+        // Current must be an approved transfer and previous can be either so should accumulate the amounts regardless.
+        previous.setAmount(previous.getAmount() + current.getAmount());
         return previous;
     }
 
