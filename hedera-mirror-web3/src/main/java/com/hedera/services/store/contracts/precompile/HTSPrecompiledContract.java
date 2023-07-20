7b24bda9fbdcbcee900289267787d5ab21f09fec
Mustafa Uzun <mustafa.uzun@limechain.tech>
2023-07-20 14:28:58 +0300

    refactor: after merge
    Signed-off-by: Mustafa Uzun <mustafa.uzun@limechain.tech>

--- hedera-mirror-web3/src/main/java/com/hedera/services/store/contracts/precompile/HTSPrecompiledContract.java
+++ hedera-mirror-web3/src/main/java/com/hedera/services/store/contracts/precompile/HTSPrecompiledContract.java
@@ -40,6 +40,7 @@ import com.hedera.node.app.service.evm.store.contracts.utils.DescriptorUtils;
 import com.hedera.node.app.service.evm.store.tokens.TokenAccessor;
 import com.hedera.services.store.contracts.precompile.codec.FunctionParam;
 import com.hedera.services.store.contracts.precompile.codec.HrcParams;
+import com.hedera.services.store.contracts.precompile.codec.TransferParams;
 import com.hedera.services.utils.EntityIdUtils;
 import com.hederahashgraph.api.proto.java.ResponseCodeEnum;
 import com.hederahashgraph.api.proto.java.TransactionBody;
@@ -244,7 +245,17 @@ public class HTSPrecompiledContract implements HTSPrecompiledContractAdapter {
                 break;
             default:
                 this.precompile = precompileMapper.lookup(functionId).orElseThrow();
-                this.transactionBody = precompile.body(input, aliasResolver, new FunctionParam(functionId));
+                if (AbiConstants.ABI_ID_TRANSFER_TOKENS == functionId
+                        || AbiConstants.ABI_ID_TRANSFER_TOKEN == functionId
+                        || AbiConstants.ABI_ID_TRANSFER_NFTS == functionId
+                        || AbiConstants.ABI_ID_TRANSFER_NFT == functionId
+                        || AbiConstants.ABI_ID_CRYPTO_TRANSFER == functionId
+                        || AbiConstants.ABI_ID_CRYPTO_TRANSFER_V2 == functionId) {
+                    this.transactionBody =
+                            precompile.body(input, aliasResolver, new TransferParams(functionId, senderAddress));
+                } else {
+                    this.transactionBody = precompile.body(input, aliasResolver, new FunctionParam(functionId));
+                }
                 break;
         }
         gasRequirement = defaultGas();
