edafbac1a695c69e2ae50e941f43ddb09b64964d
mgoelswirlds <mugdha.goel@swirldslabs.com>
2023-07-20 21:19:03 -0400

    Fixing NestNftTransferMigrationTest
    Signed-off-by: mgoelswirlds <mugdha.goel@swirldslabs.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v1/V1.83.2__add_itemized_transfers_to_transaction.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/v1/V1.83.2__add_itemized_transfers_to_transaction.sql
@@ -3,16 +3,15 @@ alter table if exists transaction
 
 with nested_itemized_transfer as (
     select nt.consensus_timestamp, jsonb_agg(jsonb_build_object(
-            'entity_id', nt.entity_id,
-            'amount', nt.amount,
-            'is_approval', nt.is_approval
-        )) as transfer
+      'entity_id', nt.entity_id,
+      'amount', nt.amount,
+      'is_approval', nt.is_approval
+      )) as transfer
     from non_fee_transfer nt
     group by nt.consensus_timestamp
-    )
+)
 
 update transaction
 set itemized_transfer = transfer
 from nested_itemized_transfer
 where nested_itemized_transfer.consensus_timestamp = transaction.consensus_timestamp;
-
