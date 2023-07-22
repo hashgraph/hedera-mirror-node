0708cf150e2d62b9cdc3ef25ae001c47363d49c3
Xin Li <xin@swirldslabs.com>
2023-07-21 20:04:34 -0500

    more test cases
    Signed-off-by: Xin Li <xin@swirldslabs.com>

--- hedera-mirror-rosetta/app/persistence/domain/transaction_test.go
+++ hedera-mirror-rosetta/app/persistence/domain/transaction_test.go
@@ -21,6 +21,7 @@
 package domain
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 
@@ -53,6 +54,21 @@ func TestItemizedTransferSliceScan(t *testing.T) {
 	assert.Equal(t, expected, actual)
 }
 
+func TestItemizedTransferSliceScanThrows(t *testing.T) {
+	inputs := []interface{}{
+		"not byte array",
+		[]byte(`[{"amount": -20, "entity_id": 101, "is_approval": true]`), // invalid json string
+	}
+
+	for _, input := range inputs {
+		t.Run(fmt.Sprintf("%s", input), func(t *testing.T) {
+			actual := ItemizedTransferSlice{}
+			err := actual.Scan(input)
+			assert.Error(t, err)
+		})
+	}
+}
+
 func TestItemizedTransferSliceValue(t *testing.T) {
 	// given
 	transfer := ItemizedTransferSlice{
@@ -75,4 +91,10 @@ func TestItemizedTransferSliceValue(t *testing.T) {
 	assert.IsType(t, []byte{}, actual)
 	bytes, _ := actual.([]byte)
 	assert.Equal(t, expected, string(bytes))
+}
+
+func TestItemizedTransferSliceValueEmpty(t *testing.T) {
+	actual, err := ItemizedTransferSlice{}.Value()
+	assert.NoError(t, err)
+	assert.Nil(t, actual)
 }
