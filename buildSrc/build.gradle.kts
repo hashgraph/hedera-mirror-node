d205f42fd9c1fe98729596999f6fa6db1149b386
Jesse Nelson <jesse@swirldslabs.com>
2023-07-24 17:16:57 -0500

    Revert "Bump org.sonarsource.scanner.gradle:sonarqube-gradle-plugin from 4.2.1.3168 to 4.3.0.3225 (#6471)"
    This reverts commit 0a8aeffc513d232293a6d42a47d700a6cdd13840.
    
    Signed-off-by: Jesse Nelson <jesse@swirldslabs.com>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
     implementation("org.gradle:test-retry-gradle-plugin:1.5.3")
     implementation("org.openapitools:openapi-generator-gradle-plugin:6.6.0")
     implementation("org.owasp:dependency-check-gradle:8.3.1")
-    implementation("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:4.3.0.3225")
+    implementation("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:4.2.1.3168")
     implementation("org.springframework.boot:spring-boot-gradle-plugin:3.1.2")
 
     // Temporary until openapi-generator updates to a swagger-parser that is compatible with SnakeYAML 2.0
