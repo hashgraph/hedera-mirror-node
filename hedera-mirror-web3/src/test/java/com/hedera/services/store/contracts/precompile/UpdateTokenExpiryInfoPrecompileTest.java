aa93b55ee7ffcf7c53b9b0873e178d3eb994b1c9
Alexander Gadzhalov <alexander.gadzhalov@limechain.tech>
2023-07-27 13:40:11 +0300

    06390: Added updateTokenExpiryInfoHappyPath unit test with comments
    Signed-off-by: Alexander Gadzhalov <alexander.gadzhalov@limechain.tech>

---
+++ hedera-mirror-web3/src/test/java/com/hedera/services/store/contracts/precompile/UpdateTokenExpiryInfoPrecompileTest.java
@@ -0,0 +1,206 @@
+/*
+ * Copyright (C) 2023 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hedera.services.store.contracts.precompile;
+
+import static com.hedera.services.store.contracts.precompile.AbiConstants.ABI_ID_UPDATE_TOKEN_EXPIRY_INFO;
+import static com.hedera.services.store.contracts.precompile.HTSTestsUtil.contractAddress;
+import static com.hedera.services.store.contracts.precompile.HTSTestsUtil.successResult;
+import static com.hedera.services.store.contracts.precompile.HTSTestsUtil.tokenUpdateExpiryInfoWrapper;
+import static com.hedera.services.store.contracts.precompile.impl.UpdateTokenExpiryInfoPrecompile.getTokenUpdateExpiryInfoWrapper;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mockStatic;
+
+import com.esaulpaugh.headlong.util.Integers;
+import com.hedera.mirror.web3.evm.properties.MirrorNodeEvmProperties;
+import com.hedera.mirror.web3.evm.store.contract.HederaEvmStackedWorldStateUpdater;
+import com.hedera.node.app.service.evm.store.contracts.precompile.EvmHTSPrecompiledContract;
+import com.hedera.node.app.service.evm.store.contracts.precompile.EvmInfrastructureFactory;
+import com.hedera.services.fees.FeeCalculator;
+import com.hedera.services.fees.HbarCentExchange;
+import com.hedera.services.fees.calculation.UsagePricesProvider;
+import com.hedera.services.fees.pricing.AssetsLoader;
+import com.hedera.services.store.contracts.precompile.impl.SystemContractAbis;
+import com.hedera.services.store.contracts.precompile.impl.UpdateTokenExpiryInfoPrecompile;
+import com.hedera.services.store.contracts.precompile.utils.PrecompilePricingUtils;
+import com.hedera.services.store.tokens.HederaTokenStore;
+import com.hedera.services.txns.validation.ContextOptionValidator;
+import com.hedera.services.utils.accessors.AccessorFactory;
+import com.hederahashgraph.api.proto.java.ExchangeRate;
+import com.hederahashgraph.api.proto.java.ResponseCodeEnum;
+import com.hederahashgraph.api.proto.java.TokenUpdateTransactionBody;
+import com.hederahashgraph.api.proto.java.TransactionBody;
+import java.util.Optional;
+import java.util.Set;
+import org.apache.tuweni.bytes.Bytes;
+import org.hyperledger.besu.datatypes.Wei;
+import org.hyperledger.besu.evm.frame.MessageFrame;
+import org.hyperledger.besu.evm.gascalculator.GasCalculator;
+import org.hyperledger.besu.evm.worldstate.WorldUpdater;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class UpdateTokenExpiryInfoPrecompileTest {
+
+    @Mock
+    private HederaTokenStore hederaTokenStore;
+
+    @Mock
+    private MirrorNodeEvmProperties evmProperties;
+
+    @Mock
+    private GasCalculator gasCalculator;
+
+    @Mock
+    private MessageFrame frame;
+
+    @Mock
+    private SyntheticTxnFactory syntheticTxnFactory;
+
+    @Mock
+    private EvmHTSPrecompiledContract evmHTSPrecompiledContract;
+
+    @Mock
+    private EvmInfrastructureFactory infrastructureFactory;
+
+    @Mock
+    private AssetsLoader assetLoader;
+
+    @Mock
+    private HbarCentExchange exchange;
+
+    @Mock
+    private ExchangeRate exchangeRate;
+
+    @Mock
+    private AccessorFactory accessorFactory;
+
+    @Mock
+    private FeeCalculator feeCalculator;
+
+    @Mock
+    private UsagePricesProvider resourceCosts;
+
+    @Mock
+    private PrecompileMapper precompileMapper;
+
+    @Mock
+    private ContextOptionValidator optionValidator;
+
+    @Mock
+    private HederaEvmStackedWorldStateUpdater worldUpdater;
+
+    @Mock
+    private TokenUpdateLogic tokenUpdateLogic;
+
+    private static final int CENTS_RATE = 12;
+    private static final int HBAR_RATE = 1;
+    public static final Bytes UPDATE_EXPIRY_INFO_FOR_TOKEN_INPUT = Bytes.fromHexString(
+            "0x593d6e8200000000000000000000000000000000000000000000000000000000000008d300000000000000000000000000000000000000000000000000000000bbf7edc700000000000000000000000000000000000000000000000000000000000008d000000000000000000000000000000000000000000000000000000000002820a8");
+    public static final Bytes UPDATE_EXPIRY_INFO_FOR_TOKEN_INPUT_V2 = Bytes.fromHexString(
+            "0xd27be6cd00000000000000000000000000000000000000000000000000000000000008d3000000000000000000000000000000000000000000000000000000000bf7edc700000000000000000000000000000000000000000000000000000000000008d00000000000000000000000000000000000000000000000000000000000000008");
+
+    private HTSPrecompiledContract subject;
+    private UpdateTokenExpiryInfoPrecompile updateTokenExpiryInfoPrecompile;
+    private MockedStatic<UpdateTokenExpiryInfoPrecompile> staticUpdateTokenExpiryInfoPrecompile;
+
+    @BeforeEach
+    void setUp() {
+        final PrecompilePricingUtils precompilePricingUtils =
+                new PrecompilePricingUtils(assetLoader, exchange, feeCalculator, resourceCosts, accessorFactory);
+
+        staticUpdateTokenExpiryInfoPrecompile = mockStatic(UpdateTokenExpiryInfoPrecompile.class);
+
+        // syntheticTxnFactory = new SyntheticTxnFactory();
+        optionValidator = new ContextOptionValidator(evmProperties);
+
+        updateTokenExpiryInfoPrecompile = new UpdateTokenExpiryInfoPrecompile(
+                tokenUpdateLogic, evmProperties, optionValidator, syntheticTxnFactory, precompilePricingUtils);
+
+        precompileMapper = new PrecompileMapper(Set.of(updateTokenExpiryInfoPrecompile));
+
+        subject = new HTSPrecompiledContract(
+                infrastructureFactory, evmProperties, precompileMapper, evmHTSPrecompiledContract);
+    }
+
+    @AfterEach
+    void closeMocks() {
+        if (!staticUpdateTokenExpiryInfoPrecompile.isClosed()) {
+            staticUpdateTokenExpiryInfoPrecompile.close();
+        }
+    }
+
+    @Test
+    void updateTokenExpiryInfoHappyPath() {
+        final var input = Bytes.of(Integers.toBytes(ABI_ID_UPDATE_TOKEN_EXPIRY_INFO));
+        given(worldUpdater.permissivelyUnaliased(any()))
+                .willAnswer(invocationOnMock -> invocationOnMock.getArgument(0));
+
+        // frame context
+        givenFrameContext();
+
+        // givenMinimalContextForSuccessfulCall
+        givenMinimalContextForSuccessfulCall();
+
+        // givenUpdateTokenContext
+        givenUpdateTokenContext();
+
+        given(syntheticTxnFactory.createTokenUpdateExpiryInfo(tokenUpdateExpiryInfoWrapper))
+                .willReturn(TransactionBody.newBuilder().setTokenUpdate(TokenUpdateTransactionBody.newBuilder()));
+
+        // givenPricingUtilsContext
+        given(worldUpdater.permissivelyUnaliased(any()))
+                .willAnswer(invocationOnMock -> invocationOnMock.getArgument(0));
+
+        // when
+        subject.prepareFields(frame);
+        subject.prepareComputation(UPDATE_EXPIRY_INFO_FOR_TOKEN_INPUT, a -> a);
+        final var result = subject.computeInternal(frame);
+
+        // then
+        assertEquals(successResult, result);
+    }
+
+    private void givenFrameContext() {
+        given(frame.getSenderAddress()).willReturn(contractAddress);
+        given(frame.getWorldUpdater()).willReturn(worldUpdater);
+        given(frame.getRemainingGas()).willReturn(300L);
+        given(frame.getValue()).willReturn(Wei.ZERO);
+    }
+
+    private void givenMinimalContextForSuccessfulCall() {
+        final Optional<WorldUpdater> parent = Optional.of(worldUpdater);
+        given(worldUpdater.permissivelyUnaliased(any()))
+                .willAnswer(invocationOnMock -> invocationOnMock.getArgument(0));
+    }
+
+    private void givenUpdateTokenContext() {
+        given(tokenUpdateLogic.validate(any())).willReturn(ResponseCodeEnum.OK);
+        staticUpdateTokenExpiryInfoPrecompile
+                .when(() -> getTokenUpdateExpiryInfoWrapper(
+                        any(), any(), eq(SystemContractAbis.UPDATE_TOKEN_EXPIRY_INFO_V1)))
+                .thenReturn(tokenUpdateExpiryInfoWrapper);
+    }
+}
