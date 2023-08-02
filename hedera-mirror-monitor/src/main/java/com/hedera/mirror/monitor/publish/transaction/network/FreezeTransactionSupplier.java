728d31bfb2a5cd607cce3b148073b89ca2164d30
Steven Sheehy <steven.sheehy@swirldslabs.com>
2023-08-02 13:01:57 -0500

    Rename migration and add monitor test
    Signed-off-by: Steven Sheehy <steven.sheehy@swirldslabs.com>

--- hedera-mirror-monitor/src/main/java/com/hedera/mirror/monitor/publish/transaction/network/FreezeTransactionSupplier.java
+++ hedera-mirror-monitor/src/main/java/com/hedera/mirror/monitor/publish/transaction/network/FreezeTransactionSupplier.java
@@ -19,23 +19,45 @@ package com.hedera.mirror.monitor.publish.transaction.network;
 import com.hedera.hashgraph.sdk.FileId;
 import com.hedera.hashgraph.sdk.FreezeTransaction;
 import com.hedera.hashgraph.sdk.FreezeType;
-import com.hedera.hashgraph.sdk.Transaction;
+import com.hedera.hashgraph.sdk.Hbar;
 import com.hedera.mirror.monitor.publish.transaction.TransactionSupplier;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.time.Instant;
+import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 
+@Data
 public class FreezeTransactionSupplier implements TransactionSupplier<FreezeTransaction> {
 
     private byte[] fileHash;
+
     private String fileId;
+
+    @NotNull
     private FreezeType freezeType = FreezeType.FREEZE_ONLY;
-    private Instant startTime;
+
+    @Min(1)
+    private long maxTransactionFee = 1_000_000_000;
+
+    @NotNull
+    private Instant startTime = Instant.now();
 
     @Override
-    public Transaction<FreezeTransaction> get() {
-        return new FreezeTransaction()
+    public FreezeTransaction get() {
+        var freezeTransaction = new FreezeTransaction()
                 .setFreezeType(freezeType)
-                .setFileId(FileId.fromString(fileId))
-                .setFileHash(fileHash)
+                .setMaxTransactionFee(Hbar.fromTinybars(maxTransactionFee))
                 .setStartTime(startTime);
+
+        if (fileHash != null) {
+            freezeTransaction.setFileHash(fileHash);
+        }
+
+        if (StringUtils.isNotBlank(fileId)) {
+            freezeTransaction.setFileId(FileId.fromString(fileId));
+        }
+
+        return freezeTransaction;
     }
 }
