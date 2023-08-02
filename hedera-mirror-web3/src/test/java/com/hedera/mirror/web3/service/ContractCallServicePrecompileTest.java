02742979955c4138a52675ee1cbad10807a0e2ad
Valentin Valkanov <valentin.valkanov@limechain.tech>
2023-08-02 17:19:54 +0300

    test: implement integration tests for estimate_Gas for the token info and token metadata read-only functions.
    Signed-off-by: Valentin Valkanov <valentin.valkanov@limechain.tech>

--- hedera-mirror-web3/src/test/java/com/hedera/mirror/web3/service/ContractCallServicePrecompileTest.java
+++ hedera-mirror-web3/src/test/java/com/hedera/mirror/web3/service/ContractCallServicePrecompileTest.java
@@ -40,7 +40,7 @@ class ContractCallServicePrecompileTest extends ContractCallTestSetup {
 
     @ParameterizedTest
     @EnumSource(ContractReadFunctions.class)
-    void evmPrecompileReadOnlyTokenFunctionsTest(ContractReadFunctions contractFunc) {
+    void evmPrecompileReadOnlyTokenFunctionsTest(final ContractReadFunctions contractFunc) {
         final var functionHash =
                 functionEncodeDecoder.functionHashFor(contractFunc.name, ABI_PATH, contractFunc.functionParameters);
         final var serviceParameters = serviceParametersForExecution(functionHash, CONTRACT_ADDRESS, ETH_CALL, 0L);
@@ -52,7 +52,7 @@ class ContractCallServicePrecompileTest extends ContractCallTestSetup {
 
     @ParameterizedTest
     @EnumSource(ContractReadFunctions.class)
-    void evmPrecompileReadOnlyTokenFunctionsTestWithNonStaticFrame(ContractReadFunctions contractFunc) {
+    void evmPrecompileReadOnlyTokenFunctionsTestWithNonStaticFrame(final ContractReadFunctions contractFunc) {
         final var functionHash =
                 functionEncodeDecoder.functionHashFor(contractFunc.name, ABI_PATH, contractFunc.functionParameters);
         final var serviceParameters = serviceParametersForExecution(functionHash, CONTRACT_ADDRESS, ETH_CALL, 0L);
@@ -63,8 +63,25 @@ class ContractCallServicePrecompileTest extends ContractCallTestSetup {
     }
 
     @ParameterizedTest
+    @EnumSource(ContractReadFunctions.class)
+    void evmPrecompileReadOnlyTokenFunctionsEstimateTest(final ContractReadFunctions contractFunc) {
+        final var functionHash =
+                functionEncodeDecoder.functionHashFor(contractFunc.name, ABI_PATH, contractFunc.functionParameters);
+        final var serviceParameters =
+                serviceParametersForExecution(functionHash, CONTRACT_ADDRESS, ETH_ESTIMATE_GAS, 0L);
+
+        final var expectedGasUsed = gasUsedAfterExecution(serviceParameters);
+
+        assertThat(longValueOf.applyAsLong(contractCallService.processCall(serviceParameters)))
+                .as("result must be within 5-20% bigger than the gas used from the first call")
+                .isGreaterThanOrEqualTo((long) (expectedGasUsed * 1.05)) // expectedGasUsed value increased by 5%
+                .isCloseTo(expectedGasUsed, Percentage.withPercentage(20)); // Maximum percentage
+    }
+
+    @ParameterizedTest
     @EnumSource(UnsupportedContractModificationFunctions.class)
-    void evmPrecompileUnsupportedModificationTokenFunctionsTest(UnsupportedContractModificationFunctions contractFunc) {
+    void evmPrecompileUnsupportedModificationTokenFunctionsTest(
+            final UnsupportedContractModificationFunctions contractFunc) {
         final var functionHash = functionEncodeDecoder.functionHashWithEmptyDataFor(
                 contractFunc.name, MODIFICATION_CONTRACT_ABI_PATH, contractFunc.functionParameters);
         final var serviceParameters =
@@ -77,7 +94,8 @@ class ContractCallServicePrecompileTest extends ContractCallTestSetup {
 
     @ParameterizedTest
     @EnumSource(SupportedContractModificationFunctions.class)
-    void evmPrecompileSupportedModificationTokenFunctionsTest(SupportedContractModificationFunctions contractFunc) {
+    void evmPrecompileSupportedModificationTokenFunctionsTest(
+            final SupportedContractModificationFunctions contractFunc) {
         final var functionHash = functionEncodeDecoder.functionHashFor(
                 contractFunc.name, MODIFICATION_CONTRACT_ABI_PATH, contractFunc.functionParameters);
         final var serviceParameters =
@@ -93,7 +111,7 @@ class ContractCallServicePrecompileTest extends ContractCallTestSetup {
 
     @ParameterizedTest
     @EnumSource(FeeCase.class)
-    void customFees(FeeCase feeCase) {
+    void customFees(final FeeCase feeCase) {
         final var functionName = "getCustomFeesForToken";
         final var functionHash = functionEncodeDecoder.functionHashFor(functionName, ABI_PATH, FUNGIBLE_TOKEN_ADDRESS);
         final var serviceParameters = serviceParametersForExecution(functionHash, CONTRACT_ADDRESS, ETH_CALL, 0L);
@@ -139,7 +157,7 @@ class ContractCallServicePrecompileTest extends ContractCallTestSetup {
 
     @ParameterizedTest
     @CsvSource({"getInformationForFungibleToken,false", "getInformationForNonFungibleToken,true"})
-    void getTokenInfo(String functionName, boolean isNft) {
+    void getTokenInfo(final String functionName, final boolean isNft) {
         final var functionHash = isNft
                 ? functionEncodeDecoder.functionHashFor(functionName, ABI_PATH, NFT_ADDRESS, 1L)
                 : functionEncodeDecoder.functionHashFor(functionName, ABI_PATH, FUNGIBLE_TOKEN_ADDRESS);
@@ -150,23 +168,23 @@ class ContractCallServicePrecompileTest extends ContractCallTestSetup {
         final Tuple decodeResult = functionEncodeDecoder
                 .decodeResult(functionName, ABI_PATH, callResult)
                 .get(0);
-        Tuple tokenInfo = decodeResult.get(0);
-        Tuple hederaToken = tokenInfo.get(0);
-        boolean deleted = tokenInfo.get(2);
-        boolean defaultKycStatus = tokenInfo.get(3);
-        boolean pauseStatus = tokenInfo.get(4);
-        Tuple[] fractionalFees = tokenInfo.get(6);
-        String ledgerId = tokenInfo.get(8);
-        String name = hederaToken.get(0);
-        String symbol = hederaToken.get(1);
-        com.esaulpaugh.headlong.abi.Address treasury = hederaToken.get(2);
-        String memo = hederaToken.get(3);
-        boolean supplyType = hederaToken.get(4);
-        long maxSupply = hederaToken.get(5);
-        boolean freezeStatus = hederaToken.get(6);
-        Tuple expiry = hederaToken.get(8);
-        com.esaulpaugh.headlong.abi.Address autoRenewAccount = expiry.get(1);
-        long autoRenewPeriod = expiry.get(2);
+        final Tuple tokenInfo = decodeResult.get(0);
+        final Tuple hederaToken = tokenInfo.get(0);
+        final boolean deleted = tokenInfo.get(2);
+        final boolean defaultKycStatus = tokenInfo.get(3);
+        final boolean pauseStatus = tokenInfo.get(4);
+        final Tuple[] fractionalFees = tokenInfo.get(6);
+        final String ledgerId = tokenInfo.get(8);
+        final String name = hederaToken.get(0);
+        final String symbol = hederaToken.get(1);
+        final com.esaulpaugh.headlong.abi.Address treasury = hederaToken.get(2);
+        final String memo = hederaToken.get(3);
+        final boolean supplyType = hederaToken.get(4);
+        final long maxSupply = hederaToken.get(5);
+        final boolean freezeStatus = hederaToken.get(6);
+        final Tuple expiry = hederaToken.get(8);
+        final com.esaulpaugh.headlong.abi.Address autoRenewAccount = expiry.get(1);
+        final long autoRenewPeriod = expiry.get(2);
 
         assertThat(deleted).isFalse();
         assertThat(defaultKycStatus).isFalse();
@@ -181,11 +199,11 @@ class ContractCallServicePrecompileTest extends ContractCallTestSetup {
         assertThat(autoRenewPeriod).isEqualTo(1800L);
 
         if (isNft) {
-            long serialNum = decodeResult.get(1);
-            com.esaulpaugh.headlong.abi.Address owner = decodeResult.get(2);
-            long creationTime = decodeResult.get(3);
-            byte[] metadata = decodeResult.get(4);
-            com.esaulpaugh.headlong.abi.Address spender = decodeResult.get(5);
+            final long serialNum = decodeResult.get(1);
+            final com.esaulpaugh.headlong.abi.Address owner = decodeResult.get(2);
+            final long creationTime = decodeResult.get(3);
+            final byte[] metadata = decodeResult.get(4);
+            final com.esaulpaugh.headlong.abi.Address spender = decodeResult.get(5);
 
             assertThat(serialNum).isEqualTo(1L);
             assertThat(owner).isEqualTo(convertAddress(OWNER_ADDRESS));
@@ -196,8 +214,8 @@ class ContractCallServicePrecompileTest extends ContractCallTestSetup {
             assertThat(supplyType).isTrue();
             assertThat(autoRenewAccount).isEqualTo(convertAddress(NFT_ADDRESS));
         } else {
-            int decimals = decodeResult.get(1);
-            long totalSupply = tokenInfo.get(1);
+            final int decimals = decodeResult.get(1);
+            final long totalSupply = tokenInfo.get(1);
             assertThat(decimals).isEqualTo(12);
             assertThat(totalSupply).isEqualTo(12345L);
             assertThat(maxSupply).isEqualTo(2525L);
