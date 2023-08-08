55eae9d2d1e3129747ba20b5e4b6280fbbcedac9
Ivan Ivanov <ivanivanov.ii726@gmail.com>
2023-08-08 17:32:44 +0300

    feat: add revert reasons
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>

--- hedera-mirror-web3/src/test/java/com/hedera/mirror/web3/service/ContractCallNestedCallsTest.java
+++ hedera-mirror-web3/src/test/java/com/hedera/mirror/web3/service/ContractCallNestedCallsTest.java
@@ -18,8 +18,8 @@ package com.hedera.mirror.web3.service;
 
 import static com.hedera.mirror.web3.service.model.CallServiceParameters.CallType.ETH_CALL;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import com.google.protobuf.ByteString;
 import com.hedera.mirror.web3.exception.InvalidTransactionException;
 import java.math.BigInteger;
 import lombok.RequiredArgsConstructor;
@@ -37,7 +37,11 @@ class ContractCallNestedCallsTest extends ContractCallTestSetup {
                 serviceParametersForExecution(functionHash, NESTED_ETH_CALLS_CONTRACT_ADDRESS, ETH_CALL, 0L);
         if (contractFunctions.shouldFail) {
             assertThatThrownBy(() -> contractCallService.processCall(serviceParameters))
-                    .isInstanceOf(InvalidTransactionException.class);
+                    .isInstanceOf(InvalidTransactionException.class)
+                    .satisfies(ex -> {
+                        InvalidTransactionException exception = (InvalidTransactionException) ex;
+                        assertEquals("Expected detail message", exception.getDetail());
+                    });
         } else {
             contractCallService.processCall(serviceParameters);
         }
@@ -45,74 +49,83 @@ class ContractCallNestedCallsTest extends ContractCallTestSetup {
 
     @RequiredArgsConstructor
     enum NestedCallsContractFunctions {
-        MINT_FUNGIBLE_TOKEN(
-                "mintTokenGetTotalSupplyAndBalanceOfTreasury",
-                new Object[] {NOT_FROZEN_FUNGIBLE_TOKEN_ADDRESS, 100L, new byte[0][0], TREASURY_ADDRESS},
-                false),
-        MINT_NFT(
-                "mintTokenGetTotalSupplyAndBalanceOfTreasury",
-                new Object[] {
-                    NFT_ADDRESS,
-                    0L,
-                    new byte[][] {ByteString.copyFromUtf8("firstMeta").toByteArray()},
-                    OWNER_ADDRESS
-                },
-                false),
-        BURN_FUNGIBLE_TOKEN(
-                "burnTokenGetTotalSupplyAndBalanceOfTreasury",
-                new Object[] {NOT_FROZEN_FUNGIBLE_TOKEN_ADDRESS, 12L, new long[0], TREASURY_ADDRESS},
-                false),
-        BURN_NFT(
-                "burnTokenGetTotalSupplyAndBalanceOfTreasury",
-                new Object[] {NFT_ADDRESS, 0L, new long[] {1L}, OWNER_ADDRESS},
-                false),
-        WIPE_FUNGIBLE_TOKEN(
-                "wipeTokenGetTotalSupplyAndBalanceOfTreasury",
-                new Object[] {NOT_FROZEN_FUNGIBLE_TOKEN_ADDRESS, 10L, new long[0], SENDER_ADDRESS},
-                false),
-        WIPE_FUNGIBLE_TOKEN_WITH_ALIAS(
-                "wipeTokenGetTotalSupplyAndBalanceOfTreasury",
-                new Object[] {NOT_FROZEN_FUNGIBLE_TOKEN_ADDRESS, 10L, new long[0], SENDER_ALIAS},
-                false),
-        WIPE_NFT(
-                "wipeTokenGetTotalSupplyAndBalanceOfTreasury",
-                new Object[] {NFT_ADDRESS_WITH_DIFFERENT_OWNER_AND_TREASURY, 0L, new long[] {1L}, SENDER_ADDRESS},
-                false),
-        WIPE_NFT_ALIAS(
-                "wipeTokenGetTotalSupplyAndBalanceOfTreasury",
-                new Object[] {NFT_ADDRESS_WITH_DIFFERENT_OWNER_AND_TREASURY, 0L, new long[] {1L}, SENDER_ALIAS},
-                false),
-        PAUSE_UNPAUSE_FUNGIBLE_TOKEN(
-                "pauseTokenGetPauseStatusUnpauseGetPauseStatus", new Object[] {FUNGIBLE_TOKEN_ADDRESS}, false),
-        FREEZE_UNFREEZE_FUNGIBLE_TOKEN(
-                "freezeTokenGetPauseStatusUnpauseGetPauseStatus",
-                new Object[] {NOT_FROZEN_FUNGIBLE_TOKEN_ADDRESS, SPENDER_ADDRESS},
-                false),
-        PAUSE_UNPAUSE_NFT("pauseTokenGetPauseStatusUnpauseGetPauseStatus", new Object[] {NFT_ADDRESS}, false),
-        FREEZE_UNFREEZE_NFT(
-                "freezeTokenGetPauseStatusUnpauseGetPauseStatus", new Object[] {NFT_ADDRESS, SPENDER_ADDRESS}, false),
+        //        MINT_FUNGIBLE_TOKEN(
+        //                "mintTokenGetTotalSupplyAndBalanceOfTreasury",
+        //                new Object[] {NOT_FROZEN_FUNGIBLE_TOKEN_ADDRESS, 100L, new byte[0][0], TREASURY_ADDRESS},
+        //                false),
+        //        MINT_NFT(
+        //                "mintTokenGetTotalSupplyAndBalanceOfTreasury",
+        //                new Object[] {
+        //                    NFT_ADDRESS,
+        //                    0L,
+        //                    new byte[][] {ByteString.copyFromUtf8("firstMeta").toByteArray()},
+        //                    OWNER_ADDRESS
+        //                },
+        //                false),
+        //        BURN_FUNGIBLE_TOKEN(
+        //                "burnTokenGetTotalSupplyAndBalanceOfTreasury",
+        //                new Object[] {NOT_FROZEN_FUNGIBLE_TOKEN_ADDRESS, 12L, new long[0], TREASURY_ADDRESS},
+        //                false),
+        //        BURN_NFT(
+        //                "burnTokenGetTotalSupplyAndBalanceOfTreasury",
+        //                new Object[] {NFT_ADDRESS, 0L, new long[] {1L}, OWNER_ADDRESS},
+        //                false),
+        //        WIPE_FUNGIBLE_TOKEN(
+        //                "wipeTokenGetTotalSupplyAndBalanceOfTreasury",
+        //                new Object[] {NOT_FROZEN_FUNGIBLE_TOKEN_ADDRESS, 10L, new long[0], SENDER_ADDRESS},
+        //                false),
+        //        WIPE_FUNGIBLE_TOKEN_WITH_ALIAS(
+        //                "wipeTokenGetTotalSupplyAndBalanceOfTreasury",
+        //                new Object[] {NOT_FROZEN_FUNGIBLE_TOKEN_ADDRESS, 10L, new long[0], SENDER_ALIAS},
+        //                false),
+        //        WIPE_NFT(
+        //                "wipeTokenGetTotalSupplyAndBalanceOfTreasury",
+        //                new Object[] {NFT_ADDRESS_WITH_DIFFERENT_OWNER_AND_TREASURY, 0L, new long[] {1L},
+        // SENDER_ADDRESS},
+        //                false),
+        //        WIPE_NFT_ALIAS(
+        //                "wipeTokenGetTotalSupplyAndBalanceOfTreasury",
+        //                new Object[] {NFT_ADDRESS_WITH_DIFFERENT_OWNER_AND_TREASURY, 0L, new long[] {1L},
+        // SENDER_ALIAS},
+        //                false),
+        //        PAUSE_UNPAUSE_FUNGIBLE_TOKEN(
+        //                "pauseTokenGetPauseStatusUnpauseGetPauseStatus", new Object[] {FUNGIBLE_TOKEN_ADDRESS},
+        // false),
+        //        FREEZE_UNFREEZE_FUNGIBLE_TOKEN(
+        //                "freezeTokenGetPauseStatusUnpauseGetPauseStatus",
+        //                new Object[] {NOT_FROZEN_FUNGIBLE_TOKEN_ADDRESS, SPENDER_ADDRESS},
+        //                false),
+        //        PAUSE_UNPAUSE_NFT("pauseTokenGetPauseStatusUnpauseGetPauseStatus", new Object[] {NFT_ADDRESS}, false),
+        //        FREEZE_UNFREEZE_NFT(
+        //                "freezeTokenGetPauseStatusUnpauseGetPauseStatus", new Object[] {NFT_ADDRESS, SPENDER_ADDRESS},
+        // false),
         ASSOCIATE_DISSOCIATE_TRANSFER_FUNGIBLE_TOKEN_FAIL(
                 "associateTokenDissociateFailTransfer",
                 new Object[] {
-                    FUNGIBLE_TOKEN_ADDRESS, TREASURY_TOKEN_ADDRESS, SPENDER_ADDRESS, BigInteger.ONE, BigInteger.ZERO
+                    TREASURY_TOKEN_ADDRESS,
+                    SPENDER_ADDRESS,
+                    NESTED_ETH_CALLS_CONTRACT_ADDRESS,
+                    BigInteger.ONE,
+                    BigInteger.ZERO
                 },
-                true),
-        ASSOCIATE_DISSOCIATE_TRANSFER_NFT_FAIL(
-                "associateTokenDissociateFailTransfer",
-                new Object[] {NFT_ADDRESS, TREASURY_TOKEN_ADDRESS, SPENDER_ADDRESS, BigInteger.ZERO, BigInteger.ONE},
-                true),
-        APPROVE_FUNGIBLE_TOKEN_GET_ALLOWANCE(
-                "approveTokenGetAllowance",
-                new Object[] {FUNGIBLE_TOKEN_ADDRESS, OWNER_ADDRESS, BigInteger.ONE, BigInteger.ZERO},
-                false),
-        APPROVE_NFT_GET_ALLOWANCE(
-                "approveTokenGetAllowance",
-                new Object[] {NFT_ADDRESS, SPENDER_ADDRESS, BigInteger.ZERO, BigInteger.ONE},
-                false),
-        APPROVE_FUNGIBLE_TOKEN_TRANSFER_GET_ALLOWANCE(
-                "approveTokenTransferFromGetAllowanceGetBalance",
-                new Object[] {TREASURY_TOKEN_ADDRESS, SPENDER_ADDRESS, BigInteger.ONE, BigInteger.ZERO},
-                false);
+                true);
+        //        ASSOCIATE_DISSOCIATE_TRANSFER_NFT_FAIL(
+        //                "associateTokenDissociateFailTransfer",
+        //                new Object[] {NFT_ADDRESS, TREASURY_TOKEN_ADDRESS, SPENDER_ADDRESS, BigInteger.ZERO,
+        // BigInteger.ONE},
+        //                true),
+        //        APPROVE_FUNGIBLE_TOKEN_GET_ALLOWANCE(
+        //                "approveTokenGetAllowance",
+        //                new Object[] {FUNGIBLE_TOKEN_ADDRESS, OWNER_ADDRESS, BigInteger.ONE, BigInteger.ZERO},
+        //                false),
+        //        APPROVE_NFT_GET_ALLOWANCE(
+        //                "approveTokenGetAllowance",
+        //                new Object[] {NFT_ADDRESS, SPENDER_ADDRESS, BigInteger.ZERO, BigInteger.ONE},
+        //                false),
+        //        APPROVE_FUNGIBLE_TOKEN_TRANSFER_GET_ALLOWANCE(
+        //                "approveTokenTransferFromGetAllowanceGetBalance",
+        //                new Object[] {TREASURY_TOKEN_ADDRESS, SPENDER_ADDRESS, BigInteger.ONE, BigInteger.ZERO},
+        //                false);
         private final String name;
         private final Object[] functionParameters;
         private final boolean shouldFail;
--- hedera-mirror-web3/src/test/resources/contracts/NestedEthCalls/NestedEthCalls.sol
+++ hedera-mirror-web3/src/test/resources/contracts/NestedEthCalls/NestedEthCalls.sol
@@ -1,4 +1,3 @@
-
 pragma solidity ^0.8.0;
 
 import "./HederaTokenService.sol";
@@ -6,13 +5,10 @@ import "./HederaResponseCodes.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-
 contract NestedEthCalls is HederaTokenService {
 
-
     // Mint fungible/non-fungible token + get token info total supply+ get balance of the treasury
-    function mintTokenGetTotalSupplyAndBalanceOfTreasury(address token, int64 amount, bytes[] memory metadata, address treasury) external
-    {
+    function mintTokenGetTotalSupplyAndBalanceOfTreasury(address token, int64 amount, bytes[] memory metadata, address treasury) external {
         uint256 balanceBeforeMint = 0;
         if(amount > 0 && metadata.length == 0) {
             balanceBeforeMint = IERC20(token).balanceOf(treasury);
@@ -21,35 +17,33 @@ contract NestedEthCalls is HederaTokenService {
         }
 
         (int responseCode, IHederaTokenService.TokenInfo memory retrievedTokenInfo) = HederaTokenService.getTokenInfo(token);
-        if (responseCode != HederaResponseCodes.SUCCESS) revert();
+        if (responseCode != HederaResponseCodes.SUCCESS) revert("Failed to retrieve token info");
         int totalSupplyBeforeMint = retrievedTokenInfo.totalSupply;
 
         (responseCode,,) = HederaTokenService.mintToken(token, amount, metadata);
-        if (responseCode != HederaResponseCodes.SUCCESS) revert();
+        if (responseCode != HederaResponseCodes.SUCCESS) revert("Failed to mint token");
 
         (responseCode, retrievedTokenInfo) = HederaTokenService.getTokenInfo(token);
-
-        if (responseCode != HederaResponseCodes.SUCCESS) revert ();
+        if (responseCode != HederaResponseCodes.SUCCESS) revert ("Failed to retrieve token info after mint");
 
         int totalSupplyAfterMint = retrievedTokenInfo.totalSupply;
         if(amount > 0 && metadata.length == 0) {
-            if(totalSupplyBeforeMint + amount != totalSupplyAfterMint) revert();
+            if(totalSupplyBeforeMint + amount != totalSupplyAfterMint) revert("Total supply mismatch after mint (Fungible)");
         } else {
-            if(totalSupplyBeforeMint + int256(metadata.length) != totalSupplyAfterMint) revert();
+            if(totalSupplyBeforeMint + int256(metadata.length) != totalSupplyAfterMint) revert("Total supply mismatch after mint (NFT)");
         }
 
         if(amount > 0 && metadata.length == 0) {
             uint256 balanceAfterMint = IERC20(token).balanceOf(treasury);
-            if(balanceAfterMint != balanceBeforeMint + uint256(int256(amount))) revert();
+            if(balanceBeforeMint + uint256(int256(amount)) != balanceAfterMint) revert("Balance mismatch after mint (Fungible)");
         } else {
             uint256 balanceAfterMint  = IERC721(token).balanceOf(treasury);
-            if(balanceAfterMint != balanceBeforeMint + uint256(int256(metadata.length))) revert();
+            if(balanceBeforeMint + uint256(int256(metadata.length)) != balanceAfterMint) revert("Balance mismatch after mint (NFT)");
         }
     }
 
     // Burn fungible/non-fungible token + get token info total supply + get balance of the treasury
-    function burnTokenGetTotalSupplyAndBalanceOfTreasury(address token, int64 amount, int64[] memory serialNumbers, address treasury) external
-    {
+    function burnTokenGetTotalSupplyAndBalanceOfTreasury(address token, int64 amount, int64[] memory serialNumbers, address treasury) external {
         uint256 balanceBeforeBurn = 0;
         if(amount > 0 && serialNumbers.length == 0) {
             balanceBeforeBurn = IERC20(token).balanceOf(treasury);
@@ -58,35 +52,33 @@ contract NestedEthCalls is HederaTokenService {
         }
 
         (int responseCode, IHederaTokenService.TokenInfo memory retrievedTokenInfo) = HederaTokenService.getTokenInfo(token);
-        if (responseCode != HederaResponseCodes.SUCCESS) revert();
+        if (responseCode != HederaResponseCodes.SUCCESS) revert("Failed to retrieve token info before burn");
         int totalSupplyBeforeBurn = retrievedTokenInfo.totalSupply;
 
         (responseCode,) = HederaTokenService.burnToken(token, amount, serialNumbers);
-        if (responseCode != HederaResponseCodes.SUCCESS) revert();
+        if (responseCode != HederaResponseCodes.SUCCESS) revert("Failed to burn token");
 
         (responseCode, retrievedTokenInfo) = HederaTokenService.getTokenInfo(token);
-
-        if (responseCode != HederaResponseCodes.SUCCESS) revert ();
+        if (responseCode != HederaResponseCodes.SUCCESS) revert ("Failed to retrieve token info after burn");
 
         int totalSupplyAfterBurn = retrievedTokenInfo.totalSupply;
         if(amount > 0 && serialNumbers.length == 0) {
-            if(totalSupplyBeforeBurn - amount != totalSupplyAfterBurn) revert();
+            if(totalSupplyBeforeBurn - amount != totalSupplyAfterBurn) revert("Total supply mismatch after burn (Fungible)");
         } else {
-            if(totalSupplyBeforeBurn - int256(serialNumbers.length) != totalSupplyAfterBurn) revert();
+            if(totalSupplyBeforeBurn - int256(serialNumbers.length) != totalSupplyAfterBurn) revert("Total supply mismatch after burn (NFT)");
         }
 
         if(amount > 0 && serialNumbers.length == 0) {
             uint256 balanceAfterBurn = IERC20(token).balanceOf(treasury);
-            if(balanceAfterBurn != balanceBeforeBurn - uint256(int256(amount))) revert();
+            if(balanceBeforeBurn - uint256(int256(amount)) != balanceAfterBurn) revert("Balance mismatch after burn (Fungible)");
         } else {
             uint256 balanceAfterBurn  = IERC721(token).balanceOf(treasury);
-            if(balanceAfterBurn != balanceBeforeBurn - uint256(int256(serialNumbers.length))) revert();
+            if(balanceBeforeBurn - uint256(int256(serialNumbers.length)) != balanceAfterBurn) revert("Balance mismatch after burn (NFT)");
         }
     }
 
     // Wipe + get token info total supply + get balance of the account which balance was wiped
-    function wipeTokenGetTotalSupplyAndBalanceOfTreasury(address token, int64 amount, int64[] memory serialNumbers, address treasury) external
-    {
+    function wipeTokenGetTotalSupplyAndBalanceOfTreasury(address token, int64 amount, int64[] memory serialNumbers, address treasury) external {
         uint256 balanceBeforeWipe = 0;
         if(amount > 0 && serialNumbers.length == 0) {
             balanceBeforeWipe = IERC20(token).balanceOf(treasury);
@@ -95,71 +87,67 @@ contract NestedEthCalls is HederaTokenService {
         }
 
         (int responseCode, IHederaTokenService.TokenInfo memory retrievedTokenInfo) = HederaTokenService.getTokenInfo(token);
-        if (responseCode != HederaResponseCodes.SUCCESS) revert();
-
+        if (responseCode != HederaResponseCodes.SUCCESS) revert("Failed to retrieve token info before wipe");
         int totalSupplyBeforeWipe = retrievedTokenInfo.totalSupply;
-
 
         if(amount > 0 && serialNumbers.length == 0) {
             responseCode = HederaTokenService.wipeTokenAccount(token, treasury, amount);
-
         } else {
             responseCode = HederaTokenService.wipeTokenAccountNFT(token, treasury, serialNumbers);
         }
-
-        if (responseCode != HederaResponseCodes.SUCCESS) revert();
+        if (responseCode != HederaResponseCodes.SUCCESS) revert("Failed to wipe token");
 
         (responseCode, retrievedTokenInfo) = HederaTokenService.getTokenInfo(token);
+        if (responseCode != HederaResponseCodes.SUCCESS) revert("Failed to retrieve token info after wipe");
 
-        if (responseCode != HederaResponseCodes.SUCCESS) revert();
-
-        int totalSupplyAfterBurn = retrievedTokenInfo.totalSupply;
+        int totalSupplyAfterWipe = retrievedTokenInfo.totalSupply;
         if(amount > 0 && serialNumbers.length == 0) {
-            if(totalSupplyBeforeWipe - amount != totalSupplyAfterBurn) revert();
+            if(totalSupplyBeforeWipe - amount != totalSupplyAfterWipe) revert("Total supply mismatch after wipe (Fungible)");
         } else {
-            if(totalSupplyBeforeWipe - int256(serialNumbers.length) != totalSupplyAfterBurn) revert();
+            if(totalSupplyBeforeWipe - int256(serialNumbers.length) != totalSupplyAfterWipe) revert("Total supply mismatch after burn (NFT)");
         }
 
         if(amount > 0 && serialNumbers.length == 0) {
             uint256 balanceAfterWipe = IERC20(token).balanceOf(treasury);
-            if(balanceAfterWipe != balanceBeforeWipe - uint256(int256(amount))) revert();
+            if(balanceBeforeWipe - uint256(int256(amount)) != balanceAfterWipe) revert("Balance mismatch after wipe (Fungible)");
         } else {
-            uint256 balanceAfterWipe  = IERC721(token).balanceOf(treasury);
-            if(balanceAfterWipe != balanceBeforeWipe - uint256(int256(serialNumbers.length))) revert();
+            uint256 balanceAfterWipe = IERC721(token).balanceOf(treasury);
+            if(balanceBeforeWipe - uint256(int256(serialNumbers.length)) != balanceAfterWipe) revert("Balance mismatch after wipe (NFT)");
         }
     }
 
     // Pause fungible/non-fungible token + get token info pause status + unpause + get token info pause status
     function pauseTokenGetPauseStatusUnpauseGetPauseStatus(address token) external {
         int responseCode = HederaTokenService.pauseToken(token);
-        if (responseCode != HederaResponseCodes.SUCCESS) revert();
+        if (responseCode != HederaResponseCodes.SUCCESS) revert("Failed to pause token");
 
         (int response, IHederaTokenService.TokenInfo memory retrievedTokenInfo) = HederaTokenService.getTokenInfo(token);
-        if (response != HederaResponseCodes.SUCCESS) revert();
-        if(!retrievedTokenInfo.pauseStatus) revert();
+        if (response != HederaResponseCodes.SUCCESS) revert("Failed to get token info after pause");
+        if(!retrievedTokenInfo.pauseStatus) revert("Token is not paused");
 
         responseCode = HederaTokenService.unpauseToken(token);
-        if (responseCode != HederaResponseCodes.SUCCESS) revert();
+        if (responseCode != HederaResponseCodes.SUCCESS) revert("Failed to unpause token");
+
         (response, retrievedTokenInfo) = HederaTokenService.getTokenInfo(token);
-        if(response != HederaResponseCodes.SUCCESS) revert();
-        if(retrievedTokenInfo.pauseStatus) revert();
+        if(response != HederaResponseCodes.SUCCESS) revert("Failed to retrieve token info after unpause");
+        if(retrievedTokenInfo.pauseStatus) revert("Token is still paused");
     }
 
     // Freeze fungible/non-fungible token + get token info freeze status + unfreeze + get token info freeze status
     function freezeTokenGetPauseStatusUnpauseGetPauseStatus(address token, address account) external {
         int responseCode = HederaTokenService.freezeToken(token, account);
-        if (responseCode != HederaResponseCodes.SUCCESS) revert();
+        if (responseCode != HederaResponseCodes.SUCCESS) revert("Failed to freeze token for the account");
 
         (int response, bool isFrozen) = HederaTokenService.isFrozen(token, account);
-        if (response != HederaResponseCodes.SUCCESS) revert();
-        if(!isFrozen) revert();
+        if (response != HederaResponseCodes.SUCCESS) revert("Failed to check freeze status of account");
+        if(!isFrozen) revert("Account is not frozen");
 
         responseCode = HederaTokenService.unfreezeToken(token, account);
-        if (responseCode != HederaResponseCodes.SUCCESS) revert();
+        if (responseCode != HederaResponseCodes.SUCCESS) revert("Failed to unfreeze account");
 
         (response, isFrozen) = HederaTokenService.isFrozen(token, account);
-        if (response != HederaResponseCodes.SUCCESS) revert();
-        if(isFrozen) revert();
+        if (response != HederaResponseCodes.SUCCESS) revert("Failed to check unfreeze status of account");
+        if(isFrozen) revert("Account is still frozen");
     }
 
     // Associate fungible/non-fungible token transfer (should pass) + dissociate + transfer (should fail)
@@ -167,10 +155,10 @@ contract NestedEthCalls is HederaTokenService {
         address[] memory tokens = new address[](1);
         tokens[0] = token;
         int responseCode = HederaTokenService.associateTokens(from, tokens);
-        if (responseCode != HederaResponseCodes.SUCCESS) revert();
+        if (responseCode != HederaResponseCodes.SUCCESS) revert("Failed to associate tokens");
 
         responseCode = HederaTokenService.dissociateTokens(from, tokens);
-        if (responseCode != HederaResponseCodes.SUCCESS) revert();
+        if (responseCode != HederaResponseCodes.SUCCESS) revert("Failed to dissociate tokens");
 
         if(amount > 0 && serialNumber == 0) {
             try IERC20(token).transferFrom(from, to, amount) {
@@ -178,9 +166,9 @@ contract NestedEthCalls is HederaTokenService {
                 revert("IERC20 transfer failed");
             }
         } else {
-            try IERC721(token).transferFrom(from, to, amount) {
+            try IERC721(token).transferFrom(from, to, serialNumber) {
             } catch {
-                revert("IERC20 transfer failed");
+                revert("IERC721 transfer failed");
             }
         }
     }
@@ -189,12 +177,12 @@ contract NestedEthCalls is HederaTokenService {
     function approveTokenGetAllowance(address token, address spender, uint256 amount, uint256 serialNumber) external {
         if(amount > 0 && serialNumber == 0) {
             int responseCode = HederaTokenService.approve(token, spender, amount);
-            if (responseCode != HederaResponseCodes.SUCCESS) revert();
-            if(IERC20(token).allowance(address(this), spender) != amount) revert();
+            if (responseCode != HederaResponseCodes.SUCCESS) revert("Failed to approve token");
+            if(IERC20(token).allowance(address(this), spender) != amount) revert("Allowance mismatch");
         } else {
             int responseCode = HederaTokenService.approveNFT(token, spender, serialNumber);
-            if (responseCode != HederaResponseCodes.SUCCESS) revert();
-            if(IERC721(token).getApproved(serialNumber) != spender) revert();
+            if (responseCode != HederaResponseCodes.SUCCESS) revert("Failed to approve NFT");
+            if(IERC721(token).getApproved(serialNumber) != spender) revert("NFT approval mismatch");
         }
     }
 
@@ -202,18 +190,19 @@ contract NestedEthCalls is HederaTokenService {
     function approveTokenTransferFromGetAllowanceGetBalance(address token, address spender, uint256 amount, uint256 serialNumber) external {
         if(amount > 0 && serialNumber == 0) {
             int responseCode = HederaTokenService.approve(token, spender, amount);
-            if (responseCode != HederaResponseCodes.SUCCESS) revert();
+            if (responseCode != HederaResponseCodes.SUCCESS) revert("Failed to approve token for transfer");
             uint256 balanceBeforeTransfer = IERC20(token).balanceOf(spender);
-            if(IERC20(token).allowance(address(this), spender) != amount) revert();
+            if(IERC20(token).allowance(address(this), spender) != amount) revert("Allowance mismatch before transfer");
             IERC20(token).transferFrom(address(this), spender, amount);
-            if(IERC20(token).balanceOf(spender) != balanceBeforeTransfer + amount) revert();
+            if(IERC20(token).balanceOf(spender) != balanceBeforeTransfer + amount) revert("Balance mismatch after transfer");
         } else {
             HederaTokenService.transferNFT(token, IERC721(token).ownerOf(serialNumber), address(this), int64(int256(serialNumber)));
             int responseCode = HederaTokenService.approveNFT(token, spender, serialNumber);
-            if (responseCode != HederaResponseCodes.SUCCESS) revert();
-            if(IERC721(token).getApproved(serialNumber) != spender) revert();
+            if (responseCode != HederaResponseCodes.SUCCESS) revert("Failed to approve NFT for transfer");
+            if(IERC721(token).getApproved(serialNumber) != spender) revert("NFT approval mismatch before transfer");
             IERC721(token).transferFrom(address(this), spender, serialNumber);
-            if(IERC721(token).ownerOf(serialNumber) != spender) revert();
+            if(IERC721(token).ownerOf(serialNumber) != spender) revert("NFT ownership mismatch after transfer");
         }
     }
+
 }
