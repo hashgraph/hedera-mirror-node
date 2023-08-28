8d0f11eead89983e757fdc5f4f024119341d3bfc
Xin Li <xin@swirldslabs.com>
2023-08-28 11:18:44 -0500

    polish comments
    Signed-off-by: Xin Li <xin@swirldslabs.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v1/R__autovacuum_insert_only_tables.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/v1/R__autovacuum_insert_only_tables.sql
@@ -36,13 +36,13 @@ alter table if exists account_balance_file set (
   autovacuum_vacuum_insert_threshold = 96
   );
 
--- based on 12 contract actions per smart contract transaction
+-- based on 12 contract actions per smart contract transaction and max 300 TPS
 alter table if exists contract_action set (
   autovacuum_vacuum_insert_scale_factor = 0,
   autovacuum_vacuum_insert_threshold = 2160000
   );
 
--- base on average of 3 contract logs per smart contract transaction
+-- base on average of 3 contract logs per smart contract transaction and max 300 TPS
 alter table if exists contract_log set (
   autovacuum_vacuum_insert_scale_factor = 0,
   autovacuum_vacuum_insert_threshold = 540000
@@ -54,7 +54,7 @@ alter table if exists contract_result set (
   autovacuum_vacuum_insert_threshold = 180000
   );
 
--- based on average of 10 state changes per smart contract transaction
+-- based on average of 10 state changes per smart contract transaction and max 300 TPS
 alter table if exists contract_state_change set (
   autovacuum_vacuum_insert_scale_factor = 0,
   autovacuum_vacuum_insert_threshold = 1800000
@@ -66,7 +66,7 @@ alter table if exists crypto_transfer set (
   autovacuum_vacuum_insert_threshold = 24000000
   );
 
--- adjust when there are higher number of ethereum transactions per second
+-- adjust when ethereum transaction TPS becomes higher
 alter table if exists ethereum_transaction set (autovacuum_vacuum_insert_scale_factor = 0.1);
 
 -- autovacuum every 60 minutes with 2s interval record files
@@ -75,14 +75,15 @@ alter table if exists record_file set (
   autovacuum_vacuum_insert_threshold = 1800
   );
 
--- normally there is just one sidecar record per each record file
+-- autovacuum every 60 minutes with 2s interval record files. Note normally there is just one sidecar record
+-- per each record file
 alter table if exists sidecar_file set (
   autovacuum_vacuum_insert_scale_factor = 0,
   autovacuum_vacuum_insert_threshold = 1800
   );
 
 -- threshold set based on recent stats so the table has about 2 to 3 automatic vacuums per day, adjust if there's more
--- daily pending reward claim
+-- daily staking reward claims
 alter table if exists staking_reward_transfer set (
   autovacuum_vacuum_insert_scale_factor = 0,
   autovacuum_vacuum_insert_threshold = 4000
@@ -94,7 +95,7 @@ alter table if exists token_balance set (
   autovacuum_vacuum_insert_threshold = 10000
   );
 
--- autovacuum every 10 minutes at 10K TPS crypto transaction transaction with two token transfer rows per transaction
+-- autovacuum every 10 minutes at 10K TPS crypto transfer transactions with two token transfer rows per transaction
 alter table if exists token_transfer set (
   autovacuum_vacuum_insert_scale_factor = 0,
   autovacuum_vacuum_insert_threshold = 12000000
