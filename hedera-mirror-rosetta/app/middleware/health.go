09aab9f1f32f0246f87fb9d1e5a52a6994555fc9
Steven Sheehy <steven.sheehy@swirldslabs.com>
2023-09-06 12:40:08 -0500

    Revert rosetta readiness probe change
    Signed-off-by: Steven Sheehy <steven.sheehy@swirldslabs.com>

--- hedera-mirror-rosetta/app/middleware/health.go
+++ hedera-mirror-rosetta/app/middleware/health.go
@@ -101,9 +101,16 @@ func checkNetworkStatus(port uint16) func(ctx context.Context) error {
 	rosettaClient := client.NewAPIClient(cfg)
 
 	return func(ctx context.Context) (checkErr error) {
-		_, _, err := rosettaClient.NetworkAPI.NetworkList(ctx, &types.MetadataRequest{})
+		networkList, _, err := rosettaClient.NetworkAPI.NetworkList(ctx, &types.MetadataRequest{})
 		if err != nil {
 			log.Errorf("Readiness check, /network/list failed: %v", err)
+			return err
+		}
+
+		network := networkList.NetworkIdentifiers[0]
+		_, _, err = rosettaClient.NetworkAPI.NetworkStatus(ctx, &types.NetworkRequest{NetworkIdentifier: network})
+		if err != nil {
+			log.Errorf("Readiness check, /network/status failed: %v", err)
 			return err
 		}
 
