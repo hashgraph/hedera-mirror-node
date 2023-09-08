ad4dce5311fc449f4735e670ddb3dd1b2b0a6187
Steven Sheehy <17552371+steven-sheehy@users.noreply.github.com>
2023-09-08 13:28:18 -0500

    Switch tests to use OutputCaptureExtension (#6832)
    * Change tests to use Spring's logging framework agnostic `OutputCaptureExtension`
    * Fix monitor and web3 including sl4fj implementations on the classpath
    
    Signed-off-by: Steven Sheehy <steven.sheehy@swirldslabs.com>

--- hedera-mirror-monitor/src/test/java/com/hedera/mirror/monitor/config/LoggingFilterTest.java
+++ hedera-mirror-monitor/src/test/java/com/hedera/mirror/monitor/config/LoggingFilterTest.java
@@ -18,56 +18,38 @@ package com.hedera.mirror.monitor.config;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.StringWriter;
 import java.time.Duration;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
-import org.apache.logging.log4j.core.appender.WriterAppender;
-import org.apache.logging.log4j.core.layout.PatternLayout;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
 import org.springframework.mock.web.server.MockServerWebExchange;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+@ExtendWith(OutputCaptureExtension.class)
 class LoggingFilterTest {
 
     private static final Duration WAIT = Duration.ofSeconds(10L);
 
-    private LoggingFilter loggingFilter;
-    private StringWriter logOutput;
-    private WriterAppender appender;
+    private final LoggingFilter loggingFilter = new LoggingFilter();
 
-    @BeforeEach
-    void setup() {
-        loggingFilter = new LoggingFilter();
-        logOutput = new StringWriter();
-        appender = WriterAppender.newBuilder()
-                .setLayout(PatternLayout.newBuilder().withPattern("%p|%m%n").build())
-                .setName("stringAppender")
-                .setTarget(logOutput)
-                .build();
-        Logger logger = (Logger) LogManager.getLogger(loggingFilter);
-        logger.addAppender(appender);
+    @BeforeAll
+    static void setup() {
+        var logger = (Logger) LogManager.getLogger(LoggingFilter.class);
         logger.setLevel(Level.DEBUG);
-        appender.start();
-    }
-
-    @AfterEach
-    void cleanup() {
-        appender.stop();
-        Logger logger = (Logger) LogManager.getLogger(loggingFilter);
-        logger.removeAppender(appender);
     }
 
     @CsvSource({"/, 200, INFO", "/actuator/, 200, DEBUG"})
     @ParameterizedTest
-    void filterOnSuccess(String path, int code, String level) {
+    void filterOnSuccess(String path, int code, String level, CapturedOutput output) {
         MockServerWebExchange exchange =
                 MockServerWebExchange.from(MockServerHttpRequest.get(path).build());
         exchange.getResponse().setRawStatusCode(code);
@@ -80,13 +62,13 @@ class LoggingFilterTest {
                 .expectComplete()
                 .verify(WAIT);
 
-        assertLog(Level.getLevel(level), "\\w+ GET " + path + " in \\d+ ms: " + code);
+        assertLog(output, level, "\\w+ GET " + path + " in \\d+ ms: " + code);
     }
 
     @Test
-    void filterXForwardedFor() {
+    void filterXForwardedFor(CapturedOutput output) {
         String clientIp = "10.0.0.100";
-        MockServerWebExchange exchange = MockServerWebExchange.from(MockServerHttpRequest.get("/")
+        var exchange = MockServerWebExchange.from(MockServerHttpRequest.get("/")
                 .header(LoggingFilter.X_FORWARDED_FOR, clientIp)
                 .build());
         exchange.getResponse().setRawStatusCode(200);
@@ -99,26 +81,24 @@ class LoggingFilterTest {
                 .expectComplete()
                 .verify(WAIT);
 
-        assertLog(Level.INFO, clientIp + " GET / in \\d+ ms: 200");
+        assertLog(output, "INFO", clientIp + " GET / in \\d+ ms: 200");
     }
 
     @Test
-    void filterOnCancel() {
-        MockServerWebExchange exchange =
-                MockServerWebExchange.from(MockServerHttpRequest.get("/").build());
+    void filterOnCancel(CapturedOutput output) {
+        var exchange = MockServerWebExchange.from(MockServerHttpRequest.get("/").build());
 
         StepVerifier.withVirtualTime(() -> loggingFilter.filter(
                         exchange, serverWebExchange -> exchange.getResponse().setComplete()))
                 .thenCancel()
                 .verify(WAIT);
 
-        assertLog(Level.WARN, "\\w+ GET / in \\d+ ms: cancelled");
+        assertLog(output, "WARN", "\\w+ GET / in \\d+ ms: cancelled");
     }
 
     @Test
-    void filterOnError() {
-        MockServerWebExchange exchange =
-                MockServerWebExchange.from(MockServerHttpRequest.get("/").build());
+    void filterOnError(CapturedOutput output) {
+        var exchange = MockServerWebExchange.from(MockServerHttpRequest.get("/").build());
         exchange.getResponse().setRawStatusCode(500);
 
         var exception = new IllegalArgumentException("error");
@@ -129,14 +109,10 @@ class LoggingFilterTest {
                 .expectComplete()
                 .verify(WAIT);
 
-        assertLog(Level.WARN, "\\w+ GET / in \\d+ ms: " + exception.getMessage());
+        assertLog(output, "WARN", "\\w+ GET / in \\d+ ms: " + exception.getMessage());
     }
 
-    private void assertLog(Level level, String pattern) {
-        assertThat(logOutput)
-                .asString()
-                .hasLineCount(1)
-                .contains(level.toString())
-                .containsPattern(pattern);
+    private void assertLog(CapturedOutput logOutput, String level, String pattern) {
+        assertThat(logOutput).asString().hasLineCount(1).contains(level).containsPattern(pattern);
     }
 }
