575ca78e3ea4b04536cafb1f5847a9e9848fead7
Edwin Greene <edwin@swirldslabs.com>
2023-09-21 14:09:24 -0400

    Rename test and migration
    Signed-off-by: Edwin Greene <edwin@swirldslabs.com>

---
+++ hedera-mirror-importer/src/test/java/com/hedera/mirror/importer/migration/AddNftHistoryTimestampRangesTest.java
@@ -0,0 +1,158 @@
+/*
+ * Copyright (C) 2023 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hedera.mirror.importer.migration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.google.common.collect.Range;
+import com.hedera.mirror.common.domain.token.NftHistory;
+import com.hedera.mirror.importer.DisableRepeatableSqlMigration;
+import com.hedera.mirror.importer.EnabledIfV1;
+import com.hedera.mirror.importer.IntegrationTest;
+import com.hedera.mirror.importer.config.Owner;
+import java.io.File;
+import java.util.ArrayList;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.test.context.TestPropertySource;
+
+@DisableRepeatableSqlMigration
+@EnabledIfV1
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Tag("migration")
+@TestPropertySource(properties = "spring.flyway.target=1.87.2")
+class AddNftHistoryTimestampRangesMigrationTest extends IntegrationTest {
+
+    private static final RowMapper<NftHistory> NFT_HISTORY_ROW_MAPPER = rowMapper(NftHistory.class);
+
+    private final @Owner JdbcOperations jdbcOperations;
+
+    @Value("classpath:db/migration/v1/V1.88.0__add_nft_history_timestamp_ranges.sql")
+    private File migrationSql;
+
+    @Test
+    void migrateEmpty() {
+        runMigration();
+        assertThat(findAllNftHistories()).isEmpty();
+    }
+
+    @Test
+    void migrate() {
+        // given
+        var expectedNftHistories = new ArrayList<NftHistory>();
+        var tokenId = 100L;
+        var serialNumber = 10L;
+        expectedNftHistories.add(domainBuilder
+                .nftHistory()
+                .customize(n -> n.tokenId(tokenId)
+                        .serialNumber(serialNumber)
+                        .timestampRange(Range.closedOpen(1683843345425999227L, 1683843345425999228L)))
+                .persist());
+        expectedNftHistories.add(domainBuilder
+                .nftHistory()
+                .customize(n -> n.tokenId(tokenId)
+                        .serialNumber(serialNumber)
+                        .timestampRange(Range.closedOpen(1683843345425999228L, 1685285944321751033L)))
+                .persist());
+        // This history entry's timestamp_range doesn't match the previous entry's timestamp_range
+        var nftHistory3 = domainBuilder
+                .nftHistory()
+                .customize(n -> n.tokenId(tokenId)
+                        .serialNumber(serialNumber)
+                        .timestampRange(Range.closedOpen(1688402586436126488L, 1689891172344878003L)))
+                .persist();
+        expectedNftHistories.add(nftHistory3);
+
+        // The migration will create this history entry to connect the two timestamp_ranges
+        var splicedHistory1 = domainBuilder
+                .nftHistory()
+                .customize(n -> n.accountId(nftHistory3.getAccountId())
+                        .createdTimestamp(nftHistory3.getCreatedTimestamp())
+                        .metadata(nftHistory3.getMetadata())
+                        .serialNumber(serialNumber)
+                        .timestampRange(Range.closedOpen(1685285944321751033L, 1688402586436126488L))
+                        .tokenId(tokenId))
+                .get();
+        expectedNftHistories.add(splicedHistory1);
+
+        var tokenId2 = 200L;
+        expectedNftHistories.add(domainBuilder
+                .nftHistory()
+                .customize(n -> n.tokenId(tokenId2).serialNumber(serialNumber).timestampRange(Range.closedOpen(1L, 2L)))
+                .persist());
+        expectedNftHistories.add(domainBuilder
+                .nftHistory()
+                .customize(n -> n.tokenId(tokenId2).serialNumber(serialNumber).timestampRange(Range.closedOpen(2L, 3L)))
+                .persist());
+        // This history's lower timestamp range doesn't match the upper timestamp range of the previous history
+        var token2History3 = domainBuilder
+                .nftHistory()
+                .customize(n -> n.tokenId(tokenId2).serialNumber(serialNumber).timestampRange(Range.closedOpen(4L, 5L)))
+                .persist();
+        expectedNftHistories.add(token2History3);
+        // The migration will add this history entry to connect the two timestamp_ranges
+        var splicedHistory2 = domainBuilder
+                .nftHistory()
+                .customize(n -> n.accountId(token2History3.getAccountId())
+                        .createdTimestamp(token2History3.getCreatedTimestamp())
+                        .metadata(token2History3.getMetadata())
+                        .serialNumber(token2History3.getSerialNumber())
+                        .timestampRange(Range.closedOpen(3L, 4L))
+                        .tokenId(token2History3.getTokenId()))
+                .get();
+        expectedNftHistories.add(splicedHistory2);
+        expectedNftHistories.add(domainBuilder
+                .nftHistory()
+                .customize(n -> n.tokenId(tokenId2).serialNumber(serialNumber).timestampRange(Range.closedOpen(5L, 6L)))
+                .persist());
+
+        // A history without any other matching serial number entries
+        expectedNftHistories.add(domainBuilder
+                .nftHistory()
+                .customize(n ->
+                        n.tokenId(tokenId2).serialNumber(serialNumber + 1).timestampRange(Range.closedOpen(2L, 3L)))
+                .persist());
+        // A history without any other matching token id entries
+        expectedNftHistories.add(domainBuilder
+                .nftHistory()
+                .customize(n ->
+                        n.tokenId(tokenId2 + 1).serialNumber(serialNumber).timestampRange(Range.closedOpen(2L, 3L)))
+                .persist());
+
+        // when
+        runMigration();
+
+        // then
+        assertThat(findAllNftHistories()).containsExactlyInAnyOrderElementsOf(expectedNftHistories);
+    }
+
+    protected Iterable<NftHistory> findAllNftHistories() {
+        return jdbcOperations.query("select * from nft_history", NFT_HISTORY_ROW_MAPPER);
+    }
+
+    @SneakyThrows
+    private void runMigration() {
+        jdbcOperations.update(FileUtils.readFileToString(migrationSql, "UTF-8"));
+    }
+}
