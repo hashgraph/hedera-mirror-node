d333948122276c9f70c037eb7d1ba9d29f413048
Xin Li <xin@swirldslabs.com>
2023-11-07 12:59:19 -0600

    fix partitionStartDate in rosetta integration test
    Signed-off-by: Xin Li <xin@swirldslabs.com>

--- hedera-mirror-rosetta/test/db/db.go
+++ hedera-mirror-rosetta/test/db/db.go
@@ -253,7 +253,7 @@ func runFlywayMigration(pool *dockertest.Pool, network *dockertest.Network, para
 		"placeholders.idPartitionSize":                  "1000000000000000",
 		"placeholders.maxEntityId":                      "5000000",
 		"placeholders.maxEntityIdRatio":                 "2.0",
-		"placeholders.partitionStartDate":               "(CURRENT_TIMESTAMP - '1970-01-01 00:00:00.000')",
+		"placeholders.partitionStartDate":               "'1970-01-01'",
 		"placeholders.partitionTimeInterval":            "'10 years'",
 		"placeholders.db-name":                          params.name,
 		"placeholders.db-user":                          params.username,
