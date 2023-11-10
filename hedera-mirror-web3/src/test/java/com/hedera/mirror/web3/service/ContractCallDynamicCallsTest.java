62f5dc0a52ba6f6e5e0234154ebdd8e11510b069
Ivan Ivanov <ivanivanov.ii726@gmail.com>
2023-11-10 16:07:52 +0200

    refactor transferFrom fungible token nested test
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>

--- hedera-mirror-web3/src/test/java/com/hedera/mirror/web3/service/ContractCallDynamicCallsTest.java
+++ hedera-mirror-web3/src/test/java/com/hedera/mirror/web3/service/ContractCallDynamicCallsTest.java
@@ -169,6 +169,11 @@ class ContractCallDynamicCallsTest extends ContractCallTestSetup {
                 "approveTokenGetAllowance",
                 new Object[] {NFT_ADDRESS, SPENDER_ALIAS, BigInteger.ZERO, BigInteger.ONE},
                 null),
+
+        APPROVE_FUNGIBLE_TOKEN_TRANSFER_FROM_GET_ALLOWANCE(
+                "approveTokenTransferFromGetAllowanceGetBalance",
+                new Object[] {TREASURY_TOKEN_ADDRESS, SPENDER_ALIAS, BigInteger.ONE, BigInteger.ZERO},
+                null),
         APPROVE_FUNGIBLE_TOKEN_TRANSFER_GET_ALLOWANCE(
                 "approveTokenTransferGetAllowanceGetBalance",
                 new Object[] {TREASURY_TOKEN_ADDRESS, SPENDER_ALIAS, BigInteger.ONE, BigInteger.ZERO},
