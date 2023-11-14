ffe3e71abc2a1bedb3784fc89df14f4f3de8ff90
Yordan Iliev <yordan.iliev@limechain.tech>
2023-11-14 17:38:19 +0200

    Reformat contracts
    Signed-off-by: Yordan Iliev <yordan.iliev@limechain.tech>

--- hedera-mirror-test/src/test/resources/solidity/contracts/EquivalenceContract.sol
+++ hedera-mirror-test/src/test/resources/solidity/contracts/EquivalenceContract.sol
@@ -1,24 +1,24 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
 contract EquivalenceContract {
-    function makeCallWithoutAmount(address _to, bytes memory _data) public returns (bool success, bytes memory returnData){
+    function makeCallWithoutAmount(address _to, bytes memory _data) external returns (bool success, bytes memory returnData) {
         (success, returnData) = _to.call(_data);
     }
 
-    function makeCallWithAmount(address _to, bytes memory _data) public payable returns (bool success, bytes memory returnData){
+    function makeCallWithAmount(address _to, bytes memory _data) external payable returns (bool success, bytes memory returnData) {
         (success, returnData) = _to.call{value: msg.value}(_data);
     }
 
-    function makeStaticCall(address _to, bytes memory _data) public returns (bool success, bytes memory returnData){
+    function makeStaticCall(address _to, bytes memory _data) external returns (bool success, bytes memory returnData) {
         (success, returnData) = _to.staticcall(_data);
     }
 
-    function makeDelegateCall(address _to, bytes memory _data) public returns (bool success, bytes memory returnData){
+    function makeDelegateCall(address _to, bytes memory _data) external returns (bool success, bytes memory returnData) {
         (success, returnData) = _to.delegatecall(_data);
     }
 
-    function callCodeToContractWithoutAmount(address _address, bytes4 _sig) public returns (address) {
+    function callCodeToContractWithoutAmount(address _address, bytes4 _sig) external returns (address) {
         bytes memory result;
         bool success;
 
@@ -35,7 +35,7 @@ contract EquivalenceContract {
         return abi.decode(result, (address));
     }
 
-    function callCodeToContractWithAmount(address _address, bytes4 _sig) public payable returns (address) {
+    function callCodeToContractWithAmount(address _address, bytes4 _sig) external payable returns (address) {
         bytes memory result;
         bool success;
 
@@ -53,35 +53,35 @@ contract EquivalenceContract {
         return abi.decode(result, (address));
     }
 
-    function getBalance(address _address) public view returns (uint) {
+    function getBalance(address _address) external view returns (uint) {
         return _address.balance;
     }
 
-    function getCodeSize(address _addr) public view returns (uint256) {
+    function getCodeSize(address _address) external view returns (uint256) {
         uint256 size;
         assembly {
-            size := extcodesize(_addr)
+            size := extcodesize(_address)
         }
         return size;
     }
 
-    function getCodeHash(address _addr) public view returns (bytes32) {
+    function getCodeHash(address _address) external view returns (bytes32) {
         bytes32 codehash;
         assembly {
-            codehash := extcodehash(_addr)
+            codehash := extcodehash(_address)
         }
         return codehash;
     }
 
-    function copyCode(address _addr) public view returns (bytes memory) {
+    function copyCode(address _address) external view returns (bytes memory) {
         uint256 size;
         assembly {
-            size := extcodesize(_addr)
+            size := extcodesize(_address)
         }
         bytes memory code = new bytes(size);
 
         assembly {
-            extcodecopy(_addr, add(code, 32), 0, size)
+            extcodecopy(_address, add(code, 32), 0, size)
         }
         return code;
     }
