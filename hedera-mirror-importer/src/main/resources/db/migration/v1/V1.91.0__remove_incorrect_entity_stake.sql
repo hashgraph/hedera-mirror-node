1d89ea17dd01ce564cd7ef09f0ce4d5e0945478a
Xin Li <xin@swirldslabs.com>
2023-12-14 15:44:59 -0600

    optimize migration SQL
    Signed-off-by: Xin Li <xin@swirldslabs.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v1/V1.91.0__remove_incorrect_entity_stake.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/v1/V1.91.0__remove_incorrect_entity_stake.sql
@@ -3,25 +3,26 @@ $$
 declare
   one_sec_in_ns constant bigint := 10^9;
 
-  last_known_good_timestamp bigint;
+  last_good_lower_timestamp bigint;
+  migration_timestamp bigint;
 begin
   -- The last known good entity stake's timestamp_range should contain the time when V1.89.2 was installed.
-  select (extract(epoch from installed_on) * one_sec_in_ns)::bigint into last_known_good_timestamp
+  select (extract(epoch from installed_on) * one_sec_in_ns)::bigint into migration_timestamp
   from flyway_schema_history
   where version = '1.89.2';
   if not found then
     return;
   end if;
+  raise notice 'migration timestamp: %', migration_timestamp;
 
-  raise notice 'last_known_good_timestamp: %', last_known_good_timestamp;
-
-  perform 1
+  select lower(timestamp_range) into last_good_lower_timestamp
   from entity_stake_history
-  where id = 800 and timestamp_range @> last_known_good_timestamp;
+  where id = 800 and timestamp_range @> migration_timestamp;
   if not found then
     return;
   end if;
 
+  -- Copy the last known good entity_stake rows to the current table
   truncate table entity_stake;
   insert into entity_stake (decline_reward_start, end_stake_period, id, pending_reward, staked_node_id_start,
     staked_to_me, stake_total_start, timestamp_range)
@@ -35,15 +36,16 @@ begin
     stake_total_start,
     int8range(lower(timestamp_range), null)
   from entity_stake_history
-  where timestamp_range @> last_known_good_timestamp;
+  where timestamp_range @> migration_timestamp;
 
-  create index if not exists entity_stake_history__upper_timestamp
-    on entity_stake_history (upper(timestamp_range));
+  -- Delete incorrect history rows
+  raise notice 'deleting entity_stake_history rows at or after: %', last_good_lower_timestamp;
   delete from entity_stake_history
-  where upper(timestamp_range) > last_known_good_timestamp;
---   delete from entity_stake_history
---   where timestamp_range @> last_known_good_timestamp or lower(timestamp_range) > last_known_good_timestamp;
-  drop index if exists entity_stake_history__upper_timestamp;
+  where timestamp_range in (
+    select timestamp_range
+    from entity_stake_history
+    where id = 800 and lower(timestamp_range) >= last_good_lower_timestamp
+  );
 end;
 $$ language plpgsql;
 
