1277ae442327171fe617d3b30199d287453bd08f
Jeff Schmidt <jeffrey.schmidt@swirldslabs.com>
2024-01-10 10:58:50 -0700

    Implemented more capable timestampBindingMatrix.js. Updated all transactions and accounts spect to pass.
    Looking into timestamp range algorithm and why timestamp eq is not being resprected.
    
    Signed-off-by: Jeff Schmidt <jeffrey.schmidt@swirldslabs.com>

--- hedera-mirror-rest/__tests__/integration/timestampBindingMatrix.js
+++ hedera-mirror-rest/__tests__/integration/timestampBindingMatrix.js
@@ -17,6 +17,28 @@
 import _ from 'lodash';
 
 const applyMatrix = (spec) => {
+  /*
+   * Process any matrix per value spec level configuration to be applied to the cloned specs. This is a top-level
+   * property to the spec:
+   *
+   *   "matrixSetup": {
+   *     "true": {
+   *       "features": {
+   *         "fakeTime": "2019-08-14Z 10:50:00"
+   *       },
+   *       "config": {
+   *         "query": {
+   *           "maxRepeatedQueryParameters": 50
+   *         }
+   *       }
+   *     }
+   *   }
+   *
+   * Setup properties such as "features" and "config" are merged with the top-level "setup" property for each defined
+   * matrix value.
+   */
+  const matrixSetupMap = typeof spec.matrixSetup == 'object' ? new Map(Object.entries(spec.matrixSetup)) : new Map();
+
   return [false, true].map((value) => {
     const clone = _.cloneDeep(spec);
     clone.name = `${spec.name} with bindTimestampRange=${value}`;
@@ -25,6 +47,33 @@ const applyMatrix = (spec) => {
         bindTimestampRange: value,
       },
     });
+
+    /*
+     * Apply any setup for this matrix value.
+     */
+    const matrixSetup = matrixSetupMap.get(value.toString());
+    if (!_.isNil(matrixSetup)) {
+      clone.setup = _.merge(clone.setup, matrixSetup);
+    }
+
+    /*
+     * A spec may contain multiple test cases (urls/response) via the "test" array, or not define that
+     * and contain a single test case within the spec. Each test case may define a "matrixResponse" object, keyed
+     * by the matrix value just like the setup above, which can be used to replace the test case "responseStatus"
+     * and "responseJson" properties per matrix value.
+     */
+    const testCases = Array.isArray(clone.tests) ? clone.tests : [clone];
+    testCases.forEach((tc) => {
+      const matrixResponseMap =
+        typeof tc.matrixResponse == 'object' ? new Map(Object.entries(tc.matrixResponse)) : new Map();
+
+      const matrixResponse = matrixResponseMap.get(value.toString());
+      if (!_.isNil(matrixResponse)) {
+        tc.responseStatus = matrixResponse.responseStatus ?? tc.responseStatus;
+        tc.responseJson = matrixResponse.responseJson ?? tc.responseJson;
+      }
+    });
+
     return clone;
   });
 };
