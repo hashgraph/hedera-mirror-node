4f4123b2840833f69b1f7f95c89454b167a88631
Edwin Greene <edwin@swirldslabs.com>
2024-01-23 10:50:43 -0500

    Update migration and rest test
    Signed-off-by: Edwin Greene <edwin@swirldslabs.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v1/V1.92.1__update_entity_stake.sql
+++
@@ -1,6 +0,0 @@
-delete from entity_stake where decline_reward_start is true;
-
-alter table if exists entity_stake
-    drop column if exists decline_reward_start;
-alter table if exists entity_stake_history
-    drop column if exists decline_reward_start;
