2a527ad8a078a63c8f90ab528d58f378b4f56553
Steven Sheehy <17552371+steven-sheehy@users.noreply.github.com>
2024-01-24 15:02:32 -0600

    Fix HCS latency regression (0.97)
    Fix the HCS E2E latency regression by invoking RedisPublisher before SqlEntityListener
    
    Signed-off-by: Steven Sheehy <steven.sheehy@swirldslabs.com>

--- hedera-mirror-importer/src/test/java/com/hedera/mirror/importer/parser/record/entity/redis/RedisPublisherIntegrationTest.java
+++ hedera-mirror-importer/src/test/java/com/hedera/mirror/importer/parser/record/entity/redis/RedisPublisherIntegrationTest.java
@@ -16,11 +16,16 @@
 
 package com.hedera.mirror.importer.parser.record.entity.redis;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.hedera.mirror.common.domain.entity.EntityId;
 import com.hedera.mirror.common.domain.topic.StreamMessage;
 import com.hedera.mirror.common.domain.topic.TopicMessage;
+import com.hedera.mirror.importer.parser.record.RecordStreamFileListener;
 import com.hedera.mirror.importer.parser.record.entity.BatchPublisherTest;
 import com.hedera.mirror.importer.parser.record.entity.ParserContext;
+import java.util.List;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
 import reactor.core.publisher.Flux;
@@ -29,18 +34,26 @@ import reactor.core.publisher.Flux;
 class RedisPublisherIntegrationTest extends BatchPublisherTest {
 
     private final ReactiveRedisOperations<String, StreamMessage> redisOperations;
+    private final List<RecordStreamFileListener> streamFileListeners;
 
     public RedisPublisherIntegrationTest(
-            RedisPublisher entityListener,
+            RedisPublisher redisPublisher,
             ParserContext parserContext,
             RedisProperties properties,
-            ReactiveRedisOperations<String, StreamMessage> redisOperations) {
-        super(entityListener, parserContext, properties);
+            ReactiveRedisOperations<String, StreamMessage> redisOperations,
+            List<RecordStreamFileListener> streamFileListeners) {
+        super(redisPublisher, parserContext, properties);
         this.redisOperations = redisOperations;
+        this.streamFileListeners = streamFileListeners;
     }
 
     @Override
     protected Flux<TopicMessage> subscribe(EntityId topicId) {
         return redisOperations.listenToChannel("topic." + topicId.getId()).map(m -> (TopicMessage) m.getMessage());
+    }
+
+    @Test
+    void publishesFirst() {
+        assertThat(streamFileListeners).first().isEqualTo(batchPublisher);
     }
 }
