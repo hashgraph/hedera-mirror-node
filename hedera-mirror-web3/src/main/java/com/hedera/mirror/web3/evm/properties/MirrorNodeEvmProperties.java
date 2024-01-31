b2a367a3974169f796b97f368aa83da8f26d79b7
Kristiyan Selveliev <kristiyan.selveliev@limechain.tech>
2024-01-31 18:59:35 +0200

    Bump hedera-evm version to 0.46 and besu evm version to 23.10.2  (#7645)
    This PR bumps the besu-evm library to 23.10.2
    It also bumps hedera-evm library to 0.46 since 0.46 has a dependency on changes in 23.10.2 besu-evm.
    Updating besu-evm to 23.10.2 won't work with hedera-evm lower than 0.46.
    Some changes are required in integration tests particularly for historical data to support the new changes in the libraries.
    
    **Modifications**:
    
    Modified `HederaExtCodeHashOperationV038` operation, now takes `EvmProperties` in the constructor.
    
    Modified `MirrorNodeEvmProperties` has 3 new added methods that need to be ovverriden, these will be used in evm version 46, now they have default false values.
    
    Modified  `HederaEvmStackedWorldStateUpdater` to override `getOrCreate` method. The new hedera-evm 0.46 version `AbstractLedgerEvmWorldUpdater.getOrCreate` method returns null so we need to override it. Previous version `getOrCreate` used the default `getOrCreate` method in `WorldUpdater` interface. Now we override in HederaEvmStackedWorldStateUpdater and copy the code from `WorldUpdater.getOrCreate`.
    
    Modified `ContractCallEvmCodesTest.getBlockHashAfterTheLatestReturnsZero`. Changed the test addresses since the test previously passed existing addresses that were created via `ContractCallTestSetup`. Added new test addresses that are not created in `ContractCallTestSetup`.
    This is needed because of new changes introduced in hedera-evm 0.46 -> `AbstractCodeCache.getIfPresent` - has new changes 
    
    Now using `entityAccess.isUsable(address)` now checks database for the address parameter.
    Previously any non system contract addresses would pass in test without this check.
    
    Modified `ContractCallServiceTest.pureCallWithCustomBlock` changed method name and switched order of given conditions - documented in code.
    
    Modified `ContractCallTestSetup.persistEntities`. This change is required for the new check `entityAccess.isUsable(address)` described above in `AbstractCodeCache.getIfPresent`. Since now `entityAccess.isUsable(address)` will internally check the store for the address and subsequently the db.
    We need to set the contract entities to have timestamp range of earlier evm versions so that they could be found during historical tests. 
    
    Added new `recordFileEvm38Latest` this is needed for the HederaBlockHashOperation integration test.
    
    ---------
    
    Signed-off-by: Kristiyan Selveliev <kristiyan.selveliev@limechain.tech>

--- hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/evm/properties/MirrorNodeEvmProperties.java
+++ hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/evm/properties/MirrorNodeEvmProperties.java
@@ -180,6 +180,21 @@ public class MirrorNodeEvmProperties implements EvmProperties {
     }
 
     @Override
+    public boolean allowCallsToNonContractAccounts() {
+        return false;
+    }
+
+    @Override
+    public Set<Address> grandfatherContracts() {
+        return new HashSet<>();
+    }
+
+    @Override
+    public boolean callsToNonExistingEntitiesEnabled(Address target) {
+        return false;
+    }
+
+    @Override
     public boolean dynamicEvmVersion() {
         return dynamicEvmVersion;
     }
