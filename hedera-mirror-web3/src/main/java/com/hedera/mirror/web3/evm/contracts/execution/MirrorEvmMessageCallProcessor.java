e825cf6168f3ffc6b262d1910e1b6ec66733cd87
Kristiyan Selveliev <kristiyan.selveliev@limechain.tech>
2024-01-31 17:18:16 +0200

    fix: Fix concurrency issue in message call processors
    Signed-off-by: Kristiyan Selveliev <kristiyan.selveliev@limechain.tech>

--- hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/evm/contracts/execution/MirrorEvmMessageCallProcessor.java
+++ hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/evm/contracts/execution/MirrorEvmMessageCallProcessor.java
@@ -18,13 +18,18 @@ package com.hedera.mirror.web3.evm.contracts.execution;
 
 import static com.hedera.node.app.service.evm.contracts.operations.HederaExceptionalHaltReason.FAILURE_DURING_LAZY_ACCOUNT_CREATE;
 import static com.hedera.services.store.contracts.precompile.codec.DecodingFacade.wrapUnsafely;
+import static org.apache.tuweni.bytes.Bytes.EMPTY;
 import static org.hyperledger.besu.evm.frame.ExceptionalHaltReason.INSUFFICIENT_GAS;
+import static org.hyperledger.besu.evm.frame.MessageFrame.State.COMPLETED_SUCCESS;
 import static org.hyperledger.besu.evm.frame.MessageFrame.State.EXCEPTIONAL_HALT;
+import static org.hyperledger.besu.evm.frame.MessageFrame.State.REVERT;
 
 import com.hedera.mirror.web3.evm.config.PrecompiledContractProvider;
 import com.hedera.mirror.web3.evm.store.contract.EntityAddressSequencer;
 import com.hedera.mirror.web3.evm.store.contract.HederaEvmStackedWorldStateUpdater;
 import com.hedera.node.app.service.evm.contracts.execution.HederaEvmMessageCallProcessor;
+import com.hedera.node.app.service.evm.store.contracts.AbstractLedgerEvmWorldUpdater;
+import com.hedera.node.app.service.evm.store.contracts.precompile.EvmHTSPrecompiledContract;
 import com.hedera.services.contracts.gascalculator.GasCalculatorHederaV22;
 import com.hedera.services.ledger.BalanceChange;
 import com.hedera.services.txns.crypto.AbstractAutoCreationLogic;
@@ -35,11 +40,13 @@ import com.hederahashgraph.api.proto.java.ResponseCodeEnum;
 import com.hederahashgraph.api.proto.java.Timestamp;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import java.util.Optional;
+import org.apache.tuweni.bytes.Bytes;
 import org.hyperledger.besu.evm.EVM;
 import org.hyperledger.besu.evm.frame.ExceptionalHaltReason;
 import org.hyperledger.besu.evm.frame.MessageFrame;
 import org.hyperledger.besu.evm.precompile.MainnetPrecompiledContracts;
 import org.hyperledger.besu.evm.precompile.PrecompileContractRegistry;
+import org.hyperledger.besu.evm.precompile.PrecompiledContract;
 import org.hyperledger.besu.evm.tracing.OperationTracer;
 
 public class MirrorEvmMessageCallProcessor extends HederaEvmMessageCallProcessor {
@@ -58,6 +65,43 @@ public class MirrorEvmMessageCallProcessor extends HederaEvmMessageCallProcessor
         this.entityAddressSequencer = entityAddressSequencer;
 
         MainnetPrecompiledContracts.populateForIstanbul(precompiles, gasCalculator);
+    }
+
+    @Override
+    protected void executeHederaPrecompile(
+            PrecompiledContract contract, MessageFrame frame, OperationTracer operationTracer) {
+        Bytes output = EMPTY;
+        Long gasRequirement = 0L;
+        if (contract instanceof EvmHTSPrecompiledContract htsPrecompile) {
+            var updater = (AbstractLedgerEvmWorldUpdater) frame.getWorldUpdater();
+            final var costedResult = htsPrecompile.computeCosted(
+                    frame.getInputData(),
+                    frame,
+                    (now, minimumTinybarCost) -> minimumTinybarCost,
+                    updater.tokenAccessor());
+            output = costedResult.getValue();
+            gasRequirement = costedResult.getKey();
+        }
+        if (!"HTS".equals(contract.getName()) && !"EvmHTS".equals(contract.getName())) {
+            output = contract.computePrecompile(frame.getInputData(), frame).getOutput();
+            gasRequirement = contract.gasRequirement(frame.getInputData());
+        }
+
+        operationTracer.tracePrecompileCall(frame, gasRequirement, output);
+        if (frame.getState() == REVERT) {
+            return;
+        }
+        if (frame.getRemainingGas() < gasRequirement) {
+            frame.decrementRemainingGas(frame.getRemainingGas());
+            frame.setExceptionalHaltReason(Optional.of(INSUFFICIENT_GAS));
+            frame.setState(EXCEPTIONAL_HALT);
+        } else if (output != null) {
+            frame.decrementRemainingGas(gasRequirement);
+            frame.setOutputData(output);
+            frame.setState(COMPLETED_SUCCESS);
+        } else {
+            frame.setState(EXCEPTIONAL_HALT);
+        }
     }
 
     /**
--- hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/evm/contracts/execution/MirrorEvmMessageCallProcessorV30.java
+++ hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/evm/contracts/execution/MirrorEvmMessageCallProcessorV30.java
@@ -16,13 +16,26 @@
 
 package com.hedera.mirror.web3.evm.contracts.execution;
 
+import static org.apache.tuweni.bytes.Bytes.EMPTY;
+import static org.hyperledger.besu.evm.frame.ExceptionalHaltReason.INSUFFICIENT_GAS;
+import static org.hyperledger.besu.evm.frame.MessageFrame.State.COMPLETED_SUCCESS;
+import static org.hyperledger.besu.evm.frame.MessageFrame.State.EXCEPTIONAL_HALT;
+import static org.hyperledger.besu.evm.frame.MessageFrame.State.REVERT;
+
 import com.hedera.mirror.web3.evm.config.PrecompiledContractProvider;
 import com.hedera.node.app.service.evm.contracts.execution.HederaEvmMessageCallProcessor;
+import com.hedera.node.app.service.evm.store.contracts.AbstractLedgerEvmWorldUpdater;
+import com.hedera.node.app.service.evm.store.contracts.precompile.EvmHTSPrecompiledContract;
 import com.hedera.services.contracts.gascalculator.GasCalculatorHederaV22;
 import jakarta.inject.Named;
+import java.util.Optional;
+import org.apache.tuweni.bytes.Bytes;
 import org.hyperledger.besu.evm.EVM;
+import org.hyperledger.besu.evm.frame.MessageFrame;
 import org.hyperledger.besu.evm.precompile.MainnetPrecompiledContracts;
 import org.hyperledger.besu.evm.precompile.PrecompileContractRegistry;
+import org.hyperledger.besu.evm.precompile.PrecompiledContract;
+import org.hyperledger.besu.evm.tracing.OperationTracer;
 
 @Named
 public class MirrorEvmMessageCallProcessorV30 extends HederaEvmMessageCallProcessor {
@@ -34,5 +47,42 @@ public class MirrorEvmMessageCallProcessorV30 extends HederaEvmMessageCallProces
             final GasCalculatorHederaV22 gasCalculator) {
         super(v30, precompiles, precompilesHolder.getHederaPrecompiles());
         MainnetPrecompiledContracts.populateForIstanbul(precompiles, gasCalculator);
+    }
+
+    @Override
+    protected void executeHederaPrecompile(
+            PrecompiledContract contract, MessageFrame frame, OperationTracer operationTracer) {
+        Bytes output = EMPTY;
+        Long gasRequirement = 0L;
+        if (contract instanceof EvmHTSPrecompiledContract htsPrecompile) {
+            var updater = (AbstractLedgerEvmWorldUpdater) frame.getWorldUpdater();
+            final var costedResult = htsPrecompile.computeCosted(
+                    frame.getInputData(),
+                    frame,
+                    (now, minimumTinybarCost) -> minimumTinybarCost,
+                    updater.tokenAccessor());
+            output = costedResult.getValue();
+            gasRequirement = costedResult.getKey();
+        }
+        if (!"HTS".equals(contract.getName()) && !"EvmHTS".equals(contract.getName())) {
+            output = contract.computePrecompile(frame.getInputData(), frame).getOutput();
+            gasRequirement = contract.gasRequirement(frame.getInputData());
+        }
+
+        operationTracer.tracePrecompileCall(frame, gasRequirement, output);
+        if (frame.getState() == REVERT) {
+            return;
+        }
+        if (frame.getRemainingGas() < gasRequirement) {
+            frame.decrementRemainingGas(frame.getRemainingGas());
+            frame.setExceptionalHaltReason(Optional.of(INSUFFICIENT_GAS));
+            frame.setState(EXCEPTIONAL_HALT);
+        } else if (output != null) {
+            frame.decrementRemainingGas(gasRequirement);
+            frame.setOutputData(output);
+            frame.setState(COMPLETED_SUCCESS);
+        } else {
+            frame.setState(EXCEPTIONAL_HALT);
+        }
     }
 }
