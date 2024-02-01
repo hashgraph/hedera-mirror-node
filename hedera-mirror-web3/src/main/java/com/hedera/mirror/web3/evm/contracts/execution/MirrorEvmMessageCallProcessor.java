1226ff6ece7de77dd88c1cd9150f1e4ece7ead9c
Kristiyan Selveliev <kristiyan.selveliev@limechain.tech>
2024-02-01 18:58:50 +0200

    Fix concurrency issue in message call processors (#7650)
    *Currently making simultaneous calls of decimals function with multiple threads against erc tokens with different decimals will result in mixed up results. If one token has 10 decimals the other has 8 decimals, when we expect the result to be 10 it might turn out to be 8 and vice versa. This is because our MessageCallProcessors - MirrorEvmMessageCallProcessor and MirrorEvmMessageCallProcessorV30 are singleton beans that extend hedera-evm HederaEvmMessageCallProcessor which maintains state with
    
    protected long gasRequirement;
    protected Bytes output;
    
    Multiple threads calling executeHederaPrecompile() will modify simultaneously shared gasRequirement and output fields and mix up the results.
    
    This Pr modifies :
    MirrorEvmMessageCallProcessor override executeHederaPrecompile() define gasRequirement and output as local variables within the executeHederaPrecompile to ensure thread safety.
    
    MirrorEvmMessageCallProcessorV30 override executeHederaPrecompile() define gasRequirement and output as local variables within the executeHederaPrecompile to ensure thread safety.
    
    ---------
    
    Signed-off-by: Kristiyan Selveliev <kristiyan.selveliev@limechain.tech>

--- hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/evm/contracts/execution/MirrorEvmMessageCallProcessor.java
+++ hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/evm/contracts/execution/MirrorEvmMessageCallProcessor.java
@@ -24,7 +24,6 @@ import static org.hyperledger.besu.evm.frame.MessageFrame.State.EXCEPTIONAL_HALT
 import com.hedera.mirror.web3.evm.config.PrecompiledContractProvider;
 import com.hedera.mirror.web3.evm.store.contract.EntityAddressSequencer;
 import com.hedera.mirror.web3.evm.store.contract.HederaEvmStackedWorldStateUpdater;
-import com.hedera.node.app.service.evm.contracts.execution.HederaEvmMessageCallProcessor;
 import com.hedera.services.contracts.gascalculator.GasCalculatorHederaV22;
 import com.hedera.services.ledger.BalanceChange;
 import com.hedera.services.txns.crypto.AbstractAutoCreationLogic;
@@ -42,7 +41,7 @@ import org.hyperledger.besu.evm.precompile.MainnetPrecompiledContracts;
 import org.hyperledger.besu.evm.precompile.PrecompileContractRegistry;
 import org.hyperledger.besu.evm.tracing.OperationTracer;
 
-public class MirrorEvmMessageCallProcessor extends HederaEvmMessageCallProcessor {
+public class MirrorEvmMessageCallProcessor extends AbstractEvmMessageCallProcessor {
     private final AbstractAutoCreationLogic autoCreationLogic;
     private final EntityAddressSequencer entityAddressSequencer;
 
