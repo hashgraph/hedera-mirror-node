8f24a8814bd21f1726512e4e3b4e02039e7cedb9
Valentin Valkanov <valentin.valkanov@limechain.tech>
2024-02-08 16:15:24 +0200

    refactor: make sql query logic use contract_result for handling contractid
    Signed-off-by: Valentin Valkanov <valentin.valkanov@limechain.tech>

--- hedera-mirror-importer/src/main/resources/db/migration/v1/V1.94.0__add_gas_consumed_field.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/v1/V1.94.0__add_gas_consumed_field.sql
@@ -1,44 +1,71 @@
- ALTER TABLE contract_result
- ADD COLUMN IF NOT EXISTS gas_consumed bigint NULL;
+alter table contract_result
+add column if not exists gas_consumed bigint null;
 
- -- Function for counting zero and non-zero bytes
- CREATE OR REPLACE FUNCTION count_zero_bytes(bytea) RETURNS bigint AS $$
- DECLARE
-   zero_count bigint := 0;
-   non_zero_count bigint := 0;
-   i int;
- BEGIN
-   FOR i IN 1..octet_length($1) LOOP
-     IF get_byte($1, i-1) = 0 THEN
-       zero_count := zero_count + 1;
-     ELSE
-       non_zero_count := non_zero_count + 1;
-     END IF;
-   END LOOP;
-   RETURN zero_count * 4 + non_zero_count * 16; -- 4 gas per zero byte, 16 gas per non-zero byte
- END;
- $$ LANGUAGE plpgsql IMMUTABLE;
+-- Function for counting zero and non-zero bytes
+create or replace function bytes_gas_calculation(bytea) returns bigint as $$
+declare
+  zero_count bigint := 0;
+  non_zero_count bigint := 0;
+  i int;
+begin
+  for i in 1..octet_length($1) loop
+    if get_byte($1, i-1) = 0 then
+      zero_count := zero_count + 1;
+    else
+      non_zero_count := non_zero_count + 1;
+    end if;
+  end loop;
+  return zero_count * 4 + non_zero_count * 16; -- 4 gas per zero byte, 16 gas per non-zero byte
+end;
+$$ language plpgsql immutable;
 
- -- Update the contract_result table with gas_consumed values, by summing up the gas used by contract actions and adding the gas cost for contract creation
- WITH gas_usage AS (
-   SELECT
-     ct.consensus_timestamp,
-     ct.entity_id,
-     COALESCE(SUM(ca.gas_used), 0) AS total_gas_used,
-     c.initcode
-   FROM
-     contract_transaction ct
-     LEFT JOIN contract_action ca ON ct.consensus_timestamp = ca.consensus_timestamp
-     LEFT JOIN contract c ON ct.entity_id = c.id
-   GROUP BY
-     ct.consensus_timestamp, ct.entity_id, c.initcode
- )
- UPDATE contract_result cr
- SET gas_consumed = gu.total_gas_used + 21000 +
-   (CASE
-     WHEN gu.initcode IS NOT NULL THEN
-       32000 + count_zero_bytes(gu.initcode) -- 32_000 EXTRA gas cost for contract creation
-     ELSE 0
-   END)
- FROM gas_usage gu
- WHERE cr.consensus_timestamp = gu.consensus_timestamp;
+-- Function to get intrinsic gas for contract creation
+create or replace function get_intrinsic_gas(initcode bytea, is_creation boolean) returns bigint as $$
+begin
+  -- Always include the base base cost of 21000 gas
+  if is_creation then
+    return 21000 + 32000 + bytes_gas_calculation(initcode); -- Calculate extra gas cost for contract creation
+  else
+    return 21000;
+  end if;
+end;
+$$ language plpgsql immutable;
+
+-- Update contract_result with gas_consumed values only if there are corresponding entries in contract_action
+with contract_action_gas_usage as (
+    select
+        consensus_timestamp,
+        sum(gas_used) as total_gas_used
+    from
+        contract_action
+    group by
+        consensus_timestamp
+), contract_transaction_info as (
+    select
+        cr.consensus_timestamp,
+        cr.contract_id,
+        -- Determine if this is a contract create or call transaction
+        (cr.contract_id = any(cr.created_contract_ids)) as is_creation,
+        c.initcode
+    from
+        contract_result cr
+    left join contract c on cr.contract_id = c.id
+), gas_calculation as (
+    select
+        cti.consensus_timestamp,
+        cti.is_creation,
+        cti.initcode,
+        cagu.total_gas_used
+    from
+        contract_transaction_info cti
+    -- Filter out only contract transactions with contract actions sidecars
+    join contract_action_gas_usage cagu on cti.consensus_timestamp = cagu.consensus_timestamp
+)
+update contract_result cr
+set gas_consumed = gc.total_gas_used +
+                   get_intrinsic_gas(gc.initcode, gc.is_creation)
+from gas_calculation gc
+where cr.consensus_timestamp = gc.consensus_timestamp;
+
+drop function if exists bytes_gas_calculation(bytea);
+drop function if exists get_intrinsic_gas(bytea);
