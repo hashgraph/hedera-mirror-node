9cf5a4e542af18d40dbc1877accb04a14936a60a
Jeff Schmidt <jeffrey.schmidt@swirldslabs.com>
2024-02-12 12:28:23 -0700

    Improve lambda parameter naming around repeatWhen().
    Signed-off-by: Jeff Schmidt <jeffrey.schmidt@swirldslabs.com>

--- hedera-mirror-monitor/src/main/java/com/hedera/mirror/monitor/publish/NodeSupplier.java
+++ hedera-mirror-monitor/src/main/java/com/hedera/mirror/monitor/publish/NodeSupplier.java
@@ -73,9 +73,9 @@ public class NodeSupplier {
                                 "{} of {} nodes are functional",
                                 valid.stream().filter(v -> v).count(),
                                 valid.size()))
-                        .repeatWhen(retry -> retry.filter(l -> nodes.isEmpty())
-                                .doOnNext(delay -> log.info("Retrying in {} ms", retryBackoff))
-                                .flatMap(delay -> Mono.delay(Duration.ofMillis(retryBackoff), scheduler))))
+                        .repeatWhen(repeat -> repeat.filter(l -> nodes.isEmpty())
+                                .doOnNext(numEmitted -> log.info("Retrying in {} ms", retryBackoff))
+                                .flatMap(numEmitted -> Mono.delay(Duration.ofMillis(retryBackoff), scheduler))))
                 .doOnSubscribe(s -> log.info("Starting node validation"))
                 .doOnError(t -> log.error("Exception validating nodes: ", t))
                 .onErrorResume(t -> Mono.empty())
