444a4f49cfa015279bb6471ae013a7df433c552d
mgoelswirlds <mugdha.goel@swirldslabs.com>
2024-02-29 11:07:19 -0500

    Intial commit for controller

---
+++ hedera-mirror-rest-java/src/main/java/com/hedera/mirror/restjava/common/RestUtils.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2024 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hedera.mirror.restjava.common;
+
+import static com.hedera.mirror.restjava.common.EntityIdUtils.parseIdFromString;
+
+public class RestUtils {
+
+    public static final Integer MAX_LIMIT = 100;
+    public static final Integer DEFAULT_LIMIT = 25;
+
+    public static Filter getFilter(String paramName, String accountIdQueryParam) {
+
+        String[] splitVal = accountIdQueryParam != null ? accountIdQueryParam.split(":") : null;
+
+        if (splitVal != null) {
+            if (splitVal.length == 1) {
+                // No operator specified. Just use "eq:"
+                return new Filter(paramName, RangeOperator.EQ, splitVal[0]);
+            }
+            return new Filter(paramName, RangeOperator.valueOf(splitVal[0].toUpperCase()), splitVal[1]);
+        } else {
+            return null;
+        }
+    }
+
+    public static Long getAccountNum(Filter filter) {
+        return filter != null ? parseIdFromString(filter.getValue())[2] : null;
+    }
+
+    public static Integer validateLimit(Integer limit) {
+        return limit > MAX_LIMIT ? MAX_LIMIT : limit;
+    }
+}
---
+++ hedera-mirror-rest-java/src/test/java/com/hedera/mirror/restjava/controller/AccountControllerTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2023-2024 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hedera.mirror.restjava.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import jakarta.annotation.Resource;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = DummyController.class)
+class AccountControllerTest {
+
+    private static final String CALL_URI = "/api/v1/accounts";
+
+    @Resource
+    private WebTestClient webClient;
+
+    @Resource
+    private MockMvc mockMvc;
+
+    @Test
+    @SneakyThrows
+    void success() {
+        String extended_uri = "/2411/allowances/nfts";
+        mockMvc.perform(get(CALL_URI + extended_uri).accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello world"));
+    }
+
+    @Test
+    @SneakyThrows
+    void methodNotAllowed() {
+        mockMvc.perform(post(CALL_URI)).andExpect(status().isMethodNotAllowed());
+    }
+}
