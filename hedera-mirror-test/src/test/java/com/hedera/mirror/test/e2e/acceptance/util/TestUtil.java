45b0de9d896918bd11f5b89a6cd45103ae26e1ee
Yordan Iliev <yordan.iliev@limechain.tech>
2024-03-19 09:38:09 +0200

    Extract pattern outside function
    Signed-off-by: Yordan Iliev <yordan.iliev@limechain.tech>

--- hedera-mirror-test/src/test/java/com/hedera/mirror/test/e2e/acceptance/util/TestUtil.java
+++ hedera-mirror-test/src/test/java/com/hedera/mirror/test/e2e/acceptance/util/TestUtil.java
@@ -51,6 +51,7 @@ public class TestUtil {
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     private static final SecureRandom RANDOM = new SecureRandom();
     public static String ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+    Pattern extractTransactionIdPattern = Pattern.compile("(\\d+\\.\\d+\\.\\d+)@(\\d+)\\.(\\d+)");
 
     public static String getAliasFromPublicKey(@NonNull PublicKey key) {
         if (key.isECDSA()) {
@@ -173,8 +174,7 @@ public class TestUtil {
     }
 
     public static String extractTransactionId(String message) {
-        Pattern pattern = Pattern.compile("(\\d+\\.\\d+\\.\\d+)@(\\d+)\\.(\\d+)");
-        Matcher matcher = pattern.matcher(message);
+        Matcher matcher = extractTransactionIdPattern.matcher(message);
         if (matcher.find()) {
             return matcher.group(1) + "-" + matcher.group(2) + "-" + matcher.group(3);
         } else {
