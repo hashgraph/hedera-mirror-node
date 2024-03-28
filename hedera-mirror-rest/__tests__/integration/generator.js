c7f27cf0afa6424d9120cba0c661a4cfe4656220
Steven Sheehy <17552371+steven-sheehy@users.noreply.github.com>
2024-03-27 22:30:14 -0500

    Suppress stacktrace for invalid content encoding (#7977)
    * Add a `clean` task to the rest module
    * Change spec test generation to remove previously generated tests first
    * Change error middleware to not require manual mapping for `HttpError`
    
    Signed-off-by: Steven Sheehy <steven.sheehy@swirldslabs.com>

--- hedera-mirror-rest/__tests__/integration/generator.js
+++ hedera-mirror-rest/__tests__/integration/generator.js
@@ -25,6 +25,11 @@ const dirname = getModuleDirname(import.meta);
 const specsPath = path.join(dirname, '..', 'specs');
 const template = fs.readFileSync(path.join(dirname, 'template.js')).toString();
 
+// Remove any generated spec tests
+fs.readdirSync(dirname, {withFileTypes: true})
+  .filter((f) => f.isFile() && f.name.endsWith('.spec.test.js'))
+  .forEach((f) => fs.rmSync(path.join(f.path, f.name)));
+
 fs.readdirSync(specsPath, {withFileTypes: true})
   .filter((dirent) => dirent.isDirectory())
   .forEach((dirent) => {
