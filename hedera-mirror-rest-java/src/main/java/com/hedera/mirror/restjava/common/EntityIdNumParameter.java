204a81112b6713a4e3fbbf24b62c7789bfcbef58
Steven Sheehy <steven.sheehy@swirldslabs.com>
2024-04-15 10:07:41 -0500

    Fix REST Java generic exception handling
    Signed-off-by: Steven Sheehy <steven.sheehy@swirldslabs.com>

--- hedera-mirror-rest-java/src/main/java/com/hedera/mirror/restjava/common/EntityIdNumParameter.java
+++ hedera-mirror-rest-java/src/main/java/com/hedera/mirror/restjava/common/EntityIdNumParameter.java
@@ -24,18 +24,7 @@ public record EntityIdNumParameter(EntityId id) implements EntityIdParameter {
     public static final String ENTITY_ID_REGEX = "^((\\d{1,5})\\.)?((\\d{1,5})\\.)?(\\d{1,10})$";
     public static final Pattern ENTITY_ID_PATTERN = Pattern.compile(ENTITY_ID_REGEX);
 
-    @Override
-    public long shard() {
-        return id().getShard();
-    }
-
-    @Override
-    public long realm() {
-        return id().getRealm();
-    }
-
     public static EntityIdNumParameter valueOf(String id) {
-
         var matcher = ENTITY_ID_PATTERN.matcher(id);
 
         if (!matcher.matches()) {
@@ -44,20 +33,26 @@ public record EntityIdNumParameter(EntityId id) implements EntityIdParameter {
 
         long shard = DEFAULT_SHARD;
         long realm = 0;
-
         String realmString;
+
         if ((realmString = matcher.group(4)) != null) {
-            // This gets the shard and realm value
             realm = Long.parseLong(realmString);
             shard = Long.parseLong(matcher.group(2));
-
         } else if ((realmString = matcher.group(2)) != null) {
-            // This gets the realm value and shard will be null
             realm = Long.parseLong(realmString);
         }
 
         var num = Long.parseLong(matcher.group(5));
-
         return new EntityIdNumParameter(EntityId.of(shard, realm, num));
+    }
+
+    @Override
+    public long shard() {
+        return id().getShard();
+    }
+
+    @Override
+    public long realm() {
+        return id().getRealm();
     }
 }
