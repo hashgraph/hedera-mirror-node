ab6a89aae387697a63fe34ca4c96fee3f5a4b527
Jesse Nelson <jesse@swirldslabs.com>
2024-04-20 02:02:02 -0500

    checkpoint
    Signed-off-by: Jesse Nelson <jesse@swirldslabs.com>

--- hedera-mirror-importer/src/main/resources/db/scripts/v2/migration.sh
+++ hedera-mirror-importer/src/main/resources/db/scripts/v2/migration.sh
@@ -44,7 +44,7 @@ insertCursors() {
      where  table_schema = 'public'
      and    table_name   = 'async_migration_status'
      );
-  ";
+  "
   local hasTable=$(queryTarget "${query}")
   if [[ "${hasTable}" = "t" ]]; then
     return
@@ -76,7 +76,7 @@ insertCursors() {
 getCursorConditions() {
   local query="
   with record_file_stats as (
-    select date_trunc('month', to_timestamp(consensus_end / 1000000000)) start_timestamp, sum(count) as record_count from record_file group by 1 order by 2 desc
+    select date_trunc('month', to_timestamp(consensus_end / 1000000000)) start_timestamp, sum(count) as record_count from record_file group by 1 order by 1 desc
   ),
   total_records as (
     select sum(record_count) as total_record_count
@@ -91,7 +91,6 @@ getCursorConditions() {
   "
 
   local upperBound=$MAX_TIMESTAMP
-  local lowerBound=$(querySource "select min(consensus_timestamp) from transaction;")
   local pctPerCursor=$((100 / CONCURRENT_CURSORS_PER_TABLE))
   local totalPct=0
   local -n cursors=$1 # Init array to hold cursor definitions
@@ -103,25 +102,20 @@ getCursorConditions() {
   if (($(echo "${newTotal} < ${pctPerCursor}" | bc))); then
     totalPct=$newTotal
     upperBound=$((upperBound > upper_bound ? upperBound : upper_bound))
-    lowerBound=$lower_bound
   else
     difference=$(echo "${newTotal} - ${pctPerCursor}" |bc)
     groupRange=$((upper_bound - lower_bound))
     cursorLowerBound=$(echo "${upper_bound} - ((100 - ${difference}) / 100) * ${groupRange}" | bc)
     cursors+=("'(${cursorLowerBound}, ${upperBound}]'")
 
-    lowerBound=$lower_bound
     upperBound=$cursorLowerBound
     totalPct=$difference
-
   if (($(echo "(${#cursors[@]} + 1) == ${CONCURRENT_CURSORS_PER_TABLE}" | bc))); then
     cursors+=("'(,${cursorLowerBound}]'")
     break;
   fi
   fi
   done <<< "${stats}"
-
-  echo "${cursors[@]}"
 }
 
 migrateTableBinary() {
@@ -165,8 +159,43 @@ EOF
 }
 
 handlePipeError() {
-  echo "SIGPIPE received for pid $$ on pipe $!{EPIPE} msg: ${1}"
+  log "SIGPIPE received for pid $$ on pipe $!{EPIPE} msg: ${1}"
   exit 1
+}
+
+handleExit() {
+  local error_code=$?
+  local error_line="${BASH_LINENO[*]}"
+  local error_command=$BASH_COMMAND
+  log "pid: $! on $$ Received non zero exit code: (${error_code}) ${1} at ${error_line}: ${error_command}"
+  exit 1
+}
+
+executeBookmark() {
+  local bookMarkFile="${outputFile}-bookmark"
+  mkfifo "${bookMarkFile}"
+  echo "\o ${bookMarkFile}" >"${source}"
+  echo "fetch 1 from crsr;" >"${source}"
+  echo "\o /dev/null" >"${source}"
+
+  # Wait for file to be written
+  local row=$(<"${bookMarkFile}")
+
+  local insertSql="
+        with insert as (
+        insert into ${table}(${columns//mt./}) values (${row})) returning consensus_timestamp)
+        update async_migration_status set last_processed=(select consensus_timestamp from insert)
+        where the_table=${table} and cursor_id=${crsrId};"
+  rm "${bookMarkFile}"
+
+  if [[ -z "${row}" ]]; then
+    log "No more data for cursor ${crsrId}"
+    echo "\q" >"${source}"
+    exit 0
+  fi
+
+  queryTarget "${insertSql}"
+  log "Updated bookmark with ${row} for cursor ${crsrId}"
 }
 
 executeCursor() {
@@ -175,28 +204,35 @@ executeCursor() {
   local tmpDir="${3}"
   local source="${tmpDir}/source${crsrId}"
   trap "handlePipeError 'for crsr ${crsrId} with def ${crsrDef}'" SIGPIPE
+  trap "handleExit" ERR
 
   mkfifo "${source}"
 
   # Initialize psql session for source
   log "Starting to copy '${table}' for crsr ${crsrId} on pid $!"
   readFifo "${source}" | sourcePipe &
+  jobs -p
+  jobs
 
   #Initialize the cursor
-  echo "set cursor_tuple_fraction = 1;" >"${source}"
+  if [[ "${table}" != "topic_message" ]]; then
+    #TODO why so slow on this table?
+    echo "set cursor_tuple_fraction = 1;" >"${source}"
+  fi
+
   echo "BEGIN;" >"${source}"
   echo "${crsrDef}" >"${source}"
 
   local count=0
   #TODO exit on bookmark call empty
   while [[ count -le 1000 ]]; do
-    ((count++))
+    count=$((count + 1))
 
     #Allow an additional process to start to prepare connection and initiate batch
-    ACTIVE_COPIES=$(ls "${tmpDir}" | grep "output-${crsrId}" | wc | awk '{print $1}')
+    ACTIVE_COPIES=$(find . -maxdepth 1 -name "output-${crsrId}*" -printf '.' | wc -m)
     while [[ "${ACTIVE_COPIES}" -ge 2 ]]; do
       sleep .5
-      ACTIVE_COPIES=$(ls "${tmpDir}" | grep "output-${crsrId}" | wc | awk '{print $1}')
+      ACTIVE_COPIES=$(find . -maxdepth 1 -name "output-${crsrId}*" -printf '.' | wc -m)
     done
 
     # Setup output pipe
@@ -205,9 +241,10 @@ executeCursor() {
     echo "\o ${outputFile}" >"${source}"
 
     # Listen to data on output pipe
-    queryTarget "\copy ${table}(${columns//mt./}) FROM program 'cat ${outputFile}' WITH CSV DELIMITER ',';" &&
+    queryTarget "\copy ${table}(${columns//mt./}) FROM program 'cat ${outputFile}' WITH CSV DELIMITER ',';" && \
     rm "${outputFile}" && \
-    log "Finished batch. Progress for ${table} cursor ${crsrId}: $((count * CURSOR_COPY_BATCH_SIZE)) records processed" &
+    log "Finished batch. Progress for ${table} cursor ${crsrId}: $((count * CURSOR_COPY_BATCH_SIZE)) records processed" && \
+    executeBookmark &
 
     # Retrieve the next batch
     echo "FETCH ${CURSOR_COPY_BATCH_SIZE} FROM crsr;" >"${source}"
@@ -216,14 +253,13 @@ executeCursor() {
 
     # Terminate the output pipe
     echo "\o /dev/null" >"${source}"
-
-    #TODO perform bookmark fetch and update bookmark migration table
   done
 
-  echo "\q" >"${source}"
   jobs
+  log "Waiting for ${table} cursor ${crsrId} to finish"
   wait
   log "Finished copying '${table}' for crsr ${crsrId}"
+  exit 0
 }
 
 migrateTableCursor() {
@@ -232,25 +268,21 @@ migrateTableCursor() {
 
   mkdir -m 700 "${tmpDir}" || die "Couldn't make safe tmp ${tmpDir}"
   pushd "${tmpDir}" || die "Couldn't change directory to tmp ${tmpDir}"
-  local maxTimestamp=${MAX_TIMESTAMP:-$2}
+  local maxTimestamp=$MAX_TIMESTAMP
   local query="
-      select string_agg(column_name, ', ' order by ordinal_position)
+      select string_agg(concat('mt.', column_name), ', ' order by ordinal_position)
       from information_schema.columns
       where table_schema = '${SOURCE_DB_SCHEMA}' and table_name = '${table}';"
 
   local columns=$(querySource "${query}")
   columns="${columns#"${columns%%[![:space:]]*}"}" # Trim leading whitespace
 
-  local minConsensusTimestamp=0
-  local migrationRangeNs=$((2**64-1))
+  local minConsensusTimestamp=$MIN_TIMESTAMP
   if [[ "${columns}" =~ consensus_timestamp ]]; then
-    minConsensusTimestamp=$(querySource "select min(consensus_timestamp) from ${table};")
-    migrationRangeNs=$((maxTimestamp - minConsensusTimestamp))
-    orderby="order by consensus_timestamp desc"
+    orderby="order by mt.consensus_timestamp desc"
   fi
 
-  local crsrRangeNs=$((migrationRangeNs / CONCURRENT_CURSORS_PER_TABLE))
-  log "Migration will process from  ${minConsensusTimestamp} to ${maxTimestamp} with each cursor processing ${crsrRangeNs} ns of timestamps for total range of ${migrationRangeNs} ns pid $$"
+  log "Migration will process from  ${minConsensusTimestamp} to ${maxTimestamp}"
 
   local cursorQuery="SELECT cursor_id, lower(cursor_range), least(last_processed - 1, upper(cursor_range)) FROM async_migration_status WHERE the_table = '${table}' ORDER BY cursor_id asc;"
   local tableCursors=$(PGPASSWORD="${TARGET_DB_PASSWORD}" psql -q --csv -t -h "${TARGET_DB_HOST}" -d "${TARGET_DB_NAME}" -p "${TARGET_DB_PORT}" -U "${TARGET_DB_USER}" -c "${cursorQuery}")
@@ -258,14 +290,16 @@ migrateTableCursor() {
   while IFS="," read -r cursor_id cursor_lower_bound cursor_upper_bound
   do
     if [[ "${columns}" =~ consensus_timestamp ]]; then
-      if [[ "${cursor_id}" -eq "${CONCURRENT_CURSORS_PER_TABLE}" ]]; then
+      if [[ -z "${cursor_lower_bound}" ]]; then
         where="where mt.consensus_timestamp <= ${cursor_upper_bound}"
       else
         where="where mt.consensus_timestamp <= ${cursor_upper_bound} and mt.consensus_timestamp > ${cursor_lower_bound}"
       fi
     fi
     if [[ "${table}" = "topic_message" ]]; then
-      join="join transaction jt on mt.topic_id = jt.entity_id and mt.consensus_timestamp = mt.consensus_timestamp"
+      join="join transaction jt on mt.topic_id = jt.entity_id and mt.consensus_timestamp = jt.consensus_timestamp"
+      where="${where//mt./jt.} and jt.type=27"
+      orderby="${orderby//mt./jt.}"
     fi
 
     local cursorDef="DECLARE crsr CURSOR FOR select ${columns} from ${table} mt ${join} ${where} ${orderby};"
@@ -280,12 +314,11 @@ migrateTableCursor() {
   jobs
 }
 # Export the functions so they can be invoked via parallel xargs
-export -f log querySource queryTarget migrateTableBinary migrateTableCursor executeCursor handlePipeError sourcePipe
+export -f log querySource queryTarget migrateTableBinary migrateTableCursor executeCursor handlePipeError sourcePipe readFifo
 
 SECONDS=0
 SCRIPTS_DIR="./"
-EXPORT_DIR="${SCRIPTS_DIR}/export"
-FLYWAY_DIR="${EXPORT_DIR}/flyway"
+FLYWAY_DIR="${FLYWAY_DIR}/flyway"
 MIGRATIONS_DIR="${SCRIPTS_DIR}/../../migration/v2"
 export PATH="${FLYWAY_DIR}/:$PATH"
 source "${SCRIPTS_DIR}/migration.config"
@@ -293,7 +326,9 @@ source "${SCRIPTS_DIR}/migration.config"
 export CONCURRENCY=${CONCURRENCY:-5}
 export CONCURRENT_CURSORS_PER_TABLE=${CONCURRENT_CURSORS_PER_TABLE:-5}
 export CURSOR_COPY_BATCH_SIZE=${CURSOR_COPY_BATCH_SIZE:-1000000}
+export CREATE_INDEXES_BEFORE_MIGRATION=${CREATE_INDEXES_BEFORE_MIGRATION:-true}
 export MAX_TIMESTAMP="${MAX_TIMESTAMP:-$(querySource "select max(consensus_end) from record_file;")}"
+export MIN_TIMESTAMP="${MIN_TIMESTAMP:-$(querySource "select min(consensus_timestamp) from transaction;")}"
 export SOURCE_DB_HOST="${SOURCE_DB_HOST:-127.0.0.1}"
 export SOURCE_DB_NAME="${SOURCE_DB_NAME:-mirror_node}"
 export SOURCE_DB_PASSWORD="${SOURCE_DB_PASSWORD:-mirror_node_pass}"
@@ -327,33 +362,34 @@ if [[ "${TARGET_DB_HEALTHY}" != "t" ]]; then
   exit 1
 fi
 
-#TODO remove?
-rm -rf "${EXPORT_DIR}"
-mkdir -p "${EXPORT_DIR}"
-cd "${EXPORT_DIR}" || die "Couldn't change directory to ${EXPORT_DIR}"
+cd "${FLYWAY_DIR}" || die "Couldn't change directory to ${FLYWAY_DIR}"
 
 log "Installing Flyway"
 wget -qO- ${FLYWAY_URL} | tar -xz && mv flyway-* flyway
 log "Flyway installed"
 
 log "Copying Flyway configuration"
-#cp "${MIGRATIONS_DIR}/V2.0."[0-2]* ${FLYWAY_DIR}/sql/
-#cat > "${FLYWAY_DIR}/conf/flyway.conf" <<EOF
-#flyway.password=${TARGET_DB_PASSWORD}
-#flyway.placeholders.hashShardCount=6
-#flyway.placeholders.idPartitionSize=1000000
-#flyway.placeholders.maxEntityId=5000000
-#flyway.placeholders.maxEntityIdRatio=2.0
-#flyway.placeholders.partitionStartDate='2019-09-01'
-#flyway.placeholders.partitionTimeInterval='1 month'
-#flyway.placeholders.schema=${TARGET_DB_SCHEMA}
-#flyway.placeholders.shardCount=16
-#flyway.url=jdbc:postgresql://${TARGET_DB_HOST}:${TARGET_DB_PORT}/${TARGET_DB_NAME}
-#flyway.user=${TARGET_DB_USER}
-#EOF
+
+if [[ "${CREATE_INDEXES_BEFORE_MIGRATION}" = "true" ]]; then
+  cp "${MIGRATIONS_DIR}/V2.0."* "${FLYWAY_DIR}"/sql/
+else
+  cp "${MIGRATIONS_DIR}/V2.0."[0-2]* "${FLYWAY_DIR}"/sql/
+fi
+cat > "${FLYWAY_DIR}/conf/flyway.conf" <<EOF
+flyway.password=${TARGET_DB_PASSWORD}
+flyway.placeholders.hashShardCount=6
+flyway.placeholders.maxEntityId=5000000
+flyway.placeholders.maxEntityIdRatio=2.0
+flyway.placeholders.partitionStartDate='2019-09-01'
+flyway.placeholders.partitionTimeInterval='1 month'
+flyway.placeholders.schema=${TARGET_DB_SCHEMA}
+flyway.placeholders.shardCount=16
+flyway.url=jdbc:postgresql://${TARGET_DB_HOST}:${TARGET_DB_PORT}/${TARGET_DB_NAME}
+flyway.user=${TARGET_DB_USER}
+EOF
 
 log "Running Flyway migrate"
-#flyway migrate
+flyway migrate
 
 TABLES_QUERY="
 select relname from pg_catalog.pg_statio_user_tables
@@ -364,26 +400,14 @@ order by pg_total_relation_size(relid) asc;"
 TABLES=$(querySource "${TABLES_QUERY}" | tr " " "\n")
 COUNT=$(echo "${TABLES}" | wc -l)
 
-log "Migrating ${COUNT} async tables from ${SOURCE_DB_HOST}:${SOURCE_DB_PORT} to ${TARGET_DB_HOST}:${TARGET_DB_PORT}. Tables: ${TABLES}"
-#echo "${TABLES}" | xargs -n 1 -P "${CONCURRENCY}" -I {} bash -c 'migrateTableBinary "$@"' _ {}
-#echo "${ASYNC_TABLES}" | xargs -n 1 -P "${CONCURRENCY}" -I {} bash -c 'migrateTableCursor "$@"' _ {}
+log "Migrating ${COUNT} tables from ${SOURCE_DB_HOST}:${SOURCE_DB_PORT} to ${TARGET_DB_HOST}:${TARGET_DB_PORT}. Tables: ${TABLES}"
+echo "${TABLES}" | xargs -n 1 -P "${CONCURRENCY}" -I {} bash -c 'migrateTableBinary "$@"' _ {}
 
+log "Starting async migration"
 insertCursors
-time migrateTableCursor transaction
+ASYNC_TABLES=$(echo "${ASYNC_TABLES//[\']/}" | tr "," "\n")
+echo "${ASYNC_TABLES}" |  xargs -n 1 -P "${CONCURRENCY}" -I {} bash -c 'migrateTableCursor "$@"' _ {}
 
 log "Migration completed in $SECONDS seconds"
 
-#TODO handle not getting future partitions
-#queryTarget "CREATE TABLE IF NOT EXISTS async_migration_status (parent TEXT, partition TEXT primary key, start_partition bigint, end_partition bigint, processed_range int8range, migration_start timestamptz, migration_stop timestamptz);"
-#queryTarget "insert into async_migration_status(parent, partition, start_partition, end_partition) select parent, name, from_timestamp, to_timestamp from mirror_node_time_partitions where parent not in (${EXCLUDED_TABLES}) and parent in (${ASYNC_TABLES}) ON CONFLICT(partition) DO NOTHING;"
-### to do need to select upper and lower processed range
-#ASYNC_TABLE_PARTITIONS=$(queryTarget "select parent, partition, start_partition, end_partition from async_migration_status where processed_range IS NULL or processed_range != int8range(start_partition, end_partition, '[)') order by start_partition DESC, partition;" | tr " " "\n")
-#echo "${ASYNC_TABLE_PARTITIONS}"
-#echo "The aysnc tables are ${ASYNC_TABLE_PARTITIONS}"
-#echo "${ASYNC_TABLE_PARTITIONS}"| awk -F , '{print $1,$2,$3,$4,$5,$6};' |xargs -P "${CONCURRENCY}" -n1 -I {} bash -c 'migrateAsyncTable $1 $2 $3 $4 $5 $6' _ {}
-
 exit 0
-
-#TODO
-#populate transaction hash
-# turn off on vacuum?
