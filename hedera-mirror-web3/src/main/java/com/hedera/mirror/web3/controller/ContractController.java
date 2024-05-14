a7cd0b9f0e29d9879674cdc41c35061258d20526
Victor Yanev <victor.yanev@limechain.tech>
2024-05-14 15:37:02 +0300

    chore: move constructServiceParameters to CallServiceParametersBuilder
    Signed-off-by: Victor Yanev <victor.yanev@limechain.tech>

--- hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/controller/ContractController.java
+++ hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/controller/ContractController.java
@@ -16,24 +16,18 @@
 
 package com.hedera.mirror.web3.controller;
 
-import static com.hedera.mirror.web3.service.model.CallServiceParameters.CallType.ETH_CALL;
-import static com.hedera.mirror.web3.service.model.CallServiceParameters.CallType.ETH_ESTIMATE_GAS;
-import static org.apache.tuweni.bytes.Bytes.EMPTY;
+import static com.hedera.mirror.web3.service.CallServiceParametersBuilder.buildFromContractCallRequest;
 
 import com.hedera.mirror.web3.evm.properties.MirrorNodeEvmProperties;
 import com.hedera.mirror.web3.exception.InvalidParametersException;
 import com.hedera.mirror.web3.exception.RateLimitException;
 import com.hedera.mirror.web3.service.ContractCallService;
-import com.hedera.mirror.web3.service.model.CallServiceParameters;
 import com.hedera.mirror.web3.viewmodel.ContractCallRequest;
 import com.hedera.mirror.web3.viewmodel.ContractCallResponse;
-import com.hedera.node.app.service.evm.store.models.HederaEvmAccount;
 import io.github.bucket4j.Bucket;
 import jakarta.validation.Valid;
 import lombok.CustomLog;
 import lombok.RequiredArgsConstructor;
-import org.apache.tuweni.bytes.Bytes;
-import org.hyperledger.besu.datatypes.Address;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -60,47 +54,10 @@ class ContractController {
         validateContractData(request);
         validateContractMaxGasLimit(request);
 
-        final var params = constructServiceParameters(request);
+        final var params = buildFromContractCallRequest(request);
         final var result = contractCallService.processCall(params);
 
         return new ContractCallResponse(result);
-    }
-
-    private CallServiceParameters constructServiceParameters(ContractCallRequest request) {
-        final var fromAddress = request.getFrom() != null ? Address.fromHexString(request.getFrom()) : Address.ZERO;
-        final var sender = new HederaEvmAccount(fromAddress);
-
-        Address receiver;
-
-        /*In case of an empty "to" field, we set a default value of the zero address
-        to avoid any potential NullPointerExceptions throughout the process.*/
-        if (request.getTo() == null || request.getTo().isEmpty()) {
-            receiver = Address.ZERO;
-        } else {
-            receiver = Address.fromHexString(request.getTo());
-        }
-        Bytes data;
-        try {
-            data = request.getData() != null ? Bytes.fromHexString(request.getData()) : EMPTY;
-        } catch (Exception e) {
-            throw new InvalidParametersException(
-                    "data field '%s' contains invalid odd length characters".formatted(request.getData()));
-        }
-        final var isStaticCall = false;
-        final var callType = request.isEstimate() ? ETH_ESTIMATE_GAS : ETH_CALL;
-        final var block = request.getBlock();
-
-        return CallServiceParameters.builder()
-                .sender(sender)
-                .receiver(receiver)
-                .callData(data)
-                .gas(request.getGas())
-                .value(request.getValue())
-                .isStatic(isStaticCall)
-                .callType(callType)
-                .isEstimate(request.isEstimate())
-                .block(block)
-                .build();
     }
 
     /*
--- hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/service/CallServiceParametersBuilder.java
+++ hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/service/CallServiceParametersBuilder.java
@@ -16,9 +16,20 @@
 
 package com.hedera.mirror.web3.service;
 
+import static com.hedera.mirror.web3.service.model.CallServiceParameters.CallType.ETH_CALL;
+import static com.hedera.mirror.web3.service.model.CallServiceParameters.CallType.ETH_ESTIMATE_GAS;
+import static org.apache.tuweni.bytes.Bytes.EMPTY;
+
 import com.hedera.mirror.web3.common.TransactionIdOrHashParameter;
+import com.hedera.mirror.web3.exception.InvalidParametersException;
 import com.hedera.mirror.web3.service.model.CallServiceParameters;
+import com.hedera.mirror.web3.viewmodel.ContractCallRequest;
+import com.hedera.node.app.service.evm.store.models.HederaEvmAccount;
 import edu.umd.cs.findbugs.annotations.NonNull;
+import java.util.Optional;
+import org.apache.tuweni.bytes.Bytes;
+import org.hyperledger.besu.datatypes.Address;
+import org.springframework.util.StringUtils;
 
 public interface CallServiceParametersBuilder {
 
@@ -27,4 +38,38 @@ public interface CallServiceParametersBuilder {
      * @return the {@link CallServiceParameters} for the given transaction id or hash
      */
     CallServiceParameters buildFromTransaction(@NonNull TransactionIdOrHashParameter transactionIdOrHash);
+
+    static CallServiceParameters buildFromContractCallRequest(ContractCallRequest request) {
+        // In case of an empty "to" field, we set a default value of the zero address
+        // to avoid any potential NullPointerExceptions throughout the process.
+        final Address sender = Optional.ofNullable(request.getFrom())
+                .filter(StringUtils::hasText)
+                .map(Address::fromHexString)
+                .orElse(Address.ZERO);
+
+        final Address receiver = Optional.ofNullable(request.getTo())
+                .filter(StringUtils::hasText)
+                .map(Address::fromHexString)
+                .orElse(Address.ZERO);
+
+        final Bytes callData;
+        try {
+            callData = request.getData() != null ? Bytes.fromHexString(request.getData()) : EMPTY;
+        } catch (Exception e) {
+            throw new InvalidParametersException(
+                    "data field '%s' contains invalid odd length characters".formatted(request.getData()));
+        }
+
+        return CallServiceParameters.builder()
+                .sender(new HederaEvmAccount(sender))
+                .receiver(receiver)
+                .callData(callData)
+                .gas(request.getGas())
+                .value(request.getValue())
+                .isStatic(false)
+                .callType(request.isEstimate() ? ETH_ESTIMATE_GAS : ETH_CALL)
+                .isEstimate(request.isEstimate())
+                .block(request.getBlock())
+                .build();
+    }
 }
