72203968fdc9ef089b3d15c4a4ff8f2876de6dc9
Steven Sheehy <steven.sheehy@swirldslabs.com>
2024-05-16 16:43:05 -0500

    Increase web3 maxDataSize to 128 KiB
    Signed-off-by: Steven Sheehy <steven.sheehy@swirldslabs.com>

--- hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/evm/properties/MirrorNodeEvmProperties.java
+++ hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/evm/properties/MirrorNodeEvmProperties.java
@@ -118,7 +118,7 @@ public class MirrorNodeEvmProperties implements EvmProperties {
     @Getter
     @NotNull
     @DataSizeUnit(DataUnit.KILOBYTES)
-    private DataSize maxDataSize = DataSize.ofKilobytes(25);
+    private DataSize maxDataSize = DataSize.ofKilobytes(128);
 
     @Getter(lazy = true)
     private final Pattern dataValidatorPattern =
