aa9298dc4350c9d22232eb31ab82d74830bc236e
Victor Yanev <victor.yanev@limechain.tech>
2024-05-16 18:57:51 +0300

    chore: add @ExtendWith(SpringExtension.class) to TransactionUtilsTest
    Signed-off-by: Victor Yanev <victor.yanev@limechain.tech>

--- hedera-mirror-web3/src/test/java/com/hedera/mirror/web3/service/utils/TransactionUtilsTest.java
+++ hedera-mirror-web3/src/test/java/com/hedera/mirror/web3/service/utils/TransactionUtilsTest.java
@@ -25,10 +25,13 @@ import com.hederahashgraph.api.proto.java.TransactionID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 class TransactionUtilsTest {
 
     private static final Pattern TRANSACTION_ID_PATTERN = Pattern.compile("^(\\d+)\\.(\\d+)\\.(\\d+)-(\\d{1,19})-(\\d{1,9})$");
