a9d3136540575daaf35c5e269011086a9ce95da5
Konstantina Blazhukova <konstantina.blajukova@gmail.com>
2024-06-20 15:27:37 +0300

    Fixes query for contract actions
    Signed-off-by: Konstantina Blazhukova <konstantina.blajukova@gmail.com>

--- hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/repository/ContractActionRepository.java
+++ hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/repository/ContractActionRepository.java
@@ -25,7 +25,7 @@ public interface ContractActionRepository extends CrudRepository<ContractAction,
 
     @Query(
             value =
-                    "select * from contract_action where consensus_timestamp = ?1 and result_data_type = 13 and call_type = 4 order by index asc",
+                    "select * from contract_action where consensus_timestamp = ?1 and result_data_type = 12 and call_type = 4 order by index asc",
             nativeQuery = true)
-    List<ContractAction> findFailedPrecompileContractActions(long consensusTimestamp);
+    List<ContractAction> findFailedSystemActionsByConsensusTimestamp(long consensusTimestamp);
 }
