a0e67f7a7b967704c94847bf4022762768adfbbd
mgoelswirlds <mugdha.goel@swirldslabs.com>
2024-07-11 15:04:09 -0400

    dropping default on port.
    Signed-off-by: mgoelswirlds <mugdha.goel@swirldslabs.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v2/V2.3.0__add_domain_name.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/v2/V2.3.0__add_domain_name.sql
@@ -4,6 +4,7 @@ alter table if exists address_book_service_endpoint drop constraint if exists ad
 
 alter table if exists address_book_service_endpoint
     alter column ip_address_v4 drop not null,
+    alter column port drop default,
     alter column port drop not null,
     add column if not exists domain_name varchar(253) default null;
 
