c14c3aa68aea8d4abf18a715dc98c70870fc5ffb
Jeff Schmidt <jeffrey.schmidt@swirldslabs.com>
2024-07-17 10:26:35 -0600

    Override postgresql container with the help of spring.main.allow-bean-definition-overriding=true.
    Signed-off-by: Jeff Schmidt <jeffrey.schmidt@swirldslabs.com>

--- hedera-mirror-rest-java/src/test/java/com/hedera/mirror/restjava/spec/RestSpecTest.java
+++ hedera-mirror-rest-java/src/test/java/com/hedera/mirror/restjava/spec/RestSpecTest.java
@@ -27,7 +27,10 @@ import com.hedera.mirror.restjava.spec.model.RestSpecNormalized;
 import jakarta.annotation.Resource;
 import java.io.File;
 import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -39,37 +42,55 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestClient;
+import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.output.OutputFrame;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.utility.DockerImageName;
 
 @RequiredArgsConstructor
 @RunWith(SpringRunner.class)
 @EnableConfigurationProperties(value = RestJavaProperties.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {"spring.main.allow-bean-definition-overriding=true"})
 public class RestSpecTest extends RestJavaIntegrationTest {
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    private static GenericContainer<?> jsRestApi = new GenericContainer<>(
+                    DockerImageName.parse("gcr.io/mirrornode/hedera-mirror-rest:latest"))
+//                    new ImageFromDockerfile("localhost/testcontainers/restapi", false)
+//                            .withDockerfile(Path.of("../hedera-mirror-rest/Dockerfile")))
+            .withExposedPorts(5551);
 
     @TestConfiguration(proxyBeanMethods = false)
     static class Configuration {
         @Bean
-        GenericContainer<?> jsRestApi(PostgreSQLContainer<?> postgresql) {
-            var logger = LoggerFactory.getLogger(GenericContainer.class);
-            return new GenericContainer<>(
-                    DockerImageName.parse("gcr.io/mirrornode/hedera-mirror-rest:latest"))
-//                    new ImageFromDockerfile("localhost/testcontainers/restapi", false)
-//                            .withDockerfile(Path.of("../hedera-mirror-rest/Dockerfile")))
-                    .dependsOn(postgresql)
-                    .withExposedPorts(5551)
-                    .withLogConsumer(new Slf4jLogConsumer(logger, true))
-                    .withEnv(Map.of(
-                    "HEDERA_MIRROR_REST_REDIS_ENABLED", "false",
-                    "HEDERA_MIRROR_REST_DB_HOST", postgresql.getHost(),
-                    "HEDERA_MIRROR_REST_DB_PORT", postgresql.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT).toString()));
+        Network postgresqlNetwork() {
+            return Network.newNetwork();
+        }
+
+        @Bean
+        @ServiceConnection("postgresql")
+        PostgreSQLContainer<?> postgresql(Network postgresqlNetwork) {
+            var dockerImageName = DockerImageName.parse("postgres:14-alpine").asCompatibleSubstituteFor("postgres");
+            var logger = LoggerFactory.getLogger(PostgreSQLContainer.class);
+            var excluded = "terminating connection due to unexpected postmaster exit";
+            var logConsumer = new FilteringConsumer(
+                    new Slf4jLogConsumer(logger, true),
+                    o -> !StringUtils.contains(o.getUtf8StringWithoutLineEnding(), excluded));
+            return new PostgreSQLContainer<>(dockerImageName)
+                    .withNetwork(postgresqlNetwork)
+                    .withNetworkAliases("postgresql")
+                    .withClasspathResourceMapping("init.sql", "/docker-entrypoint-initdb.d/init.sql", BindMode.READ_ONLY)
+                    .withDatabaseName("mirror_node")
+                    .withLogConsumer(logConsumer)
+                    .withPassword("mirror_node_pass")
+                    .withUsername("mirror_node");
         }
     }
 
@@ -77,7 +98,7 @@ public class RestSpecTest extends RestJavaIntegrationTest {
     private SpecDomainBuilder specDomainBuilder;
 
     @Autowired
-    GenericContainer<?> jsRestApi;
+    Network prostgresqlNetwork;
 
     @Autowired
     private RestJavaProperties properties;
@@ -92,6 +113,15 @@ public class RestSpecTest extends RestJavaIntegrationTest {
 
     @BeforeEach
     final void setup() {
+        if (!jsRestApi.isRunning()) {
+            jsRestApi
+                    .withNetwork(prostgresqlNetwork)
+                    .withEnv(Map.of(
+                    "HEDERA_MIRROR_REST_REDIS_ENABLED", "false",
+                    "HEDERA_MIRROR_REST_DB_HOST", "postgresql", // Postgresql container network alias
+                    "HEDERA_MIRROR_REST_DB_PORT", PostgreSQLContainer.POSTGRESQL_PORT.toString()));
+            jsRestApi.start();
+        }
 
         baseUrl = "http://localhost:%d".formatted(port);
         baseJsRestApiUrl = "http://%s:%d".formatted(jsRestApi.getHost(), jsRestApi.getMappedPort(5551));
@@ -104,7 +134,7 @@ public class RestSpecTest extends RestJavaIntegrationTest {
 
     @Test
     void phase1WithSingleFile() throws Exception {
-        var file = new File("../hedera-mirror-rest/__tests__/specs/network/supply/no-params.json");
+        var file = new File("../hedera-mirror-rest/__tests__/specs/blocks/no-records.json");
         runSpecTest(file);
         System.out.println("ran " + file.getName());
     }
@@ -122,6 +152,20 @@ public class RestSpecTest extends RestJavaIntegrationTest {
                         .toEntity(String.class);
                 assertThat(response.getStatusCode().value()).isEqualTo(specTest.responseStatus());
                 JSONAssert.assertEquals(specTest.responseJson().toString(), response.getBody(), JSONCompareMode.LENIENT);
+            }
+        }
+    }
+
+    @RequiredArgsConstructor
+    private static class FilteringConsumer implements Consumer<OutputFrame> {
+
+        private final Consumer<OutputFrame> delegate;
+        private final Predicate<OutputFrame> filter;
+
+        @Override
+        public void accept(OutputFrame outputFrame) {
+            if (filter.test(outputFrame)) {
+                delegate.accept(outputFrame);
             }
         }
     }
