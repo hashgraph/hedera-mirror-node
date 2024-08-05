a1bb60c612cc54236c2127ae56458a0b05e1e422
Jesse Nelson <jesse@hashgraph.com>
2024-08-05 12:09:25 -0500

    address pr feedback
    Signed-off-by: Jesse Nelson <jesse@hashgraph.com>

--- hedera-mirror-rest/errors/invalidArgumentError.js
+++ hedera-mirror-rest/errors/invalidArgumentError.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import RestError from './restError.js';
+import RestError from './restError';
 
 const InvalidArgumentErrorMessageFormat = 'Invalid parameter: ';
 const invalidParamUsageMessageFormat = 'Invalid parameter usage: ';
@@ -22,43 +22,43 @@ const ParameterExceedsMaxErrorMessageFormat = 'Parameter values count exceeds ma
 const unknownParamMessageFormat = 'Unknown query parameter: ';
 
 class InvalidArgumentError extends RestError {
-  static INVALID_ERROR_CODE = 'invalidArgument';
-  static PARAM_COUNT_EXCEEDS_MAX_CODE = 'paramCountExceedsMax';
-  static INVALID_PARAM_USAGE = 'invalidParamUsage';
-  static UNKNOWN_PARAM_USAGE = 'unknownParamUsage';
+    static INVALID_ERROR_CODE = 'invalidArgument';
+    static PARAM_COUNT_EXCEEDS_MAX_CODE = 'paramCountExceedsMax';
+    static INVALID_PARAM_USAGE = 'invalidParamUsage';
+    static UNKNOWN_PARAM_USAGE = 'unknownParamUsage';
 
-  constructor(errorMessage) {
-    super();
-    this.message = errorMessage;
-  }
-
-  // factory method to help common case
-  static forParams(badParams) {
-    if (!Array.isArray(badParams)) {
-      badParams = [badParams];
-    }
-    return new InvalidArgumentError(badParams.map((message) => `${InvalidArgumentErrorMessageFormat}${message}`));
-  }
-
-  static forRequestValidation(badParams) {
-    if (!Array.isArray(badParams)) {
-      badParams = [badParams];
+    constructor(errorMessage) {
+        super();
+        this.message = errorMessage;
     }
 
-    return new InvalidArgumentError(
-      badParams.map((message) => {
-        if (message.code === this.PARAM_COUNT_EXCEEDS_MAX_CODE) {
-          return `${ParameterExceedsMaxErrorMessageFormat}${message.key} count: ${message.count} max: ${message.max}`;
-        } else if (message.code === this.INVALID_PARAM_USAGE) {
-          return `${invalidParamUsageMessageFormat}${message.key} - ${message.error}`;
-        } else if (message.code === this.UNKNOWN_PARAM_USAGE) {
-          return `${unknownParamMessageFormat}${message.key || message}`;
-        } else {
-          return `${InvalidArgumentErrorMessageFormat}${message.key || message}`;
+    // factory method to help common case
+    static forParams(badParams) {
+        if (!Array.isArray(badParams)) {
+            badParams = [badParams];
         }
-      })
-    );
-  }
+        return new InvalidArgumentError(badParams.map((message) => `${InvalidArgumentErrorMessageFormat}${message}`));
+    }
+
+    static forRequestValidation(badParams) {
+        if (!Array.isArray(badParams)) {
+            badParams = [badParams];
+        }
+
+        return new InvalidArgumentError(
+            badParams.map((message) => {
+                if (message.code === this.PARAM_COUNT_EXCEEDS_MAX_CODE) {
+                    return `${ParameterExceedsMaxErrorMessageFormat}${message.key} count: ${message.count} max: ${message.max}`;
+                } else if (message.code === this.INVALID_PARAM_USAGE) {
+                    return `${invalidParamUsageMessageFormat}${message.key} - ${message.error}`;
+                } else if (message.code === this.UNKNOWN_PARAM_USAGE) {
+                    return `${unknownParamMessageFormat}${message.key || message}`;
+                } else {
+                    return `${InvalidArgumentErrorMessageFormat}${message.key || message}`;
+                }
+            })
+        );
+    }
 }
 
 export default InvalidArgumentError;
