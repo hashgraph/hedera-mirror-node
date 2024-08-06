887eb8a7364f792fb7de10b3db2859f632b441c0
Kristiyan Selveliev <kristiyan.selveliev@limechain.tech>
2024-08-06 15:22:29 +0300

    feat: Load archived openzeppelin classes to allow compilation with older solidity version
    Signed-off-by: Kristiyan Selveliev <kristiyan.selveliev@limechain.tech>

--- hedera-mirror-web3/src/test/solidity/EthCall.sol
+++ hedera-mirror-web3/src/test/solidity/EthCall.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "../../../build/openzeppelin/token/ERC20/extensions/IERC20Metadata.sol";
 import "./HederaTokenService.sol";
 import "./IHederaTokenService.sol";
 
