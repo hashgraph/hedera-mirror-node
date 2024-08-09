43fbef62602de4b6fc0ada595c5503525240ae58
Xin Li <xin@swirldslabs.com>
2024-08-09 10:13:42 -0500

    remove extra white space
    Signed-off-by: Xin Li <xin@swirldslabs.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v2/V2.4.1__change_transaction_hash_distribution.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/v2/V2.4.1__change_transaction_hash_distribution.sql
@@ -5,4 +5,4 @@ update transaction_hash
 set distribution_id = ('x' || encode(substring(hash from 1 for 2), 'hex'))::bit(32)::int >> 16;
 
 select alter_distributed_table('transaction_hash', distribution_column := 'distribution_id');
-create index if  not exists transaction_hash__hash on transaction_hash using hash (substring(hash from 1 for 32));
+create index if not exists transaction_hash__hash on transaction_hash using hash (substring(hash from 1 for 32));
