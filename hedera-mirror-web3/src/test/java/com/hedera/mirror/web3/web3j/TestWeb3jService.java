240e6ed4aef4d0d0b335997c3c7c6855ce29aaee
Kristiyan Selveliev <kristiyan.selveliev@limechain.tech>
2024-08-19 17:03:29 +0300

    feat: Refactor ContractCallAddressThisTest (#9014)
    This pr refactors ContractCallAddressThisTest to use the new approach with the web3j plugin.
    
    This PR modifies
    
    Modifies ContractCallAddressThisTest to use the new refactoring approach.
    Modifies TestWeb3jService adding a possibility to persist contracts during deploy with a custom approach.
    
    ---------
    
    Signed-off-by: Kristiyan Selveliev <kristiyan.selveliev@limechain.tech>

--- hedera-mirror-web3/src/test/java/com/hedera/mirror/web3/web3j/TestWeb3jService.java
+++ hedera-mirror-web3/src/test/java/com/hedera/mirror/web3/web3j/TestWeb3jService.java
@@ -70,17 +70,18 @@ import org.web3j.utils.Numeric;
 public class TestWeb3jService implements Web3jService {
     private static final long DEFAULT_TRANSACTION_VALUE = 10L;
     private static final String MOCK_KEY = "0x4e3c5c727f3f4b8f8e8a8fe7e032cf78b8693a2b711e682da1d3a26a6a3b58b6";
-
+    private final DomainBuilder domainBuilder;
     private final ContractExecutionService contractExecutionService;
     private final ContractGasProvider contractGasProvider;
     private final Credentials credentials;
-    private final DomainBuilder domainBuilder;
     private final Web3j web3j;
     private Address sender = Address.fromHexString("");
-    private boolean isEstimateGas = false;
     private String transactionResult;
+    private boolean isEstimateGas = false;
     private String estimatedGas;
     private long value = 0L;
+    private boolean persistContract = true;
+    private byte[] contractRuntime;
     private BlockType blockType = BlockType.LATEST;
 
     public TestWeb3jService(ContractExecutionService contractExecutionService, DomainBuilder domainBuilder) {
@@ -119,8 +120,24 @@ public class TestWeb3jService implements Web3jService {
         this.blockType = blockType;
     }
 
+    public byte[] getContractRuntime() {
+        return contractRuntime;
+    }
+
+    public void reset() {
+        this.isEstimateGas = false;
+        this.contractRuntime = null;
+        this.persistContract = true;
+    }
+
     @SneakyThrows(Exception.class)
     public <T extends Contract> T deploy(Deployer<T> deployer) {
+        return deployer.deploy(web3j, credentials, contractGasProvider).send();
+    }
+
+    @SneakyThrows(Exception.class)
+    public <T extends Contract> T deployWithoutPersist(Deployer<T> deployer) {
+        persistContract = false;
         return deployer.deploy(web3j, credentials, contractGasProvider).send();
     }
 
@@ -141,26 +158,13 @@ public class TestWeb3jService implements Web3jService {
         };
     }
 
-    private EthSendTransaction call(List<?> params, Request request) {
-        var rawTransaction = TransactionDecoder.decode(params.get(0).toString());
-        var transactionHash = generateTransactionHashHexEncoded(rawTransaction, credentials);
-        final var to = rawTransaction.getTo();
-
-        if (to.equals(HEX_PREFIX)) {
-            return sendTopLevelContractCreate(rawTransaction, transactionHash, request);
-        }
-
-        return sendEthCall(rawTransaction, transactionHash, request);
-    }
-
     private EthSendTransaction sendTopLevelContractCreate(
             RawTransaction rawTransaction, String transactionHash, Request request) {
         final var res = new EthSendTransaction();
         var serviceParameters = serviceParametersForTopLevelContractCreate(rawTransaction.getData(), ETH_CALL, sender);
         final var mirrorNodeResult = contractExecutionService.processCall(serviceParameters);
-
         try {
-            final var contractInstance = this.deployInternal(mirrorNodeResult);
+            final var contractInstance = deployInternal(mirrorNodeResult, persistContract);
             res.setResult(transactionHash);
             res.setRawResponse(contractInstance.toHexString());
             res.setId(request.getId());
@@ -171,6 +175,18 @@ public class TestWeb3jService implements Web3jService {
         }
 
         return res;
+    }
+
+    private EthSendTransaction call(List<?> params, Request request) {
+        var rawTransaction = TransactionDecoder.decode(params.getFirst().toString());
+        var transactionHash = generateTransactionHashHexEncoded(rawTransaction, credentials);
+        final var to = rawTransaction.getTo();
+
+        if (to.equals(HEX_PREFIX)) {
+            return sendTopLevelContractCreate(rawTransaction, transactionHash, request);
+        }
+
+        return sendEthCall(rawTransaction, transactionHash, request);
     }
 
     private EthSendTransaction sendEthCall(RawTransaction rawTransaction, String transactionHash, Request request) {
@@ -282,7 +298,7 @@ public class TestWeb3jService implements Web3jService {
                 .build();
     }
 
-    protected ContractExecutionParameters serviceParametersForTopLevelContractCreate(
+    public ContractExecutionParameters serviceParametersForTopLevelContractCreate(
             final String contractInitCode, final CallServiceParameters.CallType callType, final Address senderAddress) {
         final var senderEvmAccount = new HederaEvmAccount(senderAddress);
 
@@ -299,11 +315,14 @@ public class TestWeb3jService implements Web3jService {
                 .build();
     }
 
-    public Address deployInternal(String binary) {
+    public Address deployInternal(String binary, boolean persistContract) {
         final var id = domainBuilder.id();
         final var contractAddress = toAddress(EntityId.of(id));
-        contractPersist(binary, id);
-
+        if (persistContract) {
+            contractPersist(binary, id);
+        } else {
+            contractRuntime = Hex.decode(binary.replace(HEX_PREFIX, ""));
+        }
         return contractAddress;
     }
 
--- hedera-mirror-web3/src/test/solidity/TestNestedAddressThis.sol
+++ hedera-mirror-web3/src/test/solidity/TestNestedAddressThis.sol
@@ -1,8 +1,8 @@
-pragma solidity 0.8.18;
+pragma solidity ^0.8.18;
+
 import "./TestAddressThis.sol";
 
 contract TestNestedAddressThis {
-
 
     constructor() {
         TestAddressThis child = new TestAddressThis();
