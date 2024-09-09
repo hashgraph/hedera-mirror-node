f6602044c3838d3db4ab8a0aa9f81c2e5dc93d61
Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>
2024-09-09 16:33:26 +0300

    Remove web3j-cli and reuse the plugin for historical tests
    Signed-off-by: Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>

--- hedera-mirror-web3/build.gradle.kts
+++ hedera-mirror-web3/build.gradle.kts
@@ -16,6 +16,8 @@
 
 import java.net.HttpURLConnection
 import java.net.URI
+import org.web3j.solidity.gradle.plugin.SolidityCompile
+import org.web3j.solidity.gradle.plugin.SolidityResolve
 
 description = "Hedera Mirror Node Web3"
 
@@ -45,6 +47,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
     implementation("org.springframework.cloud:spring-cloud-starter-kubernetes-fabric8-config")
+    testImplementation(project(mapOf("path" to ":web3")))
+    testImplementation(project(mapOf("path" to ":web3")))
     runtimeOnly("org.postgresql:postgresql")
     testImplementation(project(path = ":common", configuration = "testClasses"))
     testImplementation("io.vertx:vertx-core")
@@ -60,25 +64,28 @@ web3j {
     useNativeJavaTypes = true
 }
 
-sourceSets { test { solidity { version = "0.8.24" } } }
+// Define "testHistorical" source set needed for the test historical solidity contracts and web3j
+sourceSets {
+    val testHistorical by creating {
+        java { setSrcDirs(listOf("src/testHistorical/java", "src/testHistorical/solidity")) }
+        resources { setSrcDirs(listOf("src/testHistorical/resources")) }
+        compileClasspath += sourceSets["test"].output + configurations["testRuntimeClasspath"]
+        runtimeClasspath += sourceSets["test"].output + configurations["testRuntimeClasspath"]
+        solidity { version = "0.8.7" }
+    }
+    test { solidity { version = "0.8.24" } }
+}
 
 tasks.bootRun { jvmArgs = listOf("--enable-preview") }
 
 tasks.compileJava { options.compilerArgs.add("--enable-preview") }
 
+tasks.compileTestJava {
+    options.compilerArgs.add("--enable-preview")
+    options.compilerArgs.removeIf { it == "-Werror" }
+}
+
 tasks.test { jvmArgs = listOf("--enable-preview") }
-
-val homeDir = System.getenv("HOME")
-val web3jLink = file("$homeDir/.web3j/web3j")
-
-val downloadWeb3j =
-    tasks.register<Exec>("downloadWeb3j") {
-        description = "Download and install Web3j CLI"
-        group = "historical"
-
-        commandLine("bash", "-c", "curl -L get.web3j.io | sh")
-        onlyIf { !web3jLink.exists() }
-    }
 
 // Tasks to download OpenZeppelin contracts
 val openZeppelinVersion = "4.9.3"
@@ -86,8 +93,8 @@ val openZeppelinFile = layout.buildDirectory.file("openzeppelin.zip").get().asFi
 val openZeppelinDir =
     layout.projectDirectory.asFile
         .resolve("src")
-        .resolve("test")
-        .resolve("solidity_historical")
+        .resolve("testHistorical")
+        .resolve("solidity")
         .resolve("openzeppelin")
 
 val downloadOpenZeppelin =
@@ -95,6 +102,10 @@ val downloadOpenZeppelin =
         description = "Download OpenZeppelin contracts"
         group = "historical"
         doLast {
+            val buildDir = layout.buildDirectory.asFile.get()
+            if (!buildDir.exists()) {
+                mkdir(buildDir)
+            }
             openZeppelinDir.mkdirs()
             val openZeppelinUrl =
                 "https://github.com/OpenZeppelin/openzeppelin-contracts/archive/v${openZeppelinVersion}.zip"
@@ -119,31 +130,104 @@ val extractOpenZeppelin =
         }
     }
 
-// Task to compile Solidity contracts and generate Java files
-val compileHistoricalSolidityContracts =
-    tasks.register<Exec>("compileHistoricalSolidityContracts") {
-        description = "Compiles the historical solidity contracts to java files using web3j-cli"
-        group = "historical"
-        mustRunAfter(tasks.named("generateTestContractWrappers"))
-        dependsOn(downloadWeb3j)
-        dependsOn(extractOpenZeppelin)
-        dependsOn(tasks.named("compileTestSolidity"))
-        val scriptPath = file("./src/main/resources/scripts/compile_solidity.sh").absolutePath
-        doFirst { file(scriptPath).setExecutable(true) }
-        commandLine("bash", scriptPath)
-    }
-
-tasks.assemble { dependsOn(tasks.processTestResources) }
-
-tasks.compileTestJava {
-    options.compilerArgs.add("--enable-preview")
-    options.compilerArgs.removeIf { it == "-Werror" }
-    dependsOn(compileHistoricalSolidityContracts)
-}
-
 tasks.openApiGenerate { mustRunAfter(tasks.named("resolveSolidity")) }
 
-tasks.processTestResources {
+tasks.processTestResources { dependsOn(tasks.named("generateTestContractWrappers")) }
+
+// Define the solidity resolve task for version 0.8.7
+tasks.register("resolveSolidityHistorical", SolidityResolve::class) {
+    group = "historical"
+    version = "0.8.7"
+    sources = fileTree("src/testHistorical/solidity")
+    allowPaths = setOf("src/testHistorical/solidity")
+    packageJson =
+        file(".gradle/nodejs/node-v18.17.1-darwin-arm64/lib/node_modules/npm/package.json")
+    dependsOn("extractContracts")
+}
+
+// Define the solidity compile task for version 0.8.7
+afterEvaluate {
+    tasks.named("compileTestHistoricalSolidity", SolidityCompile::class.java).configure {
+        group = "historical"
+        allowPaths = setOf("src/testHistorical/solidity/openzeppelin")
+        ignoreMissing = true
+        version = "0.8.7"
+        source = fileTree("src/testHistorical/solidity") { include("*.sol") }
+        dependsOn(extractOpenZeppelin)
+    }
+}
+
+afterEvaluate {
+    tasks.named("generateTestHistoricalContractWrappers") {
+        dependsOn(tasks.named("generateTestContractWrappers"))
+        dependsOn(extractOpenZeppelin)
+        dependsOn(tasks.named("resolveSolidityHistorical"))
+    }
+}
+
+val processTestHistoricalResources =
+    tasks.named("processTestHistoricalResources") {
+        group = "historical"
+
+        dependsOn(tasks.named("generateTestContractWrappers"))
+        dependsOn(tasks.named("generateTestHistoricalContractWrappers"))
+        dependsOn(tasks.named("moveAndCleanTestHistorical"))
+    }
+
+// The generated java files are moved from "testHistorical" to "test" folder in build so that
+// they can be resolved successfully in imports.
+tasks.register<Copy>("moveTestHistoricalFiles") {
+    description = "Move files from testHistorical to test in the generated web3j sources directory."
+    group = "historical"
+
+    // Define source and destination directories
+    val srcDir =
+        file(
+            "$rootDir/hedera-mirror-web3/build/generated/sources/web3j/testHistorical/java/com/hedera/mirror/web3/web3j/generated"
+        )
+    val destDir =
+        file(
+            "$rootDir/hedera-mirror-web3/build/generated/sources/web3j/test/java/com/hedera/mirror/web3/web3j/generated"
+        )
+
+    // Copy only files that match the pattern "*Historical.java"
+    from(srcDir) { include("**/*Historical.java") }
+    into(destDir)
+
+    // Only move if there are files
+    doFirst {
+        if (!srcDir.exists()) {
+            throw GradleException("Source directory $srcDir does not exist!")
+        }
+    }
     dependsOn(tasks.named("generateTestContractWrappers"))
-    dependsOn(compileHistoricalSolidityContracts)
+    dependsOn(tasks.named("generateTestHistoricalContractWrappers"))
+}
+
+tasks.register<Delete>("deleteTestHistoricalDir") {
+    description = "Delete the testHistorical directory after moving files."
+    group = "historical"
+
+    delete(file("$rootDir/hedera-mirror-web3/build/generated/sources/web3j/testHistorical"))
+
+    // Only delete if move task was successful
+    mustRunAfter("moveTestHistoricalFiles")
+}
+
+tasks.register("moveAndCleanTestHistorical") {
+    description =
+        "Move files from testHistorical to test and then clean up the testHistorical directory."
+    group = "historical"
+
+    dependsOn("moveTestHistoricalFiles")
+    finalizedBy("deleteTestHistoricalDir")
+}
+
+afterEvaluate { tasks.named("resolveSolidity") { dependsOn("extractContracts") } }
+
+tasks.named("compileTestHistoricalJava") { group = "historical" }
+
+tasks.assemble {
+    dependsOn(tasks.processTestResources)
+    dependsOn(processTestHistoricalResources)
 }
