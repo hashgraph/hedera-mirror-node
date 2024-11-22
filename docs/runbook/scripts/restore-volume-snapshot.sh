90892b203cff57e0054976d54cfbc3baf68fed0c
Jesse Nelson <jesse@hashgraph.com>
2024-11-21 18:37:51 -0600

    fix typo
    Signed-off-by: Jesse Nelson <jesse@hashgraph.com>

--- docs/runbook/scripts/restore-volume-snapshot.sh
+++ docs/runbook/scripts/restore-volume-snapshot.sh
@@ -122,14 +122,8 @@ function configureAndValidate() {
 }
 
 function prepareDiskReplacement() {
-  log "Will spin down importer and citus in the namespaces (${NAMESPACES[*]}) for context ${CURRENT_CONTEXT}"
-
   for namespace in "${NAMESPACES[@]}"; do
-
-    # Unroute traffic
     unrouteTraffic "${namespace}"
-
-    # Pause Citus
     pauseCitus "${namespace}"
   done
 
