cfd0ed56e7b64e2c3a403747bdbfdb0d741c2686
Steven Sheehy <steven.sheehy@swirldslabs.com>
2024-12-17 09:14:52 -0600

    Fixes
    Signed-off-by: Steven Sheehy <steven.sheehy@swirldslabs.com>

--- tools/mirror-report/README.md
+++ tools/mirror-report/README.md
@@ -2,3 +2,64 @@
 
 This tool is a CLI tool that queries the REST API for an account and produces a report showing any transfers to or from
 the account and its resulting balance.
+
+## Install
+
+First ensure Node and NPM are installed. If not using MacOS or Linux, please see Node's
+install [instructions](https://nodejs.org/en/download/package-manager/current).
+
+```shell
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+nvm install 22
+npm install -g @hashgraph/mirror-report
+```
+
+## Running
+
+To generate the report, use the `mirror` command line tool from the installation section.
+
+```shell
+$ mirror --help
+Usage: mirror [options] [command]
+
+Options:
+  -h, --help        display help for command
+
+Commands:
+  report [options]  Generate a report for specific accounts
+  help [command]    display help for command
+```
+
+Run the `mirror report` sub-command to generate the report. Pass at least one account you want to include in the report.
+By default, it generates a report for the current date. Pass the `--date YYYY-MM-DD` argument to customize the report
+date.
+
+```shell
+$ mirror report --help
+Usage: mirror report [options]
+
+Generate a report for the given accounts.
+
+Options:
+  -a, --account <accountId...>   The accounts to include in the report
+  -d, --date <YYYY-MM-DD>        The day the report should cover (default: "2024-12-17")
+  -n, --network <network>        The Hedera network to connect to (default: "mainnet")
+  -h, --help                     display help for command
+```
+
+Example execution :
+
+```shell
+$ mirror report -a 0.0.1000 -a 0.0.1001 -d 2024-11-29 -n testnet
+2024-12-17T04:52:04.353Z Generating testnet report for the given accounts: ["0.0.1000","0.0.1001"]
+2024-12-17T04:52:04.353Z Invoking https://testnet.mirrornode.hedera.com/api/v1/accounts/0.0.1000?timestamp=1732838400
+2024-12-17T04:52:04.643Z Starting balance of 1300000002 for account 0.0.1000 at 1732837599.823821194
+2024-12-17T04:52:04.643Z Invoking https://testnet.mirrornode.hedera.com/api/v1/transactions?account.id=0.0.1000&limit=100&order=asc&timestamp=gt:1732837599.823821194&timestamp=lt:1732924800
+2024-12-17T04:52:04.719Z Invoking https://testnet.mirrornode.hedera.com/api/v1/accounts/0.0.1001?timestamp=1732838400
+2024-12-17T04:52:04.800Z Starting balance of 197103815708295 for account 0.0.1001 at 1732837599.823821194
+2024-12-17T04:52:04.800Z Invoking https://testnet.mirrornode.hedera.com/api/v1/transactions?account.id=0.0.1001&limit=100&order=asc&timestamp=gt:1732837599.823821194&timestamp=lt:1732924800
+2024-12-17T04:52:04.875Z Generated report successfully at report-2024-11-29.csv with 1 entries
+$ cat report-2024-11-29.csv
+timestamp,sender,receiver,fees,amount,balance
+1732901875.430169000,0.0.5190744,0.0.1000,0,100000000,1400000002
+```
--- tools/mirror-report/index.js
+++ tools/mirror-report/index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 /*
  * Copyright (C) 2024 Hedera Hashgraph, LLC
  *
@@ -14,101 +16,111 @@
  * limitations under the License.
  */
 
-#!/usr/bin/env node
-
 import fetch from 'node-fetch';
 import fs from 'fs';
-import { program } from 'commander';
+import {program} from 'commander';
 
 const feeAccount = /^\d+\.\d+\.(98|800|801|[3-9]|[1-3][0-9])$/;
 const prefix = '/api/v1';
 
 const log = (message) => {
-    const timestamp = new Date().toISOString();
-    console.log(`${timestamp} ${message}`);
+  const timestamp = new Date().toISOString();
+  console.log(`${timestamp} ${message}`);
 };
 
 const restApi = async (network, path) => {
-    const prefixedPath = path.startsWith(prefix) ? path : prefix + path;
-    const url = `https://${network}.mirrornode.hedera.com${prefixedPath}`;
-    log(`Invoking ${url}`);
-    return fetch(url, {compress: true, headers: {'Accept': 'application/json'}})
-        .then(res => res.json())
-        .then(res => {
-            if (res._status != null) {
-               log(`Error invoking URL: ${JSON.stringify(res._status.messages)}`);
-               return null;
-            }
-            return res;
-        })
-        .catch(e => log(`Error invoking URL: ${e}`));
+  const prefixedPath = path.startsWith(prefix) ? path : prefix + path;
+  const url = `https://${network}.mirrornode.hedera.com${prefixedPath}`;
+  log(`Invoking ${url}`);
+
+  return fetch(url, {compress: true, headers: {'Accept': 'application/json'}})
+  .then(res => res.json())
+  .then(res => {
+    if (res._status != null) {
+      log(`Error invoking URL: ${JSON.stringify(res._status.messages)}`);
+      return null;
+    }
+    return res;
+  })
+  .catch(e => log(`Error invoking URL: ${e}`));
 };
 
-const report = async (options) => {
-    const filename = `report-${options.date}.csv`;
-    const timestampStart = new Date(options.date).getTime() / 1000;
-    const timestampEnd = timestampStart + 86400;
-    let csv = "timestamp,sender,receiver,fees,amount,balance\n";
-    log(`Generating ${filename} for ${options.network}: ${JSON.stringify(options.accounts)}`);
+export const report = async (options) => {
+  const filename = `report-${options.date}.csv`;
+  const timestampStart = new Date(options.date).getTime() / 1000;
+  const timestampEnd = timestampStart + 86400;
 
-    for (const account of options.accounts) {
-        const accountResponse = await restApi(options.network, `/accounts/${account}?timestamp=${timestampStart}`);
-        if (!accountResponse) {
-           log(`Unable to find account ${account}`);
-           continue;
-        }
+  let csv = "timestamp,sender,receiver,fees,amount,balance\n";
+  let count = 0;
+  log(`Generating ${options.network} report for the given accounts: ${JSON.stringify(options.account)}`);
 
-        let balance = BigInt(accountResponse?.balance?.balance);
-        const balanceTimestamp = accountResponse?.balance?.timestamp
-        let next = `/transactions?account.id=${account}&limit=100&order=asc&timestamp=gt:${balanceTimestamp}&timestamp=lt:${timestampEnd}`;
-        log(`Starting balance for account ${account} at time ${balanceTimestamp}: ${balance}`);
+  for (const account of options.account) {
+    const accountResponse = await restApi(options.network, `/accounts/${account}?timestamp=${timestampStart}`);
 
-        while (next) {
-            const transactionsResponse = await restApi(options.network, next);
-            if (!transactionsResponse) {
-               log(`Unable to get the transactions for account ${account}`);
-               continue;
-            }
-
-            transactionsResponse.transactions.forEach(transaction => {
-                const payer = transaction.transaction_id.startsWith(account);
-                const fees = payer ? transaction.charged_tx_fee : 0;
-                let amount = 0n;
-                let other;
-
-                transaction?.transfers.forEach(transfer => {
-                    if (transfer.account === account) {
-                        amount = BigInt(transfer.amount);
-                    } else if (!feeAccount.test(transfer.account)) {
-                        other = transfer.account;
-                    }
-                });
-
-                balance += amount;
-                const consensusTimestampSeconds = transaction.consensus_timestamp.split('.')[0];
-
-                if (consensusTimestampSeconds >= timestampStart) {
-                    const sender = amount < 0n ? account : other;
-                    const receiver =  amount < 0n ? other : account;
-                    csv += `${transaction.consensus_timestamp},${sender},${receiver},${fees},${amount},${balance}\n`;
-                }
-            });
-
-            next = transactionsResponse?.links?.next;
-        }
-
-        fs.writeFile(filename, csv, (err) => {if (err) throw err;});
+    if (!accountResponse) {
+      log(`Unable to find account ${account}`);
+      continue;
     }
+
+    let balance = BigInt(accountResponse?.balance?.balance);
+    const balanceTimestamp = accountResponse?.balance?.timestamp
+    let next = `/transactions?account.id=${account}&limit=100&order=asc&timestamp=gt:${balanceTimestamp}&timestamp=lt:${timestampEnd}`;
+    log(`Starting balance of ${balance} for account ${account} at ${balanceTimestamp}`);
+
+    while (next) {
+      const transactionsResponse = await restApi(options.network, next);
+
+      if (!transactionsResponse) {
+        log(`Unable to find any transactions for account ${account}`);
+        continue;
+      }
+
+      for (const transaction of transactionsResponse.transactions) {
+        const payer = transaction.transaction_id.startsWith(account);
+        const fees = payer ? BigInt(transaction.charged_tx_fee) : 0n;
+        let amount = 0n;
+        let other = "fee accounts";
+
+        transaction?.transfers.forEach(transfer => {
+          if (transfer.account === account) {
+            amount = BigInt(transfer.amount);
+          } else if (!feeAccount.test(transfer.account)) {
+            other = transfer.account;
+          }
+        });
+
+        balance += amount;
+        const consensusTimestampSeconds = transaction.consensus_timestamp.split('.')[0];
+
+        if (consensusTimestampSeconds >= timestampStart) {
+          const sender = amount < 0n ? account : other;
+          let receiver = amount < 0n ? other : account;
+          csv += `${transaction.consensus_timestamp},${sender},${receiver},${fees},${amount},${balance}\n`;
+          ++count;
+        }
+      }
+
+      next = transactionsResponse?.links?.next;
+    }
+
+  }
+
+  fs.writeFile(filename, csv, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+  log(`Generated report successfully at ${filename} with ${count} entries`);
 };
 
 const today = new Date().toISOString().slice(0, 10);
 
 program.command('report')
-    .description('Generate a report for specific accounts')
-    .requiredOption('-a, --accounts <accountId...>', 'The accounts to consider for report generation')
-    .requiredOption('-d, --date <YYYY-MM-DD>', 'The day the report should be generated', today)
-    .requiredOption('-n, --network <network>', 'The Hedera network to connect to', 'mainnet')
-    .action(report);
+.description('Generate a report for the given accounts.')
+.requiredOption('-a, --account <accountId...>', 'The accounts to include in the report')
+.requiredOption('-d, --date <YYYY-MM-DD>', 'The day the report should cover', today)
+.requiredOption('-n, --network <network>', 'The Hedera network to connect to', 'mainnet')
+.action(report);
 
 program.parse();
 
