08a7cba938a96f835be1179d239f185fb75db6a6
Xin Li <59580070+xin-hedera@users.noreply.github.com>
2025-01-23 09:36:51 -0600

    Fix add block columns migration (#10212)
    - Remove data backfill in the migration and change migration version
    
    Signed-off-by: Xin Li <xin@hashgraph.com>

--- hedera-mirror-importer/src/main/resources/db/migration/v2/V2.7.0.1__add_block_columns.sql
+++ hedera-mirror-importer/src/main/resources/db/migration/v2/V2.7.0.1__add_block_columns.sql
@@ -4,8 +4,3 @@ alter table if exists record_file
   add column if not exists software_version_patch        int          null,
   add column if not exists round_start                   bigint       null,
   add column if not exists round_end                     bigint       null;
-
-update record_file
-  set software_version_major = hapi_version_major,
-      software_version_minor = hapi_version_minor,
-      software_version_patch = hapi_version_patch;
